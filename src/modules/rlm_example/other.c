30a250b2632b4ce371b1305fafc1e45cd006d0b0
aland <aland>
1999-08-17 18:16:53 +0000

    Wrote LOTS of documentation in the example Makefile. 	Added a sample 'C' and 'H' files to demonstrate how to use 	the Makefile.

---
+++ src/modules/rlm_example/other.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "other.h"
+
+/*
+ *  This is a sample C file which does nothing.
+ *
+ *  It's only purpose is to show how to set up the 'Makefile'
+ *  for modules which have more than one C source file.
+ */
+void other_function(void)
+{
+  int i = 1;			/* do nothing */
+
+  i++;
+}
---
+++ src/modules/rlm_example/other.h
@@ -0,0 +1,8 @@
+#ifndef _OTHER_H
+#define _OTHER_H
+
+/* define the function */
+
+void other_function(void);
+
+#endif _OTHER_H
