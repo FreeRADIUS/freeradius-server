22df1887bb16af50aaadb79f1a944fe14fc275cb
ivanfm <ivanfm>
1999-09-19 13:35:02 +0000

    *** empty log message ***

--- src/include/missing.h
+++ src/include/missing.h
@@ -10,3 +10,17 @@
 char *crypt(char *key, char *salt);
 #endif
 
+#ifdef __EMX__
+#define OS2
+/* EMX does not have SIGIOT */
+#define SIGIOT SIGTERM
+
+/* EMX does not have this functions and must be implemented */
+#define strncasecmp    strncmp
+#define strcasecmp    strcmp
+#define inet_aton(x,y)     0
+#define setlinebuf(x)     0
+#define gethostname(x,y)  strncpy(x,getenv("HOSTNAME"),y)
+
+#endif
+
