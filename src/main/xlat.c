05dcd7ee106ad41015487e27c5c19f68b8794fc9
ivanfm <ivanfm>
1999-09-20 21:55:13 +0000

    Now using vp_prints_value and receiving the output buffer and length of output buffer

--- src/main/xlat.c
+++ src/main/xlat.c
@@ -30,61 +30,44 @@
 /*
    Convert the value on a VALUE_PAIR to string
 */
-int valuepair2str(char * buffer,VALUE_PAIR * pair,int type)
+int valuepair2str(char * out,int outlen,VALUE_PAIR * pair,int type)
 {
-   DICT_VALUE * dv;
-   switch (type) {
-	  case PW_TYPE_STRING :
-		if (pair) {
-		  strcpy(buffer,pair->strvalue);
-		} else {
-		  strcpy(buffer,"_");
-		}
-		break;
-	  case PW_TYPE_INTEGER :
-		if (pair) {
-			 dv = dict_valbyattr(pair->attribute,pair->lvalue);
-			 if (dv) {
-			   strcpy(buffer,dv->name);
-			 } else {
-			   sprintf(buffer,"%d",pair->lvalue);
-			}
-		} else {
-		  strcpy(buffer,"0");
-		}
-		break;
-	  case PW_TYPE_IPADDR :
-		if (pair) {
-		  ip_ntoa(buffer, pair->lvalue);
-		} else {
-		  strcpy(buffer,"?.?.?.?");
-		}
-		break;
-	  case PW_TYPE_DATE :
-		if (pair) {
-		  sprintf(buffer,"%d",pair->lvalue);
-		} else {
-		  strcpy(buffer,"0");
-		}
-		break;
-	  default :
-		strcpy(buffer,"unknown_type");
-	}
-   return strlen(buffer);
+   if (pair)
+	 return vp_prints_value(out,outlen,pair,0);
+   else {
+	 switch (type) {
+	   case PW_TYPE_STRING :
+		  strncpy(out,"_",outlen-1);
+		  break;
+	   case PW_TYPE_INTEGER :
+		  strncpy(out,"0",outlen-1);
+		  break;
+	   case PW_TYPE_IPADDR :
+		  strncpy(out,"?.?.?.?",outlen-1);
+		  break;
+	   case PW_TYPE_DATE :
+		  strncpy(out,"0",outlen-1);
+		  break;
+	   default :
+		  strncpy(out,"unknown_type",outlen-1);
+	 }
+	 out[outlen-1] = '\0';
+	 return strlen(out);
+   }
 }
 
 /*
   Returns a string with value of Attribute
 */
-int valuebyname(char * buffer,VALUE_PAIR * request, char * attrname)
+int valuebyname(char * out,int outlen,VALUE_PAIR * request, char * attrname)
 {
 	DICT_ATTR * da;
 
 	da = dict_attrbyname(attrname);
 	if (da) {
-	  return (valuepair2str(buffer,pairfind(request,da->attr),da->type));
+	  return (valuepair2str(out,outlen,pairfind(request,da->attr),da->type));
 	} else {
-	  *buffer = '\0';
+	  *out = '\0';
 	  return 0;
 	}
 }
@@ -116,19 +99,22 @@ int valuebyname(char * buffer,VALUE_PAIR * request, char * attrname)
  *	${reply:AttributeName}	   Corresponding value for AttributeName in reply
  */
 
-char * radius_xlat2(char *str, REQUEST * request, VALUE_PAIR *reply)
+int radius_xlat2(char * out,int outlen, char *str, REQUEST * request, VALUE_PAIR *reply)
 {
-	static char buf[4096];
 	char attrname[128];
 	char *pa;
-	int n, i, c;
+	int n, i, c,freespace;
 	char *p;
 	char *q;
 	VALUE_PAIR *tmp;
 	struct tm * TM;
+	char tmpdt[30]; /* For temporary storing of dates */
 
-	q = buf;
-	for (p = str; *p; p++) {
+	q = out;
+	for (p = str; *p ; p++) {
+		freespace = outlen - ((int)q-(int)out);
+		if (freespace <= 1)
+		  break;
 		c = *p;
 		if ((c != '%') && (c != '$')) {
 			*q++ = *p;
@@ -144,11 +130,11 @@ char * radius_xlat2(char *str, REQUEST * request, VALUE_PAIR *reply)
 				}
 				*pa = '\0';
 				if (strnicmp(attrname,"reply:",6) == 0) {
-				  q += valuebyname(q,reply,&attrname[6]);
+				  q += valuebyname(q,freespace,reply,&attrname[6]);
 				} else if (strnicmp(attrname,"request:",8) == 0) {
-				  q += valuebyname(q,request->packet->vps,&attrname[8]);
+				  q += valuebyname(q,freespace,request->packet->vps,&attrname[8]);
 				} else {
-				  q += valuebyname(q,request->packet->vps,attrname);
+				  q += valuebyname(q,freespace,request->packet->vps,attrname);
 				}
 				break;
 			default:
@@ -161,55 +147,65 @@ char * radius_xlat2(char *str, REQUEST * request, VALUE_PAIR *reply)
 				*q++ = *p;
 				break;
 			case 'f': /* Framed IP address */
-				q += valuepair2str(q,pairfind(reply,PW_FRAMED_IP_ADDRESS),PW_TYPE_IPADDR);
+				q += valuepair2str(q,freespace,pairfind(reply,PW_FRAMED_IP_ADDRESS),PW_TYPE_IPADDR);
 				break;
 			case 'n': /* NAS IP address */
-				q += valuepair2str(q,pairfind(request->packet->vps,PW_NAS_IP_ADDRESS),PW_TYPE_IPADDR);
+				q += valuepair2str(q,freespace,pairfind(request->packet->vps,PW_NAS_IP_ADDRESS),PW_TYPE_IPADDR);
 				break;
 			case 't': /* MTU */
-				q += valuepair2str(q,pairfind(reply,PW_FRAMED_MTU),PW_TYPE_INTEGER);
+				q += valuepair2str(q,freespace,pairfind(reply,PW_FRAMED_MTU),PW_TYPE_INTEGER);
 				break;
 			case 'p': /* Port number */
-				q += valuepair2str(q,pairfind(request->packet->vps,PW_NAS_PORT_ID),PW_TYPE_INTEGER);
+				q += valuepair2str(q,freespace,pairfind(request->packet->vps,PW_NAS_PORT_ID),PW_TYPE_INTEGER);
 				break;
 			case 'u': /* User name */
-				q += valuepair2str(q,pairfind(request->packet->vps,PW_USER_NAME),PW_TYPE_STRING);
+				q += valuepair2str(q,freespace,pairfind(request->packet->vps,PW_USER_NAME),PW_TYPE_STRING);
 				break;
 			case 'i': /* Calling station ID */
-				q += valuepair2str(q,pairfind(request->packet->vps,PW_CALLING_STATION_ID),PW_TYPE_STRING);
+				q += valuepair2str(q,freespace,pairfind(request->packet->vps,PW_CALLING_STATION_ID),PW_TYPE_STRING);
 				break;
 			case 'c': /* Callback-Number */
-				q += valuepair2str(q,pairfind(reply,PW_CALLBACK_NUMBER),PW_TYPE_STRING);
+				q += valuepair2str(q,freespace,pairfind(reply,PW_CALLBACK_NUMBER),PW_TYPE_STRING);
 				break;
 			case 'a': /* Protocol: */
-				q += valuepair2str(q,pairfind(reply,PW_FRAMED_PROTOCOL),PW_TYPE_INTEGER);
+				q += valuepair2str(q,freespace,pairfind(reply,PW_FRAMED_PROTOCOL),PW_TYPE_INTEGER);
 				break;
 			case 's': /* Speed */
-				q += valuepair2str(q,pairfind(request->packet->vps,PW_CONNECT_INFO),PW_TYPE_STRING);
+				q += valuepair2str(q,freespace,pairfind(request->packet->vps,PW_CONNECT_INFO),PW_TYPE_STRING);
 				break;
 			case 'C': /* ClientName */
-				strcpy(q,client_name(request->packet->src_ipaddr));
+				strncpy(q,client_name(request->packet->src_ipaddr),freespace);
+				q[i] = '\0';
 				i = strlen(q); q += i;
 				break;
 			case 'R': /* radius_dir */
-				strcpy(q,radius_dir);
+				strncpy(q,radius_dir,freespace);
+				q[i] = '\0';
 				i = strlen(q); q += i;
 				break;
-			case 'A': /* radius_dir */
-				strcpy(q,radacct_dir);
+			case 'A': /* radacct_dir */
+				strncpy(q,radacct_dir,freespace);
+				q[i] = '\0';
 				i = strlen(q); q += i;
 				break;
 			case 'L': /* radlog_dir */
-				strcpy(q,radlog_dir);
+				strncpy(q,radlog_dir,freespace);
+				q[i] = '\0';
 				i = strlen(q); q += i;
 				break;
 			case 'D': /* request date */
 				TM = localtime(&request->timestamp);
-				q += strftime(q,100,"%Y%m%d",TM);
+				strftime(tmpdt,sizeof(tmpdt),"%Y%m%d",TM);
+				strncpy(q,tmpdt,freespace);
+				q[i] = '\0';
+				i = strlen(q); q += i;
 				break;
 			case 'T': /* request timestamp */
 				TM = localtime(&request->timestamp);
-				q += strftime(q,100,"%Y-%m-%d-%H.%M.%S.000000",TM);
+				strftime(tmpdt,sizeof(tmpdt),"%Y-%m-%d-%H.%M.%S.000000",TM);
+				strncpy(q,tmpdt,freespace);
+				q[i] = '\0';
+				i = strlen(q); q += i;
 				break;
 			default:
 				*q++ = '%';
@@ -227,7 +223,19 @@ char * radius_xlat2(char *str, REQUEST * request, VALUE_PAIR *reply)
 */
 	*q = 0;
 
-	return buf;
+	return strlen(out);
 }
 
+/*
+ * print a string passing by the radius_xlat2
+ *
+ */
+void printf_xlat(char *str, REQUEST * request, VALUE_PAIR *reply)
+{
+  char * p;
+  p = malloc(4096);
+  radius_xlat2(p,4096,str,request,reply);
+  printf("%s",p);
+  free(p);
+}
 
