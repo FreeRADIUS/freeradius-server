7cdb7cbc5c57f5bae7aff8146d0d57f21c539c81
aland <aland>
2000-02-10 22:43:45 +0000

    cleaned up, and added "Realm" attribute to the request->packet->vps

--- src/modules/rlm_realm/rlm_realm.c
+++ src/modules/rlm_realm/rlm_realm.c
@@ -8,17 +8,22 @@
 #include "radiusd.h"
 #include "modules.h"
 
-/* Examine a request for a username with an @suffix, and if it corresponds
- * to something in the realms file, set that realm as Proxy-To.
+static const char rcsid[] = "$Id$";
+
+/*
+ *  Examine a request for a username with an @suffix, and if it
+ *  corresponds to something in the realms file, set that realm as
+ *  Proxy-To.
  *
- * This should very nearly duplicate the old proxy_send() code */
+ *  This should very nearly duplicate the old proxy_send() code
+ */
 static int realm_authorize(REQUEST *request,
 			   VALUE_PAIR **check_pairs, VALUE_PAIR **reply_pairs)
 {
   const char *realmname;
-  VALUE_PAIR *proxypair;
+  VALUE_PAIR *vp;
   REALM *realm;
-  const char *name=request->username->strvalue;
+  const char *name = request->username->strvalue;
 
   reply_pairs = reply_pairs; /* -Wunused */
 
@@ -27,29 +32,42 @@ static int realm_authorize(REQUEST *request,
   if ((realm = realm_find(realmname ? realmname : "NULL")) == NULL)
     return RLM_AUTZ_NOTFOUND;
 
-  if(realm->notsuffix)
+  if (realm->notsuffix)
     return RLM_AUTZ_NOTFOUND;
 
-  if(!(proxypair=paircreate(PW_PROXY_TO_REALM, PW_TYPE_STRING))) {
+  vp = pairmake("Proxy-To-Realm", realmname, T_OP_EQ);
+  if (!vp) {
     log(L_ERR|L_CONS, "no memory");
     exit(1);
   }
-  strNcpy(proxypair->strvalue, realmname, sizeof proxypair->strvalue);
-  proxypair->length=strlen(proxypair->strvalue);
-  proxypair->operator=T_OP_SET;
-  proxypair->next=0;
-  pairmove(check_pairs, &proxypair);
+
+  /*
+   *  Add it, even if it's already present.
+   */
+  pairadd(check_pairs, vp);
+
+  /*
+   *  Add a 'realm' attribute to the incoming request.
+   */
+  vp = pairmake("Realm", realmname, T_OP_EQ);
+  if (!vp) {
+    log(L_ERR|L_CONS, "no memory");
+    exit(1);
+  }
+  pairadd(&request->packet->vps, vp);
 
   return RLM_AUTZ_NOTFOUND; /* try the next module */
 }
 
-/* This does the exact same thing as the realm_authorize, it's just called
- * differently. */
+/*
+ * This does the exact same thing as the realm_authorize, it's just called
+ * differently.
+ */
 static int realm_preacct(REQUEST *request)
 {
   const char *name=request->username->strvalue;
   const char *realmname;
-  VALUE_PAIR *proxypair;
+  VALUE_PAIR *vp;
   REALM *realm;
 
   if(!name)
@@ -57,19 +75,30 @@ static int realm_preacct(REQUEST *request)
 
   if ((realmname = strrchr(name, '@')) != NULL)
     realmname++;
+
   if ((realm = realm_find(realmname ? realmname : "NULL")) == NULL)
     return RLM_PRAC_OK;
 
-  if(!(proxypair=paircreate(PW_PROXY_TO_REALM, PW_TYPE_STRING))) {
+  vp = pairmake("Proxy-To-Realm", realmname, T_OP_EQ);
+  if (!vp) {
     log(L_ERR|L_CONS, "no memory");
     exit(1);
   }
-  strNcpy(proxypair->strvalue, realmname, sizeof proxypair->strvalue);
-  proxypair->length=strlen(proxypair->strvalue);
-  proxypair->operator=T_OP_SET;
-  proxypair->next=0;
-  pairmove(&request->config_items, &proxypair);
 
+  /*
+   *  Add it, even if it's already present.
+   */
+  pairadd(&request->config_items, vp);
+
+  /*
+   *  Add a 'realm' attribute to the incoming request.
+   */
+  vp = pairmake("Realm", realmname, T_OP_EQ);
+  if (!vp) {
+    log(L_ERR|L_CONS, "no memory");
+    exit(1);
+  }
+  pairadd(&request->packet->vps, vp);
   return RLM_PRAC_OK; /* try the next module */
 }
 
