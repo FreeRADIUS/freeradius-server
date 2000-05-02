22480413ed63bae47274a3488d94fed33c10063e
aland <aland>
2000-05-02 17:27:14 +0000

    added example of how to find the module configuration

--- src/modules/rlm_example/rlm_example.c
+++ src/modules/rlm_example/rlm_example.c
@@ -5,8 +5,51 @@
 
 #include "radiusd.h"
 #include "modules.h"
+#include "conffile.h"
 
 static const char rcsid[] = "$Id$";
+
+/*
+ *	Define a structure for our module configuration.
+ *
+ *	These variables do not need to be in a structure, but it's
+ *	a lot cleaner to do so.
+ */
+typedef struct example_config_t {
+	int		boolean;
+	int		value;
+	char		*string;
+	u_int32_t	ipaddr;
+} example_config_t;
+
+/*
+ *	Define the local copy of our example configuration,
+ *	and initialize it so some values.
+ */
+static example_config_t config = {
+	FALSE,			/* boolean */
+	1,			/* integer */
+	NULL,			/* string */
+	0			/* IP address */
+};
+
+/*
+ *	A mapping of configuration file names to internal variables.
+ *
+ *	Note that the string is dynamically allocated, so it MUST
+ *	be freed.  When the configuration file parse re-reads the string,
+ *	it free's the old one, and strdup's the new one, placing the pointer
+ *	to the strdup'd string into 'config.string'.  This gets around
+ *	buffer over-flows.
+ */
+static CONF_PARSER module_config[] = {
+  { "integer", PW_TYPE_INTEGER,    &config.value },
+  { "boolean", PW_TYPE_BOOLEAN,    &config.boolean },
+  { "string",  PW_TYPE_STRING_PTR, &config.string },
+  { "ipaddr",  PW_TYPE_IPADDR,     &config.ipaddr },
+
+  { NULL, -1, NULL}		/* end the list */
+};
 
 /*
  *	Do any per-module initialization.  e.g. set up connections
@@ -16,15 +59,37 @@ static const char rcsid[] = "$Id$";
  */
 static int radius_init(int argc, char **argv)
 {
-  /*
-   *	Quiet the compiler,  This is ONLY needed if the functions
-   *	parameters are not used anywhere in the function.  If there
-   *	were code here, we wouldn't need these two "fake" code lines.
-   */
-  argc = argc;
-  argv = argv;
+	CONF_SECTION *example_cs;
 
-  return 0;
+	/*
+	 *	Quiet the compiler,  This is ONLY needed if the functions
+	 *	parameters are not used anywhere in the function.  If there
+	 *	were code here, we wouldn't need these two "fake" code lines.
+	 */
+	argc = argc;
+	argv = argv;
+	
+	/*
+	 *	Look for the module's configuration.  If it doesn't
+	 *	exists, exit quietly (and use the defaults).
+	 */
+	example_cs = cf_module_config_find("example");
+	if (!example_cs) {
+		return 0;
+	}
+
+	/*
+	 *	If the configuration parameters can't be parsed, then
+	 *	fail.
+	 */
+	if (cf_section_parse(example_cs, module_config) < 0) {
+		return -1;
+	}
+
+	/*
+	 *	Everything's OK, return without an error.
+	 */
+	return 0;
 }
 
 /*
@@ -36,12 +101,12 @@ static int radius_init(int argc, char **argv)
 static int radius_authorize(REQUEST *request,
 			    VALUE_PAIR **check_pairs, VALUE_PAIR **reply_pairs)
 {
-  /* quiet the compiler */
-  request = request;
-  check_pairs = check_pairs;
-  reply_pairs = reply_pairs;
-
-  return RLM_MODULE_OK;
+	/* quiet the compiler */
+	request = request;
+	check_pairs = check_pairs;
+	reply_pairs = reply_pairs;
+	
+	return RLM_MODULE_OK;
 }
 
 /*
@@ -49,10 +114,10 @@ static int radius_authorize(REQUEST *request,
  */
 static int radius_authenticate(REQUEST *request)
 {
-  /* quiet the compiler */
-  request = request;
-
-  return RLM_MODULE_OK;
+	/* quiet the compiler */
+	request = request;
+	
+	return RLM_MODULE_OK;
 }
 
 /*
@@ -60,10 +125,10 @@ static int radius_authenticate(REQUEST *request)
  */
 static int radius_preacct(REQUEST *request)
 {
-  /* quiet the compiler */
-  request = request;
-
-  return RLM_MODULE_OK;
+	/* quiet the compiler */
+	request = request;
+	
+	return RLM_MODULE_OK;
 }
 
 /*
@@ -71,20 +136,20 @@ static int radius_preacct(REQUEST *request)
  */
 static int radius_accounting(REQUEST *request)
 {
-  /* quiet the compiler */
-  request = request;
-
-  return RLM_MODULE_OK;
+	/* quiet the compiler */
+	request = request;
+	
+	return RLM_MODULE_OK;
 }
 
 /* globally exported name */
 module_t rlm_example = {
-  "example",
-  0,				/* type: reserved */
-  radius_init,			/* initialization */
-  radius_authorize,		/* authorization */
-  radius_authenticate,		/* authentication */
-  radius_preacct,		/* preaccounting */
-  radius_accounting,		/* accounting */
-  NULL,				/* detach */
+	"example",
+	0,				/* type: reserved */
+	radius_init,			/* initialization */
+	radius_authorize,		/* authorization */
+	radius_authenticate,		/* authentication */
+	radius_preacct,			/* preaccounting */
+	radius_accounting,		/* accounting */
+	NULL,				/* detach */
 };
