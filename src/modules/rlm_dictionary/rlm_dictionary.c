7a0d313289b5e0da3422b2d9962de199d9d62daf
aland <aland>
2000-07-26 14:18:14 +0000

    preliminary template for moving the dictionary initialization 	into a module.

---
+++ src/modules/rlm_dictionary/rlm_dictionary.c
@@ -0,0 +1,51 @@
+#include "autoconf.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "radiusd.h"
+#include "modules.h"
+#include "conffile.h"
+
+static const char rcsid[] = "$Id$";
+
+/*
+ *	Do any per-module initialization.  e.g. set up connections
+ *	to external databases, read configuration files, set up
+ *	dictionary entries, etc.
+ *
+ */
+static int radius_init(int argc, char **argv)
+{
+	/*
+	 *	Quiet the compiler,  This is ONLY needed if the functions
+	 *	parameters are not used anywhere in the function.  If there
+	 *	were code here, we wouldn't need these two "fake" code lines.
+	 */
+	argc = argc;
+	argv = argv;
+	
+        /* Initialize the dictionary */
+	if (dict_init(radius_dir, RADIUS_DICTIONARY) != 0) {
+		log(L_ERR|L_CONS, "Errors reading dictionary %s/%s: %s",
+		    radius_dir, RADIUS_DICTIONARY, librad_errstr);
+		return -1;
+	}
+
+	/*
+	 *	Everything's OK, return without an error.
+	 */
+	return 0;
+}
+
+/* globally exported name */
+module_t rlm_dictionary = {
+	"dictionary",
+	0,				/* type: reserved */
+	radius_init,			/* initialization */
+	NULL,            		/* authorization */
+	NULL,               		/* authentication */
+	NULL,            		/* preaccounting */
+	NULL,              		/* accounting */
+	NULL,				/* detach */
+};
