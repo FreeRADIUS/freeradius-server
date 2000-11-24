7f85f51ef7b7b2fcd4a6372e549954fcb3b639a8
aland <aland>
2000-11-24 21:20:54 +0000

    removed no longer used build flags from the version string

--- src/main/version.c
+++ src/main/version.c
@@ -29,29 +29,11 @@ void version(void)
 	fprintf(stderr, "Compilation flags: ");
 
 	/* here are all the conditional feature flags */
-#if defined(WITH_DBM)
-	fprintf(stderr," WITH_DBM");
-#endif
-#if defined(WITH_NDBM)
-	fprintf(stderr," WITH_NDBM");
-#endif
 #if defined(OSFC2)
 	fprintf(stderr," OSFC2");
 #endif
-#if defined(WITH_NTDOMAIN_HACK)
-	fprintf(stderr," WITH_NTDOMAIN_HACK");
-#endif
-#if defined(WITH_SPECIALIX_JETSTREAM_HACK)
-	fprintf(stderr," WITH_SPECIALIX_JETSTREAM_HACK");
-#endif
-#if defined(WITH_ASCEND_HACK)
-	fprintf(stderr," WITH_ASCEND_HACK");
-#endif
 #if defined(WITH_DICT_NOCASE)
 	fprintf(stderr," WITH_DICT_NOCASE");
-#endif
-#if defined(ATTRIB_NMC)
-	fprintf(stderr, " ATTRIB_NMC");
 #endif
 #if defined(WITH_SNMP)
 	fprintf(stderr," WITH_SNMP");
