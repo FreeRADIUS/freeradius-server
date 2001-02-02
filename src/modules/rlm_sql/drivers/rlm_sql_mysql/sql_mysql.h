9c73ac44e38e6c2fb1501c7194ef00f9308b1f71
jcarneal <jcarneal>
2001-02-02 21:34:20 +0000

    Oops, forgot these...

--- src/modules/rlm_sql/drivers/rlm_sql_mysql/sql_mysql.h
+++ src/modules/rlm_sql/drivers/rlm_sql_mysql/sql_mysql.h
@@ -15,43 +15,21 @@
 typedef MYSQL_ROW SQL_ROW;
 
 typedef struct sql_socket {
-	int		id;
+	int     id;
 #if HAVE_PTHREAD_H
-	sem_t		*semaphore;
+	sem_t  *semaphore;
 #else
-	int		in_use;
+	int     in_use;
 #endif
 	struct sql_socket *next;
 
-	MYSQL		*sock;
-	MYSQL		conn;
-	MYSQL_RES	*result;
+	MYSQL  *sock;
+	MYSQL   conn;
+	MYSQL_RES *result;
 } SQLSOCK;
 
-typedef struct sql_config {
-	char	*sql_server;
-	char	*sql_login;
-	char	*sql_password;
-	char	*sql_db;
-	char	*sql_acct_table;
-	char	*sql_authcheck_table;
-	char	*sql_authreply_table;
-	char	*sql_groupcheck_table;
-	char	*sql_groupreply_table;
-	char	*sql_usergroup_table;
-	char	*sql_realm_table;
-	char	*sql_realmgroup_table;
-	char	*sql_nas_table;
-	char	*sql_dict_table;
-	int	sensitiveusername;
-	int	sqltrace;
-	char	*tracefile;
-	int	deletestalesessions;
-	int	num_sql_socks;
-} SQL_CONFIG;
-
 typedef struct sql_inst {
-	int used;
+	int     used;
 	SQLSOCK *sqlpool;
 	SQL_CONFIG *config;
 #if HAVE_PTHREAD_H
@@ -60,18 +38,19 @@ typedef struct sql_inst {
 #endif
 } SQL_INST;
 
-SQLSOCK *sql_create_socket(SQL_INST *inst);
+SQLSOCK *sql_create_socket(SQL_INST * inst);
 int     sql_checksocket(const char *facility);
-int     sql_query(SQL_INST *inst, SQLSOCK *sqlsocket, char *querystr);
-int     sql_select_query(SQL_INST *inst, SQLSOCK *sqlsocket, char *querystr);
-int     sql_store_result(SQLSOCK *sqlsocket);
-int     sql_num_fields(SQLSOCK *sqlsocket);
-int     sql_num_rows(SQLSOCK *sqlsocket);
-SQL_ROW sql_fetch_row(SQLSOCK *sqlsocket);
-void    sql_free_result(SQLSOCK *sqlsocket);
-char   *sql_error(SQLSOCK *sqlsocket);
-void    sql_close(SQLSOCK *sqlsocket);
-void    sql_finish_query(SQLSOCK *sqlsocket);
-void    sql_finish_select_query(SQLSOCK *sqlsocket);
-int     sql_affected_rows(SQLSOCK *sqlsocket);
+int     sql_query(SQL_INST * inst, SQLSOCK * sqlsocket, char *querystr);
+int     sql_select_query(SQL_INST * inst, SQLSOCK * sqlsocket,
+												 char *querystr);
+int     sql_store_result(SQLSOCK * sqlsocket);
+int     sql_num_fields(SQLSOCK * sqlsocket);
+int     sql_num_rows(SQLSOCK * sqlsocket);
+SQL_ROW sql_fetch_row(SQLSOCK * sqlsocket);
+void    sql_free_result(SQLSOCK * sqlsocket);
+char   *sql_error(SQLSOCK * sqlsocket);
+void    sql_close(SQLSOCK * sqlsocket);
+void    sql_finish_query(SQLSOCK * sqlsocket);
+void    sql_finish_select_query(SQLSOCK * sqlsocket);
+int     sql_affected_rows(SQLSOCK * sqlsocket);
 int     sql_escape_string(char *to, char *from, int length);
