842c8bbcad8321655eb13124162846d1b689b98b
aland <aland>
2001-05-01 15:07:25 +0000

    If we're using local snprintf, ensure that we include stdarg.h

--- src/include/libradius.h
+++ src/include/libradius.h
@@ -232,6 +232,7 @@ void		print_abinary(VALUE_PAIR *vp, u_char *buffer, int len);
 #endif /*ASCEND_BINARY*/
 
 #ifdef HAVE_LOCAL_SNPRINTF
+#include <stdarg.h>
 int snprintf(char *str, size_t count, const char *fmt, ...);
 int vsnprintf(char *str, size_t count, const char *fmt, va_list arg);
 #endif
