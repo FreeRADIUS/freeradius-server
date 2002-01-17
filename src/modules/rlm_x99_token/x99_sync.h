6dc4c0c744f66d4fcd09d291a11eb12d06509bd2
fcusack <fcusack>
2002-01-17 04:02:17 +0000

    Update Copyright date.

--- src/modules/rlm_x99_token/x99_sync.h
+++ src/modules/rlm_x99_token/x99_sync.h
@@ -16,7 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Copyright 2001  Google, Inc.
+ * Copyright 2001,2002  Google, Inc.
  */
 
 #ifndef X99_SYNC_H
