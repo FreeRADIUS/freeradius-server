9bf58280c7727d843cbf30cacbb83feb53b6d418
aland <aland>
2002-07-16 18:42:00 +0000

    Updated to match latest changes.
    	Patch from Ilguiz Latypov

--- src/modules/rlm_sql/drivers/rlm_sql_postgresql/sql_postgresql.h
+++ src/modules/rlm_sql/drivers/rlm_sql_postgresql/sql_postgresql.h
@@ -29,7 +29,7 @@ int     sql_select_query(SQLSOCK *sqlsocket, SQL_CONFIG *config, char *querystr)
 int     sql_store_result(SQLSOCK * sqlsocket, SQL_CONFIG *config);
 int     sql_num_fields(SQLSOCK * sqlsocket, SQL_CONFIG *config);
 int     sql_num_rows(SQLSOCK * sqlsocket, SQL_CONFIG *config);
-SQL_ROW sql_fetch_row(SQLSOCK * sqlsocket, SQL_CONFIG *config);
+int	sql_fetch_row(SQLSOCK * sqlsocket, SQL_CONFIG *config);
 int    sql_free_result(SQLSOCK * sqlsocket, SQL_CONFIG *config);
 char   *sql_error(SQLSOCK * sqlsocket, SQL_CONFIG *config);
 int    sql_close(SQLSOCK * sqlsocket, SQL_CONFIG *config);
