41791265dc5b810292063f310693bdb606a6256e
aland <aland>
2002-12-03 16:27:37 +0000

    Code cleanups, reformatting, free memory on errors, better 	log messages, etc.

--- src/modules/rlm_acct_unique/rlm_acct_unique.c
+++ src/modules/rlm_acct_unique/rlm_acct_unique.c
@@ -34,48 +34,43 @@
 
 static const char rcsid[] = "$Id$";
 
-typedef struct unique_attr_list {
-	int		attr;
-	struct unique_attr_list *next;
-} unique_attr_list;
+typedef struct rlm_acct_unique_list_t {
+	DICT_ATTR		      *dattr;
+	struct rlm_acct_unique_list_t *next;
+} rlm_acct_unique_list_t;
 
-typedef struct unique_config_t {
+typedef struct rlm_acct_unique_t {
 	char			*key;
-	struct unique_attr_list *head;		
-} unique_config_t;
-
-static unique_config_t config;
+	rlm_acct_unique_list_t *head;		
+} rlm_acct_unique_t;
 
 static CONF_PARSER module_config[] = {
-  { "key",  PW_TYPE_STRING_PTR, offsetof(unique_config_t,key), NULL,  NULL },
+  { "key",  PW_TYPE_STRING_PTR, offsetof(rlm_acct_unique_t,key), NULL,  NULL },
   { NULL, -1, 0, NULL, NULL }    /* end the list */
 };
 
 /*
  *	Add an attribute to the list.
  */
-static void unique_add_attr(int dictattr) {
-	struct unique_attr_list 	*new;		
+static void unique_add_attr(rlm_acct_unique_t *inst, DICT_ATTR *dattr)
+{
+	rlm_acct_unique_list_t 	*new;		
 	
-	new = rad_malloc(sizeof(struct unique_attr_list));
-
-	memset((struct unique_attr_list *)new, 0, sizeof(unique_attr_list));
+	new = rad_malloc(sizeof(*new));
+	memset(new, 0, sizeof(*new));
 
 	/* Assign the attr to our new structure */
-	new->attr = dictattr;	
+	new->dattr = dattr;
 
-	if (config.head) {
-		new->next = config.head;
-		config.head = new;
-	} else {
-		config.head = new;
-	}
+	new->next = inst->head;
+	inst->head = new;
 }
 
 /*
  *	Parse a key.
  */
-static int unique_parse_key(char *key) {
+static int unique_parse_key(rlm_acct_unique_t *inst, char *key)
+{
 	char *ptr, *prev, *keyptr;
 	DICT_ATTR *a;
 	
@@ -97,14 +92,14 @@ static int unique_parse_key(char *key) {
 			}
 			*ptr = ',';
 			prev = ptr+1;
-			unique_add_attr(a->attr); 
+			unique_add_attr(inst, a);
 			break;
 		case '\0':
 			if((a = dict_attrbyname(prev)) == NULL) {
 				radlog(L_ERR, "rlm_acct_unique: Cannot find attribute '%s' in dictionary", prev);
 				return -1;
 			}
-			unique_add_attr(a->attr);
+			unique_add_attr(inst, a);
 			return 0;
 			break;
 		case ' ':
@@ -117,9 +112,27 @@ static int unique_parse_key(char *key) {
 	return 0;
 }
 
-static int unique_instantiate(CONF_SECTION *conf, void **instance) {
+/*
+ *	Needed before instantiate for cleanup.
+ */
+static int unique_detach(void *instance)
+{
+	rlm_acct_unique_t *inst = instance;
+	rlm_acct_unique_list_t *this, *next;
 
-	struct unique_config_t *inst;
+	free(inst->key);
+	for (this = inst->head; this != NULL; this = next) {
+		next = this->next;
+		free(this);
+	}
+	free(inst);
+
+	return 0;
+}
+
+static int unique_instantiate(CONF_SECTION *conf, void **instance)
+{
+	rlm_acct_unique_t *inst;
 
 	/*
 	 *  Set up a storage area for instance data
@@ -137,17 +150,18 @@ static int unique_instantiate(CONF_SECTION *conf, void **instance) {
 	 */	
 	if (!inst->key) {
 		radlog(L_ERR,"rlm_acct_unique: Cannot find value for 'key' in configuration.");
+		free(inst);
 		return -1;
 	}
 
 	/* 
 	 * Go thru the list of keys and build attr_list;
 	 */	
-	if (unique_parse_key(inst->key) < 0) {
+	if (unique_parse_key(inst, inst->key) < 0) {
+		unique_detach(inst); /* clean up memory */
 		return -1;
 	};
 
-	inst->head = config.head;
 	*instance = inst;
 
  	return 0;
@@ -159,98 +173,80 @@ static int unique_instantiate(CONF_SECTION *conf, void **instance) {
  */
 static int add_unique_id(void *instance, REQUEST *request)
 {
-  char buffer[BUFFERLEN];
-  u_char md5_buf[16];
+	char buffer[BUFFERLEN];
+	u_char md5_buf[16];
 
-  VALUE_PAIR *vp;
-  char *p;
-  int length, left;
-  struct unique_config_t *inst = instance;
-  struct unique_attr_list *cur;
+	VALUE_PAIR *vp;
+	char *p;
+	int length, left;
+	rlm_acct_unique_t *inst = instance;
+	rlm_acct_unique_list_t *cur;
   
-  /* initialize variables */
-  p = buffer;
-  left = BUFFERLEN;
-  length = 0;
-  cur = inst->head;
-
-  /*
-   *  A unique ID already exists: don't do anything.
-   */
-  vp = pairfind(request->packet->vps, PW_ACCT_UNIQUE_SESSION_ID);
-  if (vp) {
-	  return RLM_MODULE_NOOP;
-  }
-
-  /* loop over items to create unique identifiers */
-  while (cur) {
-	  vp = pairfind(request->packet->vps, cur->attr);
-	  if (!vp) {
-		  DEBUG2("rlm_acct_unique: WARNING: Attribute %d was not found in request, unique ID MAY be inconsistent", cur->attr);
-	  }
-	  length = vp_prints(p, left, vp);
-	  left -= length + 1;	/* account for ',' in between elements */
-	  p += length;
-	  *(p++) = ',';		/* ensure seperation of elements */
-	  cur = cur->next;
-  }
-  buffer[BUFFERLEN-left-1] = '\0';
-
-  DEBUG2("rlm_acct_unique: Hashing '%s'", buffer);
-  /* calculate a 'unique' string based on the above information */
-  librad_md5_calc(md5_buf, (u_char *)buffer, (p - buffer));
-  sprintf(buffer, "%02x%02x%02x%02x%02x%02x%02x%02x",
-	  md5_buf[0], md5_buf[1], md5_buf[2], md5_buf[3],
-	  md5_buf[4], md5_buf[5], md5_buf[6], md5_buf[7]);
-
-  DEBUG2("rlm_acct_unique: Acct-Unique-Session-ID = \"%s\".", buffer);
-  
-  vp = pairmake("Acct-Unique-Session-Id", buffer, 0);
-  if (!vp) {
-	  radlog(L_ERR, "%s", librad_errstr);
-	  return RLM_MODULE_FAIL;
-  }
-
-  /* add the (hopefully) unique session ID to the packet */
-  pairadd(&request->packet->vps, vp);
-  
-  /* FIXME:  Uncomment here once we iron out module_accounting() */
-  /*return RLM_MODULE_UPDATED;*/
-  return RLM_MODULE_OK;
-}
-
-static int unique_detach(void *instance) {
-	struct unique_config_t *inst = instance;
-	struct unique_attr_list *next = inst->head;
+	/* initialize variables */
+	p = buffer;
+	left = BUFFERLEN;
+	length = 0;
+	cur = inst->head;
 	
-	free(inst->key);
-	while(inst->head) {
-		next = inst->head->next;
-		DEBUG("HERE:  %d", inst->head->attr);
-		free(inst->head);
-		inst->head = next;
+	/*
+	 *  A unique ID already exists: don't do anything.
+	 */
+	vp = pairfind(request->packet->vps, PW_ACCT_UNIQUE_SESSION_ID);
+	if (vp) {
+		return RLM_MODULE_NOOP;
 	}
-	free(inst);
-
-	return 0;
+	
+	/* loop over items to create unique identifiers */
+	while (cur) {
+		vp = pairfind(request->packet->vps, cur->dattr->attr);
+		if (!vp) {
+			DEBUG2("rlm_acct_unique: WARNING: Attribute %s was not found in request, unique ID MAY be inconsistent", cur->dattr->name);
+		}
+		length = vp_prints(p, left, vp);
+		left -= length + 1;	/* account for ',' in between elements */
+		p += length;
+		*(p++) = ',';		/* ensure seperation of elements */
+		cur = cur->next;
+	}
+	buffer[BUFFERLEN-left-1] = '\0';
+	
+	DEBUG2("rlm_acct_unique: Hashing '%s'", buffer);
+	/* calculate a 'unique' string based on the above information */
+	librad_md5_calc(md5_buf, (u_char *)buffer, (p - buffer));
+	sprintf(buffer, "%02x%02x%02x%02x%02x%02x%02x%02x",
+		md5_buf[0], md5_buf[1], md5_buf[2], md5_buf[3],
+		md5_buf[4], md5_buf[5], md5_buf[6], md5_buf[7]);
+	
+	DEBUG2("rlm_acct_unique: Acct-Unique-Session-ID = \"%s\".", buffer);
+	
+	vp = pairmake("Acct-Unique-Session-Id", buffer, 0);
+	if (!vp) {
+		radlog(L_ERR, "%s", librad_errstr);
+		return RLM_MODULE_FAIL;
+	}
+	
+	/* add the (hopefully) unique session ID to the packet */
+	pairadd(&request->packet->vps, vp);
+	
+	return RLM_MODULE_OK;
 }
 
 /* globally exported name */
 module_t rlm_acct_unique = {
-  "Acct-Unique-Session-Id",
-  0,				/* type: reserved */
-  NULL,				/* initialization */
-  unique_instantiate,		/* instantiation */
-  {
-	  NULL,			/* authentication */
-	  add_unique_id,	/* authorization */
-	  add_unique_id,	/* preaccounting */
-	  add_unique_id,	/* accounting */
-	  NULL,			/* checksimul */
-	  NULL,			/* pre-proxy */
-	  NULL,			/* post-proxy */
-	  NULL			/* post-auth */
-  },
-  unique_detach,		/* detach */
-  NULL,				/* destroy */
+	"Acct-Unique-Session-Id",
+	0,				/* type: reserved */
+	NULL,				/* initialization */
+	unique_instantiate,		/* instantiation */
+	{
+		NULL,			/* authentication */
+		add_unique_id,	/* authorization */
+		add_unique_id,	/* preaccounting */
+		add_unique_id,	/* accounting */
+		NULL,			/* checksimul */
+		NULL,			/* pre-proxy */
+		NULL,			/* post-proxy */
+		NULL			/* post-auth */
+	},
+	unique_detach,		/* detach */
+	NULL,				/* destroy */
 };
