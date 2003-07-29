d1329eddda74a434cac886169edc3014074de720
pnixon <pnixon>
2003-07-29 08:44:59 +0000

    sync with 0.9 branch

--- src/modules/rlm_sql/drivers/rlm_sql_postgresql/db_postgresql.sql
+++ src/modules/rlm_sql/drivers/rlm_sql_postgresql/db_postgresql.sql
@@ -1,100 +1,84 @@
 /*
- * --- David Nicklay [ Wed Nov  3 23:18:46 EST 1999 ]
- * 
+ * $Id$
+ *
+ * Postgresql schema for FreeRADIUS
+ *
+ * All field lengths need checking as some are still suboptimal. -pnixon 2003-07-13
+ *
  */
-
-/*
- * - Postgres wants C style comments.
- * - datetimeOS DEFAULT '0000-00-00 00:00:00' should be
- *   DEFAULT now() in postgres
- * - postgres apparently creates an index for each
- *   column specified as UNIQUE 
- *   Consider implicit creation of <tablename_id_seq> for each SERIAL field!
- */
-
-
-
-/*
- * Table structure for table 'dictionary'
- */
-CREATE TABLE dictionary (
-  id SERIAL PRIMARY KEY,
-  Type VARCHAR(30),
-  Attribute VARCHAR(32),
-  Value VARCHAR(32),
-  Format VARCHAR(20),
-  Vendor VARCHAR(32)
-);
-
-/*
- * Table structure for table 'nas'
- */
-CREATE TABLE nas (
-  id SERIAL PRIMARY KEY,
-  nasname VARCHAR(128),
-  shortname VARCHAR(32),
-  ipaddr VARCHAR(15),
-  type VARCHAR(30),
-  ports int4,
-  secret VARCHAR(60),
-  community VARCHAR(50),
-  snmp VARCHAR(10)
-);
 
 /*
  * Table structure for table 'radacct'
+ *
+ * Note: Column type BIGSERIAL does not exist prior to Postgres 7.2
+ *       If you run an older version you need to change this to SERIAL
  */
 CREATE TABLE radacct (
-  RadAcctId BIGSERIAL PRIMARY KEY,
-  AcctSessionId VARCHAR(32) DEFAULT '' NOT NULL,
-  AcctUniqueId VARCHAR(32) DEFAULT '' NOT NULL,
-  UserName VARCHAR(32) DEFAULT '' NOT NULL,
-  Realm VARCHAR(30) DEFAULT '',
-  NASIPAddress VARCHAR(15) DEFAULT '' NOT NULL,
-  NASPortId NUMERIC(12),
-  NASPortType VARCHAR(32),
-  AcctStartTime timestamp without time zone NOT NULL,
-  AcctStopTime timestamp without time zone NOT NULL,
-  AcctSessionTime NUMERIC(12),
-  AcctAuthentic VARCHAR(32),
-  ConnectInfo_start VARCHAR(32),
-  ConnectInfo_stop VARCHAR(32),
-  AcctInputOctets NUMERIC(12),
-  AcctOutputOctets NUMERIC(12),
-  CalledStationId VARCHAR(30) DEFAULT '' NOT NULL,
-  CallingStationId VARCHAR(30) DEFAULT '' NOT NULL,
-  AcctTerminateCause VARCHAR(32) DEFAULT '' NOT NULL,
-  ServiceType VARCHAR(32),
-  FramedProtocol VARCHAR(32),
-  FramedIPAddress VARCHAR(15) DEFAULT '' NOT NULL,
-  AcctStartDelay NUMERIC(12),
-  AcctStopDelay NUMERIC(12)
+	RadAcctId		BIGSERIAL PRIMARY KEY,
+	AcctSessionId		VARCHAR(32) NOT NULL,
+	AcctUniqueId		VARCHAR(32) NOT NULL,
+	UserName		VARCHAR(32),
+	Realm			VARCHAR(30),
+	NASIPAddress		INET NOT NULL,
+	NASPortId		INTEGER,
+	NASPortType		VARCHAR(32),
+	AcctStartTime		TIMESTAMP with time zone,
+	AcctStopTime		TIMESTAMP with time zone,
+	AcctSessionTime		INTERVAL,
+	AcctAuthentic		VARCHAR(32),
+	ConnectInfo_start	VARCHAR(32),
+	ConnectInfo_stop	VARCHAR(32),
+	AcctInputOctets		BIGINT,
+	AcctOutputOctets	BIGINT,
+	CalledStationId		VARCHAR(50),
+	CallingStationId	VARCHAR(50),
+	AcctTerminateCause	VARCHAR(32),
+	ServiceType		VARCHAR(32),
+	FramedProtocol		VARCHAR(32),
+	FramedIPAddress		INET,
+	AcctStartDelay		INTERVAL,
+	AcctStopDelay		INTERVAL
 );
-create index radacct_UserName on radacct (UserName);
-create index radacct_AcctSessionId on radacct (AcctSessionId);
-create index radacct_AcctUniqueId on radacct (AcctUniqueId);
-create index radacct_FramedIPAddress on radacct (FramedIPAddress);
-create index radacct_NASIPAddress on radacct (NASIPAddress);
-create index radacct_AcctStartTime on radacct (AcctStartTime);
-create index radacct_AcctStopTime on radacct (AcctStopTime);
+-- This index may be usefull..
+-- CREATE UNIQUE INDEX radacct_whoson on radacct (AcctStartTime, nasipaddress);
+
+-- For use by onoff-, update-, stop- and simul_* queries
+CREATE INDEX radacct_active_user_idx ON radacct (userName) WHERE AcctStopTime IS NULL;
+-- and for common statistic queries:
+CREATE INDEX radacct_start_user_idx ON radacct (acctStartTime, UserName);
+-- and, optionally
+-- CREATE INDEX radacct_stop_user_idx ON radacct (acctStopTime, UserName);
 
 /*
- * FIXME: IMHO this is WAAAY too many indexes
- * These should be pared back.
- * Field lengths and types need fixing to for speed reasons.
- *  - pnixon 2003-07-11
+ * There was WAAAY too many indexes previously. This combo index
+ * should take care of the most common searches.
+ * I have commented out all the old indexes, but left them in case
+ * someone wants them. I don't recomend anywone use them all at once
+ * as they will slow down your DB too much.
+ *  - pnixon 2003-07-13
  */
+
+/*
+ * create index radacct_UserName on radacct (UserName);
+ * create index radacct_AcctSessionId on radacct (AcctSessionId);
+ * create index radacct_AcctUniqueId on radacct (AcctUniqueId);
+ * create index radacct_FramedIPAddress on radacct (FramedIPAddress);
+ * create index radacct_NASIPAddress on radacct (NASIPAddress);
+ * create index radacct_AcctStartTime on radacct (AcctStartTime);
+ * create index radacct_AcctStopTime on radacct (AcctStopTime);
+*/
+
 
 
 /*
  * Table structure for table 'radcheck'
  */
 CREATE TABLE radcheck (
-  id SERIAL PRIMARY KEY,
-  UserName VARCHAR(30) DEFAULT '' NOT NULL,
-  Attribute VARCHAR(30),
-  op VARCHAR(2) NOT NULL DEFAULT '==',
-  Value VARCHAR(40)
+	id		SERIAL PRIMARY KEY,
+	UserName	VARCHAR(30) DEFAULT '' NOT NULL,
+	Attribute	VARCHAR(30),
+	op VARCHAR(2)	NOT NULL DEFAULT '==',
+	Value		VARCHAR(40)
 );
 create index radcheck_UserName on radcheck (UserName,Attribute);
 
@@ -102,11 +86,11 @@ create index radcheck_UserName on radcheck (UserName,Attribute);
  * Table structure for table 'radgroupcheck'
  */
 CREATE TABLE radgroupcheck (
-  id SERIAL PRIMARY KEY,
-  GroupName VARCHAR(20) DEFAULT '' NOT NULL,
-  Attribute VARCHAR(40),
-  op VARCHAR(2) NOT NULL DEFAULT '==',
-  Value VARCHAR(40)
+	id		SERIAL PRIMARY KEY,
+	GroupName	VARCHAR(20) DEFAULT '' NOT NULL,
+	Attribute	VARCHAR(40),
+	op		VARCHAR(2) NOT NULL DEFAULT '==',
+	Value		VARCHAR(40)
 );
 create index radgroupcheck_GroupName on radgroupcheck (GroupName,Attribute);
 
@@ -114,11 +98,11 @@ create index radgroupcheck_GroupName on radgroupcheck (GroupName,Attribute);
  * Table structure for table 'radgroupreply'
  */
 CREATE TABLE radgroupreply (
-  id SERIAL PRIMARY KEY,
-  GroupName VARCHAR(20) DEFAULT '' NOT NULL,
-  Attribute VARCHAR(40),
-  op VARCHAR(2) NOT NULL DEFAULT '=',
-  Value VARCHAR(40)
+	id		SERIAL PRIMARY KEY,
+	GroupName	VARCHAR(20) DEFAULT '' NOT NULL,
+	Attribute	VARCHAR(40),
+	op		VARCHAR(2) NOT NULL DEFAULT '=',
+	Value		VARCHAR(40)
 );
 create index radgroupreply_GroupName on radgroupreply (GroupName,Attribute);
 
@@ -126,11 +110,11 @@ create index radgroupreply_GroupName on radgroupreply (GroupName,Attribute);
  * Table structure for table 'radreply'
  */
 CREATE TABLE radreply (
-  id SERIAL PRIMARY KEY,
-  UserName VARCHAR(30) DEFAULT '' NOT NULL,
-  Attribute VARCHAR(30),
-  op VARCHAR(2) NOT NULL DEFAULT '=',
-  Value VARCHAR(40)
+	id		SERIAL PRIMARY KEY,
+	UserName	VARCHAR(30) DEFAULT '' NOT NULL,
+	Attribute	VARCHAR(30),
+	op		VARCHAR(2) NOT NULL DEFAULT '=',
+	Value		VARCHAR(40)
 );
 create index radreply_UserName on radreply (UserName,Attribute);
 
@@ -138,9 +122,9 @@ create index radreply_UserName on radreply (UserName,Attribute);
  * Table structure for table 'usergroup'
  */
 CREATE TABLE usergroup (
-  id SERIAL PRIMARY KEY,
-  UserName VARCHAR(30) DEFAULT '' NOT NULL,
-  GroupName VARCHAR(30)
+	id		SERIAL PRIMARY KEY,
+	UserName	VARCHAR(30) DEFAULT '' NOT NULL,
+	GroupName	VARCHAR(30)
 );
 create index usergroup_UserName on usergroup (UserName);
 
@@ -148,18 +132,38 @@ create index usergroup_UserName on usergroup (UserName);
  * Table structure for table 'realmgroup'
  */
 CREATE TABLE realmgroup (
-  id SERIAL PRIMARY KEY,
-  RealmName VARCHAR(30) DEFAULT '' NOT NULL,
-  GroupName VARCHAR(30)
+	id		SERIAL PRIMARY KEY,
+	RealmName	VARCHAR(30) DEFAULT '' NOT NULL,
+	GroupName	VARCHAR(30)
 );
 create index realmgroup_RealmName on realmgroup (RealmName);
 
+/*
+ * Table structure for table 'realms'
+ * This is not yet used by FreeRADIUS 
+ */
 CREATE TABLE realms (
-  id SERIAL PRIMARY KEY,
-  realmname VARCHAR(64),
-  nas VARCHAR(128),
-  authport int4,
-  options VARCHAR(128) DEFAULT ''
+	id		SERIAL PRIMARY KEY,
+	realmname	VARCHAR(64),
+	nas		VARCHAR(128),
+	authport	int4,
+	options		VARCHAR(128) DEFAULT ''
 );
 
+/*
+ * Table structure for table 'nas'
+ * This is not currently used by FreeRADIUS but is usefull for reporting
+ * anyway.
+ */
+CREATE TABLE nas (
+	ipaddr		INET PRIMARY KEY,
+	shortname	VARCHAR(32) NOT NULL,
+	secret		VARCHAR(60) NOT NULL,
+	nasname		VARCHAR(128),
+	type		VARCHAR(30),
+	ports		int4,
+	community	VARCHAR(50),
+	snmp		VARCHAR(10),
+	naslocation	VARCHAR(32)
+);
 
