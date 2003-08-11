9735267e101efb33cb05a3741301039555183053
aland <aland>
2003-08-11 17:06:51 +0000

    Added pre/post-proxy sections to detail module
    	Added comments in 'radiusd.conf', to show how all stages of
    	packet processing can be logged with the 'detail' module.

--- src/modules/rlm_detail/rlm_detail.c
+++ src/modules/rlm_detail/rlm_detail.c
@@ -36,6 +36,22 @@ static const char rcsid[] = "$Id$";
 #include	"modules.h"
 #define 	DIRLEN	8192
 
+static const char *packet_codes[] = {
+  "",
+  "Access-Request",
+  "Access-Accept",
+  "Access-Reject",
+  "Accounting-Request",
+  "Accounting-Response",
+  "Accounting-Status",
+  "Password-Request",
+  "Password-Accept",
+  "Password-Reject",
+  "Accounting-Message",
+  "Access-Challenge"
+};
+
+
 struct detail_instance {
 	/* detail file */
 	char *detailfile;
@@ -89,21 +105,32 @@ static int detail_instantiate(CONF_SECTION *conf, void **instance)
 	return 0;
 }
 
-static int do_detail(void *instance, REQUEST *request, VALUE_PAIR *pair)
+/*
+ *	Do detail, compatible with old accounting
+ */
+static int do_detail(void *instance, REQUEST *request, RADIUS_PACKET *packet,
+		     int compat)
 {
 	int		outfd;
 	FILE		*outfp;
 	char		buffer[DIRLEN];
 	char		*p;
-	int		ret = RLM_MODULE_OK;
 	struct stat	st;
 	int		locked;
 	int		lock_count;
 	struct timeval	tv;
 	REALM		*proxy_realm;
 	char		proxy_buffer[16];
+	VALUE_PAIR	*pair = packet->vps;
 
 	struct detail_instance *inst = instance;
+
+	/*
+	 *	Nothing to log: don't do anything.
+	 */
+	if (!packet) {
+		return RLM_MODULE_NOOP;
+	}
 
 	/*
 	 *	Create a directory for this nas.
@@ -164,7 +191,15 @@ static int do_detail(void *instance, REQUEST *request, VALUE_PAIR *pair)
 
 	/*
 	 *	Open & create the file, with the given permissions.
-	 *
+	 */
+	if ((outfd = open(buffer, O_WRONLY | O_APPEND | O_CREAT,
+			  inst->detailperm)) < 0) {
+		radlog(L_ERR, "rlm_detail: Couldn't open file %s: %s",
+		       buffer, strerror(errno));
+		return RLM_MODULE_FAIL;
+	}
+
+	/*
 	 *	If we're not using locking, we'll just pass straight though
 	 *	the while loop.
 	 *	If we fail to aquire the filelock in 80 tries (approximately
@@ -173,13 +208,6 @@ static int do_detail(void *instance, REQUEST *request, VALUE_PAIR *pair)
 	locked = 0;
 	lock_count = 0;
 	do {
-		if ((outfd = open(buffer, O_WRONLY | O_APPEND | O_CREAT,
-				  inst->detailperm)) < 0) {
-			radlog(L_ERR, "rlm_detail: Couldn't open file %s: %s",
-			       buffer, strerror(errno));
-			ret = RLM_MODULE_FAIL;
-			break;
-		}
 		if (inst->locking) {
 			lseek(outfd, 0L, SEEK_SET);
 			if (rad_lockfd_nonblock(outfd, 0) < 0) {
@@ -189,7 +217,7 @@ static int do_detail(void *instance, REQUEST *request, VALUE_PAIR *pair)
 				select(0, NULL, NULL, NULL, &tv);
 				lock_count++;
 			} else {
-				DEBUG("rlm_detail: Aquired filelock, tried %d time(s)",
+				DEBUG("rlm_detail: Acquired filelock, tried %d time(s)",
 				      lock_count + 1);
 				locked = 1;
 			}
@@ -199,75 +227,104 @@ static int do_detail(void *instance, REQUEST *request, VALUE_PAIR *pair)
 	if (!locked && inst->locking && lock_count >= 80) {
 		radlog(L_ERR, "rlm_detail: Failed to aquire filelock for %s, giving up",
 		       buffer);
-		outfd = -1;
-		ret = RLM_MODULE_FAIL;
+		return RLM_MODULE_FAIL;
 	}
 
-	outfp = NULL;
-	if (outfd > -1) {
-		if ((outfp = fdopen(outfd, "a")) == NULL) {
-			radlog(L_ERR, "rlm_detail: Couldn't open file %s: %s",
-			       buffer, strerror(errno));
-			ret = RLM_MODULE_FAIL;
-			if (inst->locking) {
-				lseek(outfd, 0L, SEEK_SET);
-				rad_unlockfd(outfd, 0);
-				DEBUG("rlm_detail: Released filelock");
-			}
-			close(outfd);
+	/*
+	 *	Convert the FD to FP.  The FD is no longer valid
+	 *	after this operation.
+	 */
+	if ((outfp = fdopen(outfd, "a")) == NULL) {
+		radlog(L_ERR, "rlm_detail: Couldn't open file %s: %s",
+		       buffer, strerror(errno));
+		if (inst->locking) {
+			lseek(outfd, 0L, SEEK_SET);
+			rad_unlockfd(outfd, 0);
+			DEBUG("rlm_detail: Released filelock");
+		}
+		close(outfd);
+
+		return RLM_MODULE_FAIL;
+	}
+
+	/*
+	 *	Write the information to the file.
+	 */
+	if (!compat) {
+		/*
+		 *	Print out names, if they're OK.
+		 *	Numbers, if not.
+		 */
+		if ((packet->code > 0) &&
+		    (packet->code <= PW_ACCESS_CHALLENGE)) {
+			fprintf(outfp, "Packet-Type = %s\n",
+				packet_codes[packet->code]);
+		} else {
+			fprintf(outfp, "Packet-Type = %d\n", packet->code);
 		}
 	}
 
-	if (outfd > -1 && outfp) {
-		/* Post a timestamp */
-		fseek(outfp, 0L, SEEK_END);
-		fputs(ctime_r(&request->timestamp, buffer), outfp);
-
-		/* Write each attribute/value to the log file */
-		while (pair) {
-			if (pair->attribute != PW_PASSWORD) {
-				fputs("\t", outfp);
-				vp_print(outfp, pair);
-				fputs("\n", outfp);
-			}
-			pair = pair->next;
-		}
+	/*
+	 *	Post a timestamp
+	 */
+	fseek(outfp, 0L, SEEK_END);
+	fputs(ctime_r(&request->timestamp, buffer), outfp);
+	
+	/* Write each attribute/value to the log file */
+	while (pair) {
+		/*
+		 *	Don't print passwords in old format...
+		 */
+		if (compat && (pair->attribute == PW_PASSWORD)) continue;
 
 		/*
-		 *	Add non-protocol attibutes.
+		 *	Print all of the attributes.
 		 */
-		if ((pair = pairfind(request->config_items, PW_PROXY_TO_REALM))
-		    != NULL) {
+		fputs("\t", outfp);
+		vp_print(outfp, pair);
+		fputs("\n", outfp);
+		pair = pair->next;
+	}
+
+	/*
+	 *	Add non-protocol attibutes.
+	 */
+	if (compat) {
+		if ((pair = pairfind(request->config_items,
+				     PW_PROXY_TO_REALM)) != NULL) {
 			proxy_realm = realm_find(pair->strvalue, TRUE);
 			if (proxy_realm) {
 				memset((char *) proxy_buffer, 0, 16);
 				ip_ntoa(proxy_buffer, proxy_realm->acct_ipaddr);
 				fprintf(outfp, "\tFreeradius-Proxied-To = %s\n",
-				        proxy_buffer);
+					proxy_buffer);
 				DEBUG("rlm_detail: Freeradius-Proxied-To set to %s",
 				      proxy_buffer);
 			}
 		}
 		fprintf(outfp, "\tTimestamp = %ld\n", request->timestamp);
+
 		if (request->packet->verified == 2)
 			fputs("\tRequest-Authenticator = Verified\n", outfp);
 		else if (request->packet->verified == 1)
 			fputs("\tRequest-Authenticator = None\n", outfp);
-		pair = NULL;
-
-		fputs("\n", outfp);
-
-		if (inst->locking) {
-			fflush(outfp);
-			lseek(outfd, 0L, SEEK_SET);
-			rad_unlockfd(outfd, 0);
-			DEBUG("rlm_detail: Released filelock");
-		}
-	
-		fclose(outfp);
 	}
+	
+	fputs("\n", outfp);
 
-	return ret;
+	if (inst->locking) {
+		fflush(outfp);
+		lseek(outfd, 0L, SEEK_SET);
+		rad_unlockfd(outfd, 0);
+		DEBUG("rlm_detail: Released filelock");
+	}
+	
+	fclose(outfp);
+
+	/*
+	 *	And everything is fine.
+	 */
+	return RLM_MODULE_OK;
 }
 
 /*
@@ -276,7 +333,7 @@ static int do_detail(void *instance, REQUEST *request, VALUE_PAIR *pair)
 static int detail_accounting(void *instance, REQUEST *request)
 {
 
-	return do_detail(instance,request,request->packet->vps);
+	return do_detail(instance,request,request->packet, TRUE);
 }
 
 /*
@@ -284,7 +341,7 @@ static int detail_accounting(void *instance, REQUEST *request)
  */
 static int detail_authorize(void *instance, REQUEST *request)
 {
-	return do_detail(instance,request,request->packet->vps);
+	return do_detail(instance,request,request->packet, FALSE);
 }
 
 /*
@@ -292,7 +349,35 @@ static int detail_authorize(void *instance, REQUEST *request)
  */
 static int detail_postauth(void *instance, REQUEST *request)
 {
-	return do_detail(instance,request,request->reply->vps);
+	return do_detail(instance,request,request->reply, FALSE);
+}
+
+
+/*
+ *	Outgoing Access-Request to home server - write the detail files.
+ */
+static int detail_pre_proxy(void *instance, REQUEST *request)
+{
+	if (request->proxy &&
+	    request->proxy->vps) {
+		return do_detail(instance,request,request->proxy, FALSE);
+	}
+
+	return RLM_MODULE_NOOP;
+}
+
+
+/*
+ *	Outgoing Access-Request Reply - write the detail files.
+ */
+static int detail_post_proxy(void *instance, REQUEST *request)
+{
+	if (request->proxy_reply &&
+	    request->proxy_reply->vps) {
+		return do_detail(instance,request,request->proxy_reply, FALSE);
+	}
+
+	return RLM_MODULE_NOOP;
 }
 
 
@@ -323,8 +408,8 @@ module_t rlm_detail = {
 		NULL,			/* preaccounting */
 		detail_accounting,	/* accounting */
 		NULL,			/* checksimul */
-		NULL,			/* pre-proxy */
-		NULL,			/* post-proxy */
+		detail_pre_proxy,      	/* pre-proxy */
+		detail_post_proxy,	/* post-proxy */
 		detail_postauth		/* post-auth */
 	},
 	detail_detach,			/* detach */
