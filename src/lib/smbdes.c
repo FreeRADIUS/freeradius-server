3bbc26c0b423e4ae9aeee646ee58372701750b16
phampson <phampson>
2003-09-01 03:29:38 +0000

    Make arrays const unsigned char. Patch by Steve Grubb <linux_4ever@yahoo.com>

--- src/lib/smbdes.c
+++ src/lib/smbdes.c
@@ -1,11 +1,10 @@
 /* 
-   Unix SMB/Netbios implementation.
-   Version 1.9.
+   Unix SMB/CIFS implementation.
 
    a partial implementation of DES designed for use in the 
    SMB authentication protocol
 
-   Copyright (C) Andrew Tridgell 1997
+   Copyright (C) Andrew Tridgell 1998
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -49,7 +48,9 @@
 #include <ctype.h>
 
 
-static int perm1[56] = {57, 49, 41, 33, 25, 17,  9,
+#define uchar unsigned char
+
+static const uchar perm1[56] = {57, 49, 41, 33, 25, 17,  9,
 			 1, 58, 50, 42, 34, 26, 18,
 			10,  2, 59, 51, 43, 35, 27,
 			19, 11,  3, 60, 52, 44, 36,
@@ -58,7 +59,7 @@ static int perm1[56] = {57, 49, 41, 33, 25, 17,  9,
 			14,  6, 61, 53, 45, 37, 29,
 			21, 13,  5, 28, 20, 12,  4};
 
-static int perm2[48] = {14, 17, 11, 24,  1,  5,
+static const uchar perm2[48] = {14, 17, 11, 24,  1,  5,
                          3, 28, 15,  6, 21, 10,
                         23, 19, 12,  4, 26,  8,
                         16,  7, 27, 20, 13,  2,
@@ -67,7 +68,7 @@ static int perm2[48] = {14, 17, 11, 24,  1,  5,
                         44, 49, 39, 56, 34, 53,
                         46, 42, 50, 36, 29, 32};
 
-static int perm3[64] = {58, 50, 42, 34, 26, 18, 10,  2,
+static const uchar perm3[64] = {58, 50, 42, 34, 26, 18, 10,  2,
 			60, 52, 44, 36, 28, 20, 12,  4,
 			62, 54, 46, 38, 30, 22, 14,  6,
 			64, 56, 48, 40, 32, 24, 16,  8,
@@ -76,7 +77,7 @@ static int perm3[64] = {58, 50, 42, 34, 26, 18, 10,  2,
 			61, 53, 45, 37, 29, 21, 13,  5,
 			63, 55, 47, 39, 31, 23, 15,  7};
 
-static int perm4[48] = {   32,  1,  2,  3,  4,  5,
+static const uchar perm4[48] = {   32,  1,  2,  3,  4,  5,
                             4,  5,  6,  7,  8,  9,
                             8,  9, 10, 11, 12, 13,
                            12, 13, 14, 15, 16, 17,
@@ -85,7 +86,7 @@ static int perm4[48] = {   32,  1,  2,  3,  4,  5,
                            24, 25, 26, 27, 28, 29,
                            28, 29, 30, 31, 32,  1};
 
-static int perm5[32] = {      16,  7, 20, 21,
+static const uchar perm5[32] = {      16,  7, 20, 21,
                               29, 12, 28, 17,
                                1, 15, 23, 26,
                                5, 18, 31, 10,
@@ -95,7 +96,7 @@ static int perm5[32] = {      16,  7, 20, 21,
                               22, 11,  4, 25};
 
 
-static int perm6[64] ={ 40,  8, 48, 16, 56, 24, 64, 32,
+static const uchar perm6[64] ={ 40,  8, 48, 16, 56, 24, 64, 32,
                         39,  7, 47, 15, 55, 23, 63, 31,
                         38,  6, 46, 14, 54, 22, 62, 30,
                         37,  5, 45, 13, 53, 21, 61, 29,
@@ -105,9 +106,9 @@ static int perm6[64] ={ 40,  8, 48, 16, 56, 24, 64, 32,
                         33,  1, 41,  9, 49, 17, 57, 25};
 
 
-static int sc[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+static const uchar sc[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
-static int sbox[8][4][16] = {
+static const uchar sbox[8][4][16] = {
 	{{14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
 	 {0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8},
 	 {4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
@@ -148,7 +149,7 @@ static int sbox[8][4][16] = {
 	 {7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8},
 	 {2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11}}};
 
-static void permute(char *out, char *in, int *p, int n)
+static void permute(char *out, const char *in, const uchar *p, int n)
 {
 	int i;
 	for (i=0;i<n;i++)
