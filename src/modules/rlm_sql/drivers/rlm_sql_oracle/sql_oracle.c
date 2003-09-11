44739d34bb13b022e080e8ffe58594cad4bc1030
phampson <phampson>
2003-09-11 00:53:59 +0000

    Add GNU GPLv2 boilerplate to the top of files which were missing from it. Patch from Nicholas Baradakis [nbk@sitadelle.com]

--- src/modules/rlm_sql/drivers/rlm_sql_oracle/sql_oracle.c
+++ src/modules/rlm_sql/drivers/rlm_sql_oracle/sql_oracle.c
@@ -1,10 +1,24 @@
-/***************************************************************************
-*  sql_oracle.c                        rlm_sql - FreeRADIUS SQL Module     *
-*                                                                          *
-*      Oracle (OCI) routines for rlm_sql                                   *
-*                                                                          *
-*                                     David Kerry <davidk@snti.com>        *
-***************************************************************************/
+/*
+ * sql_oracle.c	Oracle (OCI) routines for rlm_sql
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Copyright 2000  The FreeRADIUS server project
+ * Copyright 2000  David Kerry <davidk@snti.com>
+ */
+
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/time.h>
