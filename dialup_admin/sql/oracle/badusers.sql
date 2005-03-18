5056b35245ad0c0120f79b244a13cf539d4d2cf3
pnixon <pnixon>
2005-03-18 22:13:58 +0000

    badusers for oracle

---
+++ dialup_admin/sql/oracle/badusers.sql
@@ -0,0 +1,31 @@
+/*
+ * $Id$
+ *
+ */
+
+/*
+ * Table structure for table 'radcheck'
+ */
+
+CREATE TABLE badusers (
+	id		INT PRIMARY KEY,
+	username	VARCHAR(30) NOT NULL,
+	actiondate	DATE,
+	reason		VARCHAR(128) NOT NULL,
+	admin		VARCHAR(128) NOT NULL
+);
+CREATE SEQUENCE badusers_seq START WITH 1 INCREMENT BY 1;
+CREATE INDEX badusers_actiondate_idx ON badusers (actiondate);
+CREATE INDEX badusers_username_idx ON badusers (username);
+
+/* Trigger to emulate a serial # on the primary key */
+CREATE OR REPLACE TRIGGER badusers_serialnumber
+        BEFORE INSERT OR UPDATE OF id ON badusers
+        FOR EACH ROW
+        BEGIN
+                if ( :new.id = 0 or :new.id is null ) then
+                        SELECT badusers_seq.nextval into :new.id from dual;
+                end if;
+        END;
+/
+
