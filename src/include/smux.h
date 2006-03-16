7d600ee13323531e548a6c69eaa9dcecf4655673
nbk <nbk>
2006-03-16 16:46:19 +0000

    Update the GPL boilerplate with the new address of the FSF.

--- src/include/smux.h
+++ src/include/smux.h
@@ -2,10 +2,19 @@
  * Copyright (C) 2000 Jochen Friedrich <jochen@scram.de>
  * Copyright (C) 1999 Kunihiro Ishiguro <kunihiro@zebra.org>
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
- * along with GNU Zebra; see the file COPYING.  If not, write to the Free
- * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
 #ifndef _SMUX_H
