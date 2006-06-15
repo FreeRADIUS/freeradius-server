aa155b20e96c05d9a572bc5f3a92817e7116932b
aland <aland>
2006-06-15 21:47:14 +0000

    Port update hash code (using hash && compare callbacks) from the 	head.

--- src/include/libradius.h
+++ src/include/libradius.h
@@ -387,15 +387,19 @@ uint32_t lrad_hash_fold(uint32_t hash, int bits);
 
 typedef struct lrad_hash_table_t lrad_hash_table_t;
 typedef void (*lrad_hash_table_free_t)(void *);
+typedef uint32_t (*lrad_hash_table_hash_t)(const void *);
+typedef int (*lrad_hash_table_cmp_t)(const void *, const void *);
 typedef int (*lrad_hash_table_walk_t)(void * /* ctx */, void * /* data */);
 
-lrad_hash_table_t *lrad_hash_table_create(lrad_hash_table_free_t freeNode);
+lrad_hash_table_t *lrad_hash_table_create(lrad_hash_table_hash_t hashNode,
+					  lrad_hash_table_cmp_t cmpNode,
+					  lrad_hash_table_free_t freeNode);
 void		lrad_hash_table_free(lrad_hash_table_t *ht);
-int		lrad_hash_table_insert(lrad_hash_table_t *ht, uint32_t key, void *data);
-int		lrad_hash_table_delete(lrad_hash_table_t *ht, uint32_t key);
-void		*lrad_hash_table_yank(lrad_hash_table_t *ht, uint32_t key);
-int		lrad_hash_table_replace(lrad_hash_table_t *ht, uint32_t key, void *data);
-void		*lrad_hash_table_finddata(lrad_hash_table_t *ht, uint32_t key);
+int		lrad_hash_table_insert(lrad_hash_table_t *ht, void *data);
+int		lrad_hash_table_delete(lrad_hash_table_t *ht, const void *data);
+void		*lrad_hash_table_yank(lrad_hash_table_t *ht, const void *data);
+int		lrad_hash_table_replace(lrad_hash_table_t *ht, void *data);
+void		*lrad_hash_table_finddata(lrad_hash_table_t *ht, const void *data);
 int		lrad_hash_table_num_elements(lrad_hash_table_t *ht);
 int		lrad_hash_table_walk(lrad_hash_table_t *ht,
 				     lrad_hash_table_walk_t callback,
