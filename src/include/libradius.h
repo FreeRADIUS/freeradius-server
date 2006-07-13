68600e2468777ce297f324f9bd7ccf6148bc1869
aland <aland>
2006-07-13 21:17:24 +0000

    Pull off the top bit, too for VENDOR, due to recent Diameter 	integration hacks

--- src/include/libradius.h
+++ src/include/libradius.h
@@ -96,7 +96,7 @@
 #  define VENDORPEC_USR		429
 #define VENDORPEC_LUCENT	4846
 #define VENDORPEC_STARENT	8164
-#  define VENDOR(x)		((x >> 16) & 0xffff)
+#  define VENDOR(x)		((x >> 16) & 0x7fff)
 #  define DEBUG			if (librad_debug) printf
 #  define debug_pair(vp)	do { if (librad_debug) { \
 					putchar('\t'); \
