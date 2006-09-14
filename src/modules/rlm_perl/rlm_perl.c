f2db73867244e543ed38e8794a9227e52d799b82
aland <aland>
2006-09-14 17:41:34 +0000

    Reverted change 1.38 -> 1.39.  pairmove() uses operators, which 	is unnecessary here.  The Perl code can decide which attributes 	should be in which lists, so the operators are entirely unnecessary.
    	In addition, pairmove() may leave attributes in the source list,
    	so the code in 1.39 has a memory leak, because it doesn't call
    	pairfree.
    
    	A fix for bug #390 will be committed separately.

--- src/modules/rlm_perl/rlm_perl.c
+++ src/modules/rlm_perl/rlm_perl.c
@@ -1073,29 +1073,34 @@ static int rlmperl_call(void *instance, REQUEST *request, char *function_name)
 	
 	vp = NULL;
 	if ((get_hv_content(rad_request_hv, &vp)) > 0 ) {
-		pairmove(&request->packet->vps, &vp);
+		pairfree(&request->packet->vps);
+		request->packet->vps = vp;
 		vp = NULL;
 	}
 
 	if ((get_hv_content(rad_reply_hv, &vp)) > 0 ) {
-		pairmove(&request->reply->vps, &vp);
+		pairfree(&request->reply->vps);
+		request->reply->vps = vp;
 		vp = NULL;
 	}
 
 	if ((get_hv_content(rad_check_hv, &vp)) > 0 ) {
-		pairmove(&request->config_items, &vp);
+		pairfree(&request->config_items);
+		request->config_items = vp;
 		vp = NULL;
 	}
 	
 	if (request->proxy &&
 	    (get_hv_content(rad_request_proxy_hv, &vp) > 0)) {
-		pairmove(&request->proxy->vps, &vp);
+		pairfree(&request->proxy->vps);
+		request->proxy->vps = vp;
 		vp = NULL;
 	}
 
 	if (request->proxy_reply &&
 	    (get_hv_content(rad_request_proxy_reply_hv, &vp) > 0)) {
-		pairmove(&request->proxy_reply->vps, &vp);
+		pairfree(&request->proxy_reply->vps);
+		request->proxy_reply->vps = vp;
 		vp = NULL;
 	}
 
