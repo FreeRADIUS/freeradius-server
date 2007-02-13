fef418b304b26a8f2060cdd2b2619bde02c485ba
aland <aland>
2007-02-13 14:11:17 +0000

    Casts to quiet the compiler

--- src/lib/sha1.c
+++ src/lib/sha1.c
@@ -150,9 +150,9 @@ uint8_t finalcount[8];
         finalcount[i] = (uint8_t)((context->count[(i >= 4 ? 0 : 1)]
          >> ((3-(i & 3)) * 8) ) & 255);  /* Endian independent */
     }
-    SHA1Update(context, "\200", 1);
+    SHA1Update(context, (const unsigned char *) "\200", 1);
     while ((context->count[0] & 504) != 448) {
-        SHA1Update(context, "\0", 1);
+        SHA1Update(context, (const unsigned char *) "\0", 1);
     }
     SHA1Update(context, finalcount, 8);  /* Should cause a SHA1Transform() */
     for (i = 0; i < 20; i++) {
