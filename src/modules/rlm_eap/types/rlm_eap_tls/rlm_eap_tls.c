048d04a71ea6eae43674e8b70b3b115f3ad8819a
aland <aland>
2007-04-20 11:58:45 +0000

    Note warnings if there's no DH, and how to fix it.

--- src/modules/rlm_eap/types/rlm_eap_tls/rlm_eap_tls.c
+++ src/modules/rlm_eap/types/rlm_eap_tls/rlm_eap_tls.c
@@ -88,6 +88,12 @@ static int load_dh_params(SSL_CTX *ctx, char *file)
 
 	dh = PEM_read_bio_DHparams(bio, NULL, NULL, NULL);
 	BIO_free(bio);
+	if (!dh) {
+		radlog(L_INFO, "WARNING: rlm_eap_tls: Unable to set DH parameters.  DH cipher suites may not work!");
+		DEBUG2("WARNING: Fix this by running the OpenSSL command listed in eap.conf");
+		return 0;
+	}
+
 	if (SSL_CTX_set_tmp_dh(ctx, dh) < 0) {
 		radlog(L_ERR, "rlm_eap_tls: Unable to set DH parameters");
 		DH_free(dh);
