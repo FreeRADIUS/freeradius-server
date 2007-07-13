f632f6f879f6c8b2deff44709200a9645111cfd3
pnixon <pnixon>
2007-07-13 20:29:52 +0000

    add indexes and sequence for ippool

--- doc/examples/oracle.sql
+++ doc/examples/oracle.sql
@@ -207,3 +207,19 @@ CREATE TABLE radippool (
 	username		VARCHAR(100)
 );
 
+CREATE INDEX radippool_poolname_ipaadr ON radippool (pool_name, framedipaddress);
+CREATE INDEX radippool_poolname_expire ON radippool (pool_name, expiry_time);
+CREATE INDEX radippool_nasipaddr_key ON radippool (nasipaddress, pool_key);
+CREATE INDEX radippool_nasipaddr_calling ON radippool (nasipaddress, callingstationid);
+
+CREATE SEQUENCE radippool_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER radippool_serialnumber
+        BEFORE INSERT OR UPDATE OF id ON radippool
+        FOR EACH ROW
+        BEGIN
+                if ( :new.id = 0 or :new.id is null ) then
+                        SELECT radippool_seq.nextval into :new.id from dual;
+                end if;
+        END;
+/
