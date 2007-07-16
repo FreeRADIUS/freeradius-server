83f836d441043370713c55b50e35a5e17ecec412
pnixon <pnixon>
2007-07-16 10:03:45 +0000

    Fix syntax error in new radippool table

--- doc/examples/mysql.sql
+++ doc/examples/mysql.sql
@@ -170,12 +170,12 @@ CREATE TABLE nas (
 CREATE TABLE radippool (
   id                    int(11) unsigned NOT NULL auto_increment,
   pool_name             varchar(30) NOT NULL,
-  FramedIPAddress       varchar(15) NOT NULL default ,
-  NASIPAddress          varchar(15) NOT NULL default ,
+  FramedIPAddress       varchar(15) NOT NULL default '',
+  NASIPAddress          varchar(15) NOT NULL default '',
   CalledStationId       VARCHAR(30) NOT NULL,
   CallingStationID      VARCHAR(30) NOT NULL,
   expiry_time           DATETIME NOT NULL default '0000-00-00 00:00:00',
-  username              varchar(64) NOT NULL default ,
+  username              varchar(64) NOT NULL default '',
   pool_key              varchar(30) NOT NULL,
   PRIMARY KEY (id)
 );
