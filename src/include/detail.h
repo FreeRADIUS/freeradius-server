84c3fb3cfeaee0e4f4c6b35b4b1b030da40ece01
aland <aland>
2007-08-25 04:06:58 +0000

    Moved detail file handling to its own source file

---
+++ src/include/detail.h
@@ -0,0 +1,22 @@
+#ifndef DETAIL_H
+#define DETAIL_H
+/*
+ *	detail.h	Routines to handle detail files.
+ *
+ * Version:	$Id$
+ *
+ */
+
+#include <freeradius-devel/ident.h>
+RCSIDH(detail_h, "$Id$")
+
+int detail_recv(rad_listen_t *listener,
+		RAD_REQUEST_FUNP *pfun, REQUEST **prequest);
+int detail_send(rad_listen_t *listener, REQUEST *request);
+void detail_free(rad_listen_t *this);
+int detail_print(rad_listen_t *this, char *buffer, size_t bufsize);
+int detail_encode(UNUSED rad_listen_t *this, UNUSED REQUEST *request);
+int detail_decode(UNUSED rad_listen_t *this, UNUSED REQUEST *request);
+int detail_parse(CONF_SECTION *cs, rad_listen_t *this);
+
+#endif /* DETAIL_H */
