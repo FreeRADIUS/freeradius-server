9016368f569662e1fbd896da8ec5527d93aadb0a
pnixon <pnixon>
2007-09-17 21:09:49 +0000

    Standardise radpostauth column names

--- raddb/sql/mysql/schema.sql
+++ raddb/sql/mysql/schema.sql
@@ -125,9 +125,9 @@ CREATE TABLE radusergroup (
 
 CREATE TABLE radpostauth (
   id int(11) NOT NULL auto_increment,
-  user varchar(64) NOT NULL default '',
+  username varchar(64) NOT NULL default '',
   pass varchar(64) NOT NULL default '',
   reply varchar(32) NOT NULL default '',
-  date timestamp(14) NOT NULL,
+  authdate timestamp(14) NOT NULL,
   PRIMARY KEY  (id)
 ) ;
