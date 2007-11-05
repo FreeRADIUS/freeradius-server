cd9452613a1395523811e6b3feddecad62e55d7c
aland <aland>
2007-11-05 08:59:00 +0000

    Updated Schema

--- raddb/sql/mysql/ippool.sql
+++ raddb/sql/mysql/ippool.sql
@@ -8,7 +8,7 @@ CREATE TABLE radippool (
   nasipaddress          varchar(15) NOT NULL default '',
   calledstationid       VARCHAR(30) NOT NULL,
   callingstationid      VARCHAR(30) NOT NULL,
-  expiry_time           DATETIME NOT NULL default '0000-00-00 00:00:00',
+  expiry_time           DATETIME NULL default NULL,
   username              varchar(64) NOT NULL default '',
   pool_key              varchar(30) NOT NULL,
   PRIMARY KEY (id)
