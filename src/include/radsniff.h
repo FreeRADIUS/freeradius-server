8e2046133318522755918aee2e2747832b31dac8
aland <aland>
2008-05-09 19:36:08 +0000

    u_int* -> uint*

--- src/include/radsniff.h
+++ src/include/radsniff.h
@@ -36,9 +36,9 @@ RCSIDH(radsniff_h, "$Id$")
  *	Structure of a DEC/Intel/Xerox or 802.3 Ethernet header.
  */
 struct  ethernet_header {
-        u_int8_t	ethernet_dhost[ETHER_ADDR_LEN];
-        u_int8_t	ethernet_shost[ETHER_ADDR_LEN];
-        u_int16_t	ethernet_type;
+        uint8_t	ethernet_dhost[ETHER_ADDR_LEN];
+        uint8_t	ethernet_shost[ETHER_ADDR_LEN];
+        uint16_t	ethernet_type;
 };
 
 /*
@@ -53,19 +53,19 @@ struct  ethernet_header {
  *	Structure of an internet header, naked of options.
  */
 struct ip_header {
-        u_int8_t        ip_vhl;         /* header length, version */
+        uint8_t        ip_vhl;         /* header length, version */
 #define IP_V(ip)        (((ip)->ip_vhl & 0xf0) >> 4)
 #define IP_HL(ip)       ((ip)->ip_vhl & 0x0f)
-        u_int8_t        ip_tos;         /* type of service */
-        u_int16_t       ip_len;         /* total length */
-        u_int16_t       ip_id;          /* identification */
-        u_int16_t       ip_off;         /* fragment offset field */
-#define IP_DF 0x4000                    /* dont fragment flag */
+        uint8_t        ip_tos;         /* type of service */
+        uint16_t       ip_len;         /* total length */
+        uint16_t       ip_id;          /* identification */
+        uint16_t       ip_off;         /* fragment offset field */
+#define I_DF 0x4000                    /* dont fragment flag */
 #define IP_MF 0x2000                    /* more fragments flag */
 #define IP_OFFMASK 0x1fff               /* mask for fragmenting bits */
-        u_int8_t        ip_ttl;         /* time to live */
-        u_int8_t        ip_p;           /* protocol */
-        u_int16_t       ip_sum;         /* checksum */
+        uint8_t        ip_ttl;         /* time to live */
+        uint8_t        ip_p;           /* protocol */
+        uint16_t       ip_sum;         /* checksum */
         struct in_addr  ip_src,ip_dst;  /* source and dest address */
 };
 
@@ -74,10 +74,10 @@ struct ip_header {
  *	Per RFC 768, September, 1981.
  */
 struct udp_header {
-        u_int16_t       udp_sport;               /* source port */
-        u_int16_t       udp_dport;               /* destination port */
-        u_int16_t       udp_ulen;                /* udp length */
-        u_int16_t       udp_sum;                 /* udp checksum */
+        uint16_t       udp_sport;               /* source port */
+        uint16_t       udp_dport;               /* destination port */
+        uint16_t       udp_ulen;                /* udp length */
+        uint16_t       udp_sum;                 /* udp checksum */
 };
 
 /*
