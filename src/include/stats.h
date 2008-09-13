5456bdf49040d18de88ce662bd30f14f7f41cf38
Alan T. DeKok <aland@freeradius.org>
2008-09-13 16:29:18 +0200

    Added declaration for proxy stats

--- src/include/stats.h
+++ src/include/stats.h
@@ -58,6 +58,10 @@ typedef struct fr_stats_ema_t {
 
 extern fr_stats_t	radius_auth_stats;
 extern fr_stats_t	radius_acct_stats;
+#ifdef WITH_PROXY
+extern fr_stats_t	proxy_auth_stats;
+extern fr_stats_t	proxy_acct_stats;
+#endif
 
 void radius_stats_init(int flag);
 void request_stats_final(REQUEST *request);
