1d07d14506aa96792cee69bf7887c13fbc0cbc42
Alan T. DeKok <aland@freeradius.org>
2009-10-06 08:48:02 +0200

    Mark home server dead if it doesn't respond to pings

--- src/main/event.c
+++ src/main/event.c
@@ -100,6 +100,7 @@ static fr_packet_list_t *proxy_list = NULL;
  */
 static int		proxy_fds[32];
 static rad_listen_t	*proxy_listeners[32];
+static void check_for_zombie_home_server(REQUEST *request);
 #else
 #define remove_from_proxy_hash(foo)
 #endif
@@ -633,6 +634,8 @@ static void no_response_to_ping(void *ctx)
 			 &request->proxy->dst_ipaddr.ipaddr,
 			 buffer, sizeof(buffer)),
 	       request->proxy->dst_port);
+
+	check_for_zombie_home_server(request);
 
 	wait_for_proxy_id_to_expire(request);
 }
