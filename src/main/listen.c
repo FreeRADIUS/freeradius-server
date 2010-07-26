18b43da070a9fb44f5f31337ff3e462a0afc038b
Alan T. DeKok <aland@freeradius.org>
2010-07-26 16:53:00 +0200

    Revert "Delete control socket when the server exits."
    This reverts commit 061916989a6328f5d725d4085d58adfc49d5940c.

--- src/main/listen.c
+++ src/main/listen.c
@@ -1350,8 +1350,8 @@ static const rad_listen_master_t master_listen[RAD_LISTEN_MAX] = {
 #endif
 
 #ifdef WITH_COMMAND_SOCKET
-	/* Unix domain command socket */
-	{ command_socket_parse, command_socket_free,
+	/* TCP command socket */
+	{ command_socket_parse, NULL,
 	  command_domain_accept, command_domain_send,
 	  command_socket_print, command_socket_encode, command_socket_decode },
 #endif
