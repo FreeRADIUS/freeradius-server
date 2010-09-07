f865232e427f7d7c0b8ac778641769a9745a5ad9
Alan T. DeKok <aland@freeradius.org>
2010-09-07 22:20:55 +0200

    Fix escaping rules.  Closes bug #46

--- src/lib/valuepair.c
+++ src/lib/valuepair.c
@@ -933,6 +933,10 @@ VALUE_PAIR *pairparsevalue(VALUE_PAIR *vp, const char *value)
 						c = '\'';
 						cp++;
 						break;
+					case '\\':
+						c = '\\';
+						cp++;
+						break;
 					case '`':
 						c = '`';
 						cp++;
