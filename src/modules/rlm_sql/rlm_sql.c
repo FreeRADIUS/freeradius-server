e511e4dda1eef5ced3db42c26d352bcb56cf5b2a
Alan T. DeKok <aland@freeradius.org>
2010-12-17 14:28:03 +0100

    Release group list on error

--- src/modules/rlm_sql/rlm_sql.c
+++ src/modules/rlm_sql/rlm_sql.c
@@ -696,6 +696,7 @@ static int rlm_sql_process_groups(SQL_INST *inst, REQUEST *request, SQLSOCK *sql
 		if (!sql_group) {
 			radlog_request(L_ERR, 0, request,
 				       "Error creating Sql-Group attribute");
+			sql_grouplist_free(&group_list);
 			return -1;
 		}
 		pairadd(&request->packet->vps, sql_group);
@@ -704,6 +705,7 @@ static int rlm_sql_process_groups(SQL_INST *inst, REQUEST *request, SQLSOCK *sql
 				       "Error generating query; rejecting user");
 			/* Remove the grouup we added above */
 			pairdelete(&request->packet->vps, PW_SQL_GROUP, 0);
+			sql_grouplist_free(&group_list);
 			return -1;
 		}
 		rows = sql_getvpdata(inst, sqlsocket, &check_tmp, querystr);
@@ -713,6 +715,7 @@ static int rlm_sql_process_groups(SQL_INST *inst, REQUEST *request, SQLSOCK *sql
 			/* Remove the grouup we added above */
 			pairdelete(&request->packet->vps, PW_SQL_GROUP, 0);
 			pairfree(&check_tmp);
+			sql_grouplist_free(&group_list);
 			return -1;
 		} else if (rows > 0) {
 			/*
@@ -730,6 +733,7 @@ static int rlm_sql_process_groups(SQL_INST *inst, REQUEST *request, SQLSOCK *sql
 					/* Remove the grouup we added above */
 					pairdelete(&request->packet->vps, PW_SQL_GROUP, 0);
 					pairfree(&check_tmp);
+					sql_grouplist_free(&group_list);
 					return -1;
 				}
 				if (sql_getvpdata(inst, sqlsocket, &reply_tmp, querystr) < 0) {
@@ -739,6 +743,7 @@ static int rlm_sql_process_groups(SQL_INST *inst, REQUEST *request, SQLSOCK *sql
 					pairdelete(&request->packet->vps, PW_SQL_GROUP, 0);
 					pairfree(&check_tmp);
 					pairfree(&reply_tmp);
+					sql_grouplist_free(&group_list);
 					return -1;
 				}
 				*dofallthrough = fallthrough(reply_tmp);
@@ -763,6 +768,7 @@ static int rlm_sql_process_groups(SQL_INST *inst, REQUEST *request, SQLSOCK *sql
 				/* Remove the grouup we added above */
 				pairdelete(&request->packet->vps, PW_SQL_GROUP, 0);
 				pairfree(&check_tmp);
+				sql_grouplist_free(&group_list);
 				return -1;
 			}
 			if (sql_getvpdata(inst, sqlsocket, &reply_tmp, querystr) < 0) {
@@ -772,6 +778,7 @@ static int rlm_sql_process_groups(SQL_INST *inst, REQUEST *request, SQLSOCK *sql
 				pairdelete(&request->packet->vps, PW_SQL_GROUP, 0);
 				pairfree(&check_tmp);
 				pairfree(&reply_tmp);
+				sql_grouplist_free(&group_list);
 				return -1;
 			}
 			*dofallthrough = fallthrough(reply_tmp);
