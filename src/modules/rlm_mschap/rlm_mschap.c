e6297b738175ea46997d88554c1610a17ad4346d
Alan T. DeKok <aland@freeradius.org>
2011-02-23 11:01:17 +0100

    Use rad_digest_cmp() to avoid timing attacks

--- src/modules/rlm_mschap/rlm_mschap.c
+++ src/modules/rlm_mschap/rlm_mschap.c
@@ -678,7 +678,7 @@ static int do_mschap(rlm_mschap_t *inst,
 		}
 
 		smbdes_mschap(password->vp_octets, challenge, calculated);
-		if (memcmp(response, calculated, 24) != 0) {
+		if (rad_digest_cmp(response, calculated, 24) != 0) {
 			return -1;
 		}
 
