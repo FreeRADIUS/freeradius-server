df01a9ba9f39a5c9b78021e21ec5d107aebdf3f8
Alan T. DeKok <aland@freeradius.org>
2011-04-10 16:28:27 +0200

    Fix arguments to pairmove2

--- src/modules/rlm_eap/types/rlm_eap_mschapv2/rlm_eap_mschapv2.c
+++ src/modules/rlm_eap/types/rlm_eap_mschapv2/rlm_eap_mschapv2.c
@@ -675,8 +675,8 @@ static int mschapv2_authenticate(void *arg, EAP_HANDLER *handler)
 		data->code = PW_EAP_MSCHAPV2_SUCCESS;
 
 	} else if (inst->send_error) {
-		pairmove2(&handler->request->reply->vps, &response,
-			  PW_MSCHAP_ERROR);
+	  pairmove2(&response, &handler->request->reply->vps,
+		    PW_MSCHAP_ERROR);
 		data->code = PW_EAP_MSCHAPV2_FAILURE;
 	} else {
 		eap_ds->request->code = PW_EAP_FAILURE;
