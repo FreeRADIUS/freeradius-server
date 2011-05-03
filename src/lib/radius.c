8ba9f00f55a992bb7fbb0a3f79281210a0ae5bab
Alan T. DeKok <aland@freeradius.org>
2011-05-03 09:23:41 +0200

    Don't decode attributes if there's no secret
    This is really a bug in the caller, but it's a reasonable
    change to make.

--- src/lib/radius.c
+++ src/lib/radius.c
@@ -2858,7 +2858,7 @@ static ssize_t data2vp_any(const RADIUS_PACKET *packet,
 	/*
 	 *	Decrypt the attribute.
 	 */
-	switch (vp->flags.encrypt) {
+	if (secret) switch (vp->flags.encrypt) {
 		/*
 		 *  User-Password
 		 */
