9f6556a91142db79a6c735087dddd06f7b89107b
Alan T. DeKok <aland@freeradius.org>
2011-10-19 14:04:47 +0200

    Clean up error message so it makes more sense

--- src/main/modcall.c
+++ src/main/modcall.c
@@ -2134,7 +2134,7 @@ static modcallable *do_compile_modsingle(modcallable *parent,
 		}
 		
 		*modname = NULL;
-		cf_log_err(ci, "Failed to load module \"%s\".", modrefname);
+		cf_log_err(ci, "Failed to find \"%s\" in the \"modules\" section.", modrefname);
 		return NULL;
 	} while (0);
 
