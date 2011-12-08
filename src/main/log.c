8ae6b94c9b87dd35192360e5acfea283d804ad8b
Alan T. DeKok <aland@freeradius.org>
2011-12-08 14:48:55 +0100

    Added "log { use_utc = yes/no }" configuration

--- src/main/log.c
+++ src/main/log.c
@@ -49,6 +49,9 @@ static const FR_NAME_NUMBER levels[] = {
 	{ NULL, 0 }
 };
 
+int log_dates_utc = 0;
+
+
 /*
  *	Log the message to the logfile. Include the severity and
  *	a time stamp.
@@ -280,7 +283,14 @@ void radlog_request(int lvl, int priority, REQUEST *request, const char *msg, ..
 		time_t timeval;
 		timeval = time(NULL);
 
-		CTIME_R(&timeval, buffer + len, sizeof(buffer) - len - 1);
+#ifdef HAVE_GMTIME_R
+		if (log_dates_utc) {
+			struct tm utc;
+			gmtime_r(&timeval, &utc);
+			asctime_r(&utc, buffer + len);
+		} else
+#endif
+			CTIME_R(&timeval, buffer + len, sizeof(buffer) - len - 1);
 		
 		s = strrchr(buffer, '\n');
 		if (s) {
@@ -288,9 +298,7 @@ void radlog_request(int lvl, int priority, REQUEST *request, const char *msg, ..
 			s[1] = '\0';
 		}
 		
-		s = fr_int2str(levels, (lvl & ~L_CONS), ": ");
-		
-		strcat(buffer, s);
+		strcat(buffer, fr_int2str(levels, (lvl & ~L_CONS), ": "));
 		len = strlen(buffer);
 	}
 	
