a7e2b67cdd4e569c713aa3b83daa3193bf14e2c4
Alan T. DeKok <aland@freeradius.org>
2012-01-24 13:17:58 +0100

    One last build rule
    CFLAGS points to -Isrc, instead of -I$(BUILDDIR)/make/include

--- src/all.mk
+++ src/all.mk
@@ -1,11 +1,14 @@
 # add this dependency BEFORE including the other submakefiles.
-all: ${BUILD_DIR}/make/include/freeradius-devel
+all: ${BUILD_DIR}/make/include/freeradius-devel src/freeradius-devel
 
 #TARGET	:= src/freeradius-devel
 
 # Ensure that the devel files have access to radpaths.h
 ${BUILD_DIR}/make/include/freeradius-devel: src/include/radpaths.h
-	[ -e $@ ] || (mkdir -p $(dir $@) && ln -s ${top_builddir}/src/include $@)
+	@[ -e $@ ] || (mkdir -p $(dir $@) && ln -s ${top_builddir}/src/include $@)
+
+src/freeradius-devel: src/include/radpaths.h
+	@[ -e $@ ] || ln -s include $@
 
 SUBMAKEFILES := include/all.mk lib/all.mk modules/all.mk main/all.mk
 
