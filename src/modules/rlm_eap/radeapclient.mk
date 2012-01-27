f6a2fb2103027e9e80ebfb644a1b778e8ab95f96
Alan T. DeKok <aland@freeradius.org>
2012-01-27 10:48:40 +0100

    A bit better fix.  Not done yet

--- src/modules/rlm_eap/radeapclient.mk
+++ src/modules/rlm_eap/radeapclient.mk
@@ -1,4 +1,4 @@
-TARGET   := radeapclient
+TARGET   := 
 SOURCES := radeapclient.c
 
 ifneq ($(OPENSSL_LIBS),)
@@ -6,6 +6,6 @@ SOURCES += ${top_srcdir}/src/main/cb.c ${top_srcdir}/src/main/tls.c
 endif
 
 TGT_PREREQS := libfreeradius-radius.a libfreeradius-eap.a
-TGT_PRLIBS  := ${OPENSSL_LIBS}
+TGT_PRLIBS  := ${OPENSSL_LIBS} ${LIBS}
 
 SRC_INCDIRS  := libeap
