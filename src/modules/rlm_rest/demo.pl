edc39ed116326cc50e74aed4ed1395f19045ebc7
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2012-02-22 10:37:17 +0100

    Fix content of demo http server

--- src/modules/rlm_rest/demo.pl
+++ src/modules/rlm_rest/demo.pl
@@ -15,14 +15,9 @@ while (my $c = $d->accept) {
 		if ($r->method eq 'POST' and $r->url->path eq "/") {
 			my $resp = HTTP::Response->new( '200', 'OK' );
 	
-			#$resp->header("Content-Type" => "application/x-www-form-urlencoded");	
-			#$resp->content("reply:User-Name=kittens&reply:Service-Type=Framed-User&reply:HP-Cos=0000&reply:HP-Bandwidth-Max-Ingress=999999999999999999999999999999999999&reply:User-Name=mittens");
+			$resp->header("Content-Type" => "application/x-www-form-urlencoded");	
+			$resp->content("control:Cleartext-Password=password&reply:Reply-Message=testing123");
 		
-			$resp->header("Content-Type" => "application/json");	
-			$resp->content('{
-				"reply|User-Name":"kittens %{User-Name}",
-			}');
-
 			$c->send_response($resp);
 		} else {
 			$c->send_error(RC_FORBIDDEN)
