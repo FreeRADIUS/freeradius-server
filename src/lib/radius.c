d1df0b76adf295d8f9ace6093aa902606f81cb77
Alan T. DeKok <aland@freeradius.org>
2012-05-07 18:33:31 +0200

    oreect Message-Authenticator calculation for CoA packets.
    Patch from Jouni Malinen.

--- src/lib/radius.c
+++ src/lib/radius.c
@@ -2772,11 +2772,7 @@ int rad_verify(RADIUS_PACKET *packet, RADIUS_PACKET *original,
 
 			case PW_CODE_ACCOUNTING_REQUEST:
 			case PW_CODE_DISCONNECT_REQUEST:
-			case PW_CODE_DISCONNECT_ACK:
-			case PW_CODE_DISCONNECT_NAK:
 			case PW_CODE_COA_REQUEST:
-			case PW_CODE_COA_ACK:
-			case PW_CODE_COA_NAK:
 			  	memset(packet->data + 4, 0, AUTH_VECTOR_LEN);
 				break;
 
@@ -2784,6 +2780,10 @@ int rad_verify(RADIUS_PACKET *packet, RADIUS_PACKET *original,
 			case PW_CODE_AUTHENTICATION_ACK:
 			case PW_CODE_AUTHENTICATION_REJECT:
 			case PW_CODE_ACCESS_CHALLENGE:
+			case PW_CODE_DISCONNECT_ACK:
+			case PW_CODE_DISCONNECT_NAK:
+			case PW_CODE_COA_ACK:
+			case PW_CODE_COA_NAK:
 				if (!original) {
 					fr_strerror_printf("ERROR: Cannot validate Message-Authenticator in response packet without a request packet");
 					return -1;
