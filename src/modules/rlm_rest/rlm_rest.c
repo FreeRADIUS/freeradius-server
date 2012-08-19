4b6cc0e914cca0196466ed2ede7c371235fe0257
Alan Buxey <alan.buxey@gmail.com>
2012-08-19 20:41:44 +0100

    updated old date in copyright
    updated the copyright year but also fixed a few typos

--- src/modules/rlm_rest/rlm_rest.c
+++ src/modules/rlm_rest/rlm_rest.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  *
- * Copyright 2011  Arran Cudbard-Bell <arran.cudbardb@freeradius.org>>
+ * Copyright 2012  Arran Cudbard-Bell <arran.cudbardb@freeradius.org>>
  */
 
 #include <freeradius-devel/ident.h>
