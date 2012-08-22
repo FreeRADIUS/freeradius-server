2a21eeb2de0b3b44178e1e426e0f8ad8a13115f8
Alan T. DeKok <aland@freeradius.org>
2012-08-22 09:58:09 +0200

    Nuke the '[' on expansion

--- src/main/xlat.c
+++ src/main/xlat.c
@@ -227,6 +227,7 @@ static size_t xlat_packet(void *instance, REQUEST *request,
 		 */
 		p = strchr(p, '[');
 		if (p) {
+			*p = '\0';
 			do_array = TRUE;
 			if (p[1] == '#') {
 				do_count = TRUE;
@@ -241,7 +242,6 @@ static size_t xlat_packet(void *instance, REQUEST *request,
 					return 0;
 				}
 			}
-			*p = '\0';
 		}
 
 		/*
