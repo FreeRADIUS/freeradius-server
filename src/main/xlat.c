5a4b1da66c333a59aaeb2fae8a1b025520b5f385
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2012-09-20 13:55:08 +0100

    Move string manipulation functions from xlat.c to rlm_expr

--- src/main/xlat.c
+++ src/main/xlat.c
@@ -26,7 +26,6 @@
 RCSID("$Id$")
 
 #include	<freeradius-devel/radiusd.h>
-#include	<freeradius-devel/md5.h>
 #include	<freeradius-devel/rad_assert.h>
 
 #include	<ctype.h>
@@ -591,99 +590,6 @@ static size_t xlat_debug(UNUSED void *instance, REQUEST *request,
 	return strlen(out);
 }
 
-
-/*
- *	Calculate the MD5 hash of a string.
- */
-static size_t xlat_md5(UNUSED void *instance, REQUEST *request,
-		       char *fmt, char *out, size_t outlen,
-		       UNUSED RADIUS_ESCAPE_STRING func)
-{
-	int i;
-	uint8_t digest[16];
-	FR_MD5_CTX ctx;
-	char buffer[1024];
-
-	if (!radius_xlat(buffer, sizeof(buffer), fmt, request, func)) {
-		*out = '\0';
-		return 0;
-	}
-
-	fr_MD5Init(&ctx);
-	fr_MD5Update(&ctx, (void *) buffer, strlen(buffer));
-	fr_MD5Final(digest, &ctx);
-
-	if (outlen < 33) {
-		snprintf(out, outlen, "md5_overflow");
-		return strlen(out);
-	}
-
-	for (i = 0; i < 16; i++) {
-		snprintf(out + i * 2, 3, "%02x", digest[i]);
-	}
-
-	return strlen(out);
-}
-
-
-/*
- *	Convert a string to lowercase
- */
-static size_t xlat_lc(UNUSED void *instance, REQUEST *request,
-		       char *fmt, char *out, size_t outlen,
-		       UNUSED RADIUS_ESCAPE_STRING func)
-{
-	char *p, *q;
-	char buffer[1024];
-
-	if (outlen <= 1) return 0;
-
-	if (!radius_xlat(buffer, sizeof(buffer), fmt, request, func)) {
-		*out = '\0';
-		return 0;
-	}
-
-	for (p = buffer, q = out; *p != '\0'; p++, outlen--) {
-		if (outlen <= 1) break;
-
-		*(q++) = tolower((int) *p);
-	}
-
-	*q = '\0';
-
-	return strlen(out);
-}
-
-
-/*
- *	Convert a string to uppercase
- */
-static size_t xlat_uc(UNUSED void *instance, REQUEST *request,
-		       char *fmt, char *out, size_t outlen,
-		       UNUSED RADIUS_ESCAPE_STRING func)
-{
-	char *p, *q;
-	char buffer[1024];
-
-	if (outlen <= 1) return 0;
-
-	if (!radius_xlat(buffer, sizeof(buffer), fmt, request, func)) {
-		*out = '\0';
-		return 0;
-	}
-
-	for (p = buffer, q = out; *p != '\0'; p++, outlen--) {
-		if (outlen <= 1) break;
-
-		*(q++) = toupper((int) *p);
-	}
-
-	*q = '\0';
-
-	return strlen(out);
-}
-
-
 /*
  *	Compare two xlat_t structs, based ONLY on the module name.
  */
@@ -789,21 +695,6 @@ int xlat_register(const char *module, RAD_XLAT_FUNC func, void *instance)
 
 		xlat_register("debug", xlat_debug, &xlat_inst[0]);
 		c = xlat_find("debug");
-		rad_assert(c != NULL);
-		c->internal = TRUE;
-
-		xlat_register("md5", xlat_md5, &xlat_inst[0]);
-		c = xlat_find("md5");
-		rad_assert(c != NULL);
-		c->internal = TRUE;
-
-		xlat_register("tolower", xlat_lc, &xlat_inst[0]);
-		c = xlat_find("tolower");
-		rad_assert(c != NULL);
-		c->internal = TRUE;
-
-		xlat_register("toupper", xlat_uc, &xlat_inst[0]);
-		c = xlat_find("toupper");
 		rad_assert(c != NULL);
 		c->internal = TRUE;
 	}
