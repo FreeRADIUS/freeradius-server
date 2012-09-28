d1f6666fcb7d9e88aac7a5e75a768b517126470d
Alan T. DeKok <aland@freeradius.org>
2012-09-28 13:13:31 +0200

    Include all sub-modules, if they're built

--- src/modules/rlm_sql/all.mk
+++ src/modules/rlm_sql/all.mk
@@ -2,4 +2,4 @@ TARGET		= rlm_sql.a
 SOURCES		= rlm_sql.c sql.c
 # this uses the RLM_CFLAGS and RLM_LIBS and SOURCES defs to make TARGET.
 
-SUBMAKEFILES := drivers/rlm_sql_null/all.mk
+SUBMAKEFILES := $(wildcard ${top_srcdir}/src/modules/rlm_sql/drivers/rlm_sql_*/all.mk)
