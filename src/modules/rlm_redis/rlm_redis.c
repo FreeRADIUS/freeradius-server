043d5de94310231860453cbde08b599497865901
Phil Mayers <p.mayers@imperial.ac.uk>
2012-11-14 17:30:39 +0000

    don't finish the query, our caller already does that

--- src/modules/rlm_redis/rlm_redis.c
+++ src/modules/rlm_redis/rlm_redis.c
@@ -413,10 +413,6 @@ int rlm_redis_query(REDISSOCK *dissocket, REDIS_INST *inst, char *query)
 	if (dissocket->reply->type == REDIS_REPLY_ERROR) {
 		radlog(L_ERR, "rlm_redis (%s): query failed, %s",
 		       inst->xlat_name, query);
-
-		/* Free the reply just in case */
-		rlm_redis_finish_query(dissocket);
-
 		return -1;
 	}
 
