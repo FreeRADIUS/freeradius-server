7de86a36aca1e33447ee65e0b36c9d1526949517
Alan T. DeKok <aland@freeradius.org>
2012-11-20 08:44:11 -0500

    Link rlm_eap against libfreeradius-eap

--- src/modules/rlm_eap/rlm_eap.mk
+++ src/modules/rlm_eap/rlm_eap.mk
@@ -4,7 +4,4 @@ SOURCES := rlm_eap.c eap.c mem.c
 
 SRC_INCDIRS := . libeap
 
-# FIXME: This target is "phony", which means that every "make"
-# re-builds rlm_eap.  We need to re-write this to be the name
-# of the ${BUILD_DIR}/.../filename!
-#TGT_PREREQS := libfreeradius-eap.a
+TGT_PREREQS := libfreeradius-eap.a
