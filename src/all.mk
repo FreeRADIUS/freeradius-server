45e0ff7a8d87a673c546b5be2287b074626548f5
Alan T. DeKok <aland@freeradius.org>
2012-11-21 11:06:29 -0500

    Clean up the rules a bit

--- src/all.mk
+++ src/all.mk
@@ -1,13 +1,12 @@
 # add this dependency BEFORE including the other submakefiles.
 all: ${BUILD_DIR}/make/include/freeradius-devel src/freeradius-devel
 
-#TARGET	:= src/freeradius-devel
-
-# Ensure that the devel files have access to radpaths.h
-${BUILD_DIR}/make/include/freeradius-devel: src/include/radpaths.h
+.PHONY: ${BUILD_DIR}/make/include/freeradius-devel
+${BUILD_DIR}/make/include/freeradius-devel:
 	@[ -e $@ ] || (mkdir -p $(dir $@) && ln -s ${top_builddir}/src/include $@)
 
-src/freeradius-devel: src/include/radpaths.h
+.PHONY: src/freeradius-devel
+src/freeradius-devel:
 	@[ -e $@ ] || ln -s include $@
 
 SUBMAKEFILES := include/all.mk lib/all.mk modules/all.mk main/all.mk
