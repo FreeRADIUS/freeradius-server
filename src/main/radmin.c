5a9d5eae6508ade01fcf7dea575e73733c2717db
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2012-12-01 12:34:28 +0000

    radius_xlat should return size_t

--- src/main/radmin.c
+++ src/main/radmin.c
@@ -94,9 +94,9 @@ int debug_flag = 0;
 struct main_config_t mainconfig;
 char *request_log_file = NULL;
 char *debug_log_file = NULL;
-int radius_xlat(UNUSED char *out, UNUSED int outlen, UNUSED const char *fmt,
-		UNUSED REQUEST *request,
-		UNUSED RADIUS_ESCAPE_STRING func, UNUSED void *arg)
+size_t radius_xlat(UNUSED char *out, UNUSED int outlen, UNUSED const char *fmt,
+		   UNUSED REQUEST *request,
+		   UNUSED RADIUS_ESCAPE_STRING func, UNUSED void *arg)
 {
 	return -1;
 }
