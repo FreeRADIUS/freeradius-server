09018dee1c56fe9584b2434662652b570d01b6ba
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2012-12-19 18:50:57 +0000

    Set Post-Proxy-Type Reject if the request was rejected by the proxy server

--- src/main/process.c
+++ src/main/process.c
@@ -1765,6 +1765,24 @@ static int process_proxy_reply(REQUEST *request)
 	 *	BEFORE playing games with the attributes.
 	 */
 	vp = pairfind(request->config_items, PW_POST_PROXY_TYPE, 0);
+	
+	/*
+	 *	If we have a proxy_reply, and it was a reject, setup
+	 *	post-proxy-type Reject
+	 */
+	if (!vp && request->proxy_reply &&
+	    request->proxy_reply->code == PW_AUTHENTICATION_REJECT) {
+	    	DICT_VALUE	*dval;
+	
+		dval = dict_valbyname(PW_POST_PROXY_TYPE, 0, "Reject");
+		if (!dval) return 0;
+			
+		vp = radius_paircreate(request, &request->config_items,
+				       PW_POST_PROXY_TYPE, 0, PW_TYPE_INTEGER);
+
+		vp->vp_integer = dval->value;
+	}
+	
 	if (vp) {
 		RDEBUG2("  Found Post-Proxy-Type %s", vp->vp_strvalue);
 		post_proxy_type = vp->vp_integer;
