e847dfc57c0b85f52555efbfd33cf48ce3f4f395
Alan T. DeKok <aland@freeradius.org>
2013-02-16 21:24:34 -0500

    Check size of buffer for raw attributes

--- src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
+++ src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
@@ -234,6 +234,7 @@ static VALUE_PAIR *diameter2vp(REQUEST *request, SSL *ssl,
 		raw:
 				vp = paircreate_raw(vp->attribute,
 						    PW_TYPE_OCTETS, vp);
+				if (size >= 253) size = 253;
 				vp->length = size;
 				memcpy(vp->vp_octets, data, vp->length);
 				break;
