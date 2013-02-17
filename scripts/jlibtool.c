91f76e5c5f58a93c8d3f9b0894b978a114cb246e
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-02-16 22:23:14 -0500

    Fix for coverity (fix load_install_path so it doesn't leak memory if passed a non absolute path)

--- scripts/jlibtool.c
+++ scripts/jlibtool.c
@@ -1131,21 +1131,26 @@ static char * load_install_path(const char *arg)
     FILE *f;
     char *path;
 
-    path = malloc(PATH_MAX);
-
     f = fopen(arg,"r");
     if (f == NULL) {
         return NULL;
     }
+    
+    path = malloc(PATH_MAX);
+
     fgets(path, PATH_MAX, f);
     fclose(f);
+    
     if (path[strlen(path)-1] == '\n') {
         path[strlen(path)-1] = '\0';
     }
+    
     /* Check that we have an absolute path.
      * Otherwise the file could be a GNU libtool file.
      */
     if (path[0] != '/') {
+    	free(path);
+    	
         return NULL;
     }
     return path;
