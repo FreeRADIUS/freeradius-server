944677dcf9b9086f5e70457c21afafb3682c865a
Alan T. DeKok <aland@freeradius.org>
2013-02-20 19:48:29 -0500

    mark unused

--- src/main/dhcpd.c
+++ src/main/dhcpd.c
@@ -538,7 +538,7 @@ static int dhcp_socket_send(rad_listen_t *listener, REQUEST *request)
 }
 
 
-static int dhcp_socket_encode(UNUSED rad_listen_t *listener, REQUEST *request)
+static int dhcp_socket_encode(UNUSED rad_listen_t *listener, UNUSED REQUEST *request)
 {
 	DEBUG2("NO ENCODE!");
 	return 0;
