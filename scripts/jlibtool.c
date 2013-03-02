1b5fa9e60dbd502f870b2ac595829c9df0c98395
Alan T. DeKok <aland@freeradius.org>
2013-03-02 10:37:58 -0500

    Remove dead code

--- scripts/jlibtool.c
+++ scripts/jlibtool.c
@@ -1076,37 +1076,17 @@ static char *gen_install_name(const char *name, enum lib_type genlib)
 static const char *check_object_exists(command_t *cmd, const char *arg, int arglen)
 {
 	char *newarg, *ext;
-	int pass, rv;
+	struct stat sb;
 
 	newarg = (char *)lt_malloc(arglen + 10);
 	memcpy(newarg, arg, arglen);
 	newarg[arglen] = 0;
 	ext = newarg + arglen;
 
-	pass = 0;
-
-	do {
-		struct stat sb;
-
-		switch (pass) {
-		case 0:
-			strcpy(ext, OBJECT_EXT);
-			break;
-/*
-		case 1:
-			strcpy(ext, NO_PIC_EXT);
-			break;
-*/
-		default:
-			break;
-		}
-
-		DEBUG("Checking (obj): %s\n", newarg);
-		rv = stat(newarg, &sb);
-	}
-	while (rv != 0 && ++pass < 1);
-
-	if (rv == 0) {
+	strcpy(ext, OBJECT_EXT);
+	
+	DEBUG("Checking (obj): %s\n", newarg);
+	if (stat(newarg, &sb) == 0) {
 		return newarg;
 	}
 	
