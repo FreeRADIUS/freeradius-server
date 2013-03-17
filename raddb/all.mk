6f71ef8c7b862c9f6593ad6a7dfe4e50c18746d1
Alan T. DeKok <aland@freeradius.org>
2013-03-17 10:43:34 -0400

    Don't enable cuisql by default.

--- raddb/all.mk
+++ raddb/all.mk
@@ -10,7 +10,7 @@ DEFAULT_SITES := default inner-tunnel
 LOCAL_SITES   := $(addprefix raddb/sites-enabled/,$(DEFAULT_SITES))
 
 DEFAULT_MODULES := always attr_filter attr_rewrite cache_eap chap checkval \
-		cui detail detail.log digest dhcp dynamic_clients eap \
+		detail detail.log digest dhcp dynamic_clients eap \
 		echo exec expiration expr files linelog logintime \
 		mschap ntlm_auth pap passwd preprocess radutmp realm \
 		replicate soh sradutmp unix utf8 wimax
