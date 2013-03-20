a431e909381d9dc89265e124794e599a55a86d4c
Alan T. DeKok <aland@freeradius.org>
2013-03-20 09:05:08 -0400

    Fix typo

--- src/modules/rlm_mschap/all.mk
+++ src/modules/rlm_mschap/all.mk
@@ -1,5 +1,5 @@
 SUBMAKEFILES := rlm_mschap.mk smbencrypt.mk
 
 src/modules/rlm_mschap/rlm_mschap.mk: src/modules/rlm_mschap/rlm_mschap.mk.in src/modules/rlm_mschap/configure
-	@ECHO CONFIGURE $(dir $<)
+	@echo CONFIGURE $(dir $<)
 	@cd $(dir $<) && ./configure $(CONFIGURE_ARGS)
