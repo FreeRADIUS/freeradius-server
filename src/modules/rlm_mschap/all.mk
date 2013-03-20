483266981aaf4cd88403ddef8068ebe6b445bce9
Alan T. DeKok <aland@freeradius.org>
2013-03-19 23:00:02 -0400

    Hard-code configure rule

--- src/modules/rlm_mschap/all.mk
+++ src/modules/rlm_mschap/all.mk
@@ -1,1 +1,5 @@
 SUBMAKEFILES := rlm_mschap.mk smbencrypt.mk
+
+src/modules/rlm_mschap/rlm_mschap.mk: src/modules/rlm_mschap/rlm_mschap.mk.in src/modules/rlm_mschap/configure
+	@ECHO CONFIGURE $(dir $<)
+	@cd $(dir $<) && ./configure $(CONFIGURE_ARGS)
