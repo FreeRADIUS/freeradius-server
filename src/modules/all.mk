5f653b304f631b7dc5eba753e0fcdeebd2c8d45a
Alan T. DeKok <aland@freeradius.org>
2013-03-20 08:54:59 -0700

    Ignore modules which have not yet been configured

--- src/modules/all.mk
+++ src/modules/all.mk
@@ -5,8 +5,14 @@
 CFLAGS += -DIS_MODULE=1
 
 #
-#  Load in all of the module makefiles, including ones which
-#  have not yet been configured.  We do the "sort" to remove duplicates.
+#  If we haven't run configure, ignore the modules which require it.
+#  Otherwise, load in all of the module makefiles, including ones
+#  which have not yet been configured.  We do the "sort" to remove
+#  duplicates.
 #
+ifeq "$(CONFIGURE_ARGS)" ""
+SUBMAKEFILES := $(wildcard ${top_srcdir}/src/modules/rlm_*/all.mk)
+else
 SUBMAKEFILES := $(sort $(wildcard ${top_srcdir}/src/modules/rlm_*/all.mk) \
 		$(patsubst %.in,%,$(wildcard ${top_srcdir}/src/modules/rlm_*/all.mk.in)))
+endif
