ef812d6c992536eb91c433f5b2646ff3028c99ee
Alan T. DeKok <aland@freeradius.org>
2013-03-20 08:48:45 -0400

    Ensure we link the header directory before building anything
    And the top-level IMACRO shouldn't really be poking the src
    tree...

--- src/all.mk
+++ src/all.mk
@@ -9,5 +9,7 @@ ${BUILD_DIR}/make/include/freeradius-devel:
 src/freeradius-devel:
 	@[ -e $@ ] || ln -s include $@
 
-SUBMAKEFILES := include/all.mk lib/all.mk modules/all.mk main/all.mk
+src/%c src/%h src/%mk: | src/freeradius-devel \
+			${BUILD_DIR}/make/include/freeradius-devel
 
+SUBMAKEFILES := include/all.mk lib/all.mk modules/all.mk main/all.mk
