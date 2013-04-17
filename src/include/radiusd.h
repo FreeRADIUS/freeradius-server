a0060bc9a4cfd1cb748210e0cd7bf8b369aefff9
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-04-17 17:58:57 -0400

    dictionary_dir should be const

--- src/include/radiusd.h
+++ src/include/radiusd.h
@@ -462,7 +462,7 @@ typedef struct main_config_t {
 	int		kill_unresponsive_children;
 #endif
 	char		*log_file;
-	char		*dictionary_dir;
+	const char	*dictionary_dir;
 	char		*checkrad;
 	const char      *pid_file;
 	rad_listen_t	*listen;
