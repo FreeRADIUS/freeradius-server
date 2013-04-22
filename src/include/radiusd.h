8561e2b9001c2d42a404a600e5059d1907690582
Alan T. DeKok <aland@freeradius.org>
2013-04-22 11:00:45 -0400

    Moved tmpl / map functions to their own C / H file.
    We haven't updated the parser, so it may not build.
    That will be fixed in the next commit

--- src/include/radiusd.h
+++ src/include/radiusd.h
@@ -32,6 +32,7 @@ RCSIDH(radiusd_h, "$Id$")
 #include <freeradius-devel/conffile.h>
 #include <freeradius-devel/event.h>
 #include <freeradius-devel/connection.h>
+#include <freeradius-devel/map.h>
 
 typedef struct request REQUEST;
 
@@ -297,44 +298,6 @@ struct request {
 #define REQUEST_DONE		(6)
 
 typedef struct radclient_list RADCLIENT_LIST;
-
-typedef enum pair_lists {
-	PAIR_LIST_UNKNOWN = 0,
-	PAIR_LIST_REQUEST,
-	PAIR_LIST_REPLY,
-	PAIR_LIST_CONTROL,
-#ifdef WITH_PROXY
-	PAIR_LIST_PROXY_REQUEST,
-	PAIR_LIST_PROXY_REPLY,
-#endif
-#ifdef WITH_COA
-	PAIR_LIST_COA,
-	PAIR_LIST_COA_REPLY,
-	PAIR_LIST_DM,
-	PAIR_LIST_DM_REPLY
-#endif
-} pair_lists_t;
-
-extern const FR_NAME_NUMBER pair_lists[];
-
-typedef enum requests {
-	REQUEST_UNKNOWN = 0,
-	REQUEST_OUTER,
-	REQUEST_CURRENT,
-	REQUEST_PARENT	/* For future use */
-} request_refs_t;
-
-extern const FR_NAME_NUMBER request_refs[];
-
-typedef struct pair_list {
-	char const		*name;
-	VALUE_PAIR		*check;
-	VALUE_PAIR		*reply;
-	int			lineno;
-	int			order;
-	struct pair_list	*next;
-	struct pair_list	*lastdefault;
-} PAIR_LIST;
 
 typedef int (*rad_listen_recv_t)(rad_listen_t *);
 typedef int (*rad_listen_send_t)(rad_listen_t *, REQUEST *);
@@ -822,95 +785,9 @@ VALUE_PAIR **radius_list(REQUEST *request, pair_lists_t list);
 pair_lists_t radius_list_name(char const **name, pair_lists_t unknown);
 int radius_request(REQUEST **request, request_refs_t name);
 request_refs_t radius_request_name(char const **name, request_refs_t unknown);
-
-
-typedef enum vpt_type {
-	VPT_TYPE_UNKNOWN = 0,
-  	VPT_TYPE_LITERAL,		//!< Is a literal string.
-    	VPT_TYPE_XLAT,			//!< Needs to be expanded.
-	VPT_TYPE_ATTR,			//!< Is an attribute.
-	VPT_TYPE_LIST,			//!< Is a list.
-	VPT_TYPE_REGEX,			//!< Is a regex.
-	VPT_TYPE_EXEC			//!< Needs to be executed.
-} vpt_type_t;
-
-extern const FR_NAME_NUMBER vpt_types[];
-
-/** A pre-parsed template attribute
- *
- *  Value pair template, used when processing various mappings sections
- *  to create a real valuepair later.
- *
- * @see value_pair_map_t
- */
-typedef struct value_pair_tmpl_t {
-	char const		*name;   //!< Original attribute ref string, or
-					 //!< where this refers to a none FR
-					 //!< attribute, just the string id for
-					 //!< the attribute.
-
-	request_refs_t		request; //!< Request to search or insert in.
-	pair_lists_t		list;	 //!< List to search or insert in.
-				
-	const DICT_ATTR		*da;	 //!< Resolved dictionary attribute.
-	vpt_type_t		type;	 //!< What type of value tmpl refers to.
-} value_pair_tmpl_t;
-
-/** Value pair map
- *
- * Value pair maps contain a pair of templates, that describe a src attribute
- * or value, and a destination attribute.
- *
- * Neither src or dst need to be an FR attribute, and their type can be inferred
- * from whether map->da is NULL (not FR).
- *
- * @see value_pair_tmpl_t
- */
-typedef struct value_pair_map {
-	value_pair_tmpl_t	*dst;	//!< Typically describes the attribute
-					//!< to add or modify.
-	value_pair_tmpl_t	*src;   //!< Typically describes a value or a
-					//!< src attribute to copy.
-	
-	FR_TOKEN		op; 	//!< The operator that controls
-					//!< insertion of the dst attribute.
-	
-	CONF_ITEM		*ci;	//!< Config item that the map was
-					//!< created from. Mainly used for
-					//!< logging validation errors.
-	
-	struct value_pair_map	*next;	//!< The next valuepair map.
-} value_pair_map_t;
-
 typedef VALUE_PAIR *(*radius_tmpl_getvalue_t)(REQUEST *request,
 					      value_pair_map_t const *map,
 					      void *ctx);
-void radius_tmplfree(value_pair_tmpl_t **tmpl);
-int radius_parse_attr(char const *name, value_pair_tmpl_t *vpt,
-		      request_refs_t request_def,
-		      pair_lists_t list_def);
-value_pair_tmpl_t *radius_attr2tmpl(TALLOC_CTX *ctx, char const *name,
-				    request_refs_t request_def,
-				    pair_lists_t list_def);
-				
-value_pair_tmpl_t *radius_str2tmpl(TALLOC_CTX *ctx, char const *name, FR_TOKEN type);
-size_t radius_tmpl2str(char *buffer, size_t bufsize, value_pair_tmpl_t const *vpt);
-int radius_attrmap(CONF_SECTION *cs, value_pair_map_t **head,
-		   pair_lists_t dst_list_def, pair_lists_t src_list_def,
-		   unsigned int max);
-value_pair_map_t *radius_str2map(TALLOC_CTX *ctx, const char *lhs, FR_TOKEN lhs_type,
-				 FR_TOKEN op, const char *rhs, FR_TOKEN rhs_type,
-				 request_refs_t dst_request_def,
-				 pair_lists_t dst_list_def,
-				 request_refs_t src_request_def,
-				 pair_lists_t src_list_def);
-size_t radius_map2str(char *buffer, size_t bufsize, value_pair_map_t const *map);
-value_pair_map_t *radius_cp2map(TALLOC_CTX *ctx, CONF_PAIR *cp,
-				request_refs_t dst_request_def,
-				pair_lists_t dst_list_def,
-				request_refs_t src_request_def,
-				pair_lists_t src_list_def);
-				
 VALUE_PAIR *radius_map2vp(REQUEST *request, value_pair_map_t const *map,
 			  void *ctx);
 int radius_map2request(REQUEST *request, value_pair_map_t const *map,
@@ -918,6 +795,7 @@ int radius_map2request(REQUEST *request, value_pair_map_t const *map,
 
 int radius_str2vp(REQUEST *request, char const *str, request_refs_t request_def, pair_lists_t list_def);
 int radius_get_vp(REQUEST *request, char const *name, VALUE_PAIR **vp_p);
+
 
 #ifdef WITH_TLS
 /*
--- src/main/radattr.mk
+++ src/main/radattr.mk
@@ -1,5 +1,5 @@
 TARGET		:= radattr
-SOURCES		:= radattr.c parser.c xlat.c util.c
+SOURCES		:= radattr.c parser.c xlat.c util.c map.c
 
 TGT_PREREQS	:= libfreeradius-radius.a
 TGT_LDLIBS	:= $(LIBS)
