344829c1b34865692c0101ed13046e63f3df5bfd
Alan T. DeKok <aland@freeradius.org>
2013-04-23 12:13:00 -0400

    Added map.c
    We really need a libfreeradius-server thingy...

--- src/main/radconf2xml.mk
+++ src/main/radconf2xml.mk
@@ -1,5 +1,5 @@
 TARGET		:= radconf2xml
-SOURCES		:= radconf2xml.c util.c log.c conffile.c parser.c
+SOURCES		:= radconf2xml.c util.c log.c conffile.c parser.c map.c
 
 TGT_PREREQS	:= libfreeradius-radius.a
 TGT_LDLIBS	:= $(LIBS)
--- src/main/radwho.mk
+++ src/main/radwho.mk
@@ -1,5 +1,5 @@
 TARGET		:= radwho
-SOURCES		:= radwho.c conffile.c log.c util.c parser.c
+SOURCES		:= radwho.c conffile.c log.c util.c parser.c map.c
 
 TGT_PREREQS	:= libfreeradius-radius.a
 TGT_LDLIBS	:= $(LIBS)
