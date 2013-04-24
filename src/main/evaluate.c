FreeRADIUS/freeradius-server

commit 56689d8b24a049cf0f8014d023d1a1abdcbdc029

Fix typo

src/main/evaluate.c
@@ -115,9 +115,7 @@ static char *radius_expand_tmpl(REQUEST *request, value_pair_tmpl_t const *vpt)
 	case VPT_TYPE_ATTR:
 		EVAL_DEBUG("TMPL ATTR");
 		vp = radius_vpt_get_vp(request, vpt);
-		if (!vpt) return NULL;
-
-		buffer = vp_aprint(request, vp);
+		if (!vp) return NULL;
 		break;
 
 	default:

commit a3eea9701de6a5b8e820463696a785e26641b805

Fix typo introduced by 56689d8b24a049cf0f8014d023d1a1abdcbdc029

src/main/evaluate.c
@@ -116,6 +116,7 @@ static char *radius_expand_tmpl(REQUEST *request, value_pair_tmpl_t const *vpt)
 		EVAL_DEBUG("TMPL ATTR");
 		vp = radius_vpt_get_vp(request, vpt);
 		if (!vp) return NULL;
+		buffer = vp_aprint(request, vp);
 		break;
 
 	default: