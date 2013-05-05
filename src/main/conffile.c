2e41082f47862dbb3caddbdbdeaab1a8c51835bc
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-05-05 18:13:26 -0400

    GCC fix

--- src/main/conffile.c
+++ src/main/conffile.c
@@ -732,7 +732,7 @@ static char const *cf_expand_variables(char const *cf, int *lineno,
 				ptr = end + 1;
 			} else if (ci->type == CONF_ITEM_SECTION) {
 				cf_item_add(outercs, ci);
-				talloc_reference(outercs, ci);
+				outercs = talloc_reference(outercs, ci);
 			} else {
 				ERROR("%s[%d]: Reference \"%s\" type is invalid", cf, *lineno, input);
 				return NULL;
