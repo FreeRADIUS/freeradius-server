6d78b5fbb30be63f2adc3da8e106036d59bfda56
Alan T. DeKok <aland@freeradius.org>
2013-05-12 17:11:53 -0400

    Abstract cidr comparison into common function
    And use it for v4 / v6 CIDRs!

--- src/lib/valuepair.c
+++ src/lib/valuepair.c
@@ -2067,6 +2067,117 @@ VALUE_PAIR *readvp2(TALLOC_CTX *ctx, FILE *fp, int *pfiledone, char const *errpr
 }
 
 /*
+ *	We leverage the fact that IPv4 and IPv6 prefixes both
+ *	have the same format:
+ *
+ *	reserved, prefix-len, data...
+ */
+static int paircmp_cidr(FR_TOKEN op, int bytes, uint8_t const *one, uint8_t const *two)
+{
+	int i, common;
+	uint32_t mask;
+
+	/*
+	 *	Handle the case of netmasks being identical.
+	 */
+	if (one[1] == two[1]) {
+		int compare;
+
+		compare = memcmp(one + 2, two + 2, bytes);
+
+		/*
+		 *	If they're identical return true for
+		 *	identical.
+		 */
+		if ((compare == 0) &&
+		    ((op == T_OP_CMP_EQ) ||
+		     (op == T_OP_LE) ||
+		     (op == T_OP_GE))) {
+			return true;
+		}
+
+		/*
+		 *	Everything else returns false.
+		 *
+		 *	10/8 == 24/8  --> false
+		 *	10/8 <= 24/8  --> false
+		 *	10/8 >= 24/8  --> false
+		 */
+		return false;
+	}
+
+	/*
+	 *	Netmasks are different.  That limits the
+	 *	possible results, based on the operator.
+	 */
+	switch (op) {
+	case T_OP_CMP_EQ:
+		return false;
+
+	case T_OP_NE:
+		return true;
+
+	case T_OP_LE:
+	case T_OP_LT:	/* 192/8 < 192.168/16 --> false */
+		if (one[1] < two[1]) {
+			return false;
+		}
+		break;
+
+	case T_OP_GE:
+	case T_OP_GT:	/* 192/16 > 192.168/8 --> false */
+		if (one[1] > two[1]) {
+			return false;
+		}
+		break;
+
+	default:
+		return false;
+	}
+
+	if (one[1] < two[1]) {
+		common = one[1];
+	} else {
+		common = two[1];
+	}
+
+	/*
+	 *	Do the check byte by byte.  If the bytes are
+	 *	identical, it MAY be a match.  If they're different,
+	 *	it is NOT a match.
+	 */
+	i = 2;
+	while (i < (2 + bytes)) {
+		/*
+		 *	All leading bytes are identical.
+		 */
+		if (common == 0) return true;
+
+		/*
+		 *	Doing bitmasks takes more work.
+		 */
+		if (common < 8) break;
+
+		if (one[i] != two[i]) return false;
+
+		common -= 8;
+		i++;
+		continue;
+	}
+
+	mask = 1;
+	mask <<= (8 - common);
+	mask--;
+	mask = ~mask;
+	
+	if ((one[i] & mask) == ((two[i] & mask))) {
+		return true;
+	}
+	
+	return false;
+}
+
+/*
  *	Compare two pairs, using the operator from "one".
  *
  *	i.e. given two attributes, it does:
@@ -2227,117 +2338,15 @@ int paircmp_op(VALUE_PAIR const *one, FR_TOKEN op, VALUE_PAIR const *two)
 				 sizeof(one->vp_ipv6addr));
 		break;
 
-		/*
-		 *	FIXME: do a smarter comparison...
-		 */
 	case PW_TYPE_IPV6PREFIX:
-		compare = memcmp(&one->vp_ipv6prefix, &two->vp_ipv6prefix,
-				 sizeof(one->vp_ipv6prefix));
-		break;
+		return paircmp_cidr(op, 16,
+				    (uint8_t const *) &one->vp_ipv6prefix,
+				    (uint8_t const *) &two->vp_ipv6prefix);
 
 	case PW_TYPE_IPV4PREFIX:
-		/*
-		 *	Handle the case of netmasks being identical.
-		 */
-		if (one->vp_ipv4prefix[1] == two->vp_ipv4prefix[1]) {
-			compare = memcmp(one->vp_ipv4prefix + 2,
-					 two->vp_ipv4prefix + 2,
-					 4);
-			/*
-			 *	If they're identical return true for
-			 *	identical.
-			 */
-			if ((compare == 0) &&
-			    ((op == T_OP_CMP_EQ) ||
-			     (op == T_OP_LE) ||
-			     (op == T_OP_GE))) {
-				return true;
-			}
-
-			/*
-			 *	Everything else returns false.
-			 *
-			 *	10/8 == 24/8  --> false
-			 *	10/8 <= 24/8  --> false
-			 *	10/8 >= 24/8  --> false
-			 */
-			return false;
-		}
-
-		/*
-		 *	Netmasks are different.  That limits the
-		 *	possible results, based on the operator.
-		 */
-		switch (op) {
-		case T_OP_CMP_EQ:
-			return false;
-
-		case T_OP_NE:
-			return true;
-
-		case T_OP_LE:
-		case T_OP_LT:	/* 192/8 < 192.168/16 --> false */
-			if (one->vp_ipv4prefix[1] < two->vp_ipv4prefix[1]) {
-				return false;
-			}
-			break;
-
-		case T_OP_GE:
-		case T_OP_GT:	/* 192/16 > 192.168/8 --> false */
-			if (one->vp_ipv4prefix[1] > two->vp_ipv4prefix[1]) {
-				return false;
-			}
-			break;
-
-		default:
-			return false;
-		}
-
-		/*
-		 *	Now check the prefixes.
-		 */
-		{
-			uint32_t ip1, ip2;
-			uint32_t mask;
-			int len;
-
-			memcpy(&ip1, one->vp_ipv4prefix + 2, 4);
-			memcpy(&ip2, two->vp_ipv4prefix + 2, 4);
-
-			ip1 = ntohl(ip1);
-			ip2 = ntohl(ip2);
-
-			if (one->vp_ipv4prefix[1] < two->vp_ipv4prefix[1]) {
-				len = one->vp_ipv4prefix[1];
-			} else {
-				len = two->vp_ipv4prefix[1];
-			}
-
-			/*
-			 *	Mask off the LOWER bits, because we
-			 *	don't care about them.
-			 */
-			mask = 1;
-			mask <<= (32 - len);
-			mask--;
-			mask = ~mask;
-
-			ip1 &= mask; /* one of these is unnecessary, but oh well */
-			ip2 &= mask;
-
-			/*
-			 *	If the prefixes are identical, the one
-			 *	is within two, or two is within one.
-			 *	We don't care which one it is, as that
-			 *	was checked above with the op /
-			 *	netmask length checks.
-			 */
-			if (ip1 == ip2) return true;
-
-			return false;
-		}
-
-		break;
+		return paircmp_cidr(op, 4,
+				    (uint8_t const *) &one->vp_ipv4prefix,
+				    (uint8_t const *) &two->vp_ipv4prefix);
 
 	case PW_TYPE_IFID:
 		compare = memcmp(&one->vp_ifid, &two->vp_ifid,
