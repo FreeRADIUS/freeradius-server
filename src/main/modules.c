11d1a7e7fce162d55e01f2974f1b03cf8ac06f5c
Alan T. DeKok <aland@freeradius.org>
2013-05-15 10:13:33 -0400

    Ignore subsections which statically evaluate to "false"
    So that large swaths of the configuration can be taken out
    of the configuration with zero run-time memory or CPU impact

--- src/main/modules.c
+++ src/main/modules.c
@@ -27,6 +27,7 @@ RCSID("$Id$")
 #include <freeradius-devel/radiusd.h>
 #include <freeradius-devel/modpriv.h>
 #include <freeradius-devel/modcall.h>
+#include <freeradius-devel/parser.h>
 #include <freeradius-devel/rad_assert.h>
 
 extern int check_config;
@@ -877,6 +878,21 @@ static int load_component_section(CONF_SECTION *cs,
 			}
 
 			cp = NULL;
+
+			if ((strcmp(name1, "if") == 0) ||
+			    (strcmp(name1, "elsif") == 0)) {
+				fr_cond_t const *c;
+
+				c = cf_data_find(scs, "if");
+				if (!c) continue;
+
+				if (c->type == COND_TYPE_FALSE) {
+					DEBUG(" # Skipping contents of '%s' at %s:%d as it statically evaluates to 'false'",
+					     name1, cf_section_filename(scs), cf_section_lineno(scs));
+					continue;
+				}
+			}
+
 
 		} else if (cf_item_is_pair(modref)) {
 			cp = cf_itemtopair(modref);
