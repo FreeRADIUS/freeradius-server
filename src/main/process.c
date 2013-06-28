b575ae4767cfb77b15c67fe9fc1c9c34a47999c0
Alan T. DeKok <aland@freeradius.org>
2013-06-28 17:03:57 -0400

    Remove debug macro

--- src/main/process.c
+++ src/main/process.c
@@ -67,7 +67,6 @@ static char const *action_codes[] = {
 #endif
 };
 
-#define DEBUG_STATE_MACHINE
 #ifdef DEBUG_STATE_MACHINE
 #define TRACE_STATE_MACHINE if (debug_flag) printf("(%u) ********\tSTATE %s action %s live M%u C%u\t********\n", request->number, __FUNCTION__, action_codes[action], request->master_state, request->child_state)
 #else
