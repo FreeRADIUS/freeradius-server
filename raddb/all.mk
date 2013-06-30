0a03920982a7c4786fd37f4dd24a1e3d3b9299be
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-06-30 17:36:10 +0100

    Apparently making the symlink targets as order only dependencies isnt good enough

--- raddb/all.mk
+++ raddb/all.mk
@@ -93,15 +93,15 @@ $(R)$(raddbdir)/%: | raddb/%
 # Create symbolic links for legacy files
 $(R)$(raddbdir)/huntgroups : | $(R)$(modconfdir)/preprocess/huntgroups
 	@echo LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
-	@ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
+	@[ -f $@ ] || ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
 	
 $(R)$(raddbdir)/hints : | $(R)$(modconfdir)/preprocess/hints
 	@echo LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
-	@ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
+	@[ -f $@ ] || ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
 	
 $(R)$(raddbdir)/users : | $(R)$(modconfdir)/files/authorize
 	@echo LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
-	@ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
+	@[ -f $@ ] || ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
 
 $(LOCAL_CERT_PRODUCTS):
 	@echo BOOTSTRAP raddb/certs/
