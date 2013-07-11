b2fd92b8433ab6c7806cfc3221804f9352a506b5
Alan T. DeKok <aland@freeradius.org>
2013-07-11 14:05:25 +0200

    Don't echo if we do nothing

--- raddb/all.mk
+++ raddb/all.mk
@@ -92,15 +92,15 @@ $(R)$(raddbdir)/%: | raddb/%
 
 # Create symbolic links for legacy files
 $(R)$(raddbdir)/huntgroups: $(R)$(modconfdir)/preprocess/huntgroups
-	@echo LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
+	@[ -e $@ ] || lecho LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
 	@[ -e $@ ] || ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
 
 $(R)$(raddbdir)/hints: $(R)$(modconfdir)/preprocess/hints
-	@echo LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
+	@[ -e $@ ] || lecho LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
 	@[ -e $@ ] || ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
 
 $(R)$(raddbdir)/users: $(R)$(modconfdir)/files/authorize
-	@echo LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
+	@[ -e $@ ] || lecho LN-S $(patsubst $(R)$(raddbdir)/%,raddb/%,$@)
 	@[ -e $@ ] || ln -s $(patsubst $(R)$(raddbdir)/%,./%,$<) $@
 
 $(LOCAL_CERT_PRODUCTS):
