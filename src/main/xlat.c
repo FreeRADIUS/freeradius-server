6310aa82b58db45bb9611400486b401ee4e0055d
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-07-17 17:07:48 +0100

    Allow abinary now the print function is fixed.

--- src/main/xlat.c
+++ src/main/xlat.c
@@ -358,7 +358,6 @@ static ssize_t xlat_debug_attr(UNUSED void *instance, REQUEST *request, char con
 			switch (type->number) {
 				case PW_TYPE_INVALID:		/* Not real type */
 				case PW_TYPE_MAX:		/* Not real type */
-				case PW_TYPE_ABINARY:		/* Not safe/appropriate */
 				case PW_TYPE_EXTENDED:		/* Not safe/appropriate */
 				case PW_TYPE_LONG_EXTENDED:	/* Not safe/appropriate */
 				case PW_TYPE_TLV:		/* Not safe/appropriate */
