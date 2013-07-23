7d66bc31c930245b68f6a8529127085e6a907e71
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-07-23 15:33:04 +0100

    Link against libpcap when available

--- src/lib/all.mk
+++ src/lib/all.mk
@@ -15,4 +15,4 @@ SRC_CFLAGS	:= -D_LIBRADIUS -I$(top_builddir)/src
 
 # System libraries discovered by our top level configure script, links things
 # like pthread and the regexp libraries.
-TGT_LDLIBS	:= $(LIBS)
+TGT_LDLIBS	:= $(LIBS) $(PCAP_LIBS)
