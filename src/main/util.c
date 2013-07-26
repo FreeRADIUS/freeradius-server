096667c42674d74f1fab233a5b5bd31879be4c21
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-07-26 12:21:39 +0100

    Only clear matches on new match so nested regex comparisons work correctly
    Also add some useful debug output

--- src/main/util.c
+++ src/main/util.c
@@ -1046,19 +1046,24 @@ void rad_regcapture(REQUEST *request, int compare, char const *value, regmatch_t
 	char *p;
 	size_t len;
 
+	if (compare == REG_NOMATCH) {
+		return;
+	}
+
 	/*
 	 *	Add new %{0}, %{1}, etc.
 	 */
 	for (i = 0; i <= REQUEST_MAX_REGEX; i++) {
-
-
 		/*
 		 *	Didn't match: delete old match, if it existed.
 		 */
-		if ((compare != 0) || (rxmatch[i].rm_so == -1)) {
+		if (rxmatch[i].rm_so == -1) {
 			p = request_data_get(request, request, REQUEST_DATA_REGEX | i);
 			if (p) {
+				RDEBUG4("%%{%i}: Clearing old value \"%s\"", i, p);
 				talloc_free(p);
+			} else {
+				RDEBUG4("%%{%i}: Was empty", i);
 			}
 
 			continue;
@@ -1069,6 +1074,7 @@ void rad_regcapture(REQUEST *request, int compare, char const *value, regmatch_t
 		memcpy(p, value + rxmatch[i].rm_so, len);
 		p[len] = '\0';
 
+		RDEBUG4("%%{%i}: Inserting new value \"%s\"", i, p);
 		/*
 		 *	Copy substring, and add it to
 		 *	the request.
