43aabe6332b3d8dc522e301306c96c749f412a41
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-10-08 15:21:42 +0100

    TGT_PRLIBS not needed for libraries

--- src/main/radmin.mk
+++ src/main/radmin.mk
@@ -4,4 +4,4 @@ SOURCES		:= radmin.c
 
 TGT_INSTALLDIR  := ${sbindir}
 TGT_PREREQS	:= libfreeradius-server.a libfreeradius-radius.a
-TGT_LDLIBS	:= $(LIBS) $(LIBREADLINE) -ltalloc
+TGT_LDLIBS	:= $(LIBS) $(LIBREADLINE)
--- src/tests/rbmonkey.mk
+++ src/tests/rbmonkey.mk
@@ -3,3 +3,4 @@ TARGET := rbmonkey
 SOURCES := rbmonkey.c
 
 TGT_PREREQS	:= libfreeradius-radius.a
+TGT_LDLIBS	:= $(LIBS)
