343d6d6e14348bf213999c9f7565ef82000ce8a9
Alan T. DeKok <aland@freeradius.org>
2013-10-30 19:00:15 -0400

    Added preliminary unit test program
    Which thinks it's "radiusd".  But, it never opens any sockets.
    All it does is read a "packet" from a file, and dump the "reply"
    to another file.

--- src/main/all.mk
+++ src/main/all.mk
@@ -1,3 +1,3 @@
 SUBMAKEFILES := radclient.mk radiusd.mk radsniff.mk radmin.mk radattr.mk \
 	radconf2xml.mk radwho.mk radlast.mk radtest.mk radzap.mk checkrad.mk \
-	libfreeradius-server.mk
+	libfreeradius-server.mk unittest.mk
