fc9da1163852b78a7d71096cb30778150a553b8f
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-12-16 22:11:03 +0000

    Clean up more code
    Break entry writing out into a separate function to allow us to separate it from acquiring the FP in future...

--- src/modules/rlm_detail/rlm_detail.c
+++ src/modules/rlm_detail/rlm_detail.c
@@ -22,25 +22,25 @@
  */
 RCSID("$Id$")
 
-#include	<freeradius-devel/radiusd.h>
-#include	<freeradius-devel/modules.h>
-#include	<freeradius-devel/rad_assert.h>
-#include	<freeradius-devel/detail.h>
+#include <freeradius-devel/radiusd.h>
+#include <freeradius-devel/modules.h>
+#include <freeradius-devel/rad_assert.h>
+#include <freeradius-devel/detail.h>
 
-#include	<ctype.h>
-#include	<fcntl.h>
-#include	<sys/stat.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #ifdef HAVE_FNMATCH_H
-#include	<fnmatch.h>
+#  include <fnmatch.h>
 #endif
 
 #ifdef HAVE_UNISTD_H
-#include <unistd.h>
+#  include <unistd.h>
 #endif
 
 #ifdef HAVE_GRP_H
-#include <grp.h>
+#  include <grp.h>
 #endif
 
 #define DIRLEN	8192		//!< Maximum path length.
@@ -65,17 +65,17 @@ typedef struct detail_instance {
 } detail_instance_t;
 
 static const CONF_PARSER module_config[] = {
-	{ "detailfile", PW_TYPE_FILE_OUTPUT | PW_TYPE_DEPRECATED, offsetof(struct detail_instance,filename),
+	{ "detailfile", PW_TYPE_FILE_OUTPUT | PW_TYPE_DEPRECATED, offsetof(detail_instance_t, filename),
 	 NULL, NULL },
-	{ "filename", PW_TYPE_FILE_OUTPUT | PW_TYPE_REQUIRED, offsetof(struct detail_instance,filename),
-	NULL, "%A/%{Client-IP-Address}/detail" },
-	{ "header", PW_TYPE_STRING_PTR, offsetof(struct detail_instance,header), NULL, "%t" },
-	{ "detailperm",	PW_TYPE_INTEGER | PW_TYPE_DEPRECATED, offsetof(struct detail_instance,perm), NULL, NULL },
-	{ "permissions", PW_TYPE_INTEGER, offsetof(struct detail_instance,perm), NULL, "0600" },
-	{ "group", PW_TYPE_STRING_PTR, offsetof(struct detail_instance,group), NULL,  NULL},
-	{ "dir_permissions", PW_TYPE_INTEGER, offsetof(struct detail_instance,dirperm), NULL, "0755" },
-	{ "locking", PW_TYPE_BOOLEAN, offsetof(struct detail_instance,locking), NULL, "no" },
-	{ "log_packet_header", PW_TYPE_BOOLEAN, offsetof(struct detail_instance,log_srcdst), NULL, "no" },
+	{ "filename", PW_TYPE_FILE_OUTPUT | PW_TYPE_REQUIRED, offsetof(detail_instance_t, filename),
+	 NULL, "%A/%{Client-IP-Address}/detail" },
+	{ "header", PW_TYPE_STRING_PTR, offsetof(detail_instance_t, header), NULL, "%t" },
+	{ "detailperm",	PW_TYPE_INTEGER | PW_TYPE_DEPRECATED, offsetof(detail_instance_t, perm), NULL, NULL },
+	{ "permissions", PW_TYPE_INTEGER, offsetof(detail_instance_t, perm), NULL, "0600" },
+	{ "group", PW_TYPE_STRING_PTR, offsetof(detail_instance_t, group), NULL,  NULL},
+	{ "dir_permissions", PW_TYPE_INTEGER, offsetof(detail_instance_t, dirperm), NULL, "0755" },
+	{ "locking", PW_TYPE_BOOLEAN, offsetof(detail_instance_t, locking), NULL, "no" },
+	{ "log_packet_header", PW_TYPE_BOOLEAN, offsetof(detail_instance_t, log_srcdst), NULL, "no" },
 	{ NULL, -1, 0, NULL, NULL }
 };
 
@@ -85,7 +85,7 @@ static const CONF_PARSER module_config[] = {
  */
 static int mod_detach(void *instance)
 {
-	struct detail_instance *inst = instance;
+	detail_instance_t *inst = instance;
 	if (inst->ht) fr_hash_table_free(inst->ht);
 	return 0;
 }
@@ -136,8 +136,7 @@ static int mod_instantiate(CONF_SECTION *conf, void *instance)
 
 			da = dict_attrbyname(attr);
 			if (!da) {
-				cf_log_err_cs(conf, "No such attribute '%s'",
-					      attr);
+				cf_log_err_cs(conf, "No such attribute '%s'", attr);
 			}
 
 			if (!fr_hash_table_insert(inst->ht, da)) {
@@ -150,22 +149,146 @@ static int mod_instantiate(CONF_SECTION *conf, void *instance)
 	return 0;
 }
 
+/** Write a single detail entry to file pointer
+ *
+ * @param[in] out Where to write entry.
+ * @param[in] inst Instance of rlm_detail.
+ * @param[in] request The current request.
+ * @param[in] packet associated with the request (request, reply, proxy-request, proxy-reply...).
+ * @param[in] compat Write out entry in compatibility mode.
+ */
+static int detail_write(FILE *out, detail_instance_t *inst, REQUEST *request, RADIUS_PACKET *packet, bool compat)
+{
+	VALUE_PAIR *vp;
+	char timestamp[256];
+
+	if (radius_xlat(timestamp, sizeof(timestamp), request, inst->header, NULL, NULL) < 0) {
+		return -1;
+	}
+
+#define WRITE(fmt, ...) do {\
+	if (fprintf(out, fmt, ## __VA_ARGS__) < 0) {\
+		RERROR("Failed writing to detail file: %s", strerror(errno));\
+		return -1;\
+	}\
+} while(0)
+
+	WRITE("%s\n", timestamp);
+
+	/*
+	 *	Write the information to the file.
+	 */
+	if (!compat) {
+		/*
+		 *	Print out names, if they're OK.
+		 *	Numbers, if not.
+		 */
+		if ((packet->code > 0) && (packet->code < FR_MAX_PACKET_CODE)) {
+			WRITE("\tPacket-Type = %s\n", fr_packet_codes[packet->code]);
+		} else {
+			WRITE("\tPacket-Type = %d\n", packet->code);
+		}
+	}
+
+	if (inst->log_srcdst) {
+		VALUE_PAIR src_vp, dst_vp;
+
+		memset(&src_vp, 0, sizeof(src_vp));
+		memset(&dst_vp, 0, sizeof(dst_vp));
+		src_vp.op = dst_vp.op = T_OP_EQ;
+
+		switch (packet->src_ipaddr.af) {
+		case AF_INET:
+			src_vp.da = dict_attrbyvalue(PW_PACKET_SRC_IP_ADDRESS, 0);
+			src_vp.vp_ipaddr = packet->src_ipaddr.ipaddr.ip4addr.s_addr;
+
+			dst_vp.da = dict_attrbyvalue(PW_PACKET_DST_IP_ADDRESS, 0);
+			dst_vp.vp_ipaddr = packet->dst_ipaddr.ipaddr.ip4addr.s_addr;
+			break;
+
+		case AF_INET6:
+			src_vp.da = dict_attrbyvalue(PW_PACKET_SRC_IPV6_ADDRESS, 0);
+			memcpy(&src_vp.vp_ipv6addr, &packet->src_ipaddr.ipaddr.ip6addr,
+			       sizeof(packet->src_ipaddr.ipaddr.ip6addr));
+			dst_vp.da = dict_attrbyvalue(PW_PACKET_DST_IPV6_ADDRESS, 0);
+			memcpy(&dst_vp.vp_ipv6addr, &packet->dst_ipaddr.ipaddr.ip6addr,
+			       sizeof(packet->dst_ipaddr.ipaddr.ip6addr));
+			break;
+
+		default:
+			break;
+		}
+
+		vp_print(out, &src_vp);
+		vp_print(out, &dst_vp);
+
+		src_vp.da = dict_attrbyvalue(PW_PACKET_SRC_PORT, 0);
+		src_vp.vp_integer = packet->src_port;
+		dst_vp.da = dict_attrbyvalue(PW_PACKET_DST_PORT, 0);
+		dst_vp.vp_integer = packet->dst_port;
+
+		vp_print(out, &src_vp);
+		vp_print(out, &dst_vp);
+	}
+
+	{
+		vp_cursor_t cursor;
+		/* Write each attribute/value to the log file */
+		for (vp = paircursor(&cursor, &packet->vps);
+		     vp;
+		     vp = pairnext(&cursor)) {
+			if (inst->ht &&
+			    fr_hash_table_finddata(inst->ht, &vp->da)) continue;
+
+			/*
+			 *	Don't print passwords in old format...
+			 */
+			if (compat && !vp->da->vendor && (vp->da->attr == PW_USER_PASSWORD)) continue;
+
+			/*
+			 *	Print all of the attributes.
+			 */
+			vp_print(out, vp);
+		}
+	}
+
+	/*
+	 *	Add non-protocol attributes.
+	 */
+	if (compat) {
+#ifdef WITH_PROXY
+		if (request->proxy) {
+			char proxy_buffer[128];
+
+			inet_ntop(request->proxy->dst_ipaddr.af, &request->proxy->dst_ipaddr.ipaddr,
+				  proxy_buffer, sizeof(proxy_buffer));
+			WRITE("\tFreeradius-Proxied-To = %s\n", proxy_buffer);
+		}
+#endif
+
+		WRITE("\tTimestamp = %ld\n", (unsigned long) request->timestamp);
+	}
+
+	WRITE("\n");
+
+	return 0;
+}
+
 /*
  *	Do detail, compatible with old accounting
  */
-static rlm_rcode_t do_detail(void *instance, REQUEST *request, RADIUS_PACKET *packet, int compat)
+static rlm_rcode_t detail_do(void *instance, REQUEST *request, RADIUS_PACKET *packet, bool compat)
 {
 	int		outfd;
-	char		timestamp[256];
 	char		buffer[DIRLEN];
 	char		*p;
 	struct stat	st;
 	int		locked;
 	int		lock_count;
 	struct timeval	tv;
-	VALUE_PAIR	*vp;
+
 	off_t		fsize;
-	FILE		*fp;
+	FILE		*outfp;
 
 #ifdef HAVE_GRP_H
 	gid_t		gid;
@@ -173,7 +296,7 @@ static rlm_rcode_t do_detail(void *instance, REQUEST *request, RADIUS_PACKET *pa
 	char		*endptr;
 #endif
 
-	struct detail_instance *inst = instance;
+	detail_instance_t *inst = instance;
 
 	rad_assert(request != NULL);
 
@@ -185,15 +308,12 @@ static rlm_rcode_t do_detail(void *instance, REQUEST *request, RADIUS_PACKET *pa
 	}
 
 	/*
-	 *	Create a directory for this nas.
-	 *
-	 *	Generate the path for the detail file.  Use the
-	 *	same format, but truncate at the last /.  Then
-	 *	feed it through radius_xlat() to expand the
-	 *	variables.
+	 *	Generate the path for the detail file.  Use the same
+	 *	format, but truncate at the last /.  Then feed it
+	 *	through radius_xlat() to expand the variables.
 	 */
 	if (radius_xlat(buffer, sizeof(buffer), request, inst->filename, NULL, NULL) < 0) {
-	    return RLM_MODULE_FAIL;
+		return RLM_MODULE_FAIL;
 	}
 	RDEBUG2("%s expands to %s", inst->filename, buffer);
 
@@ -316,145 +436,42 @@ static rlm_rcode_t do_detail(void *instance, REQUEST *request, RADIUS_PACKET *pa
 skip_group:
 #endif
 
-	/*
-	 *	Post a timestamp
-	 */
 	fsize = lseek(outfd, 0L, SEEK_END);
 	if (fsize < 0) {
-		RERROR("Failed to seek to the end of detail file %s", buffer);
-		close(outfd);
-		return RLM_MODULE_FAIL;
-	}
-
-	if (radius_xlat(timestamp, sizeof(timestamp), request, inst->header, NULL, NULL) < 0) {
+		RERROR("Failed to seek to the end of detail file '%s'", buffer);
 		close(outfd);
 		return RLM_MODULE_FAIL;
 	}
 
 	/*
-	 *	Open the FP for buffering.
+	 *	Open the output fp for buffering.
 	 */
-	if ((fp = fdopen(outfd, "a")) == NULL) {
+	if ((outfp = fdopen(outfd, "a")) == NULL) {
 		RERROR("Couldn't open file %s: %s", buffer, strerror(errno));
 		close(outfd);
 		return RLM_MODULE_FAIL;
 	}
 
-	fprintf(fp, "%s\n", timestamp);
-
-	/*
-	 *	Write the information to the file.
-	 */
-	if (!compat) {
-		/*
-		 *	Print out names, if they're OK.
-		 *	Numbers, if not.
-		 */
-		if ((packet->code > 0) &&
-		    (packet->code < FR_MAX_PACKET_CODE)) {
-			fprintf(fp, "\tPacket-Type = %s\n",
-				fr_packet_codes[packet->code]);
-		} else {
-			fprintf(fp, "\tPacket-Type = %d\n", packet->code);
-		}
+	if (detail_write(outfp, inst, request, packet, compat) < 0) {
+		fclose(outfp);
+		return RLM_MODULE_FAIL;
 	}
-
-	if (inst->log_srcdst) {
-		VALUE_PAIR src_vp, dst_vp;
-
-		memset(&src_vp, 0, sizeof(src_vp));
-		memset(&dst_vp, 0, sizeof(dst_vp));
-		src_vp.op = dst_vp.op = T_OP_EQ;
-
-		switch (packet->src_ipaddr.af) {
-		case AF_INET:
-			src_vp.da = dict_attrbyvalue(PW_PACKET_SRC_IP_ADDRESS, 0);
-			src_vp.vp_ipaddr = packet->src_ipaddr.ipaddr.ip4addr.s_addr;
-
-			dst_vp.da = dict_attrbyvalue(PW_PACKET_DST_IP_ADDRESS, 0);
-			dst_vp.vp_ipaddr = packet->dst_ipaddr.ipaddr.ip4addr.s_addr;
-			break;
-
-		case AF_INET6:
-			src_vp.da = dict_attrbyvalue(PW_PACKET_SRC_IPV6_ADDRESS, 0);
-			memcpy(&src_vp.vp_ipv6addr, &packet->src_ipaddr.ipaddr.ip6addr,
-			       sizeof(packet->src_ipaddr.ipaddr.ip6addr));
-			dst_vp.da = dict_attrbyvalue(PW_PACKET_DST_IPV6_ADDRESS, 0);
-			memcpy(&dst_vp.vp_ipv6addr, &packet->dst_ipaddr.ipaddr.ip6addr,
-			       sizeof(packet->dst_ipaddr.ipaddr.ip6addr));
-			break;
-
-		default:
-			break;
-		}
-
-		vp_print(fp, &src_vp);
-		vp_print(fp, &dst_vp);
-
-		src_vp.da = dict_attrbyvalue(PW_PACKET_SRC_PORT, 0);
-		src_vp.vp_integer = packet->src_port;
-		dst_vp.da = dict_attrbyvalue(PW_PACKET_DST_PORT, 0);
-		dst_vp.vp_integer = packet->dst_port;
-
-		vp_print(fp, &src_vp);
-		vp_print(fp, &dst_vp);
-	}
-
-	{
-		vp_cursor_t cursor;
-		/* Write each attribute/value to the log file */
-		for (vp = paircursor(&cursor, &packet->vps);
-		     vp;
-		     vp = pairnext(&cursor)) {
-			if (inst->ht &&
-			    fr_hash_table_finddata(inst->ht, &vp->da)) continue;
-
-			/*
-			 *	Don't print passwords in old format...
-			 */
-			if (compat && !vp->da->vendor && (vp->da->attr == PW_USER_PASSWORD)) continue;
-
-			/*
-			 *	Print all of the attributes.
-			 */
-			vp_print(fp, vp);
-		}
-	}
-
-	/*
-	 *	Add non-protocol attributes.
-	 */
-	if (compat) {
-#ifdef WITH_PROXY
-		if (request->proxy) {
-			char proxy_buffer[128];
-
-			inet_ntop(request->proxy->dst_ipaddr.af, &request->proxy->dst_ipaddr.ipaddr,
-				  proxy_buffer, sizeof(proxy_buffer));
-			fprintf(fp, "\tFreeradius-Proxied-To = %s\n", proxy_buffer);
-		}
-#endif
-
-		fprintf(fp, "\tTimestamp = %ld\n", (unsigned long) request->timestamp);
-	}
-
-	fprintf(fp, "\n");
 
 	/*
 	 *	If we can't flush it to disk, truncate the file and return an error.
 	 */
-	if (fflush(fp) != 0) {
+	if (fflush(outfp) != 0) {
 		int ret;
 		ret = ftruncate(outfd, fsize);
 		if (ret) {
 			REDEBUG4("Failed truncating detail file: %s", strerror(ret));
 		}
 
-		fclose(fp);
+		fclose(outfp);
 		return RLM_MODULE_FAIL;
 	}
 
-	fclose(fp);
+	fclose(outfp);
 
 	/*
 	 *	And everything is fine.
@@ -469,14 +486,14 @@ static rlm_rcode_t mod_accounting(void *instance, REQUEST *request)
 {
 #ifdef WITH_DETAIL
 	if (request->listener->type == RAD_LISTEN_DETAIL &&
-	    strcmp(((struct detail_instance *)instance)->filename,
+	    strcmp(((detail_instance_t *)instance)->filename,
 		   ((listen_detail_t *)request->listener->data)->filename) == 0) {
 		RDEBUG("Suppressing writes to detail file as the request was just read from a detail file.");
 		return RLM_MODULE_NOOP;
 	}
 #endif
 
-	return do_detail(instance,request,request->packet, true);
+	return detail_do(instance, request, request->packet, true);
 }
 
 /*
@@ -484,7 +501,7 @@ static rlm_rcode_t mod_accounting(void *instance, REQUEST *request)
  */
 static rlm_rcode_t mod_authorize(void *instance, REQUEST *request)
 {
-	return do_detail(instance,request,request->packet, false);
+	return detail_do(instance, request, request->packet, false);
 }
 
 /*
@@ -492,7 +509,7 @@ static rlm_rcode_t mod_authorize(void *instance, REQUEST *request)
  */
 static rlm_rcode_t mod_post_auth(void *instance, REQUEST *request)
 {
-	return do_detail(instance,request,request->reply, false);
+	return detail_do(instance, request, request->reply, false);
 }
 
 #ifdef WITH_COA
@@ -501,7 +518,7 @@ static rlm_rcode_t mod_post_auth(void *instance, REQUEST *request)
  */
 static rlm_rcode_t mod_recv_coa(void *instance, REQUEST *request)
 {
-	return do_detail(instance,request,request->packet, false);
+	return detail_do(instance, request, request->packet, false);
 }
 
 /*
@@ -509,7 +526,7 @@ static rlm_rcode_t mod_recv_coa(void *instance, REQUEST *request)
  */
 static rlm_rcode_t mod_send_coa(void *instance, REQUEST *request)
 {
-	return do_detail(instance,request,request->reply, false);
+	return detail_do(instance, request, request->reply, false);
 }
 #endif
 
@@ -519,9 +536,8 @@ static rlm_rcode_t mod_send_coa(void *instance, REQUEST *request)
 #ifdef WITH_PROXY
 static rlm_rcode_t mod_pre_proxy(void *instance, REQUEST *request)
 {
-	if (request->proxy &&
-	    request->proxy->vps) {
-		return do_detail(instance,request,request->proxy, false);
+	if (request->proxy && request->proxy->vps) {
+		return detail_do(instance, request, request->proxy, false);
 	}
 
 	return RLM_MODULE_NOOP;
@@ -533,9 +549,8 @@ static rlm_rcode_t mod_pre_proxy(void *instance, REQUEST *request)
  */
 static rlm_rcode_t mod_post_proxy(void *instance, REQUEST *request)
 {
-	if (request->proxy_reply &&
-	    request->proxy_reply->vps) {
-		return do_detail(instance,request,request->proxy_reply, false);
+	if (request->proxy_reply && request->proxy_reply->vps) {
+		return detail_do(instance, request, request->proxy_reply, false);
 	}
 
 	/*
