eb9ccad5e63b92cef3eb08304929e7f2653f0f58
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-12-16 22:25:24 +0000

    Print the value of the VP, after assigning the value to it...

--- src/modules/rlm_sql/rlm_sql.c
+++ src/modules/rlm_sql/rlm_sql.c
@@ -435,8 +435,8 @@ int sql_set_user(rlm_sql_t *inst, REQUEST *request, char const *username)
 		return -1;
 	}
 
-	RDEBUG2("SQL-User-Name set to \"%s\"", vp->vp_strvalue);
 	pairstrsteal(vp, expanded);
+	RDEBUG2("SQL-User-Name set to '%s'", vp->vp_strvalue);
 	vp->op = T_OP_SET;
 	pairmove(request, &request->packet->vps, &vp);	/* needs to be pair move else op is not respected */
 
