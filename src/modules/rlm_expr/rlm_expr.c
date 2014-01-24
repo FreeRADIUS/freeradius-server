d065f21dd5f086a3502d710c5a50d0dc55bc958d
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-01-24 23:05:44 +0000

    Fix CID #1020960

--- src/modules/rlm_expr/rlm_expr.c
+++ src/modules/rlm_expr/rlm_expr.c
@@ -593,7 +593,7 @@ static ssize_t md5_xlat(UNUSED void *instance, UNUSED REQUEST *request,
 static ssize_t sha1_xlat(UNUSED void *instance, UNUSED REQUEST *request,
                          char const *fmt, char *out, size_t outlen)
 {
-	uint8_t digest[16];
+	uint8_t digest[20];
 	ssize_t i, len, inlen;
 	uint8_t const *p;
         fr_SHA1_CTX ctx;
