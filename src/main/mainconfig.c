74a5c8b70a26b89ca101364b8f7481e2fc0fabe6
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-01-27 23:00:42 +0000

    Don't overwrite dictionary value set from command line

--- src/main/mainconfig.c
+++ src/main/mainconfig.c
@@ -773,7 +773,9 @@ int read_mainconfig(int reload)
 	 *	pre-compilation in conffile.c.  That should probably
 	 *	be fixed to be done as a second stage.
 	 */
-	mainconfig.dictionary_dir = DICTDIR;
+	if (!mainconfig.dictionary_dir) {
+		mainconfig.dictionary_dir = DICTDIR;
+	}
 
 	/*
 	 *	Read the distribution dictionaries first, then
