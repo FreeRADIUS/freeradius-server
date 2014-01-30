0a893476348081a9d4f817fbbac79aee48dc3824
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-01-30 15:53:53 +0000

    Explain why DHCP decoding failed

--- src/modules/proto_dhcp/rlm_dhcp.c
+++ src/modules/proto_dhcp/rlm_dhcp.c
@@ -62,7 +62,7 @@ static ssize_t dhcp_options_xlat(UNUSED void *instance, REQUEST *request,
 
 	if ((fr_dhcp_decode_options(request->packet,
 				    vp->vp_octets, vp->length, &head) < 0) || (!head)) {
-		RWDEBUG("DHCP option decoding failed");
+		RWDEBUG("DHCP option decoding failed: %s", fr_strerror());
 		*out = '\0';
 		return -1;
 	}
