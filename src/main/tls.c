4707c73350b0831df133aba21d36ae8fc4bb7455
Alan T. DeKok <aland@freeradius.org>
2014-02-18 20:23:42 -0500

    Use talloc destructor for tls_server_conf_free

--- src/main/tls.c
+++ src/main/tls.c
@@ -50,8 +50,6 @@ USES_APPLE_DEPRECATED_API	/* OpenSSL API has been deprecated by Apple */
 #include <openssl/ocsp.h>
 #endif
 
-static void tls_server_conf_free(fr_tls_server_conf_t *conf);
-
 /* record */
 static void 		record_init(record_t *buf);
 static void 		record_close(record_t *buf);
@@ -2288,10 +2286,8 @@ post_ca:
  *	added to automatically free the data when the CONF_SECTION
  *	is freed.
  */
-static void tls_server_conf_free(fr_tls_server_conf_t *conf)
+static int tls_server_conf_free(fr_tls_server_conf_t *conf)
 {
-	if (!conf) return;
-
 	if (conf->ctx) SSL_CTX_free(conf->ctx);
 
 #ifdef HAVE_OPENSSL_OCSP_H
@@ -2302,7 +2298,7 @@ static void tls_server_conf_free(fr_tls_server_conf_t *conf)
 #ifndef NDEBUG
 	memset(conf, 0, sizeof(*conf));
 #endif
-	talloc_free(conf);
+	return 0;
 }
 
 
@@ -2326,9 +2322,11 @@ fr_tls_server_conf_t *tls_server_conf_parse(CONF_SECTION *cs)
 		return NULL;
 	}
 
+	talloc_set_destructor(conf, tls_server_conf_free);
+
 	if (cf_section_parse(cs, conf, tls_server_config) < 0) {
 	error:
-		tls_server_conf_free(conf);
+		talloc_free(conf);
 		return NULL;
 	}
 
@@ -2388,7 +2386,7 @@ fr_tls_server_conf_t *tls_server_conf_parse(CONF_SECTION *cs)
 	/*
 	 *	Cache conf in cs in case we're asked to parse this again.
 	 */
-	cf_data_add(cs, "tls-conf", conf, (void *)(void *) tls_server_conf_free);
+	cf_data_add(cs, "tls-conf", conf, NULL);
 
 	return conf;
 }
@@ -2436,7 +2434,7 @@ fr_tls_server_conf_t *tls_client_conf_parse(CONF_SECTION *cs)
 		goto error;
 	}
 
-	cf_data_add(cs, "tls-conf", conf, (void *)(void *) tls_server_conf_free);
+	cf_data_add(cs, "tls-conf", conf, NULL);
 
 	return conf;
 }
