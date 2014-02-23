1a516542c626ea4a5e5f0bd4c86a9cc364d4d549
Alan T. DeKok <aland@freeradius.org>
2014-02-23 17:55:41 -0500

    Operator precedence

--- src/main/connection.c
+++ src/main/connection.c
@@ -383,7 +383,7 @@ static fr_connection_t *fr_connection_spawn(fr_connection_pool_t *pool,
 	pool->spawning = false;
 	pool->last_spawned = time(NULL);
 	pool->delay_interval = pool->cleanup_interval;
-	pool->next_delay = pool->delay_interval + pool->delay_interval >> 1;
+	pool->next_delay = pool->delay_interval + (pool->delay_interval >> 1);
 
 	pthread_mutex_unlock(&pool->mutex);
 
