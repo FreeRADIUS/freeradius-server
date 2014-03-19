c7e5c06f948348a2d93c95dffb6cfa3abfe20169
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-03-19 23:00:22 +0000

    Fixup formatting of SQL queries

--- raddb/mods-config/sql/ippool-dhcp/oracle/schema.sql
+++ raddb/mods-config/sql/ippool-dhcp/oracle/schema.sql
@@ -1,28 +1,28 @@
-CREATE TABLE RADIPPOOL (
-        ID                      INT PRIMARY KEY,
-        POOL_NAME               VARCHAR(30) NOT NULL,
-        FRAMEDIPADDRESS         VARCHAR(30) NOT NULL,
-        NASIPADDRESS            VARCHAR(30) NOT NULL,
-        POOL_KEY                VARCHAR(64) NOT NULL,
-        CALLEDSTATIONID         VARCHAR(64),
-        CALLINGSTATIONID        VARCHAR(64) NOT NULL,
-        EXPIRY_TIME             TIMESTAMP(0) NOT NULL,
-        USERNAME                VARCHAR(100)
+CREATE TABLE radippool (
+        id                      INT PRIMARY KEY,
+        pool_name               VARCHAR(30) NOT NULL,
+        framedipaddress         VARCHAR(30) NOT NULL,
+        nasipaddress            VARCHAR(30) NOT NULL,
+        pool_key                VARCHAR(64) NOT NULL,
+        calledstationid         VARCHAR(64),
+        callingstationid        VARCHAR(64) NOT NULL,
+        expiry_time             TIMESTAMP(0) NOT NULL,
+        username                VARCHAR(100)
 );
- 
-CREATE INDEX RADIPPOOL_POOLNAME_IPADDR ON RADIPPOOL (POOL_NAME, FRAMEDIPADDRESS);
-CREATE INDEX RADIPPOOL_POOLNAME_EXPIRE ON RADIPPOOL (POOL_NAME, EXPIRY_TIME);
-CREATE INDEX RADIPPOOL_NASIPADDR_KEY ON RADIPPOOL (NASIPADDRESS, POOL_KEY);
-CREATE INDEX RADIPPOOL_NASIPADDR_CALLING ON RADIPPOOL (NASIPADDRESS, CALLINGSTATIONID);
- 
-CREATE SEQUENCE RADIPPOOL_SEQ START WITH 1 INCREMENT BY 1;
- 
-CREATE OR REPLACE TRIGGER RADIPPOOL_SERIALNUMBER
-        BEFORE INSERT OR UPDATE OF ID ON RADIPPOOL
+
+CREATE INDEX radippool_poolname_ipaddr ON radippool (pool_name, framedipaddress);
+CREATE INDEX radippool_poolname_expire ON radippool (pool_name, expiry_time);
+CREATE INDEX radippool_nasipaddr_key ON radippool (nasipaddress, pool_key);
+CREATE INDEX radippool_nasipaddr_calling ON radippool (nasipaddress, callingstationid);
+
+CREATE SEQUENCE radippool_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER radippool_serialnumber
+        BEFORE INSERT OR UPDATE OF id ON radippool
         FOR EACH ROW
         BEGIN
-                IF ( :NEW.ID = 0 OR :NEW.ID IS NULL ) THEN
-                        SELECT RADIPPOOL_SEQ.NEXTVAL INTO :NEW.ID FROM DUAL;
+                IF ( :NEW.id = 0 OR :NEW.id IS NULL ) THEN
+                        SELECT radippool_seq.NEXTVAL INTO :NEW.id FROM dual;
                 END IF;
         END;
 /
