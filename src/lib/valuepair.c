454d3fe696359e1f4ca6efd86dbcf4e20b122d57
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-03-21 19:38:12 +0000

    CID 1187989

--- src/lib/valuepair.c
+++ src/lib/valuepair.c
@@ -2302,7 +2302,7 @@ VALUE_PAIR *readvp2(TALLOC_CTX *ctx, FILE *fp, bool *pfiledone, char const *errp
 		if (!vp) {
 			if (last_token != T_EOL) {
 				fr_perror("%s", errprefix);
-				error = false;
+				error = true;
 				break;
 			}
 			break;
