2ea7654c5bd5e61cf0f0d3f021b766dd252828f6
Alan T. DeKok <aland@freeradius.org>
2014-03-22 20:19:57 -0400

    Redirect output again.
    The panic action doesn't work for me, and I'd rather fight with
    the makefiles on segv than see hundreds of lines of crap output
    the other 99% of the time, when nothing is wrong

--- src/tests/keywords/all.mk
+++ src/tests/keywords/all.mk
@@ -85,12 +85,9 @@ KEYWORD_LIBS	:= $(addsuffix .la,$(addprefix rlm_,$(KEYWORD_MODULES)))
 #  Otherwise, check the log file for a parse error which matches the
 #  ERROR line in the input.
 #
-#  Only redirect STDOUT, which should contain details of why the test failed.
-#  Don't molest STDERR as this may be used to receive output from a debugger.
 $(BUILD_DIR)/tests/keywords/%: $(DIR)/% $(BUILD_DIR)/tests/keywords/%.attrs $(TESTBINDIR)/unittest | $(BUILD_DIR)/tests/keywords $(KEYWORD_RADDB) $(KEYWORD_LIBS) build.raddb
 	@echo UNIT-TEST $(notdir $@)
-	@if ! KEYWORD=$(notdir $@) $(TESTBIN)/unittest -D share -d src/tests/keywords/ -i $@.attrs -f $@.attrs -xx > $@.log; then \
-		cat $@.log; \
+	@if ! KEYWORD=$(notdir $@) $(TESTBIN)/unittest -D share -d src/tests/keywords/ -i $@.attrs -f $@.attrs -xx > $@.log 2>&1; then \
 		if ! grep ERROR $< 2>&1 > /dev/null; then \
 			cat $@.log; \
 			echo "# $@.log"; \
