6017b96ccbccd77757b43c89f83739e3f9153d3a
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-04-06 10:28:34 +0100

    CID 1197120

--- src/modules/proto_bfd/bfd.c
+++ src/modules/proto_bfd/bfd.c
@@ -334,9 +334,11 @@ static int bfd_pthread_create(bfd_state_t *session)
 	fcntl(session->pipefd[1], F_SETFL, O_NONBLOCK | FD_CLOEXEC);
 #endif
 
-	fr_event_fd_insert(session->el, 0, session->pipefd[0],
-			   bfd_pipe_recv, session);
-
+	if (!fr_event_fd_insert(session->el, 0, session->pipefd[0],
+				bfd_pipe_recv, session)) {
+		ERROR("Failed inserting file descriptor into event list: %s", fr_strerror());
+		goto close_pipes;
+	}
 
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
