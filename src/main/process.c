d87db41e1611bc52b066d147e542c6f5d472d6f2
Alan T. DeKok <aland@freeradius.org>
2014-04-07 11:16:58 -0400

    Handle more corner cases with CoA cleanups

--- src/main/process.c
+++ src/main/process.c
@@ -3611,6 +3611,7 @@ static void request_coa_separate(REQUEST *request)
 	rad_assert(request->parent->coa == request);
 	rad_assert(request->ev == NULL);
 	rad_assert(!request->in_request_hash);
+	rad_assert(request->coa == NULL);
 
 	rad_assert(request->proxy_listener != NULL);
 
@@ -4486,6 +4487,12 @@ static int request_delete_cb(UNUSED void *ctx, void *data)
 			request->packet->id,
 			(unsigned int) (request->timestamp - fr_start_time));
 	}
+
+#ifdef WITH_COA
+	if (request->coa) {
+		rad_assert(!request->coa->in_proxy_hash);
+	}
+#endif
 
 	request_free(&request);
 
