d45681f14400a690563f9d1c8a51a075261b01b8
Alan T. DeKok <aland@freeradius.org>
2014-04-29 17:07:20 -0400

    Link to -lssl only if we're using ssl

--- src/modules/rlm_eap/radeapclient.mk
+++ src/modules/rlm_eap/radeapclient.mk
@@ -1,16 +1,16 @@
 TARGET   := radeapclient
 SOURCES := radeapclient.c
 
-ifneq ($(OPENSSL_LIBS),)
-SOURCES += ${top_srcdir}/src/main/cb.c ${top_srcdir}/src/main/tls.c
-endif
-
 SOURCES += ${top_srcdir}/src/main/files.c \
-${top_srcdir}/src/main/threads.c \
-${top_srcdir}/src/main/version.c
+	   ${top_srcdir}/src/main/threads.c \
+	   ${top_srcdir}/src/main/version.c
 
 TGT_PREREQS := libfreeradius-radius.a libfreeradius-eap.a libfreeradius-server.a
 TGT_LDLIBS  := $(LIBS)
+
+ifneq ($(OPENSSL_LIBS),)
+SOURCES += ${top_srcdir}/src/main/cb.c ${top_srcdir}/src/main/tls.c
 TGT_LDLIBS	+= -lssl
+endif
 
 SRC_INCDIRS  := libeap
