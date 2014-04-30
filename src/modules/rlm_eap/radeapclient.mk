a5c115567a30354422e81b72b8f07ebbdf8c86b8
Alan T. DeKok <aland@freeradius.org>
2014-04-30 08:28:19 -0400

    Hacks for later

--- src/modules/rlm_eap/radeapclient.mk
+++ src/modules/rlm_eap/radeapclient.mk
@@ -5,12 +5,20 @@ SOURCES += ${top_srcdir}/src/main/files.c \
 	   ${top_srcdir}/src/main/threads.c \
 	   ${top_srcdir}/src/main/version.c
 
-TGT_PREREQS := libfreeradius-radius.a libfreeradius-eap.a libfreeradius-server.a
+TGT_PREREQS := libfreeradius-radius.a libfreeradius-server.a
 TGT_LDLIBS  := $(LIBS)
+
+#
+#  For future work, if we want radeapclient to become radclient
+#
+ifneq "$(filter libfreeradius-eap%,${ALL_TGTS})" ""
+TGT_PREREQS += libfreeradius-eap.a
 
 ifneq ($(OPENSSL_LIBS),)
 SOURCES += ${top_srcdir}/src/main/cb.c ${top_srcdir}/src/main/tls.c
-TGT_LDLIBS	+= -lssl
+TGT_LDLIBS  += $(OPENSSL_LIBS)
 endif
 
-SRC_INCDIRS  := libeap
+SRC_CFLAGS += -DWITH_EAPCLIENT
+SRC_INCDIRS  := ${top_srcdir}/src/modules/rlm_eap/libeap
+endif
