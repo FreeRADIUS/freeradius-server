433fa7a59ab8b6dec3406f8ef8583a3d5f6ef96b
Alan T. DeKok <aland@freeradius.org>
2014-05-11 08:54:26 -0400

    prefix is "const"

--- src/include/connection.h
+++ src/include/connection.h
@@ -76,7 +76,7 @@ fr_connection_pool_t *fr_connection_pool_init(CONF_SECTION *cs,
 					      fr_connection_create_t c,
 					      fr_connection_alive_t a,
 					      fr_connection_delete_t d,
-					      char *prefix);
+					      char const *prefix);
 void fr_connection_pool_delete(fr_connection_pool_t *pool);
 
 void *fr_connection_get(fr_connection_pool_t *pool);
