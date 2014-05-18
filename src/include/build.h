47806042f33e54a5c65e8f951fce2ac285c1a2f2
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-05-18 16:42:40 +0100

    Remove some duplicate build macros

--- src/include/build.h
+++ src/include/build.h
@@ -17,6 +17,7 @@ extern "C" {
  */
 #define XSTRINGIFY(x) #x
 #define STRINGIFY(x) XSTRINGIFY(x)
+#define JOINSTR(x,y) XSTRINGIFY(x ## y)
 
 /*
  *	HEX concatenation macros
@@ -44,24 +45,27 @@ extern "C" {
 #else
 #  define CC_HINT(_x)
 #endif
+
+/*
+ *	Macros to add pragmas
+ */
+#define PRAGMA(x) _Pragma(#x)
+
 /*
  *	Macros for controlling warnings in GCC >= 4.2 and clang >= 2.8
  */
-#define DIAG_JOINSTR(x,y) XSTRINGIFY(x ## y)
-#define DIAG_DO_PRAGMA(x) _Pragma (#x)
-
 #if defined(__GNUC__) && ((__GNUC__ * 100) + __GNUC_MINOR__) >= 402
-#  define DIAG_PRAGMA(x) DIAG_DO_PRAGMA(GCC diagnostic x)
+#  define DIAG_PRAGMA(x) PRAGMA(GCC diagnostic x)
 #  if ((__GNUC__ * 100) + __GNUC_MINOR__) >= 406
-#    define DIAG_OFF(x) DIAG_PRAGMA(push) DIAG_PRAGMA(ignored DIAG_JOINSTR(-W,x))
+#    define DIAG_OFF(x) DIAG_PRAGMA(push) DIAG_PRAGMA(ignored JOINSTR(-W,x))
 #    define DIAG_ON(x) DIAG_PRAGMA(pop)
 #  else
-#    define DIAG_OFF(x) DIAG_PRAGMA(ignored DIAG_JOINSTR(-W,x))
-#    define DIAG_ON(x)  DIAG_PRAGMA(warning DIAG_JOINSTR(-W,x))
+#    define DIAG_OFF(x) DIAG_PRAGMA(ignored JOINSTR(-W,x))
+#    define DIAG_ON(x)  DIAG_PRAGMA(warning JOINSTR(-W,x))
 #  endif
 #elif defined(__clang__) && ((__clang_major__ * 100) + __clang_minor__ >= 208)
-#  define DIAG_PRAGMA(x) DIAG_DO_PRAGMA(clang diagnostic x)
-#  define DIAG_OFF(x) DIAG_PRAGMA(push) DIAG_PRAGMA(ignored DIAG_JOINSTR(-W,x))
+#  define DIAG_PRAGMA(x) PRAGMA(clang diagnostic x)
+#  define DIAG_OFF(x) DIAG_PRAGMA(push) DIAG_PRAGMA(ignored JOINSTR(-W,x))
 #  define DIAG_ON(x) DIAG_PRAGMA(pop)
 #else
 #  define DIAG_OFF(x)
@@ -81,16 +85,15 @@ extern "C" {
 
 #if defined(__GNUC__)
 /* force inclusion of ident keywords in the face of optimization */
-#define RCSID(id) static char const rcsid[] __attribute__ ((used)) = id;
-#define RCSIDH(h, id) static char const rcsid_ ## h [] __attribute__ ((used)) = id;
+#  define RCSID(id) static char const rcsid[] __attribute__ ((used)) = id;
+#  define RCSIDH(h, id) static char const rcsid_ ## h [] __attribute__ ((used)) = id;
 #elif defined(__SUNPRO_C)
 /* put ident keyword into comment section (nicer than gcc way) */
-#define DO_PRAGMA(x) _Pragma(#x)
-#define RCSID(id) DO_PRAGMA(sun ident id)
-#define RCSIDH(h, id) DO_PRAGMA(sun ident id)
+#  define RCSID(id) PRAGMA(sun ident id)
+#  define RCSIDH(h, id) PRAGMA(sun ident id)
 #else
-#define RCSID(id)
-#define RCSIDH(h, id)
+#  define RCSID(id)
+#  define RCSIDH(h, id)
 #endif
 
 /*
