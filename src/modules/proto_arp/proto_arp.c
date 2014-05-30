a65af3b74ab0e550da483c5c3205248e3ecc3971
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-05-30 19:06:39 +0100

    Fixup fake client for proto_arp

--- src/modules/proto_arp/proto_arp.c
+++ src/modules/proto_arp/proto_arp.c
@@ -291,7 +291,7 @@ static int arp_socket_parse(CONF_SECTION *cs, rad_listen_t *this)
 	memset(client, 0, sizeof(*client));
 	client->ipaddr.af = AF_INET;
 	client->ipaddr.ipaddr.ip4addr.s_addr = INADDR_NONE;
-	client->prefix = 0;
+	client->ipaddr.prefix = 0;
 	client->longname = client->shortname = sock->interface;
 	client->secret = client->shortname;
 	client->nas_type = talloc_typed_strdup(sock, "none");
