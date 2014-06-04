53dd024365d147750bf32a006e1324ec641cbf9d
Alan T. DeKok <aland@freeradius.org>
2014-06-04 14:21:12 -0400

    More talloc_steal

--- src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
+++ src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
@@ -1202,7 +1202,7 @@ PW_CODE eapttls_process(eap_handler_t *handler, tls_session_t *tls_session)
 			 *	tunneled request.
 			 */
 			rad_assert(!request->proxy);
-			request->proxy = fake->packet;
+			request->proxy = talloc_steal(request, fake->packet);
 			memset(&request->proxy->src_ipaddr, 0,
 			       sizeof(request->proxy->src_ipaddr));
 			memset(&request->proxy->src_ipaddr, 0,
