fddcb36c9eb2eaa5716eb0e897fc8fb6b02fb132
Kevin Wasserman <kevin.wasserman@painless-security.com>
2014-06-30 11:41:32 -0400

    Don't call free on talloc'ed channel bindings packet

--- src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
+++ src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
@@ -1196,9 +1196,6 @@ PW_CODE eapttls_process(eap_handler_t *handler, tls_session_t *tls_session)
 		}
 		chbind_code = chbind_process(request, req);
 
-		/* free the chbind packet; we're done with it */
-		free(chbind);
-
 		/* encapsulate response here */
 		if (req->response) {
 			RDEBUG("sending chbind response");
