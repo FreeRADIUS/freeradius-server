63bc79eef26f0e61fcd13aa7da00d38ed7d440ab
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-07-27 10:14:56 -0400

    Putting every source file on its own line makes it easier to resolve merge errors

--- src/lib/all.mk
+++ src/lib/all.mk
@@ -5,14 +5,43 @@
 #
 TARGET		:= libfreeradius-radius.a
 
-SOURCES		:= cbuff.c cursor.c debug.c dict.c filters.c hash.c hmacmd5.c hmacsha1.c \
-			   isaac.c log.c  misc.c missing.c md4.c md5.c pcap.c print.c radius.c rbtree.c \
-			   sha1.c snprintf.c strlcat.c strlcpy.c token.c udpfromto.c valuepair.c fifo.c \
-			   packet.c event.c getaddrinfo.c heap.c tcp.c base64.c version.c
+SOURCES		:= cbuff.c \
+		   cursor.c \
+		   debug.c \
+		   dict.c \
+		   filters.c \
+		   hash.c \
+		   hmacmd5.c \
+		   hmacsha1.c \
+		   isaac.c \
+		   log.c \
+		   misc.c \
+		   missing.c \
+		   md4.c \
+		   md5.c \
+		   pcap.c \
+		   print.c \
+		   radius.c \
+		   rbtree.c \
+		   sha1.c \
+		   snprintf.c \
+		   strlcat.c \
+		   strlcpy.c \
+		   token.c \
+		   udpfromto.c \
+		   valuepair.c \
+		   fifo.c \
+		   packet.c \
+		   event.c \
+		   getaddrinfo.c \
+		   heap.c \
+		   tcp.c \
+		   base64.c \
+		   version.c
 
 SRC_CFLAGS	:= -D_LIBRADIUS -I$(top_builddir)/src
 
 # System libraries discovered by our top level configure script, links things
 # like pthread and the regexp libraries.
 TGT_LDLIBS	:= $(LIBS) $(PCAP_LIBS)
-TGT_LDFLAGS := $(LDFLAGS) $(PCAP_LDFLAGS)
+TGT_LDFLAGS	:= $(LDFLAGS) $(PCAP_LDFLAGS)
