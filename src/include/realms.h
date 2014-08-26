12069c91a05417c0b933717d01948b02314ad916
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-08-26 13:43:26 +0200

    Break out validation logic from realm_home_server_add

--- src/include/realms.h
+++ src/include/realms.h
@@ -159,6 +159,7 @@ int realms_init(CONF_SECTION *config);
 void realms_free(void);
 REALM *realm_find(char const *name); /* name is from a packet */
 REALM *realm_find2(char const *name); /* ... with name taken from realm_find */
+void realm_home_server_sanitize( home_server_t *home, CONF_SECTION *cs);
 int realm_home_server_add(home_server_t *home, CONF_SECTION *cs, int dual);
 int realm_pool_add(home_pool_t *pool, CONF_SECTION *cs);
 int realm_realm_add( REALM *r, CONF_SECTION *cs);
