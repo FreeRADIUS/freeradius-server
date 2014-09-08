b87eb26447c2db8f6ef5cd15c71c1f66e5e1f7e4
Alan T. DeKok <aland@freeradius.org>
2014-09-08 09:48:57 -0400

    Pull autoconf.h definitions into the Make variable space

--- src/tests/all.mk
+++ src/tests/all.mk
@@ -1,1 +1,12 @@
 SUBMAKEFILES := rbmonkey.mk unit/all.mk keywords/all.mk auth/all.mk
+
+#
+#  Include all of the autoconf definitions into the Make variable space
+#
+-include $(BUILD_DIR)/tests/keywords/autoconf.h.mk
+
+#
+#  Pull all of the autoconf stuff into here.
+#
+$(BUILD_DIR)/tests/keywords/autoconf.h.mk: src/include/autoconf.h
+	@grep '^#define' $^ | sed 's/#define /AC_/;s/ / := /' > $@
