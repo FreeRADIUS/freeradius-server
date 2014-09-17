c46430d9674572b2ad9de538ea51a7163041e710
Alan T. DeKok <aland@freeradius.org>
2014-09-17 10:02:49 -0400

    Fix typo from 97b8c9

--- src/main/realms.c
+++ src/main/realms.c
@@ -459,7 +459,7 @@ static int home_server_add(realm_config_t *rc, CONF_SECTION *cs)
 	 *	Figure out which one to use.
 	 */
 	if (cf_pair_find(cs, "ipaddr") || cf_pair_find(cs, "ipv4addr") || cf_pair_find(cs, "ipv6addr")) {
-		if (fr_inaddr_any(&hs_ipaddr == 1)) {
+		if (fr_inaddr_any(&hs_ipaddr) == 1) {
 			cf_log_err_cs(cs, "Wildcard '*' addresses are not permitted for home servers");
 			goto error;
 		}
