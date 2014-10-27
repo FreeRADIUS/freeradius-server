71ee740c92d3e66e6ffc882ecf0c643ca5fce848
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-10-26 20:20:29 -0400

    change paircmp_value et al to pairdata_cmp
    Will help simplify multivalued attribute comparison later

--- src/lib/valuepair.c
+++ src/lib/valuepair.c
@@ -2315,21 +2315,23 @@ error:
 	return -1;
 }
 
-/** Compare two attribute values
+/** Compare two values
  *
- * @param[in] one the first attribute.
- * @param[in] two the second attribute.
- * @return -1 if one is less than two, 0 if both are equal, 1 if one is more than two, < -1 on error.
+ * @param[in] a_type of data to compare.
+ * @param[in] a_len of data to compare.
+ * @param[in] a Value to compare.
+ * @param[in] b_type of data to compare.
+ * @param[in] b_len of data to compare.
+ * @param[in] b Value to compare.
+ * @return -1 if a is less than b, 0 if both are equal, 1 if a is more than b, < -1 on error.
  */
-int paircmp_value(VALUE_PAIR const *one, VALUE_PAIR const *two)
+int pairdata_cmp(PW_TYPE a_type, size_t a_len, value_data_t const *a,
+		 PW_TYPE b_type, size_t b_len, value_data_t const *b)
 {
 	int compare = 0;
 
-	VERIFY_VP(one);
-	VERIFY_VP(two);
-
-	if (one->da->type != two->da->type) {
-		fr_strerror_printf("Can't compare attribute values of different types");
+	if (a_type != b_type) {
+		fr_strerror_printf("Can't compare values of different types");
 		return -2;
 	}
 
@@ -2337,21 +2339,21 @@ int paircmp_value(VALUE_PAIR const *one, VALUE_PAIR const *two)
 	 *	After doing the previous check for special comparisons,
 	 *	do the per-type comparison here.
 	 */
-	switch (one->da->type) {
+	switch (a_type) {
 	case PW_TYPE_ABINARY:
 	case PW_TYPE_OCTETS:
 	case PW_TYPE_STRING:	/* We use memcmp to be \0 safe */
 	{
 		size_t length;
 
-		if (one->length > two->length) {
-			length = one->length;
+		if (a_len > b_len) {
+			length = a_len;
 		} else {
-			length = two->length;
+			length = b_len;
 		}
 
 		if (length) {
-			compare = memcmp(one->vp_octets, two->vp_octets, length);
+			compare = memcmp(a->octets, b->octets, length);
 			if (compare != 0) break;
 		}
 
@@ -2361,15 +2363,15 @@ int paircmp_value(VALUE_PAIR const *one, VALUE_PAIR const *two)
 		 *
 		 *	i.e. "0x00" is smaller than "0x0000"
 		 */
-		compare = one->length - two->length;
+		compare = a_len - b_len;
 	}
 		break;
 
 		/*
 		 *	Short-hand for simplicity.
 		 */
-#define CHECK(_type) if (one->vp_##_type < two->vp_##_type)   { compare = -1; \
-		} else if (one->vp_##_type > two->vp_##_type) { compare = +1; }
+#define CHECK(_type) if (a->_type < b->_type)   { compare = -1; \
+		} else if (a->_type > b->_type) { compare = +1; }
 
 	case PW_TYPE_BOOLEAN:	/* this isn't a RADIUS type, and shouldn't really ever be used */
 	case PW_TYPE_BYTE:
@@ -2378,7 +2380,7 @@ int paircmp_value(VALUE_PAIR const *one, VALUE_PAIR const *two)
 
 
 	case PW_TYPE_SHORT:
-		CHECK(short);
+		CHECK(ushort);
 		break;
 
 	case PW_TYPE_DATE:
@@ -2390,7 +2392,7 @@ int paircmp_value(VALUE_PAIR const *one, VALUE_PAIR const *two)
 		break;
 
 	case PW_TYPE_SIGNED:
-		CHECK(signed);
+		CHECK(sinteger);
 		break;
 
 	case PW_TYPE_INTEGER64:
@@ -2398,40 +2400,40 @@ int paircmp_value(VALUE_PAIR const *one, VALUE_PAIR const *two)
 		break;
 
 	case PW_TYPE_ETHERNET:
-		compare = memcmp(&one->vp_ether, &two->vp_ether, sizeof(one->vp_ether));
+		compare = memcmp(&a->ether, &b->ether, sizeof(a->ether));
 		break;
 
 	case PW_TYPE_IPV4_ADDR: {
-			uint32_t a, b;
+			uint32_t a_int, b_int;
 
-			a = ntohl(one->vp_ipaddr);
-			b = ntohl(two->vp_ipaddr);
-			if (a < b) {
+			a_int = ntohl(a->ipaddr.s_addr);
+			b_int = ntohl(b->ipaddr.s_addr);
+			if (a_int < b_int) {
 				compare = -1;
-			} else if (a > b) {
+			} else if (a_int > b_int) {
 				compare = +1;
 			}
 		}
 		break;
 
 	case PW_TYPE_IPV6_ADDR:
-		compare = memcmp(&one->vp_ipv6addr, &two->vp_ipv6addr, sizeof(one->vp_ipv6addr));
+		compare = memcmp(&a->ipv6addr, &b->ipv6addr, sizeof(a->ipv6addr));
 		break;
 
 	case PW_TYPE_IPV6_PREFIX:
-		compare = memcmp(&one->vp_ipv6prefix, &two->vp_ipv6prefix, sizeof(one->vp_ipv6prefix));
+		compare = memcmp(&a->ipv6prefix, &b->ipv6prefix, sizeof(a->ipv6prefix));
 		break;
 
 	case PW_TYPE_IPV4_PREFIX:
-		compare = memcmp(&one->vp_ipv4prefix, &two->vp_ipv4prefix, sizeof(one->vp_ipv4prefix));
+		compare = memcmp(&a->ipv4prefix, &b->ipv4prefix, sizeof(a->ipv4prefix));
 		break;
 
 	case PW_TYPE_IFID:
-		compare = memcmp(&one->vp_ifid, &two->vp_ifid, sizeof(one->vp_ifid));
+		compare = memcmp(&a->ifid, &b->ifid, sizeof(a->ifid));
 		break;
 
 	/*
-	 *	None of the types below should be in the REQUEST
+	 *	Na of the types below should be in the REQUEST
 	 */
 	case PW_TYPE_INVALID:		/* We should never see these */
 	case PW_TYPE_IP_ADDR:		/* This should have been converted into IPADDR/IPV6ADDR */
@@ -2466,9 +2468,9 @@ int paircmp_value(VALUE_PAIR const *one, VALUE_PAIR const *two)
  *
  *	reserved, prefix-len, data...
  */
-static int paircmp_op_cidr(FR_TOKEN op, int bytes,
-			   uint8_t one_net, uint8_t const *one,
-			   uint8_t two_net, uint8_t const *two)
+static int pairdata_cidr_cmp_op(FR_TOKEN op, int bytes,
+				uint8_t a_net, uint8_t const *a,
+				uint8_t b_net, uint8_t const *b)
 {
 	int i, common;
 	uint32_t mask;
@@ -2476,10 +2478,10 @@ static int paircmp_op_cidr(FR_TOKEN op, int bytes,
 	/*
 	 *	Handle the case of netmasks being identical.
 	 */
-	if (one_net == two_net) {
+	if (a_net == b_net) {
 		int compare;
 
-		compare = memcmp(one, two, bytes);
+		compare = memcmp(a, b, bytes);
 
 		/*
 		 *	If they're identical return true for
@@ -2515,14 +2517,14 @@ static int paircmp_op_cidr(FR_TOKEN op, int bytes,
 
 	case T_OP_LE:
 	case T_OP_LT:	/* 192/8 < 192.168/16 --> false */
-		if (one_net < two_net) {
+		if (a_net < b_net) {
 			return false;
 		}
 		break;
 
 	case T_OP_GE:
 	case T_OP_GT:	/* 192/16 > 192.168/8 --> false */
-		if (one_net > two_net) {
+		if (a_net > b_net) {
 			return false;
 		}
 		break;
@@ -2531,10 +2533,10 @@ static int paircmp_op_cidr(FR_TOKEN op, int bytes,
 		return false;
 	}
 
-	if (one_net < two_net) {
-		common = one_net;
+	if (a_net < b_net) {
+		common = a_net;
 	} else {
-		common = two_net;
+		common = b_net;
 	}
 
 	/*
@@ -2554,7 +2556,7 @@ static int paircmp_op_cidr(FR_TOKEN op, int bytes,
 		 */
 		if (common < 8) break;
 
-		if (one[i] != two[i]) return false;
+		if (a[i] != b[i]) return false;
 
 		common -= 8;
 		i++;
@@ -2566,7 +2568,7 @@ static int paircmp_op_cidr(FR_TOKEN op, int bytes,
 	mask--;
 	mask = ~mask;
 
-	if ((one[i] & mask) == ((two[i] & mask))) {
+	if ((a[i] & mask) == ((b[i] & mask))) {
 		return true;
 	}
 
@@ -2575,26 +2577,31 @@ static int paircmp_op_cidr(FR_TOKEN op, int bytes,
 
 /** Compare two attributes using an operator
  *
- * @param[in] a the first attribute
- * @param[in] op the operator for comparison.
- * @param[in] b the second attribute
+ * @param[in] a_type of data to compare.
+ * @param[in] a_len of data to compare.
+ * @param[in] a Value to compare.
+ * @param[in] b_type of data to compare.
+ * @param[in] b_len of data to compare.
+ * @param[in] b Value to compare.
  * @return 1 if true, 0 if false, -1 on error.
  */
-int paircmp_op(VALUE_PAIR const *a, FR_TOKEN op, VALUE_PAIR const *b)
+int pairdata_cmp_op(PW_TYPE a_type, size_t a_len, value_data_t const *a,
+		    FR_TOKEN op,
+		    PW_TYPE b_type, size_t b_len, value_data_t const *b)
 {
 	int compare = 0;
 
 	if (!a || !b) return -1;
 
-	switch (a->da->type) {
+	switch (a_type) {
 	case PW_TYPE_IPV4_ADDR:
-		switch (b->da->type) {
+		switch (b_type) {
 		case PW_TYPE_IPV4_ADDR:		/* IPv4 and IPv4 */
 			goto cmp;
 
 		case PW_TYPE_IPV4_PREFIX:	/* IPv4 and IPv4 Prefix */
-			return paircmp_op_cidr(op, 4, 32, (uint8_t const *) &a->vp_ipaddr,
-					       b->vp_ipv4prefix[1], (uint8_t const *) &b->vp_ipv4prefix + 2);
+			return pairdata_cidr_cmp_op(op, 4, 32, (uint8_t const *) &a->ipaddr,
+						    b->ipv4prefix[1], (uint8_t const *) &b->ipv4prefix + 2);
 
 		default:
 			fr_strerror_printf("Cannot compare IPv4 with IPv6 address");
@@ -2603,16 +2610,16 @@ int paircmp_op(VALUE_PAIR const *a, FR_TOKEN op, VALUE_PAIR const *b)
 		break;
 
 	case PW_TYPE_IPV4_PREFIX:		/* IPv4 and IPv4 Prefix */
-		switch (b->da->type) {
+		switch (b_type) {
 		case PW_TYPE_IPV4_ADDR:
-			return paircmp_op_cidr(op, 4, a->vp_ipv4prefix[1],
-					       (uint8_t const *) &a->vp_ipv4prefix + 2,
-					       32, (uint8_t const *) &b->vp_ipaddr);
+			return pairdata_cidr_cmp_op(op, 4, a->ipv4prefix[1],
+						    (uint8_t const *) &a->ipv4prefix + 2,
+						    32, (uint8_t const *) &b->ipaddr);
 
 		case PW_TYPE_IPV4_PREFIX:	/* IPv4 Prefix and IPv4 Prefix */
-			return paircmp_op_cidr(op, 4, a->vp_ipv4prefix[1],
-					       (uint8_t const *) &a->vp_ipv4prefix + 2,
-					       b->vp_ipv4prefix[1], (uint8_t const *) &b->vp_ipv4prefix + 2);
+			return pairdata_cidr_cmp_op(op, 4, a->ipv4prefix[1],
+						    (uint8_t const *) &a->ipv4prefix + 2,
+						    b->ipv4prefix[1], (uint8_t const *) &b->ipv4prefix + 2);
 
 		default:
 			fr_strerror_printf("Cannot compare IPv4 with IPv6 address");
@@ -2621,13 +2628,13 @@ int paircmp_op(VALUE_PAIR const *a, FR_TOKEN op, VALUE_PAIR const *b)
 		break;
 
 	case PW_TYPE_IPV6_ADDR:
-		switch (b->da->type) {
+		switch (b_type) {
 		case PW_TYPE_IPV6_ADDR:		/* IPv6 and IPv6 */
 			goto cmp;
 
 		case PW_TYPE_IPV6_PREFIX:	/* IPv6 and IPv6 Preifx */
-			return paircmp_op_cidr(op, 16, 128, (uint8_t const *) &a->vp_ipv6addr,
-					       b->vp_ipv6prefix[1], (uint8_t const *) &b->vp_ipv6prefix + 2);
+			return pairdata_cidr_cmp_op(op, 16, 128, (uint8_t const *) &a->ipv6addr,
+						    b->ipv6prefix[1], (uint8_t const *) &b->ipv6prefix + 2);
 			break;
 
 		default:
@@ -2637,16 +2644,16 @@ int paircmp_op(VALUE_PAIR const *a, FR_TOKEN op, VALUE_PAIR const *b)
 		break;
 
 	case PW_TYPE_IPV6_PREFIX:
-		switch (b->da->type) {
+		switch (b_type) {
 		case PW_TYPE_IPV6_ADDR:		/* IPv6 Prefix and IPv6 */
-			return paircmp_op_cidr(op, 16, a->vp_ipv6prefix[1],
-					       (uint8_t const *) &a->vp_ipv6prefix + 2,
-					       128, (uint8_t const *) &b->vp_ipv6addr);
+			return pairdata_cidr_cmp_op(op, 16, a->ipv6prefix[1],
+						    (uint8_t const *) &a->ipv6prefix + 2,
+						    128, (uint8_t const *) &b->ipv6addr);
 
 		case PW_TYPE_IPV6_PREFIX:	/* IPv6 Prefix and IPv6 */
-			return paircmp_op_cidr(op, 16, a->vp_ipv6prefix[1],
-					       (uint8_t const *) &a->vp_ipv6prefix + 2,
-					       b->vp_ipv6prefix[1], (uint8_t const *) &b->vp_ipv6prefix + 2);
+			return pairdata_cidr_cmp_op(op, 16, a->ipv6prefix[1],
+						    (uint8_t const *) &a->ipv6prefix + 2,
+						    b->ipv6prefix[1], (uint8_t const *) &b->ipv6prefix + 2);
 
 		default:
 			fr_strerror_printf("Cannot compare IPv6 with IPv4 address");
@@ -2656,7 +2663,8 @@ int paircmp_op(VALUE_PAIR const *a, FR_TOKEN op, VALUE_PAIR const *b)
 
 	default:
 	cmp:
-		compare = paircmp_value(a, b);
+		compare = pairdata_cmp(a_type, a_len, a,
+				       b_type, b_len, b);
 		if (compare < -1) {	/* comparison error */
 			return -1;
 		}
@@ -2802,7 +2810,8 @@ int pairlistcmp(VALUE_PAIR *a, VALUE_PAIR *b)
 			return 1;
 		}
 
-		ret = paircmp_value(a_p, b_p);
+		ret = pairdata_cmp(a_p->da->type, a_p->length, &a_p->data,
+				   b_p->da->type, b_p->length, &b_p->data);
 		if (ret != 0) {
 			fr_assert(ret >= -1); 	/* Comparison error */
 			return ret;
