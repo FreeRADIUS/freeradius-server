7b9edfe2c6e9b9c50afeda7ae1dd86a49dbf256b
Alan T. DeKok <aland@freeradius.org>
2014-10-27 16:32:04 -0400

    Don't do \n --> CR mashing in xlat.c
    As it turns out, it's almost always wrong.  The correct escaping
    rules are that the conf file parser reads a string, and that string
    is passed verbatim through all of the layers to the final consumer.
    
    Only the final consumer does the mashing of \n --> CR.
    
    This means that the tests are currently broken.
    
    %{md5:...\n} expects to get a CR on input, and it now gets a "\n".
    Fixing that is the next stage.

--- src/main/xlat.c
+++ src/main/xlat.c
@@ -1184,7 +1184,7 @@ static ssize_t xlat_tokenize_expansion(TALLOC_CTX *ctx, char *fmt, xlat_exp_t **
 static ssize_t xlat_tokenize_literal(TALLOC_CTX *ctx, char *fmt, xlat_exp_t **head,
 				     int brace, char const **error)
 {
-	char *p, *q;
+	char *p;
 	xlat_exp_t *node;
 
 	if (!*fmt) return 0;
@@ -1197,58 +1197,15 @@ static ssize_t xlat_tokenize_literal(TALLOC_CTX *ctx, char *fmt, xlat_exp_t **he
 	node->type = XLAT_LITERAL;
 
 	p = fmt;
-	q = fmt;
 
 	while (*p) {
-		/*
-		 *	Convert \n to it's literal representation.
-		 */
-		if (p[0] == '\\') switch (p[1]) {
-		case 't':
-			*(q++) = '\t';
-			p += 2;
-			node->len++;
-			continue;
-
-		case 'n':
-			*(q++) = '\n';
-			p += 2;
-			node->len++;
-			continue;
-
-		case 'x':
-			p += 2;
-			if (!p[0] || !p[1]) {
+		if (*p == '\\') {
+			if (!p[1]) {
 				talloc_free(node);
-				*error = "Hex expansion requires two hex digits";
+				*error = "Invalid escape at end of string";
 				return -(p - fmt);
 			}
-
-			if (!fr_hex2bin((uint8_t *) q, 1, p, 2)) {
-				talloc_free(node);
-				*error = "Invalid hex characters";
-				return -(p - fmt);
-			}
-
-			/*
-			 *	Don't let people shoot themselves in the foot.
-			 *	\x00 is forbidden.
-			 */
-			if (!*q) {
-				talloc_free(node);
-				*error = "Cannot add zero byte to printable string";
-				return -(p - fmt);
-			}
-
 			p += 2;
-			q++;
-			node->len++;
-			continue;
-
-		default:
-			*(q++) = *p;
-			p += 2;
-			node->len++;
 			continue;
 		}
 
@@ -1348,12 +1305,12 @@ static ssize_t xlat_tokenize_literal(TALLOC_CTX *ctx, char *fmt, xlat_exp_t **he
 		 *	If required, eat the brace.
 		 */
 		if (brace && (*p == '}')) {
-			*q = '\0';
+			*p = '\0';
 			p++;
 			break;
 		}
 
-		*(q++) = *(p++);
+		p++;
 		node->len++;
 	}
 
@@ -2114,6 +2071,10 @@ static char *xlat_aprint(TALLOC_CTX *ctx, REQUEST *request, xlat_exp_t const * c
 
 		str = talloc_array(ctx, char, 2048); /* FIXME: have the module call talloc_typed_asprintf */
 		*str = '\0';	/* Be sure the string is NULL terminated, we now only free on error */
+
+		/*
+		 *	Convert \n --> CR if requested.
+		 */
 
 		rcode = node->xlat->func(node->xlat->instance, request, child, str, 2048);
 		talloc_free(child);
