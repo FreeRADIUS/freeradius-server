6b68c6dddca48cc00d2e99cbf13e6cabc35fb419
LarsKollstedt <lk@man-da.de>
2014-11-14 15:58:19 +0100

    modified:   src/modules/rlm_ldap/rlm_ldap.c
    Additional Log-Messages for the eDirectory policy check. Otherwise there is no hint the login has failed due to the eDirectory policyCheck.

--- src/modules/rlm_ldap/rlm_ldap.c
+++ src/modules/rlm_ldap/rlm_ldap.c
@@ -2238,7 +2238,8 @@ static int ldap_postauth(void *instance, REQUEST * request)
 						ldap_unbind_s(conn->ld);
 					}
 					if ((conn->ld = ldap_connect(instance, (char *)vp_fdn->vp_strvalue, password, 0, &res, &error_msg)) == NULL) {
-						radlog(L_ERR, "  [%s] eDirectory account policy check failed.", inst->xlat_name);
+						// Log message changed: Lars Kollstedt 18.3.2014
+						radlog(L_ERR, "  [%s] eDirectory account policy check for %s failed.", inst->xlat_name,(char *)vp_fdn->vp_strvalue);
 
 						if (error_msg != NULL) {
 							RDEBUG("%s", error_msg);
@@ -2257,6 +2258,10 @@ static int ldap_postauth(void *instance, REQUEST * request)
 						goto postauth_reconnect;
 					}
 					RDEBUG("eDirectory account policy check failed.");
+					
+					// Log message added: Lars Kollstedt 18.3.2014
+					radlog(L_ERR, "  [%s] eDirectory account policy check for %s failed.", inst->xlat_name,(char *)vp_fdn->vp_strvalue);
+					
 					ldap_get_option(conn->ld, LDAP_OPT_ERROR_STRING, &error_msg);
 					if (error_msg != NULL) {
 						RDEBUG("%s", error_msg);
