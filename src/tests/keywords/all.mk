9eb0204e5fbf829bb57e25a6d9bffae8dfec5be5
Alan T. DeKok <aland@freeradius.org>
2014-11-21 14:55:57 -0500

    Make "example" a dependency, too

--- src/tests/keywords/all.mk
+++ src/tests/keywords/all.mk
@@ -65,7 +65,7 @@ $(BUILD_DIR)/tests/keywords/%.attrs: $(DIR)/%.attrs | $(BUILD_DIR)/tests/keyword
 #
 .PRECIOUS: $(BUILD_DIR)/tests/keywords/%.attrs
 
-KEYWORD_MODULES := $(shell grep -- mods-enabled src/tests/keywords/radiusd.conf  | sed 's,.*/,,')
+KEYWORD_MODULES := "$(shell grep -- mods-enabled src/tests/keywords/radiusd.conf  | sed 's,.*/,,') example"
 KEYWORD_RADDB	:= $(addprefix raddb/mods-enabled/,$(KEYWORD_MODULES))
 KEYWORD_LIBS	:= $(addsuffix .la,$(addprefix rlm_,$(KEYWORD_MODULES)))
 
