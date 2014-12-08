5afb80ad8b6b0cbd790696caa84950304f3e4b22
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-12-08 10:07:12 -0500

    Break out serialization functions from rlm_cache_memcached

--- src/modules/rlm_cache/drivers/rlm_cache_memcached/rlm_cache_memcached.c
+++ src/modules/rlm_cache/drivers/rlm_cache_memcached/rlm_cache_memcached.c
@@ -26,6 +26,7 @@
 #include <freeradius-devel/rad_assert.h>
 
 #include "../../rlm_cache.h"
+#include "../../serialize.h"
 
 typedef struct rlm_cache_memcached_handle {
 	memcached_st *handle;
@@ -169,116 +170,37 @@ static cache_status_t cache_entry_find(rlm_cache_entry_t **out, UNUSED rlm_cache
 {
 	rlm_cache_memcached_handle_t *mandle = *handle;
 
+	memcached_return_t mret;
 	size_t len;
-
-	vp_cursor_t packet, control, reply;
-
-	memcached_return_t ret;
+	int ret;
 	uint32_t flags;
 
-	TALLOC_CTX *store = NULL;
-	char *from_store, *p, *q;
+	char *from_store;
 
 	rlm_cache_entry_t *c;
 
-	from_store = memcached_get(mandle->handle, key, strlen(key), &len, &flags, &ret);
+	from_store = memcached_get(mandle->handle, key, strlen(key), &len, &flags, &mret);
 	if (!from_store) {
-		if (ret == MEMCACHED_NOTFOUND) return CACHE_MISS;
+		if (mret == MEMCACHED_NOTFOUND) return CACHE_MISS;
 
-		RERROR("Failed retrieving entry for key \"%s\": %s: %s", key, memcached_strerror(mandle->handle, ret),
+		RERROR("Failed retrieving entry for key \"%s\": %s: %s", key, memcached_strerror(mandle->handle, mret),
 		       memcached_last_error_message(mandle->handle));
 
 		return CACHE_ERROR;
 	}
-
-	c = talloc_zero(request, rlm_cache_entry_t);
-	if (!c) return CACHE_ERROR;
-
-	store = talloc_pool(c, 1024);
-	if (!store) return CACHE_ERROR;
-
-	fr_cursor_init(&packet, &c->packet);
-	fr_cursor_init(&control, &c->control);
-	fr_cursor_init(&reply, &c->reply);
-
-	p = from_store;
 	RDEBUG2("Retrieved %zu bytes from memcached", len);
-	while (((size_t)(p - from_store)) < len) {
-		value_pair_map_t *map;
-		VALUE_PAIR *vp;
+	RDEBUG2("%s", from_store);
 
-		q = strchr(p, '\n');
-		if (!q) break;	/* List should also be terminated with a \n */
-		*q = '\0';
-
-		RDEBUG3("Parsing: %s", p);
-
-		if (map_afrom_attr_str(store, &map, p,
-				       REQUEST_CURRENT, PAIR_LIST_REQUEST,
-				       REQUEST_CURRENT, PAIR_LIST_REQUEST) < 0) {
-			REDEBUG("Failed parsing pair: %s", p);
-		error:
-			free(from_store);
-			talloc_free(c);	/* frees all other heap memory too */
-			return CACHE_ERROR;
-		}
-
-		if (map->lhs->type != TMPL_TYPE_ATTR) {
-			RWDEBUG("Left hand side of pair (%s) was not parsed as an attribute.  "
-				"Check local dictionaries.  Skipping...", map->lhs->name);
-			talloc_free(map);
-			continue;
-		}
-
-		if (map_to_vp(&vp, request, map, NULL) < 0) goto error;
-
-		/*
-		 *	Pull out the special attributes, and set the
-		 *	relevant cache entry fields.
-		 */
-		if (vp->da->vendor == 0) switch (vp->da->attr) {
-		case PW_CACHE_CREATED:
-			c->created = vp->vp_date;
-			talloc_free(vp);
-			talloc_free(map);
-			goto next;
-
-		case PW_CACHE_EXPIRES:
-			c->expires = vp->vp_date;
-			talloc_free(vp);
-			talloc_free(map);
-			goto next;
-
-		default:
-			break;
-		}
-
-		pairsteal(c, vp);	/* @fixme map_to_vp should take a ctx */
-
-		switch (map->lhs->tmpl_list) {
-		case PAIR_LIST_REQUEST:
-			fr_cursor_insert(&packet, vp);
-			break;
-
-		case PAIR_LIST_CONTROL:
-			fr_cursor_insert(&control, vp);
-			break;
-
-		case PAIR_LIST_REPLY:
-			fr_cursor_insert(&reply, vp);
-			break;
-
-		default:
-			REDEBUG("Invalid cache list for pair: %s", p);
-			goto error;
-		}
-	next:
-		p = q + 1;
+	c = talloc_zero(NULL,  rlm_cache_entry_t);
+	ret = cache_deserialize(c, from_store, len);
+	free(from_store);
+	if (ret < 0) {
+		RERROR("%s", fr_strerror());
+		talloc_free(c);
+		return CACHE_ERROR;
 	}
 	c->key = talloc_strdup(c, key);
 	*out = c;
-
-	free(from_store);
 
 	return CACHE_OK;
 }
@@ -297,96 +219,32 @@ static cache_status_t cache_entry_insert(UNUSED rlm_cache_t *inst, REQUEST *requ
 	rlm_cache_memcached_handle_t *mandle = *handle;
 
 	memcached_return_t ret;
-	cache_status_t rcode;
 
-	TALLOC_CTX *pairs = NULL;
-	TALLOC_CTX *store = NULL;
+	TALLOC_CTX *pool;
+	char *to_store;
 
-	vp_cursor_t cursor;
-	VALUE_PAIR *vp;
+	pool = talloc_pool(NULL, 1024);
+	if (!pool) return CACHE_ERROR;
 
-	char *to_store = NULL, *pair;
+	if (cache_serialize(pool, &to_store, c) < 0) {
+		talloc_free(pool);
 
-	store = talloc_pool(request, 1024);
-	if (!store) goto error;
-
-	to_store = talloc_asprintf(store, "&Cache-Expires = %" PRIu64 "\n&Cache-Created = %" PRIu64 "\n",
-				   (uint64_t)c->expires, (uint64_t)c->created);
-	if (!to_store) goto error;
-
-	/*
-	 *	It's valid to have an empty cache entry (save allocing the
-	 *	pairs pool)
-	 */
-	if (!c->control && !c->packet && !c->reply) goto insert;
-
-	/*
-	 *  In the majority of cases using these pools reduces the number of mallocs
-	 *  to two, except in the case where the total serialized pairs length is
-	 *  greater than the pairs pool, or the total serialized string is greater
-	 *  than the store pool.
-	 */
-	pairs = talloc_pool(request, 512);
-	if (!pairs) {
-	error:
-		rcode = CACHE_ERROR;
-		goto finish;
+		return CACHE_ERROR;
 	}
 
-	if (c->control) {
-		for (vp = fr_cursor_init(&cursor, &c->control);
-		     vp;
-		     vp = fr_cursor_next(&cursor)) {
-			pair = vp_aprints(pairs, vp, '\'');
-			if (!pair) goto error;
-
-			to_store = talloc_asprintf_append_buffer(to_store, "&control:%s\n", pair);
-			if (!to_store) goto error;
-		}
-	}
-
-	if (c->packet) {
-		for (vp = fr_cursor_init(&cursor, &c->packet);
-		     vp;
-		     vp = fr_cursor_next(&cursor)) {
-			pair = vp_aprints(pairs, vp, '\'');
-			if (!pair) goto error;
-
-			to_store = talloc_asprintf_append_buffer(to_store, "&%s\n", pair);
-			if (!to_store) goto error;
-		}
-	}
-
-	if (c->reply) {
-		for (vp = fr_cursor_init(&cursor, &c->reply);
-		     vp;
-		     vp = fr_cursor_next(&cursor)) {
-			pair = vp_aprints(pairs, vp, '\'');
-			if (!pair) goto error;
-
-			to_store = talloc_asprintf_append_buffer(to_store, "&reply:%s\n", pair);
-			if (!to_store) goto error;
-		}
-	}
-
-insert:
 	ret = memcached_set(mandle->handle, c->key, talloc_array_length(c->key) - 1,
 		            to_store ? to_store : "",
 		            to_store ? talloc_array_length(to_store) - 1 : 0, c->expires, 0);
 	if (ret != MEMCACHED_SUCCESS) {
 		RERROR("Failed storing entry with key \"%s\": %s: %s", c->key, memcached_strerror(mandle->handle, ret),
 		       memcached_last_error_message(mandle->handle));
+		talloc_free(pool);
 
-		goto error;
+		return CACHE_ERROR;
 	}
-	rcode = CACHE_OK;
+	talloc_free(pool);
 
-finish:
-	talloc_free(pairs);
-	talloc_free(store);
-
-	return rcode;
-
+	return CACHE_OK;
 }
 
 /** Call delete the cache entry from memcached
