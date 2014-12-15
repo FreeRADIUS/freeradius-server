e41705c28e7f8d01c4f368b3c17627cccd570df1
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-12-15 15:24:27 -0500

    Coverity CID #1258569

--- src/modules/rlm_cache/rlm_cache.c
+++ src/modules/rlm_cache/rlm_cache.c
@@ -588,7 +588,6 @@ static ssize_t cache_xlat(void *instance, REQUEST *request,
 		vps = c->control;
 		break;
 
-	case PAIR_LIST_UNKNOWN:
 	default:
 		REDEBUG("Unsupported list \"%s\"", fr_int2str(pair_lists, list, "<UNKNOWN>"));
 		ret = -1;
