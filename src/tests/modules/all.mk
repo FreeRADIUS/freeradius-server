de3b172ef5fc023492df49d5faae04ea6786fb2d
Alan T. DeKok <aland@freeradius.org>
2014-12-22 12:06:15 -0500

    Ensure framework includes drivers, too

--- src/tests/modules/all.mk
+++ src/tests/modules/all.mk
@@ -11,17 +11,31 @@ TEST_TARGETS := $(foreach x,$(TEST_SUBDIRS),$(findstring rlm_$x.la,$(ALL_TGTS)))
 TEST_BUILT := $(patsubst rlm_%.la,%,$(TEST_TARGETS))
 
 #
-#  For the remaining ones, add on the directory to include.
-#
-SUBMAKEFILES := $(addsuffix /all.mk,$(TEST_BUILT))
-
-#
 #  Ensure that the tests depend on the module, so that changes to the
 #  module will re-run the test
 #
 $(foreach x,$(TEST_BUILT),$(eval $x.test: rlm_$x.la))
 
+######################################################################
+
+#
+#  And do the same thing for sub-directories
+#
+TEST_SUBSUBDIRS := $(patsubst src/tests/modules/%/all.mk,%,$(wildcard src/tests/modules/*/*/all.mk))
+
+TEST_SUBTARGETS := $(foreach x,$(TEST_SUBSUBDIRS),$(findstring rlm_$(subst /,_,$x).la,$(ALL_TGTS)))
+
+TEST_SUBBUILT := $(patsubst rlm_%.la,%,$(TEST_SUBTARGETS))
+
+$(foreach x,$(TEST_SUBBUILT),$(eval $x.test: rlm_$(subst /,_,$x).la))
+
+######################################################################
+#
+#  For the remaining subdirs, add on the directory to include.
+#
+SUBMAKEFILES := $(addsuffix /all.mk,$(TEST_BUILT) $(subst _,/,$(TEST_SUBBUILT)))
+
 #
 #  Add the module tests to the overall dependencies
 #
-tests.modules: tests.unit tests.keywords tests.auth $(patsubst %,%.test,$(TEST_BUILT))
+tests.modules: tests.unit tests.keywords tests.auth $(patsubst %,%.test,$(TEST_BUILT) $(TEST_SUBBUILT))
