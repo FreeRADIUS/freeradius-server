e4badac1a1b4189b3af9e8a70bcc89a88e8891cc
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-12-30 15:39:14 -0500

    Duplicate the pair's filename and line number too in cf_pair_dup

--- src/main/conffile.c
+++ src/main/conffile.c
@@ -342,11 +342,16 @@ CONF_PAIR *cf_pair_alloc(CONF_SECTION *parent, char const *attr, char const *val
  */
 CONF_PAIR *cf_pair_dup(CONF_SECTION *parent, CONF_PAIR *cp)
 {
+	CONF_PAIR *new;
+
 	rad_assert(parent);
 	rad_assert(cp);
 
-	return cf_pair_alloc(parent, cf_pair_attr(cp), cf_pair_value(cp),
-			     cf_pair_operator(cp), cf_pair_value_type(cp));
+	new = cf_pair_alloc(parent, cf_pair_attr(cp), cf_pair_value(cp), cf_pair_operator(cp), cf_pair_value_type(cp));
+	new->item.lineno = cp->item.lineno;
+	new->item.filename = talloc_strdup(cp, cp->item.filename);
+
+	return new;
 }
 
 /** Add a configuration pair to a section
