4d3dca630b157357a8ce1a38b9afa549819f648d
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2014-12-31 13:52:45 -0500

    Remove cf_template_copy and replace with cf_section_dup
    Has side effect of fixing infinite loop that caused the server to hang when processing templates with nested sections

--- src/include/conffile.h
+++ src/include/conffile.h
@@ -159,7 +159,8 @@ CONF_PAIR	*cf_pair_dup(CONF_SECTION *parent, CONF_PAIR *cp);
 void		cf_pair_add(CONF_SECTION *parent, CONF_PAIR *cp);
 
 CONF_SECTION	*cf_section_alloc(CONF_SECTION *parent, char const *name1, char const *name2);
-CONF_SECTION	*cf_section_dup(CONF_SECTION *parent, CONF_SECTION const *cs, char const *name1, char const *name2);
+CONF_SECTION	*cf_section_dup(CONF_SECTION *parent, CONF_SECTION const *cs,
+				char const *name1, char const *name2, bool copy_meta);
 void		cf_section_add(CONF_SECTION *parent, CONF_SECTION *cs);
 int		cf_pair_replace(CONF_SECTION *cs, CONF_PAIR *cp, char const *value);
 int		cf_item_parse(CONF_SECTION *cs, char const *name, int type, void *data, char const *dflt);
