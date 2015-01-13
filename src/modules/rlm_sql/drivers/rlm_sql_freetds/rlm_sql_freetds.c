e7635e0e9458d743e151b30f77424a9a792a8316
Alan T. DeKok <aland@freeradius.org>
2015-01-12 22:51:26 -0500

    s/if/of/ in the GPL license

--- src/modules/rlm_sql/drivers/rlm_sql_freetds/rlm_sql_freetds.c
+++ src/modules/rlm_sql/drivers/rlm_sql_freetds/rlm_sql_freetds.c
@@ -1,6 +1,6 @@
  /*
  *   This program is is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License, version 2 if the
+ *   it under the terms of the GNU General Public License, version 2 of the
  *   License as published by the Free Software Foundation.
  *
  *   This program is distributed in the hope that it will be useful,
