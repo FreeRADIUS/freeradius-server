5a0933dfaf22bb98a4296000bb46d35683045202
Alan T. DeKok <aland@freeradius.org>
2015-01-12 22:51:26 -0500

    s/if/of/ in the GPL license

--- src/include/pcap.h
+++ src/include/pcap.h
@@ -3,7 +3,7 @@
 #ifdef HAVE_LIBPCAP
 /*
  *   This program is is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License, version 2 if the
+ *   it under the terms of the GNU General Public License, version 2 of the
  *   License as published by the Free Software Foundation.
  *
  *   This program is distributed in the hope that it will be useful,
