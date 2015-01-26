245920295aa932f0597260117ca1f91a73da982d
Alan T. DeKok <aland@freeradius.org>
2015-01-26 16:28:35 -0500

    Remove unneeded assertion.

--- src/main/tmpl.c
+++ src/main/tmpl.c
@@ -1545,7 +1545,6 @@ ssize_t tmpl_expand(char const **out, char *buff, size_t bufflen, REQUEST *reque
 		/* Error in expansion, this is distinct from zero length expansion */
 		slen = radius_xlat_struct(buff, bufflen, request, vpt->tmpl_xlat, escape, escape_ctx);
 		if (slen < 0) {
-			rad_assert(!*out);
 			return slen;
 		}
 		slen = strlen(buff);
