ff526ad4ad985b94dc6fd30ee5bbce21f1f66e8e
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2015-01-30 12:54:39 +0200

    dhcp: Use correct format specifiers in a message
    Format size_t with %zu specifier, instead of %d, as size_t is not
    guaranteed to be the same size as int.
    
    This fixes the following compiler warnings:
    
    freeradius-server-2.2.6/src/lib/dhcp.c: scope_hint: In function 'fr_dhcp_add_arp_entry'
    freeradius-server-2.2.6/src/lib/dhcp.c:1536: warning: format '%d' expects type 'int', but argument 2 has type 'long unsigned int'
    freeradius-server-2.2.6/src/lib/dhcp.c:1536: warning: format '%d' expects type 'int', but argument 3 has type 'size_t'

--- src/lib/dhcp.c
+++ src/lib/dhcp.c
@@ -1530,8 +1530,8 @@ int fr_dhcp_add_arp_entry(int fd, const char *interface,
 	}
 
 	if (macaddr->length > sizeof (req.arp_ha.sa_data)) {
-		fr_strerror_printf("ERROR: DHCP only supports up to %d octets for "
-				   "Client Hardware Address (got %d octets)\n",
+		fr_strerror_printf("ERROR: DHCP only supports up to %zu octets for "
+				   "Client Hardware Address (got %zu octets)\n",
 				   sizeof(req.arp_ha.sa_data),
 				   macaddr->length);
 		return -1;
