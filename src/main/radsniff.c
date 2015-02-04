2d5c09064fbcbba4d2c3c92f05efec0ab0696470
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-02-04 18:48:38 +0700

    Use C99 way of declaring a variable length field

--- src/main/radsniff.c
+++ src/main/radsniff.c
@@ -1060,7 +1060,7 @@ static void rs_packet_process(uint64_t count, rs_event_t *event, struct pcap_pkt
 	/*
 	 *	End of variable length bits, do basic check now to see if packet looks long enough
 	 */
-	len = (p - data) + sizeof(udp_header_t) + (sizeof(radius_packet_t) - 1);	/* length value */
+	len = (p - data) + sizeof(udp_header_t) + sizeof(radius_packet_t);	/* length value */
 	if ((size_t) len > header->caplen) {
 		REDEBUG("Packet too small, we require at least %zu bytes, captured %i bytes",
 			(size_t) len, header->caplen);
