b6ea5d9047a445890e7ca3e9bae6e9403f9a9738
Alan T. DeKok <aland@freeradius.org>
2015-02-13 09:10:45 -0500

    use correct assert macro

--- src/main/radsniff.c
+++ src/main/radsniff.c
@@ -854,7 +854,7 @@ static void rs_packet_cleanup(rs_request_t *request)
 	 */
 	if (!request->silent_cleanup) {
 		if (!request->linked) {
-			rad_assert(request && request->stats_req);
+			RS_ASSERT(request && request->stats_req);
 			request->stats_req->interval.lost_total++;
 
 			if (conf->event_flags & RS_LOST) {
