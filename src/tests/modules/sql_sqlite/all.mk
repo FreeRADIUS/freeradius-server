ce53100fb77fced17766a0a8e58406ac37e12f25
Alan T. DeKok <aland@freeradius.org>
2015-02-19 11:47:52 -0500

    Manually order sqlite tests
    So we can do "make -j 4 tests.modules"

--- src/tests/modules/sql_sqlite/all.mk
+++ src/tests/modules/sql_sqlite/all.mk
@@ -5,3 +5,14 @@
 #  MODULE.test is the main target for this module.
 sql_sqlite.test:
 	@echo OK: sql_sqlite.test
+
+SQLITE_TESTDIR := $(BUILD_DIR)/tests/modules/sql_sqlite
+
+$(SQLITE_TESTDIR)/acct_update_no_start: $(SQLITE_TESTDIR)/acct_start_conflict
+
+$(SQLITE_TESTDIR)/acct_start_conflict: $(SQLITE_TESTDIR)/acct_2_stop
+
+$(SQLITE_TESTDIR)/acct_2_stop: $(SQLITE_TESTDIR)/acct_1_update
+
+$(SQLITE_TESTDIR)/acct_1_update: $(SQLITE_TESTDIR)/acct_0_start
+
