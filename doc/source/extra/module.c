fb3d16c986ccb5e54c4bfb6fa67624398b00d99f
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-02-22 12:11:38 -0500

    Change 'public_module_api' group to 'module_safe'

--- doc/source/extra/module.c
+++ doc/source/extra/module.c
@@ -6,12 +6,12 @@
  *
 @page module_doc
 
-@defgroup public_module_api Functions that are guaranteed to be consistent between minor versions, suitable for use by third party modules.
-
 @section module_intro Introduction
 
 FreeRADIUS uses a pluggable module infrastructure to extend the
 basic functionality of the server.
 
 Modules in 3.0 are dynamically linked at runtime using dlopen.
+
+@defgroup module_safe module_safe - Internal API functions that are safe for use by modules
 */
