b116bafa5366d4c8d171f45c3947c4521ecc18fb
Alan T. DeKok <aland@freeradius.org>
2015-02-24 14:08:51 -0500

    Free ctx on error

--- src/main/listen.c
+++ src/main/listen.c
@@ -1494,6 +1494,7 @@ static int auth_socket_recv(rad_listen_t *listener)
 	 */
 	packet = rad_recv(ctx, listener->fd, client->message_authenticator);
 	if (!packet) {
+		talloc_free(ctx);
 		FR_STATS_INC(auth, total_malformed_requests);
 		DEBUG("%s", fr_strerror());
 		return 0;
@@ -1525,7 +1526,7 @@ static int auth_socket_recv(rad_listen_t *listener)
 
 	if (!request_receive(ctx, listener, packet, client, fun)) {
 		FR_STATS_INC(auth, total_packets_dropped);
-		rad_free(&packet);
+		talloc_free(ctx);
 		return 0;
 	}
 
