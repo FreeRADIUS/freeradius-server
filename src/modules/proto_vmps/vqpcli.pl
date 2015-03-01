52b517d413e5f8124ef8aa1e75d5aac5024bc53d
Alan T. DeKok <aland@freeradius.org>
2015-03-01 10:03:51 -0500

    Add license

--- src/modules/proto_vmps/vqpcli.pl
+++ src/modules/proto_vmps/vqpcli.pl
@@ -1,4 +1,22 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
+#
+# vqpcli.pl -s localhost -v mydomain -w 10.0.0.1 -i 2/4 -m 0010.a49f.30e3
+#
 
 use Socket;
 $|=0;
