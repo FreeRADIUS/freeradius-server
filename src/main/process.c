eeccdca5058ef34d592d36aad223f0cdaaaeb745
Alan T. DeKok <aland@freeradius.org>
2015-03-13 13:19:27 -0400

    initialize child state before printing it

--- src/main/process.c
+++ src/main/process.c
@@ -1816,6 +1816,7 @@ static REQUEST *request_setup(TALLOC_CTX *ctx, rad_listen_t *listener, RADIUS_PA
 	}
 
 	request->master_state = REQUEST_ACTIVE;
+	request->child_state = REQUEST_RUNNING;
 #ifdef DEBUG_STATE_MACHINE
 	if (debug_flag) printf("(%u) ********\tSTATE %s C-%s -> C-%s\t********\n",
 			       request->number, __FUNCTION__,
@@ -1825,7 +1826,6 @@ static REQUEST *request_setup(TALLOC_CTX *ctx, rad_listen_t *listener, RADIUS_PA
 #ifdef HAVE_PTHREAD_H
 	request->child_pid = NO_SUCH_CHILD_PID;
 #endif
-	request->child_state = REQUEST_RUNNING;
 	request->handle = fun;
 	NO_CHILD_THREAD;
 
