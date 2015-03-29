9a7b78154d9acb7694a4070deb15d2c4eae42ee6
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-03-29 10:19:48 -0400

    Clang scan analyzer errors

--- src/main/command.c
+++ src/main/command.c
@@ -543,7 +543,6 @@ static int fr_server_domain_socket_perm(char const *path, uid_t uid, gid_t gid)
 		if (uid != (uid_t)-1) rad_seuid(euid);
 		if (gid != (gid_t)-1) rad_segid(egid);
 		close(sock_fd);
-		sock_fd = -1;
 
 		goto error;
 	}
