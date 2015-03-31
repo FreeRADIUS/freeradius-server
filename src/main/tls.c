93076379bf91ba414f210dd7c0068b860484e256
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-03-31 12:24:51 -0400

    Add version strings for TLS 1.1, 1.2 and 1.3

--- src/main/tls.c
+++ src/main/tls.c
@@ -711,7 +711,7 @@ void tls_session_information(tls_session_t *tls_session)
 
 	switch (tls_session->info.version) {
 	case SSL2_VERSION:
-		str_version = "SSL 2.0";
+		str_version = "SSL 2.0 ";
 		break;
 	case SSL3_VERSION:
 		str_version = "SSL 3.0 ";
@@ -719,6 +719,22 @@ void tls_session_information(tls_session_t *tls_session)
 	case TLS1_VERSION:
 		str_version = "TLS 1.0 ";
 		break;
+#ifdef TLS1_1_VERSION
+	case TLS1_1_VERSION:
+		str_version = "TLS 1.1 ";
+		break;
+#endif
+#ifdef TLS1_2_VERSION
+	case TLS1_2_VERSION:
+		str_version = "TLS 1.2 ";
+		break;
+#endif
+#ifdef TLS1_3_VERSON
+	case TLS1_3_VERSION:
+		str_version = "TLS 1.3 ";
+		break;
+#endif
+
 	default:
 		str_version = "Unknown TLS version";
 		break;
