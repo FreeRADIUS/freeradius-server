d7b5c2f90ad8c7f6beb240fe2be43139e44418ca
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-04-08 11:13:24 -0400

    Doxygen fixups

--- src/include/log.h
+++ src/include/log.h
@@ -46,7 +46,7 @@ typedef enum log_type {
 } log_type_t;
 
 typedef enum log_lvl {
-	L_DBG_LVL_MIN = -1,	//!< Hack for stupid GCC warnings (comparison with 0 always true)
+	L_DBG_LVL_DISABLE = -1,	//!< Don't print messages.
 	L_DBG_LVL_OFF = 0,	//!< No debug messages.
 	L_DBG_LVL_1,		//!< Highest priority debug messages (-x).
 	L_DBG_LVL_2,		//!< 2nd highest priority debug messages (-xx | -X).
