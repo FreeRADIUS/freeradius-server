02d6494b98fd10076e9ea588905caf6cc0c18a53
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-04-23 00:31:40 +0100

    Fix mismatches between CONF_PARSER structs and the default config

--- src/modules/rlm_eap/rlm_eap.c
+++ src/modules/rlm_eap/rlm_eap.c
@@ -34,7 +34,7 @@ static const CONF_PARSER module_config[] = {
 	{ "default_eap_type", FR_CONF_OFFSET(PW_TYPE_STRING, rlm_eap_t, default_method_name), "md5" },
 	{ "timer_expire", FR_CONF_OFFSET(PW_TYPE_INTEGER, rlm_eap_t, timer_limit), "60" },
 	{ "ignore_unknown_eap_types", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_eap_t, ignore_unknown_types), "no" },
-	{ "mod_accounting_username_bug", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_eap_t, mod_accounting_username_bug), "no" },
+	{ "cisco_accounting_username_bug", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_eap_t, mod_accounting_username_bug), "no" },
 	{ "max_sessions", FR_CONF_OFFSET(PW_TYPE_INTEGER, rlm_eap_t, max_sessions), "2048" },
 
 	{ NULL, -1, 0, NULL, NULL }	   /* end the list */
