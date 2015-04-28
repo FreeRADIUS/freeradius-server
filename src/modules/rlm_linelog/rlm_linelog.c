119afaaa5f733a1d40f15e3796cb345165ebedc6
Alan T. DeKok <aland@freeradius.org>
2015-04-28 08:22:16 -0400

    Make locking configurable, and re-enable detail "locking"

--- src/modules/rlm_linelog/rlm_linelog.c
+++ src/modules/rlm_linelog/rlm_linelog.c
@@ -358,7 +358,7 @@ static int mod_instantiate(CONF_SECTION *conf, void *instance)
 			return -1;
 		}
 
-		inst->file.ef = exfile_init(inst, 64, 30);
+		inst->file.ef = exfile_init(inst, 64, 30, true);
 		if (!inst->file.ef) {
 			cf_log_err_cs(conf, "Failed creating log file context");
 			return -1;
