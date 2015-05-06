41437f39a446cc6a470005916178817af90dff81
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-05-05 22:13:11 -0400

    Not every driver provides sql_num_rows

--- src/modules/rlm_sqlhpwippool/rlm_sqlhpwippool.c
+++ src/modules/rlm_sqlhpwippool/rlm_sqlhpwippool.c
@@ -162,7 +162,7 @@ static int nvp_select(unsigned int line, rlm_sqlhpwippool_t *data,
 		return 0;
 	}
 
-	if ((data->db->sql_num_rows)(sqlsock, data->sql_inst->config) < 1) {
+	if (data->db->sql_num_rows && ((data->db->sql_num_rows)(sqlsock, data->sql_inst->config) < 1)) {
 		nvp_log(__LINE__, data, L_DBG,
 			"nvp_select(): no results in query from line %u", line);
 		return -1;
