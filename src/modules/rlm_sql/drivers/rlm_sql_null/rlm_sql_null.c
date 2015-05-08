b6c063633824c09c4ebe7f8732ff8cbd0a69fe1e
Jorge Pereira <jpereiran@gmail.com>
2015-05-07 21:42:32 -0300

    Add sql_fields function for rlm_sql_null refs #993

--- src/modules/rlm_sql/drivers/rlm_sql_null/rlm_sql_null.c
+++ src/modules/rlm_sql/drivers/rlm_sql_null/rlm_sql_null.c
@@ -54,6 +54,12 @@ static int sql_num_fields(UNUSED rlm_sql_handle_t * handle, UNUSED rlm_sql_confi
 	return 0;
 }
 
+static sql_rcode_t sql_fields(UNUSED char const **out[], UNUSED rlm_sql_handle_t *handle,
+					UNUSED rlm_sql_config_t *config)
+{
+	return 0;
+}
+
 static sql_rcode_t sql_select_query(UNUSED rlm_sql_handle_t *handle,
 				    UNUSED rlm_sql_config_t *config, UNUSED char const *query)
 {
@@ -112,6 +118,7 @@ rlm_sql_module_t rlm_sql_null = {
 	.sql_store_result		= sql_store_result,
 	.sql_num_fields			= sql_num_fields,
 	.sql_num_rows			= sql_num_rows,
+	.sql_fields			= sql_fields,
 	.sql_fetch_row			= sql_fetch_row,
 	.sql_free_result		= sql_free_result,
 	.sql_error			= sql_error,
