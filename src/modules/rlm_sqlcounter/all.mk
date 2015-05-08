f2fecba27a2c27f12d9fff68f753230d45905d7a
Alan T. DeKok <aland@freeradius.org>
2015-05-08 15:36:19 -0400

    Depend on rlm_sql make file, instead of re-running configure

---
+++ src/modules/rlm_sqlcounter/all.mk
@@ -0,0 +1,14 @@
+-include src/modules/rlm_sql/all.mk
+
+ifneq "${TARGETNAME}" ""
+  TARGETNAME	:= rlm_sqlcounter
+  TARGET	:= $(TARGETNAME).a
+
+  # Be sure to NOT include the rlm_sql drivers
+  SUBMAKEFILES	:= 
+endif
+
+SOURCES		:= $(TARGETNAME).c
+
+SRC_CFLAGS	:= $(rlm_sql_CFLAGS) -I$(top_builddir)/src/modules/rlm_sql
+TGT_LDLIBS	:= $(rlm_sql_LDLIBS)
