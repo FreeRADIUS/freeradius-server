2b77b7e830222d0192f42efe66cae38f061aa34c
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-05-20 12:19:06 -0400

    Commit mk files for sql modules

---
+++ src/modules/rlm_sqlhpwippool/all.mk
@@ -0,0 +1,11 @@
+TARGETNAME	:= rlm_sqlhpwippool
+
+ifneq "$(TARGETNAME)" ""
+TARGET		:= $(TARGETNAME).a
+endif
+
+SOURCES		:= $(TARGETNAME).c
+
+SRC_CFLAGS	:= 
+SRC_CFLAGS	+= -I$(top_builddir)/src/modules/rlm_sql
+TGT_LDLIBS	:= 
