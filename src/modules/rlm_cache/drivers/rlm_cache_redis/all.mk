91f2b988bba9d99192c6ba30ba8bb11564d2fb7f
Alan T. DeKok <aland@freeradius.org>
2015-05-20 18:27:29 -0400

    Use the correct path to the libfreeradius-redis make file

--- src/modules/rlm_cache/drivers/rlm_cache_redis/all.mk
+++ src/modules/rlm_cache/drivers/rlm_cache_redis/all.mk
@@ -1,11 +1,11 @@
--include libfreeradius-redis.mk
+-include $(top_builddir)/src/modules/rlm_redis/libfreeradius-redis.mk
 
 ifneq "${TARGETNAME}" ""
   TARGETNAME	:= rlm_cache_redis
-  TARGET	    := $(TARGETNAME).a
+  TARGET	:= $(TARGETNAME).a
 endif
 
-SOURCES		    := $(TARGETNAME).c
+SOURCES		:= $(TARGETNAME).c
 
 SRC_CFLAGS	+= -I$(top_builddir)/src/modules/rlm_redis
 TGT_PREREQS	:= libfreeradius-redis.a
