1e9a1e8212e8abc0f28e7238cad252110f538619
Alan T. DeKok <aland@freeradius.org>
2015-06-03 11:24:30 -0400

    Fix LGPL text broken by 1af9582b2

--- src/lib/udpfromto.c
+++ src/lib/udpfromto.c
@@ -1,8 +1,7 @@
 /*
  *   This library is free software; you can redistribute it and/or
  *   modify it under the terms of the GNU Lesser General Public
- *   the Free Software Foundation; either version 2 of the License, or (at
- *   your option) any later version. either
+ *   License as published by the Free Software Foundation; either
  *   version 2.1 of the License, or (at your option) any later version.
  *
  *   This library is distributed in the hope that it will be useful,
