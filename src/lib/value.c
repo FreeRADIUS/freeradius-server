420f1b6ac204d55de1e4bc092be6c16d340a220e
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-06-17 13:18:41 -0400

    Allow casting from date to 64bit integer

--- src/lib/value.c
+++ src/lib/value.c
@@ -1083,6 +1083,10 @@ int value_data_cast(TALLOC_CTX *ctx, value_data_t *dst,
 			dst->integer64 = src->integer;
 			break;
 
+		case PW_TYPE_DATE:
+			dst->integer64 = src->date;
+			break;
+
 		case PW_TYPE_OCTETS:
 			goto do_octets;
 
