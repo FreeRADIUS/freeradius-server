13a0170e4e9fb14db42e5ec4992bc4c0b7a91631
Alan T. DeKok <aland@freeradius.org>
2015-06-26 14:03:04 -0400

    Use the %-SQL-Group

--- src/modules/rlm_sql/rlm_sql.c
+++ src/modules/rlm_sql/rlm_sql.c
@@ -846,9 +846,9 @@ static rlm_rcode_t rlm_sql_process_groups(rlm_sql_t *inst, REQUEST *request, rlm
 	 *	Add the Sql-Group attribute to the request list so we know
 	 *	which group we're retrieving attributes for
 	 */
-	sql_group = pairmake_packet("Sql-Group", NULL, T_OP_EQ);
+	sql_group = pairmake_packet(inst->group_da->name, NULL, T_OP_EQ);
 	if (!sql_group) {
-		REDEBUG("Error creating Sql-Group attribute");
+		REDEBUG("Error creating %s attribute", inst->group_da->name);
 		rcode = RLM_MODULE_FAIL;
 		goto finish;
 	}
