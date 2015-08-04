934bfb505edd5a1d8424dee14b609264297bac89
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-08-03 22:28:34 -0400

    json_object_get_string_len should operate on output of json_object_get_string

--- src/modules/rlm_couchbase/jsonc_missing.c
+++ src/modules/rlm_couchbase/jsonc_missing.c
@@ -40,7 +40,7 @@ RCSID("$Id$")
 int json_object_get_string_len(json_object *obj) {
 	if (json_object_get_type(obj) != json_type_string)
 		return 0;
-	return (int)strlen(json_object_to_json_string(obj));
+	return (int)strlen(json_object_get_string(obj));
 }
 #endif
 
