8cc57b205d882cd9566b04dd071fd486df1c68c1
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-10-02 01:15:43 -0400

    Content type macros are too new for travis...

--- src/main/tls.c
+++ src/main/tls.c
@@ -785,49 +785,71 @@ void tls_session_information(tls_session_t *tls_session)
 		str_content_type = "heartbeat ";
 		break;
 
+#ifdef TLS1_RT_CRYPTO
 	case TLS1_RT_CRYPTO:
 		str_content_type = "crypto ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_PREMASTER
 	case TLS1_RT_CRYPTO_PREMASTER:
 		str_content_type = "crypto_premaster ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_CLIENT_RANDOM
 	case TLS1_RT_CRYPTO_CLIENT_RANDOM:
 		str_content_type = "client_random ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_SERVER_RANDOM
 	case TLS1_RT_CRYPTO_SERVER_RANDOM:
 		str_content_type = "server_random ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_MASTER
 	case TLS1_RT_CRYPTO_MASTER:
 		str_content_type = "crypto_master ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_READ
 	case TLS1_RT_CRYPTO_READ:
 		str_content_type = "crypto_read ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_WRITE
 	case TLS1_RT_CRYPTO_WRITE:
 		str_content_type = "crypto_write ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_MAC
 	case TLS1_RT_CRYPTO_MAC:
 		str_content_type = "crypto_mac ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_KEY
 	case TLS1_RT_CRYPTO_KEY:
 		str_content_type = "crypto_key ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_IV
 	case TLS1_RT_CRYPTO_IV:
 		str_content_type = "crypto_iv ";
 		break;
+#endif
 
+#ifdef TLS1_RT_CRYPTO_FIXED_IV
 	case TLS1_RT_CRYPTO_FIXED_IV:
 		str_content_type = "crypto_fixed_iv ";
 		break;
+#endif
 
 	default:
 	{
