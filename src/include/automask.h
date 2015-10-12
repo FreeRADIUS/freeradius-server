c4ddd60b85a761f419d81c9e6c9cda8f18959d70
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-10-12 00:14:10 -0400

    Formatting

--- src/include/automask.h
+++ src/include/automask.h
@@ -11,11 +11,11 @@
  *  AC_INIT() was called with full arguments.
  */
 #ifdef IS_MODULE
-#	undef PACKAGE_BUGREPORT
-#	undef PACKAGE_NAME
-#	undef PACKAGE_STRING
-#	undef PACKAGE_TARNAME
-#	undef PACKAGE_URL
-#	undef PACKAGE_VERSION
+#  undef PACKAGE_BUGREPORT
+#  undef PACKAGE_NAME
+#  undef PACKAGE_STRING
+#  undef PACKAGE_TARNAME
+#  undef PACKAGE_URL
+#  undef PACKAGE_VERSION
 #endif
 
