4ad6c60117008d8ff53b57ba29a2d296f359db84
Jorge Pereira <jpereiran@gmail.com>
2015-10-23 16:35:46 -0200

    Bugfix - increase a wrong stats-list

--- src/main/stats.c
+++ src/main/stats.c
@@ -330,7 +330,7 @@ void request_stats_final(REQUEST *request)
 	case PW_CODE_COA_NAK:
 		proxy_coa_stats.total_responses++;
 		request->home_server->stats.total_responses++;
-		stats_time(&proxy_acct_stats,
+		stats_time(&proxy_coa_stats,
 			   &request->proxy->timestamp,
 			   &request->proxy_reply->timestamp);
 		stats_time(&request->home_server->stats,
@@ -340,9 +340,9 @@ void request_stats_final(REQUEST *request)
 
 	case PW_CODE_DISCONNECT_ACK:
 	case PW_CODE_DISCONNECT_NAK:
-		proxy_coa_stats.total_responses++;
+		proxy_dsc_stats.total_responses++;
 		request->home_server->stats.total_responses++;
-		stats_time(&proxy_acct_stats,
+		stats_time(&proxy_dsc_stats,
 			   &request->proxy->timestamp,
 			   &request->proxy_reply->timestamp);
 		stats_time(&request->home_server->stats,
