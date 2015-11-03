84e29459765f3cfbbb70cffa150d40cb90129c22
Alan T. DeKok <aland@freeradius.org>
2015-11-03 10:15:25 -0500

    Remove unused variable

--- src/main/unittest.c
+++ src/main/unittest.c
@@ -652,7 +652,6 @@ int main(int argc, char *argv[])
 	VALUE_PAIR *vp;
 	VALUE_PAIR *filter_vps = NULL;
 	bool xlat_only = false;
-	char *p;
 	fr_state_tree_t *state = NULL;
 
 	fr_talloc_fault_setup();
@@ -667,13 +666,6 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 #endif
-
-	p = strrchr(argv[0], FR_DIR_SEP);
-	if (!p) {
-		main_config.name = argv[0];
-	} else {
-		main_config.name = p + 1;
-	}
 
 	rad_debug_lvl = 0;
 	set_radius_dir(NULL, RADIUS_DIR);
