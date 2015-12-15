f3fd8d5b112afe5fe5885092cbb8b58d57f6f2ee
Philippe Wooding <philippe.wooding@networkradius.com>
2015-12-15 10:02:14 +0100

    After removing a vp, if removed element was cursor->found it should be set to null, not current

--- src/lib/cursor.c
+++ src/lib/cursor.c
@@ -469,7 +469,7 @@ fixup:
 	/*
 	 *	Fixup cursor->found if we removed the VP it was referring to
 	 */
-	if (vp == cursor->found) cursor->found = cursor->current;
+	if (vp == cursor->found) cursor->found = NULL;
 
 	/*
 	 *	Fixup cursor->last if we removed the VP it was referring to
