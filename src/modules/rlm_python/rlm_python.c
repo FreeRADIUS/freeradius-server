fd9f0065965d1d02207d2b1876eaa5d309de23b3
Herwin Weststrate <herwin@snt.utwente.nl>
2015-12-17 20:00:25 +0100

    Show operator that is actually used instead of the default in rlm_python
    So if we remove a certain attribute, display "!* ANY" instead of "= ANY"

--- src/modules/rlm_python/rlm_python.c
+++ src/modules/rlm_python/rlm_python.c
@@ -364,9 +364,9 @@ static void mod_vptuple(TALLOC_CTX *ctx, REQUEST *request, VALUE_PAIR **vps, PyO
 
 		vp->op = op;
 		if (fr_pair_value_from_str(vp, s2, -1) < 0) {
-			DEBUG("rlm_python:%s: Failed: '%s:%s' = '%s'", funcname, list_name, s1, s2);
+			DEBUG("rlm_python:%s: Failed: '%s:%s' %s '%s'", funcname, list_name, s1, fr_int2str(fr_tokens_table, op, "="), s2);
 		} else {
-			DEBUG("rlm_python:%s: '%s:%s' = '%s'", funcname, list_name, s1, s2);
+			DEBUG("rlm_python:%s: '%s:%s' %s '%s'", funcname, list_name, s1, fr_int2str(fr_tokens_table, op, "="), s2);
 		}
 
 		radius_pairmove(current, vps, vp, false);
