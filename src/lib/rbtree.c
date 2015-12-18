c25d7628e14ffddfed4a0066498486fa14f95316
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2015-12-18 09:40:31 -0500

    Unused state conditional on pthreads, not debugging

--- src/lib/rbtree.c
+++ src/lib/rbtree.c
@@ -110,7 +110,11 @@ void rbtree_free(rbtree_t *tree)
 	talloc_unlink(tree, tree->parent);
 }
 
-static int _rbtree_free(NDEBUG_UNUSED rbtree_t *tree)
+#ifdef HAVE_PTHREAD_H
+static int _rbtree_free(rbtree_t *tree)
+#else
+static int _rbtree_free(UNUSED rbtree_t *tree)
+#endif
 {
 #ifdef HAVE_PTHREAD_H
 	if (tree->lock) pthread_mutex_destroy(&tree->mutex);
