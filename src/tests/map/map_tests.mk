33ed5387c3a2ff611c1b7ec1522fe10651900444
Alan T. DeKok <aland@freeradius.org>
2016-01-06 15:36:11 -0500

    Use jlibtool when running local binaries.
    It knows how to find the libraries

--- src/tests/map/map_tests.mk
+++ src/tests/map/map_tests.mk
@@ -1,6 +1,7 @@
 MAP_TESTS	:= $(patsubst $(top_srcdir)/src/tests/map/%,%,$(filter-out %.conf %.md %.attrs %.c %.mk %~ %.rej %.out,$(wildcard $(top_srcdir)/src/tests/map/*)))
 MAP_OUTPUT	:= $(addsuffix .out,$(addprefix $(BUILD_DIR)/tests/map/,$(MAP_TESTS)))
-MAP_UNIT	:= $(BUILD_DIR)/bin/local/map_unit
+MAP_UNIT_BIN	:= $(BUILD_DIR)/bin/local/map_unit
+MAP_UNIT	:= ./build/make/jlibtool --mode=execute $(MAP_UNIT_BIN)
 
 .PHONY: $(BUILD_DIR)/tests/map/
 $(BUILD_DIR)/tests/map/:
@@ -11,7 +12,7 @@ $(BUILD_DIR)/tests/map/:
 #
 #	Create the output directory before the files
 #
-$(MAP_OUTPUT): $(MAP_UNIT) | $(BUILD_DIR)/tests/map/
+$(MAP_OUTPUT): $(MAP_UNIT_BIN) | $(BUILD_DIR)/tests/map/
 
 #
 #	Re-run the tests if the input file changes
