a9d039751c9518cc63dd7dacece84720279f7b86
Matthew Newton <mcn4@leicester.ac.uk>
2016-01-14 16:29:02 +0000

    Update elasticsearch example files
     - update mappings to correctly set common attributes as "long" rather
       than "string"
     - logstash now creates useful sub-fields, as well as combining
       Gigawords/Octets to a single 64-bit value.
     - add example log-courier configuration
     - add dashboards for Kibana3 and Kibana4

--- doc/schemas/logstash/radius-mapping.sh
+++ doc/schemas/logstash/radius-mapping.sh
@@ -4,8 +4,34 @@
 # Matthew Newton
 # April 2015
 
-# This should be run on an elasticsearch node. Alternatively, adjust
-# the curl URI below.
+# This should be run on an elasticsearch node. Alternatively,
+# adjust the curl URI below.
+
+# The template will be called "radius", and will apply to all
+# indices prefixed with "radius-" that contain data type "detail".
+# As not all RADIUS attributes are known to begin with it has the
+# following starting point that can be modified to suit the local
+# configuration:
+#
+#   Acct-Input- or Acct-Output- attributes are numbers;
+#   Acct-Session-Time is a number;
+#   Everything else is a string.
+
+# Additionally, the supplied logstash config will try and extract
+# MAC addresses, IP addresses and ports from the data. These are
+# stored as sub-fields under the respective attribute. For
+# example, an attribute
+#
+#   Called-Station-Id := "10.0.4.6[4500]"
+#
+# will be broken down into the following fields in elasticsearch:
+#
+#   Called-Station-Id = "10.0.4.6[4500]"
+#   Called-Station-Id.ip = "10.0.4.6"
+#   Called-Station-Id.port = "4500"
+#
+# This mapping ensures that these have an appropriate data type.
+
 
 curl -XPUT '127.0.0.1:9200/_template/radius' -d '
 {
@@ -13,26 +39,56 @@ curl -XPUT '127.0.0.1:9200/_template/radius' -d '
   "order":0,
   "mappings":{
     "detail":{
-      "dynamic_templates":[
-        { "keep_message":{
-            "match":"message",
-            "mapping":{
-              "type":"string",
-              "index":"analyzed"
+
+      "properties": {
+        "@timestamp": { "format": "dateOptionalTime", "type": "date" },
+        "@version": { "type" : "string" },
+        "message": { "type" : "string" },
+        "Acct-Session-Time": { "type" : "long", "doc_values": true },
+        "offset": { "type" : "long", "doc_values": true }
+      },
+
+      "dynamic_templates": [
+
+        { "acct_io_numbers": {
+            "match_pattern": "regex",
+            "match": "^Acct-(Input|Output)-.*$",
+            "mapping": {
+              "type": "long",
+              "doc_values": true
             }
           }
         },
-        { "no_analyze_strings":{
-            "match":"*",
-            "match_mapping_type":"string",
-            "mapping":{
-              "type":"string",
-              "index":"not_analyzed"
+
+        { "ipv4_address": {
+            "path_match": "*.ip",
+            "mapping": {
+              "type": "ip",
+              "doc_values": true
+            }
+          }
+        },
+
+        { "network_port": {
+            "path_match": "*.port",
+            "mapping": {
+              "type": "integer",
+              "doc_values": true
+            }
+          }
+        },
+
+        { "no_analyze_strings": {
+            "match": "*",
+            "mapping": {
+              "type": "string",
+              "index": "not_analyzed",
+              "doc_values": true
             }
           }
         }
+
       ]
     }
   }
 }'
-
