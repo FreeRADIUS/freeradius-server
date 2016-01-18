a9f32a140ac62fb643483a40665afd761c455891
Matthew Newton <mcn4@leicester.ac.uk>
2016-01-18 17:41:49 +0000

    Update elasticsearch fields to use "_" rather than "."
    It seems that while elasticsearch can have data in a field as well as sub-fields, logstash
    can't currently do this (it stores the event in a hash, so an entry either contains data
    or another hash or list).
    
    Work around this by using underscores, which should in theory also mean these examples will
    work with elasticsearch 2, though I haven't tried it.

--- doc/schemas/logstash/radius-mapping.sh
+++ doc/schemas/logstash/radius-mapping.sh
@@ -19,16 +19,16 @@
 
 # Additionally, the supplied logstash config will try and extract
 # MAC addresses, IP addresses and ports from the data. These are
-# stored as sub-fields under the respective attribute. For
-# example, an attribute
+# stored with suffixes on the respective attribute. For example,
+# an attribute
 #
 #   Called-Station-Id := "10.0.4.6[4500]"
 #
 # will be broken down into the following fields in elasticsearch:
 #
 #   Called-Station-Id = "10.0.4.6[4500]"
-#   Called-Station-Id.ip = "10.0.4.6"
-#   Called-Station-Id.port = "4500"
+#   Called-Station-Id_ip = "10.0.4.6"
+#   Called-Station-Id_port = "4500"
 #
 # This mapping ensures that these have an appropriate data type.
 
@@ -61,7 +61,7 @@ curl -XPUT '127.0.0.1:9200/_template/radius' -d '
         },
 
         { "ipv4_address": {
-            "path_match": "*.ip",
+            "path_match": "*_ip",
             "mapping": {
               "type": "ip",
               "doc_values": true
@@ -70,7 +70,16 @@ curl -XPUT '127.0.0.1:9200/_template/radius' -d '
         },
 
         { "network_port": {
-            "path_match": "*.port",
+            "path_match": "*_port",
+            "mapping": {
+              "type": "integer",
+              "doc_values": true
+            }
+          }
+        },
+
+        { "long_number": {
+            "path_match": "*_long",
             "mapping": {
               "type": "integer",
               "doc_values": true
