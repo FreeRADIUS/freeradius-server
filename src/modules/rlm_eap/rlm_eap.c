e3b734caf77f3f81602280ca797719178ee8a679
Alan T. DeKok <aland@freeradius.org>
2016-02-06 21:07:33 -0500

    Highlight message a bit more.

--- src/modules/rlm_eap/rlm_eap.c
+++ src/modules/rlm_eap/rlm_eap.c
@@ -357,7 +357,7 @@ static rlm_rcode_t CC_HINT(nonnull) mod_authenticate(void *instance, REQUEST *re
 		 */
 		fr_pair_delete_by_num(&request->proxy->vps, PW_FREERADIUS_PROXIED_TO, VENDORPEC_FREERADIUS, TAG_ANY);
 
-		RDEBUG2("Tunneled session will be proxied.  Not doing EAP");
+		RWDEBUG2("Tunneled session will be proxied.  Not doing EAP");
 		return RLM_MODULE_HANDLED;
 	}
 #endif
