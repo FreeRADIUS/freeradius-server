2ae1d9975fef0f755b74b6451b070aa8ef494823
Alan T. DeKok <aland@freeradius.org>
2016-02-10 10:07:35 -0500

    Move fr_stats_bins to util.c

--- src/main/stats.c
+++ src/main/stats.c
@@ -58,43 +58,6 @@ fr_stats_t proxy_dsc_stats = FR_STATS_INIT;
 #endif
 #endif
 
-/** Sort latency times into bins
- *
- * This solves the problem of attempting to keep min/max/avg latencies, whilst
- * not knowing what the polling frequency will be.
- *
- * @param[out] stats Holding monotonically increasing stats bins.
- * @param[in] start of the request.
- * @param[in] end of the request.
- */
-void fr_stats_bins(fr_stats_t *stats, struct timeval *start, struct timeval *end)
-{
-	struct timeval diff;
-	uint32_t delay;
-
-	if ((start->tv_sec == 0) || (end->tv_sec == 0) || (end->tv_sec < start->tv_sec)) return;
-
-	fr_timeval_subtract(&diff, end, start);
-
-	if (diff.tv_sec >= 10) {
-		stats->elapsed[7]++;
-	} else {
-		int i;
-		uint32_t cmp;
-
-		delay = (diff.tv_sec * USEC) + diff.tv_usec;
-
-		cmp = 10;
-		for (i = 0; i < 7; i++) {
-			if (delay < cmp) {
-				stats->elapsed[i]++;
-				break;
-			}
-			cmp *= 10;
-		}
-	}
-}
-
 void request_stats_final(REQUEST *request)
 {
 	if (request->master_state == REQUEST_COUNTED) return;
