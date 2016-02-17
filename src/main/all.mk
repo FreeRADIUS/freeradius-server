7140dd5d217ae8cc39ab02b18222344c48275134
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2016-02-16 21:00:43 -0800

    Break sub make files across newlines

--- src/main/all.mk
+++ src/main/all.mk
@@ -1,3 +1,14 @@
-SUBMAKEFILES := radclient.mk radiusd.mk radsniff.mk radmin.mk radattr.mk \
-	radwho.mk radsnmp.mk radlast.mk radtest.mk radzap.mk checkrad.mk \
-	libfreeradius-server.mk unittest.mk
+SUBMAKEFILES := \
+    radclient.mk \
+    radiusd.mk \
+    radsniff.mk \
+    radmin.mk \
+    radattr.mk \
+    radwho.mk \
+    radsnmp.mk \
+    radlast.mk \
+    radtest.mk \
+    radzap.mk \
+    checkrad.mk \
+    libfreeradius-server.mk \
+    unittest.mk
