FreeRADIUS/freeradius-server

commit 9858792a56dbf6a82b722c5a0220040713892ab6

Expose prf_label in eap_tls_gen_challenge()
and rename it from eap_ttls_gen_challenge()

src/modules/rlm_eap/libeap/mppe_keys.c
@@ -151,33 +151,36 @@ void eap_tls_gen_mppe_keys(REQUEST *request, SSL *s, char const *prf_label)
 }
 
 
-#define FR_TLS_PRF_CHALLENGE	"ttls challenge"
-
 /*
- *	Generate the TTLS challenge
+ *	Generate the challenge using a PRF label.
  *
  *	It's in the TLS module simply because it's only a few lines
  *	of code, and it needs access to the TLS PRF functions.
  */
-void eap_ttls_gen_challenge(SSL *s, uint8_t *buffer, size_t size)
+void eap_tls_gen_challenge(SSL *s, uint8_t *buffer, size_t size, char const *prf_label)
 {
 #if OPENSSL_VERSION_NUMBER >= 0x10001000L
-	SSL_export_keying_material(s, buffer, size, FR_TLS_PRF_CHALLENGE,
-				   sizeof(FR_TLS_PRF_CHALLENGE) - 1, NULL, 0, 0);
+	SSL_export_keying_material(s, buffer, size, prf_label,
+				   strlen(prf_label), NULL, 0, 0);
 
 #else
 	uint8_t out[32], buf[32];
-	uint8_t seed[sizeof(FR_TLS_PRF_CHALLENGE)-1 + 2*SSL3_RANDOM_SIZE];
+	uint8_t seed[128 + 2*SSL3_RANDOM_SIZE];
 	uint8_t *p = seed;
+	size_t len;
 
-	memcpy(p, FR_TLS_PRF_CHALLENGE, sizeof(FR_TLS_PRF_CHALLENGE)-1);
-	p += sizeof(FR_TLS_PRF_CHALLENGE)-1;
+	len = strlen(prf_label);
+	if (len > 128) len = 128;
+
+	memcpy(p, prf_label, len);
+	p += len;
 	memcpy(p, s->s3->client_random, SSL3_RANDOM_SIZE);
 	p += SSL3_RANDOM_SIZE;
 	memcpy(p, s->s3->server_random, SSL3_RANDOM_SIZE);
+	p += SSL3_RANDOM_SIZE;
 
 	PRF(s->session->master_key, s->session->master_key_length,
-	    seed, sizeof(seed), out, buf, sizeof(out));
+	    seed, p - seed, out, buf, sizeof(out));
 	memcpy(buffer, out, size);
 #endif
 }

src/modules/rlm_eap/libeap/eap_tls.h
@@ -169,7 +169,7 @@ int			eap_tls_compose(eap_session_t *eap_session, eap_tls_status_t status, uint8
 /* MPPE key generation */
 void			eap_tls_gen_mppe_keys(REQUEST *request, SSL *s, char const *prf_label);
 
-void			eap_ttls_gen_challenge(SSL *ssl, uint8_t *buffer, size_t size);
+void			eap_tls_gen_challenge(SSL *ssl, uint8_t *buffer, size_t size, char const *prf_label);
 
 void			eap_tls_gen_eap_key(RADIUS_PACKET *packet, SSL *s, uint32_t header);
 

src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
@@ -264,8 +264,8 @@ static VALUE_PAIR *diameter2vp(REQUEST *request, REQUEST *fake, SSL *ssl,
 				return NULL;
 			}
 
-			eap_ttls_gen_challenge(ssl, challenge,
-					      sizeof(challenge));
+			eap_tls_gen_challenge(ssl, challenge,
+					      sizeof(challenge), "ttls challenge");
 
 			if (memcmp(challenge, vp->vp_octets,
 				   vp->vp_length) != 0) {

commit 0de07c2cad29b17d5288ed61466da8810f6a86be

Fix what 9858792a56dbf6a82b722c5a0220040713892ab6 broke

src/modules/rlm_eap/libeap/mppe_keys.c
@@ -112,10 +112,7 @@ void eap_tls_gen_mppe_keys(REQUEST *request, SSL *s, char const *prf_label)
 	prf_size = strlen(prf_label);
 
 #if OPENSSL_VERSION_NUMBER >= 0x10001000L
-	if (SSL_export_keying_material(s, out, sizeof(out), prf_label, prf_size, NULL, 0, 0) != 1) {
-		ERROR("Failed generating keying material");
-		return;
-	}
+	if (SSL_export_keying_material(s, out, sizeof(out), prf_label, prf_size, NULL, 0, 0) != 1) /* Fallback */
 #endif
 
 	{