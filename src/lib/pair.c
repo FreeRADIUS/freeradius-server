0a143a1e6883d0fc71f6f4dba2bd84f450ad9e6e
lkpdn <den.not.denmark@gmail.com>
2016-06-08 09:19:17 -0400

    check after the variable declarations

--- src/lib/pair.c
+++ src/lib/pair.c
@@ -2267,11 +2267,11 @@ size_t fr_pair_snprint(char *out, size_t outlen, VALUE_PAIR const *vp)
  */
 void fr_pair_fprint(FILE *fp, VALUE_PAIR const *vp)
 {
-	if (!fp) return;
 	char	buf[1024];
 	char	*p = buf;
 	size_t	len;
 
+	if (!fp) return;
 	VERIFY_VP(vp);
 
 	*p++ = '\t';
