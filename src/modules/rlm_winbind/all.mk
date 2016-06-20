a2e276425defacdb91c7c5caf622a3f1bc9edb7b
Matthew Newton <matthew-git@newtoncomputing.co.uk>
2016-06-20 21:48:24 +0100

    rlm_winbind: simplify makefile/configure
    fixes #1629

--- src/modules/rlm_winbind/all.mk
+++
@@ -1,5 +0,0 @@
-SUBMAKEFILES := rlm_winbind.mk
-
-src/modules/rlm_winbind/rlm_winbind.mk: src/modules/rlm_winbind/rlm_winbind.mk.in src/modules/rlm_winbind/configure
-	@echo CONFIGURE $(dir $<)
-	@cd $(dir $<) && ./configure $(CONFIGURE_ARGS)
