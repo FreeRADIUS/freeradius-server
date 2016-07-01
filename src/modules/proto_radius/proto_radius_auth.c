a3ac2ac4eaa69e693d2d9c5b701f8ec2371a44e0
Alan T. DeKok <aland@freeradius.org>
2016-07-01 09:53:34 -0400

    save state for internally generated packets, too

--- src/modules/proto_radius/proto_radius_auth.c
+++ src/modules/proto_radius/proto_radius_auth.c
@@ -663,12 +663,21 @@ static void auth_running(REQUEST *request, fr_state_action_t action)
 
 	send_reply:
 		/*
+		 *	Save session-state list for Access-Challenge,
+		 *	discard it for everything else.
+		 */
+		if (request->reply->code == PW_CODE_ACCESS_CHALLENGE) {
+			fr_request_to_state(global_state, request, request->packet, request->reply);
+
+		} else {
+			fr_state_discard(global_state, request, request->packet);
+		}
+
+		/*
 		 *	Check for "do not respond".
 		 */
 		if (!request->reply->code) {
 			RDEBUG("Not sending reply to client.");
-
-			fr_state_discard(global_state, request, request->packet);
 
 			/*
 			 *	If it's an internally generated request, clean it up now.
@@ -680,8 +689,8 @@ static void auth_running(REQUEST *request, fr_state_action_t action)
 
 		/*
 		 *	This is an internally generated request.
-		 *	Don't print IP addresses, and don't discard
-		 *	the state.
+		 *	Don't print IP addresses, and don't do cleanup
+		 *	/ reject delay.
 		 */
 		if (request->packet->data_len == 0) {
 			radlog_request(L_DBG, L_DBG_LVL_1, request, "Sent %s ID %i",
@@ -727,17 +736,6 @@ static void auth_running(REQUEST *request, fr_state_action_t action)
 		 */
 
 		common_packet_debug(request, request->reply, false);
-
-		/*
-		 *	Save session-state list for Access-Challenge,
-		 *	discard it for everything else.
-		 */
-		if (request->reply->code == PW_CODE_ACCESS_CHALLENGE) {
-			fr_request_to_state(global_state, request, request->packet, request->reply);
-
-		} else {
-			fr_state_discard(global_state, request, request->packet);
-		}
 
 		gettimeofday(&request->reply->timestamp, NULL);
 
