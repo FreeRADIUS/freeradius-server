8833fe62344f5e79dece485260783f4c518e0c69
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2016-07-22 15:44:18 -0400

    Update copyright on radmin

--- src/main/radmin.c
+++ src/main/radmin.c
@@ -1,8 +1,4 @@
 /*
- * radmin.c	RADIUS Administration tool.
- *
- * Version:	$Id$
- *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
@@ -16,11 +12,18 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
- *
- * Copyright 2012   The FreeRADIUS server project
- * Copyright 2012   Alan DeKok <aland@deployingradius.com>
  */
 
+/**
+ * $Id$
+ *
+ * @file radmin.c
+ * @brief Control a running radiusd process.
+ *
+ * @copyright 2012-2016 The FreeRADIUS server project
+ * @copyright 2016 Arran Cudbard-Bell <a.cudbardb@freeradius.org>
+ * @copyright 2012 Alan DeKok <aland@deployingradius.com>
+ */
 RCSID("$Id$")
 
 #include <freeradius-devel/radiusd.h>
