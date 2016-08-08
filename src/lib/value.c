8d1b6efc75188d776286fcbf974a39f66c4a6f0e
James Rouzier <jrouzier@inverse.ca>
2016-08-08 15:29:29 -0400

    Relax casting from signed to integer

--- src/lib/value.c
+++ src/lib/value.c
@@ -1306,6 +1306,15 @@ int value_data_cast(TALLOC_CTX *ctx, value_data_t *dst,
 			dst->integer = src->ushort;
 			break;
 
+		case PW_TYPE_SIGNED:
+			if (src->sinteger < 0 ) {
+				fr_strerror_printf("Invalid cast: From signed to integer.  signed value %d is negative ",
+						    src->sinteger);
+				return -1;
+			}
+			dst->integer = (uint32_t)src->sinteger;
+			break;
+
 		case PW_TYPE_OCTETS:
 			goto do_octets;
 
