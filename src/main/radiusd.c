1d8d2ed02c00ec191ccfcadced40ffe644b8b025
Terry Burton <tez@terryburton.co.uk>
2016-08-18 16:48:24 +0100

    Add support for systemd watchdog notifications.
    systemd is able to monitor a process to ensure that it periodically sends
    watchdog notifications.
    
    We install a watchdog event into the event loop that emits the notification and
    reschedules the next event in half the watchdog interval provided by systemd.
    
    To enable watchdogs add something like this to the unit file:
    
    [Service]
    WatchdogSec=10
    NotifyAccess=all
    
    Reliably detecting that systemd requires watchdog notifications at a certain
    interval must be performed in the initial process, however the subsequent
    notification can be emitted by any child.
    
    At present this simply ensures that the event loop is running.

--- src/main/radiusd.c
+++ src/main/radiusd.c
@@ -74,6 +74,11 @@ char const *radiusd_version = "FreeRADIUS Version " RADIUSD_VERSION_STRING
 
 static pid_t radius_pid;
 
+#ifdef HAVE_SYSTEMD_WATCHDOG
+uint64_t sd_watchdog_interval = 0;
+#endif
+
+
 /*
  *  Configuration items.
  */
@@ -348,6 +353,20 @@ int main(int argc, char *argv[])
 	 */
 #if defined(HAVE_OPENSSL_CRYPTO_H) && defined(ENABLE_OPENSSL_VERSION_CHECK)
 	if (tls_global_version_check(main_config.allow_vulnerable_openssl) < 0) exit(EXIT_FAILURE);
+#endif
+
+	/*
+	 *  The systemd watchdog enablement must be checked before we
+	 *  daemonize, but the notifications can come from any process.
+	 */
+#ifdef HAVE_SYSTEMD_WATCHDOG
+	if (!check_config) {
+		if (sd_watchdog_enabled(0, &sd_watchdog_interval) > 0) {
+			INFO("systemd watchdog interval is %d secs.\n", (int) sd_watchdog_interval / 1000000);
+		} else {
+			INFO("systemd watchdog is disabled.\n");
+		}
+	}
 #endif
 
 #ifndef __MINGW32__
