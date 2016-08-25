add0ab1c725b3aa122cc32ed2ed92c664825be56
root <root@build.kawo1.rwth-aachen.de>
2016-08-25 16:33:04 +0200

    Changed rlm_python to build against python3(.4). There is currently an issue where the module (probably) loses the threadstate or the heap gets corrupted. This manifests itself as the python functions reading random/broken data from parameters.

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -23,7 +23,7 @@ CFLAGS="${BUILD_CFLAGS}" ./configure -C \
     --with-udpfromto=$LIBS_OPTIONAL \
     --with-openssl=$LIBS_OPTIONAL \
     --with-pcre=$LIBS_OPTIONAL \
-    --with-rlm-python-bin=/usr/bin/python2.7   # Otherwise travis picks up /opt/python, which doesn't have .so available
+    --with-rlm-python-bin=/usr/bin/python3   # Otherwise travis picks up /opt/python, which doesn't have .so available
 
 echo "Contents of src/include/autoconf.h"
 cat "./src/include/autoconf.h"
