FreeRADIUS/freeradius-server

commit aa9a3c51e74d27e46e54972da7d77068fd318d25

Build with systemd on Debian-based distributions.
As the systemd libraries aren't available on older distributions we add a build
step to run the clean target which causes the debian/control file to be
regenerated with/without Build-Depends: libsystemd-dev.

Build steps that should work for all distributions:

$ sudo apt-get install devscripts
$ fakeroot debian/rules clean
$ sudo mk-build-deps -ir debian/control
$ dpkg-buildpackage -rfakeroot -b -uc

debian/rules
@@ -59,6 +59,16 @@ else
        SUBSTVARS = -Vdist:Depends="libssl1.0.0 (>= 1.0.1e-2+deb7u5)"
 endif
 
+# Add libsystemd-dev as a Build-Depends to debian/control if it is known to this system
+ifeq ($(shell apt-cache show libsystemd-dev &>/dev/null && echo yes),yes)
+       CONTROL_BUILDDEPS += libsystemd-dev,
+endif
+
+debian/control: debian/control.in
+	sed -e 's/@BUILDDEPS@/$(CONTROL_BUILDDEPS)/g' \
+	     debian/control.in > debian/control.tmp
+	mv debian/control.tmp debian/control
+
 include /usr/share/quilt/quilt.make
 
 config.status: configure
@@ -99,7 +109,8 @@ endif
 		--with-iodbc-include-dir='/usr/include/iodbc' \
 		--without-rlm_eap_ikev2 \
 		--without-rlm_sql_oracle \
-		--without-rlm_sql_unixodbc
+		--without-rlm_sql_unixodbc \
+		--with-systemd
 
 	rm config.guess
 	mv config.guess.dist config.guess
@@ -118,7 +129,7 @@ build-indep: patch build-indep-stamp
 build-indep-stamp: config.status
 	touch $@
 
-clean: unpatch
+clean: debian/control unpatch
 	dh_testdir
 	dh_testroot
 	rm -f build-arch-stamp build-indep-stamp

debian/control.in
@@ -1,5 +1,5 @@
 Source: freeradius
-Build-Depends: debhelper (>= 9),
+Build-Depends: @BUILDDEPS@ debhelper (>= 9),
  quilt,
  dpkg-dev (>= 1.13.19),
  autotools-dev,
@@ -21,7 +21,6 @@ Build-Depends: debhelper (>= 9),
  libsasl2-dev,
  libsqlite3-dev,
  libssl-dev,
- libsystemd-dev,
  libtalloc-dev,
  libwbclient-dev,
  libyubikey-dev,

debian/freeradius.service
@@ -4,7 +4,9 @@ After=syslog.target network.target
 Documentation=man:radiusd(8) man:radiusd.conf(5) http://wiki.freeradius.org/ http://networkradius.com/doc/
 
 [Service]
-Type=forking
+Type=notify
+WatchdogSec=60
+NotifyAccess=all
 PIDFile=/run/freeradius/freeradius.pid
 EnvironmentFile=-/etc/default/freeradius
 ExecStartPre=/usr/sbin/freeradius $FREERADIUS_OPTIONS -Cx -lstdout

commit 3c0c26cb39d070ed368c925ab0a70c8a929c4431

Fixed deb make target, as broken by aa9a3c51e74d27e46e54972da7d77068fd318d25

Makefile
@@ -292,6 +292,7 @@ dist-tag: freeradius-server-$(RADIUSD_VERSION_STRING).tar.gz freeradius-server-$
 #
 .PHONY: deb
 deb:
+	fakeroot debian/rules debian/control #clean
 	fakeroot dpkg-buildpackage -b -uc
 
 # Developer checks

debian/rules
@@ -151,6 +151,8 @@ ifeq (config.guess.dist,$(wildcard config.guess.dist))
 	rm -f config.guess
 	mv config.guess.dist config.guess
 endif
+	#distclean destroys debian/control, however we need it for dh_clean
+	$(MAKE) -f debian/rules debian/control
 	dh_clean
 
 install: install-arch