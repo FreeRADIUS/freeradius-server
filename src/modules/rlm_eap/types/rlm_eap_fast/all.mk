0d222f479bc2b33fa42710e0792f7e1f14277b99
James Rouzier <jrouzier@inverse.ca>
2016-09-09 11:38:38 -0400

    Compile all sources

--- src/modules/rlm_eap/types/rlm_eap_fast/all.mk
+++ src/modules/rlm_eap/types/rlm_eap_fast/all.mk
@@ -4,8 +4,7 @@ ifneq "$(OPENSSL_LIBS)" ""
 TARGET		:= $(TARGETNAME).a
 endif
 
-#SOURCES		:= $(TARGETNAME).c eap_fast.c eap_fast_crypto.c
-SOURCES		:= $(TARGETNAME).c eap_fast.c
+SOURCES		:= $(TARGETNAME).c eap_fast.c eap_fast_crypto.c
 
 SRC_INCDIRS	:= ${top_srcdir}/src/modules/rlm_eap/ ${top_srcdir}/src/modules/rlm_eap/libeap/
 TGT_PREREQS	:= libfreeradius-eap.a
