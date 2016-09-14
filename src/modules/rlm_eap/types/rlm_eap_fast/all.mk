6ef87243594f3b07e27f2649aa8513a62e9bfb9a
James Rouzier <jrouzier@inverse.ca>
2016-09-14 15:53:02 -0400

    Fix include paths

--- src/modules/rlm_eap/types/rlm_eap_fast/all.mk
+++ src/modules/rlm_eap/types/rlm_eap_fast/all.mk
@@ -6,5 +6,6 @@ endif
 
 SOURCES		:= $(TARGETNAME).c eap_fast.c eap_fast_crypto.c
 
-SRC_INCDIRS	:= ${top_srcdir}/src/modules/rlm_eap/ ${top_srcdir}/src/modules/rlm_eap/libeap/
+SRC_INCDIRS	:= ${top_srcdir}/src/modules/rlm_eap/ ${top_srcdir}/src/modules/rlm_eap/lib/base/
+
 TGT_PREREQS	:= libfreeradius-eap.a
