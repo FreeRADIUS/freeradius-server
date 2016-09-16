098c95506c500525f12cadf8f3f40597117e42c4
Philippe Wooding <philippe.wooding@networkradius.com>
2016-09-17 00:07:11 +0200

    Ignore '}' in redis key if it's before initial '{'

--- src/modules/rlm_redis/cluster.c
+++ src/modules/rlm_redis/cluster.c
@@ -285,8 +285,8 @@ static uint16_t cluster_key_hash(uint8_t const *key, size_t key_len)
 		return fr_crc16_xmodem(key, key_len) & (KEY_SLOTS - 1);
 	}
 
-	q = memchr(key, '}', key_len);
-	if (!q || (q < p) || (q == p + 1)) goto all; /* no }, or } before {, or {}, hash everything */
+	q = memchr(p, '}', key_len - (p - key)); /* look for } after { */
+	if (!q || (q == p + 1)) goto all; /* no } or {}, hash everything */
 
 	p++;	/* skip '{' */
 
