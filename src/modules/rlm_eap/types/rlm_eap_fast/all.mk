414293c3b01bafb10c4cb48a79d7a2abc60a24e2
Alan T. DeKok <aland@freeradius.org>
2016-10-23 10:20:40 -0400

    typo

--- src/modules/rlm_eap/types/rlm_eap_fast/all.mk
+++ src/modules/rlm_eap/types/rlm_eap_fast/all.mk
@@ -7,5 +7,4 @@ endif
 SOURCES		:= $(TARGETNAME).c eap_fast.c eap_fast_crypto.c
 
 SRC_INCDIRS	:= ${top_srcdir}/src/modules/rlm_eap/ ${top_srcdir}/src/modules/rlm_eap/lib/base/
-
 TGT_PREREQS	:= libfreeradius-eap.a
