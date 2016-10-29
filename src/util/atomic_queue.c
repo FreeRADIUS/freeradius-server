68718d67a630ea9923fb76b6a2c7c72c5bde5b21
Alan T. DeKok <aland@freeradius.org>
2016-10-29 10:00:21 -0400

    Build with local stdatomic.h, and licenses

--- src/util/atomic_queue.c
+++ src/util/atomic_queue.c
@@ -1,6 +1,37 @@
+/*
+ * atomic_queue.c	Thread-safe queues.
+ *
+ * Version:	$Id$
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Copyright 2016  Alan DeKok <aland@freeradius.org>
+ */
+RCSID("$Id$")
+
 #include <stdint.h>
-#include <stdatomic.h>
 #include <stdalign.h>
+
+#include <freeradius-devel/autoconf.h>
+
+#ifdef HAVE_STDATOMIC_H
+#  include <stdatomic.h>
+#else
+#  include <freeradius-devel/stdatomic.h>
+#endif
+
 #include <freeradius-devel/util/atomic_queue.h>
 
 /*
