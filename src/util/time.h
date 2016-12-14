acbf01fdf95806fa636ce70de02507ff16d8ccda
Alan T. DeKok <aland@freeradius.org>
2016-12-14 13:41:05 -0500

    Added FR_INSERT_TAIL_PTR

--- src/util/time.h
+++ src/util/time.h
@@ -82,6 +82,7 @@ typedef struct fr_time_tracking_t {
  */
 #define FR_DLIST_INIT(head) do { head.prev = head.next = &head; } while (0)
 #define FR_DLIST_INSERT_TAIL(head, entry) do { entry.prev = head.prev; entry.next = &head; head.prev->next = &entry; head.prev = &entry; } while (0)
+#define FR_DLIST_INSERT_TAIL_PTR(p_head, entry) do { entry.prev = p_head->prev; entry.next = p_head; p_head->prev->next = &entry; p_head->prev = &entry; } while (0)
 #define FR_DLIST_REMOVE(entry) do { entry.prev->next = entry.next; entry.next->prev = entry.prev; FR_DLIST_INIT(entry); } while (0)
 #define FR_DLIST_FIRST(head) (head.next == &head) ? NULL : head.next
 #define FR_DLIST_NEXT(head, p_entry) (p_entry->next == &head) ? NULL : p_entry->next
