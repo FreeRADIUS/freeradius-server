e8fb25fafe195e145373d42456fee5948eab4b0a
Alan T. DeKok <aland@freeradius.org>
2016-12-19 16:35:55 -0500

    use correct order for event status

--- src/main/radsniff.c
+++ src/main/radsniff.c
@@ -1840,7 +1840,7 @@ static void rs_got_packet(fr_event_list_t *el, int fd, void *ctx)
 	}
 }
 
-static int  _rs_event_status(struct timeval *wake, UNUSED void *ctx)
+static int  _rs_event_status(UNUSED void *ctx, struct timeval *wake)
 {
 	if (wake && ((wake->tv_sec != 0) || (wake->tv_usec >= 100000))) {
 		DEBUG2("Waking up in %d.%01u seconds.", (int) wake->tv_sec, (unsigned int) wake->tv_usec / 100000);
