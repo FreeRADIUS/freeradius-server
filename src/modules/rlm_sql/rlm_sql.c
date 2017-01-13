e46afdbd77cef96242c3801522edcad78b8dcc57
Alan T. DeKok <aland@freeradius.org>
2017-01-13 10:46:22 -0500

    Revert ""no more rows" isn't an ERROR"
    This reverts commit 9cd2d57c6f3594ae8c4d74f34fdc7770361d3bdb.
    
    Better fix is coming

--- src/modules/rlm_sql/rlm_sql.c
+++ src/modules/rlm_sql/rlm_sql.c
@@ -180,11 +180,7 @@ static ssize_t sql_xlat(void *instance, REQUEST *request, char const *query, cha
 		rcode = rlm_sql_query(inst, request, &handle, query);
 		if (rcode != RLM_SQL_OK) {
 		query_error:
-			if (rcode != RLM_SQL_NO_MORE_ROWS) {
-				RERROR("SQL query failed: %s", fr_int2str(sql_rcode_table, rcode, "<INVALID>"));
-			} else {
-				RDEBUG("No rows were returned.");
-			}
+			RERROR("SQL query failed: %s", fr_int2str(sql_rcode_table, rcode, "<INVALID>"));
 
 			ret = -1;
 			goto finish;
