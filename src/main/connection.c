6d876025c6b8cb8d446021a6e68c590134f0961b
Alan T. DeKok <aland@freeradius.org>
2017-02-15 11:04:36 -0500

    remove extra assertion

--- src/main/connection.c
+++ src/main/connection.c
@@ -836,11 +836,6 @@ static void *fr_connection_get_internal(fr_connection_pool_t *pool, REQUEST *req
 		goto do_return;
 	}
 
-	/*
-	 *	We don't have a connection.  Try to open a new one.
-	 */
-	rad_assert(pool->state.active == pool->state.num);
-
 	if (pool->state.num == pool->max) {
 		bool complain = false;
 
