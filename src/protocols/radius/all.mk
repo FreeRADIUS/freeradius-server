268b736200c55d64379e25f1e41367010ef5a087
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2017-05-03 13:24:11 -0400

    Add all.mk file for libfreeradius-radius

---
+++ src/protocols/radius/all.mk
@@ -0,0 +1,16 @@
+#
+# Makefile
+#
+# Version:      $Id$
+#
+TARGET		:= libfreeradius-radius.a
+
+SOURCES		:= base.c \
+		   decode.c \
+		   encode.c \
+		   list.c \
+		   packet.c \
+
+SRC_CFLAGS	:= -D_LIBRADIUS -I$(top_builddir)/src
+
+TGT_PREREQS	:= libfreeradius-util.a
