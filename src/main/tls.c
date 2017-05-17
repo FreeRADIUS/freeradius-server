23aac1076dd3b248ae568e8716a5f9fd5ca69da7
Alan T. DeKok <aland@freeradius.org>
2017-05-17 12:06:57 -0400

    move shutdown calls into check for ssn->ssl.  Found by PVS-Studio

--- src/main/tls.c
+++ src/main/tls.c
@@ -823,10 +823,10 @@ static void session_init(tls_session_t *ssn)
 
 static void session_close(tls_session_t *ssn)
 {
-	SSL_set_quiet_shutdown(ssn->ssl, 1);
-	SSL_shutdown(ssn->ssl);
-
 	if (ssn->ssl) {
+		SSL_set_quiet_shutdown(ssn->ssl, 1);
+		SSL_shutdown(ssn->ssl);
+
 		SSL_free(ssn->ssl);
 		ssn->ssl = NULL;
 	}
