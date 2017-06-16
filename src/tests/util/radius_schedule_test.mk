21c40857604993ae9a8442abebb0b3cbb833d90b
Alan T. DeKok <aland@freeradius.org>
2017-06-16 11:02:16 -0400

    re-order libraries which might help travis...

--- src/tests/util/radius_schedule_test.mk
+++ src/tests/util/radius_schedule_test.mk
@@ -2,6 +2,6 @@ TARGET := radius_schedule_test
 
 SOURCES		:= radius_schedule_test.c
 
-TGT_PREREQS	:= libfreeradius-util.a libfreeradius-radius.a libfreeradius-server.a libfreeradius-io.a
+TGT_PREREQS	:= libfreeradius-io.a libfreeradius-util.a libfreeradius-radius.a libfreeradius-server.a
 TGT_LDLIBS	:= $(LIBS)
 
--- src/tests/util/schedule_test.mk
+++ src/tests/util/schedule_test.mk
@@ -2,6 +2,6 @@ TARGET := schedule_test
 
 SOURCES		:= schedule_test.c
 
-TGT_PREREQS	:= libfreeradius-util.a libfreeradius-server.a libfreeradius-radius.a libfreeradius-io.a
+TGT_PREREQS	:= libfreeradius-io.a libfreeradius-util.a libfreeradius-server.a libfreeradius-radius.a
 TGT_LDLIBS	:= $(LIBS)
 
