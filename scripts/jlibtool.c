a05ade6a46e7cc12452908f1c42f4f1284188a62
Alan T. DeKok <aland@freeradius.org>
2017-06-29 07:53:38 -0400

    Add OpenBSD

--- scripts/jlibtool.c
+++ scripts/jlibtool.c
@@ -79,7 +79,7 @@
 #  define LD_LIBRARY_PATH_LOCAL		"DYLD_FALLBACK_LIBRARY_PATH"
 #endif
 
-#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || (defined(__sun) && defined(__GNUC__))
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || (defined(__sun) && defined(__GNUC__))
 #  define SHELL_CMD 			"/bin/sh"
 #  define DYNAMIC_LIB_EXT		"so"
 #  define MODULE_LIB_EXT		"so"
