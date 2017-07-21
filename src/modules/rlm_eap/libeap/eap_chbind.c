FreeRADIUS/freeradius-server

commit fd672a969f46ce1f1a964f1a49635b6ad4d3e003

remove duplicate checks.  Found by PVS-Studio

src/modules/rlm_eap/libeap/eap_chbind.c
@@ -286,7 +286,7 @@ VALUE_PAIR *eap_chbind_packet2vp(RADIUS_PACKET *packet, chbind_packet_t *chbind)
 
 	if (!chbind) return NULL; /* don't produce garbage */
 
-	vp = fr_pair_afrom_num(packet, PW_UKERNA_CHBIND, VENDORPEC_UKERNA);
+	vp = fr_pair_afrom_num(packet, VENDORPEC_UKERNA, PW_UKERNA_CHBIND);
 	if (!vp) return NULL;
 	fr_pair_value_memcpy(vp, (uint8_t *) chbind, talloc_array_length((uint8_t *)chbind));
 

src/lib/cbuff.c
@@ -104,7 +104,7 @@ fr_cbuff_t *fr_cbuff_alloc(TALLOC_CTX *ctx, uint32_t size, UNUSED bool lock)
 void fr_cbuff_rp_insert(fr_cbuff_t *cbuff, void *obj)
 {
 #ifdef HAVE_PTHREAD_H
-	if (cbuff->lock) PTHREAD_MUTEX_LOCK(cbuff);
+	PTHREAD_MUTEX_LOCK(cbuff);
 #endif
 
 	if (cbuff->elem[cbuff->in]) {
@@ -121,7 +121,7 @@ void fr_cbuff_rp_insert(fr_cbuff_t *cbuff, void *obj)
 	}
 
 #ifdef HAVE_PTHREAD_H
-	if (cbuff->lock) PTHREAD_MUTEX_UNLOCK(cbuff);
+	PTHREAD_MUTEX_UNLOCK(cbuff);
 #endif
 }
 
@@ -136,7 +136,7 @@ void *fr_cbuff_rp_next(fr_cbuff_t *cbuff, TALLOC_CTX *ctx)
 	void *obj = NULL;
 
 #ifdef HAVE_PTHREAD_H
-	if (cbuff->lock) PTHREAD_MUTEX_LOCK(cbuff);
+	PTHREAD_MUTEX_LOCK(cbuff);
 #endif
 
 	/* Buffer is empty */
@@ -147,7 +147,7 @@ void *fr_cbuff_rp_next(fr_cbuff_t *cbuff, TALLOC_CTX *ctx)
 
 done:
 #ifdef HAVE_PTHREAD_H
-	if (cbuff->lock) PTHREAD_MUTEX_UNLOCK(cbuff);
+	PTHREAD_MUTEX_UNLOCK(cbuff);
 #endif
 	return obj;
 }

commit 0bd5a4cec846d42673cfa6b664c41e87ab12eb9e

fix typo introduced by commit fd672a96

src/modules/rlm_eap/libeap/eap_chbind.c
@@ -284,7 +284,7 @@ VALUE_PAIR *eap_chbind_packet2vp(RADIUS_PACKET *packet, chbind_packet_t *chbind)
 
 	if (!chbind) return NULL; /* don't produce garbage */
 
-	vp = fr_pair_afrom_num(packet, VENDORPEC_UKERNA, PW_UKERNA_CHBIND);
+	vp = fr_pair_afrom_num(packet, PW_UKERNA_CHBIND, VENDORPEC_UKERNA);
 	if (!vp) return NULL;
 	fr_pair_value_memcpy(vp, (uint8_t *) chbind, talloc_array_length((uint8_t *)chbind));
 