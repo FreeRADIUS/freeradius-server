10db503f4a8fc6a2ad2f84202f182a0bc4a91c8e
Alan T. DeKok <aland@freeradius.org>
2017-09-01 08:25:37 -0400

    Include trailing \0 in "used" count.  Part of #2051

--- src/modules/rlm_rest/rest.c
+++ src/modules/rlm_rest/rest.c
@@ -1781,7 +1781,7 @@ static size_t rest_response_body(void *ptr, size_t size, size_t nmemb, void *use
 		return t;
 
 	default:
-		if (t > (ctx->alloc - ctx->used)) {
+		if ((t + 1) > (ctx->alloc - ctx->used) {
 			ctx->alloc += ((t + 1) > REST_BODY_INIT) ? t + 1 : REST_BODY_INIT;
 
 			tmp = ctx->buffer;
@@ -1795,7 +1795,7 @@ static size_t rest_response_body(void *ptr, size_t size, size_t nmemb, void *use
 			}
 		}
 		strlcpy(ctx->buffer + ctx->used, p, t + 1);
-		ctx->used += t;
+		ctx->used += t;	/* don't include the trailing zero */
 
 		break;
 	}
