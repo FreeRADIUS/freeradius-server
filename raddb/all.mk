5645937d80eab949464f5de36a100ee8d6a633c4
Alan T. DeKok <aland@freeradius.org>
2017-10-03 11:49:09 -0400

    enable RADIUS module by default

--- raddb/all.mk
+++ raddb/all.mk
@@ -10,7 +10,7 @@ LOCAL_SITES :=		$(addprefix raddb/sites-enabled/,$(DEFAULT_SITES))
 DEFAULT_MODULES :=	always attr_filter cache_eap chap client \
 			detail detail.log digest dhcpv4 eap \
 			eap_inner echo exec expiration expr files linelog logintime \
-			mschap ntlm_auth pam pap passwd radutmp \
+			mschap ntlm_auth pam pap passwd radius radutmp \
 			soh sradutmp unix unpack utf8
 
 LOCAL_MODULES :=	$(addprefix raddb/mods-enabled/,$(DEFAULT_MODULES))
