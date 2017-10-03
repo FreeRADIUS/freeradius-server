c192e566c2cae774701169e09878d1e8e4bfa705
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2017-10-03 23:52:18 +0800

    Follow the normal state transition if we have a ->failed callback and it returns FR_CONNECTION_STATE_INIT

--- src/main/connection.c
+++ src/main/connection.c
@@ -140,8 +140,6 @@ static void connection_state_failed(fr_connection_t *conn, struct timeval *now)
 		ret = conn->failed(conn->fd, prev, conn->uctx);
 		switch (ret) {
 		case FR_CONNECTION_STATE_INIT:
-			conn->state = FR_CONNECTION_STATE_FAILED;	/* so init function prints the correct transition */
-			connection_state_init(conn, now);
 			break;
 
 		case FR_CONNECTION_STATE_HALTED:		/* Do nothing */
