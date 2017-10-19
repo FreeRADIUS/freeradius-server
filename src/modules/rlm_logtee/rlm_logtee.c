48769263e2508dbfa7ae9eca8549a067aa02c40f
Alan T. DeKok <aland@freeradius.org>
2017-10-19 15:45:04 -0400

    we don't care if write() fails here.  CID #1414420

--- src/modules/rlm_logtee/rlm_logtee.c
+++ src/modules/rlm_logtee/rlm_logtee.c
@@ -281,7 +281,7 @@ static void _logtee_conn_writable(UNUSED fr_event_list_t *el, int sock, UNUSED i
 				rad_assert(0);
 			}
 		}
-		write(sock, t->inst->delimiter, t->inst->delimiter_len);
+		(void) write(sock, t->inst->delimiter, t->inst->delimiter_len);
 	}
 
 	logtee_fd_idle(t);
