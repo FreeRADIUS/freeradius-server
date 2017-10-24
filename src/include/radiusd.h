738da8cd794e419ad10c8dcc1eff69130efce9a9
Alan T. DeKok <aland@freeradius.org>
2017-10-24 11:59:58 -0400

    remove prototypes for functions that no longer exist

--- src/include/radiusd.h
+++ src/include/radiusd.h
@@ -371,14 +371,6 @@ typedef enum {
  *	Function prototypes.
  */
 
-rlm_rcode_t    	rad_coa_recv(REQUEST *request);
-
-/* session.c */
-int		rad_check_ts(uint32_t nasaddr, uint32_t nas_port, char const *user, char const *sessionid);
-int		session_zap(REQUEST *request, uint32_t nasaddr,
-			    uint32_t nas_port, char const *user,
-			    char const *sessionid, uint32_t cliaddr,
-			    char proto, int session_time);
 
 /* radiusd.c */
 #undef debug_pair
