9e12c5550f1f74f9531130a663f8abac526f87fa
Alan T. DeKok <aland@freeradius.org>
2017-11-10 15:59:20 -0500

    remove unnecessary check.  CID #1420937

--- src/main/xlat_eval.c
+++ src/main/xlat_eval.c
@@ -162,8 +162,6 @@ static rlm_rcode_t xlat_eval_one_letter(TALLOC_CTX *ctx, fr_cursor_t *out, REQUE
 		break;
 
 	case 'I': /* Request ID */
-		if (!request->packet) return 0;
-
 		MEM(value = fr_value_box_alloc(ctx, FR_TYPE_UINT32, NULL, false));
 		value->datum.uint32 = request->packet->id;
 		break;
