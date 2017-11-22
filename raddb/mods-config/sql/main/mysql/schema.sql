b838f5178fe092598fb3459dedb5e1ea49b41340
Martin Dulin <dulinoz@gmail.com>
2017-11-22 11:57:47 +0000

    Fix missing id column in radusergroup

--- raddb/mods-config/sql/main/mysql/schema.sql
+++ raddb/mods-config/sql/main/mysql/schema.sql
@@ -115,9 +115,11 @@ CREATE TABLE radreply (
 #
 
 CREATE TABLE radusergroup (
+  id int(11) unsigned NOT NULL auto_increment,
   username varchar(64) NOT NULL default '',
   groupname varchar(64) NOT NULL default '',
   priority int(11) NOT NULL default '1',
+  PRIMARY KEY  (id),
   KEY username (username(32))
 );
 
