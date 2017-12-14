443e361c23f129b287a44e518104ea2942fcd76c
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2017-12-14 18:40:42 +0000

    data can be null

--- src/include/cf_parse.h
+++ src/include/cf_parse.h
@@ -452,7 +452,7 @@ struct CONF_PARSER {
  *	Type validation and conversion
  */
 int		cf_pair_parse(TALLOC_CTX *ctx, CONF_SECTION *cs, char const *name,
-			      unsigned int type, void *data, char const *dflt, FR_TOKEN dflt_quote) CC_HINT(nonnull(2,3,5));
+			      unsigned int type, void *data, char const *dflt, FR_TOKEN dflt_quote) CC_HINT(nonnull(2,3));
 int		cf_section_parse(TALLOC_CTX *ctx, void *base, CONF_SECTION *cs);
 int		cf_section_parse_pass2(void *base, CONF_SECTION *cs);
 CONF_PARSER const	*cf_section_parse_table(CONF_SECTION *cs);
