969bebef8395c5bb49ec0ef1603743624d123fea
Alan T. DeKok <aland@freeradius.org>
2018-01-10 09:58:50 -0500

    don't re-generate `configure` unless asked to

--- src/modules/all.mk
+++ src/modules/all.mk
@@ -19,6 +19,8 @@ endif
 
 SUBMAKEFILES += $(wildcard ${top_srcdir}/src/modules/proto_*/all.mk)
 
+ifeq "$(MAKECMDGOALS)" "reconfig"
 src/modules/%/configure: src/modules/%/configure.ac
 	@echo AUTOCONF $(dir $@)
 	cd $(dir $@) && $(AUTOCONF) -I $(top_builddir) -I $(top_builddir)/m4 -I $(top_builddir)/$(dir $@)m4
+endif
