da4c1cc026cf260cd191db60ed096784d4719a13
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2018-01-26 17:01:23 -0700

    Fix talloc_list_get_type_abort when get_type_abort is disabled

--- src/include/talloc.h
+++ src/include/talloc.h
@@ -90,7 +90,7 @@ static inline void *_talloc_list_get_type_abort(void *head, size_t offset, char 
 	return head;
 }
 #else
-#  define talloc_list_get_type_abort(_head, _type)
+#  define talloc_list_get_type_abort(_head, _type) (_type *)(_head)
 #endif
 
 /*
