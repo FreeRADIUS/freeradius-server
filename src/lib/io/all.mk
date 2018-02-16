de348ff4fd281f61452d0c3dccd4edb87a629725
Alan T. DeKok <aland@freeradius.org>
2018-02-16 11:14:23 -0500

    add rules to create / link directories

--- src/lib/io/all.mk
+++ src/lib/io/all.mk
@@ -14,9 +14,25 @@ TGT_LDFLAGS	:= $(LDFLAGS)
 HEADERS :=	$(SOURCES:.c=.h)
 
 define ADD_UTIL_HEADER
-${SRC_INCLUDE_DIR}/io/${1}: src/include/io/${1}
+${SRC_INCLUDE_DIR}/io/${1}: src/include/io/${1} | ${SRC_INCLUDE_DIR}/io
 
 install.src.include: ${SRC_INCLUDE_DIR}/io/${1}
+
+src/freeradius-devel/io/${1}: | src/freeradius-devel/io
 endef
+
+#
+#  Create the installation directory
+#
+.PHONY: ${SRC_INCLUDE_DIR}/io
+${SRC_INCLUDE_DIR}/io:
+	${Q}$(INSTALL) -d -m 755 $@
+
+#
+#  Create the build directory.
+#
+.PHONY: src/freeradius-devel/io
+src/freeradius-devel/io:
+	${Q}[ -e $@ ] || ln -s ${top_srcdir}/src/lib/io src/include
 
 $(foreach x,$(HEADERS),$(eval $(call ADD_UTIL_HEADER,$x)))
