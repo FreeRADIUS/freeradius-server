ba4e909cbbe3b31c9228851e1fa0e6ceaf5ded05
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2018-03-10 21:44:20 +0600

    Typo

--- src/main/unlang_interpret.c
+++ src/main/unlang_interpret.c
@@ -1436,7 +1436,7 @@ rlm_rcode_t unlang_module_yield(REQUEST *request, fr_module_unlang_resume_t call
 
 	rad_assert(stack->depth > 0);
 
-	VERIFY_REQUEST(request);	/* Check the yielded request is sane */
+	REQUEST_VERIFY(request);	/* Check the yielded request is sane */
 
 	switch (frame->instruction->type) {
 	case UNLANG_TYPE_MODULE_CALL:
