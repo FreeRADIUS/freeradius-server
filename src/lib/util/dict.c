98fdc3dbeae4251572b4ee6fb06ff7509a468bfd
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2018-03-19 14:08:03 +0000

    Add missing INTERNAL_IF_NULL

--- src/lib/util/dict.c
+++ src/lib/util/dict.c
@@ -1699,6 +1699,8 @@ int fr_dict_attr_add(fr_dict_t *dict, fr_dict_attr_t const *parent,
 	fr_dict_attr_t		*mutable;
 	fr_dict_attr_flags_t	our_flags = *flags;
 
+	INTERNAL_IF_NULL(dict);
+
 	/*
 	 *	Check it's valid
 	 */
