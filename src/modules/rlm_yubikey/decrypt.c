FreeRADIUS/freeradius-server

commit ccced6155a50e54406acf2a1cb3d85ef30bbc912

Convert some pairfind calls to pairfind_da calls

src/modules/rlm_yubikey/decrypt.c
@@ -32,7 +32,7 @@ rlm_rcode_t rlm_yubikey_decrypt(rlm_yubikey_t *inst, REQUEST *request, char cons
 		return RLM_MODULE_FAIL;
 	}
 
-	key = pairfind(request->config_items, da->attr, da->vendor, TAG_ANY);
+	key = pairfind_da(request->config_items, da, TAG_ANY);
 	if (!key) {
 		REDEBUG("Yubikey-Key attribute not found in control list, can't decrypt OTP data");
 		return RLM_MODULE_INVALID;
@@ -119,7 +119,7 @@ rlm_rcode_t rlm_yubikey_decrypt(rlm_yubikey_t *inst, REQUEST *request, char cons
 	/*
 	 *	Now we check for replay attacks
 	 */
-	vp = pairfind(request->config_items, vp->da->attr, vp->da->vendor, TAG_ANY);
+	vp = pairfind_da(request->config_items, da, TAG_ANY);
 	if (!vp) {
 		RWDEBUG("Yubikey-Counter not found in control list, skipping replay attack checks");
 		return RLM_MODULE_OK;

src/lib/pair.c
@@ -886,7 +886,7 @@ void pairmove(TALLOC_CTX *ctx, VALUE_PAIR **to, VALUE_PAIR **from)
 		 *	it doesn't already exist.
 		 */
 		case T_OP_EQ:
-			found = pairfind(*to, i->da->attr, i->da->vendor, TAG_ANY);
+			found = pairfind_da(*to, i->da, TAG_ANY);
 			if (!found) goto do_add;
 
 			tail_from = &(i->next);
@@ -897,7 +897,7 @@ void pairmove(TALLOC_CTX *ctx, VALUE_PAIR **to, VALUE_PAIR **from)
 		 *	of the same vendor/attr which already exists.
 		 */
 		case T_OP_SET:
-			found = pairfind(*to, i->da->attr, i->da->vendor, TAG_ANY);
+			found = pairfind_da(*to, i->da, TAG_ANY);
 			if (!found) goto do_add;
 
 			/*

src/main/xlat.c
@@ -1663,7 +1663,7 @@ static char *xlat_getvp(TALLOC_CTX *ctx, REQUEST *request, pair_lists_t list, DI
 	 *	This allows users to manipulate virtual attributes as if they
 	 *	were real ones.
 	 */
-	vp = pairfind(vps, da->attr, da->vendor, tag);
+	vp = pairfind_da(vps, da, tag);
 	if (vp) goto do_print;
 
 	/*

src/modules/rlm_eap/types/rlm_eap_peap/peap.c
@@ -1250,9 +1250,7 @@ static int CC_HINT(nonnull) setup_fake_request(REQUEST *request, REQUEST *fake, 
 			 *	AND attributes which are copied there
 			 *	from below.
 			 */
-			if (pairfind(fake->packet->vps, vp->da->attr, vp->da->vendor, TAG_ANY)) {
-				continue;
-			}
+			if (pairfind_da(fake->packet->vps, vp->da, TAG_ANY)) continue;
 
 			/*
 			 *	Some attributes are handled specially.

src/modules/rlm_eap/types/rlm_eap_ttls/ttls.c
@@ -1122,7 +1122,7 @@ PW_CODE eapttls_process(eap_handler_t *handler, tls_session_t *tls_session)
 			 *	AND attributes which are copied there
 			 *	from below.
 			 */
-			if (pairfind(fake->packet->vps, vp->da->attr, vp->da->vendor, TAG_ANY)) {
+			if (pairfind_da(fake->packet->vps, vp->da, TAG_ANY)) {
 				continue;
 			}
 

src/modules/rlm_ldap/attrmap.c
@@ -277,7 +277,7 @@ int rlm_ldap_map_xlat(REQUEST *request, value_pair_map_t const *maps, rlm_ldap_m
 			}
 			if (!from) continue;
 
-			found = pairfind(*from, map->rhs->tmpl_da->attr, map->rhs->tmpl_da->vendor, TAG_ANY);
+			found = pairfind_da(*from, map->rhs->tmpl_da, TAG_ANY);
 			if (!found) continue;
 
 			expanded->attrs[total++] = talloc_typed_strdup(request, found->vp_strvalue);

src/modules/rlm_otp/otp_mppe.c
@@ -46,8 +46,8 @@ void otp_mppe(REQUEST *request, otp_pwe_t pwe, rlm_otp_t const *opt, char const 
 {
 	VALUE_PAIR *cvp, *rvp;
 
-	cvp = pairfind(request->packet->vps, pwattr[pwe - 1]->attr, pwattr[pwe - 1]->vendor, TAG_ANY);
-	rvp = pairfind(request->packet->vps, pwattr[pwe]->attr, pwattr[pwe]->vendor, TAG_ANY);
+	cvp = pairfind_da(request->packet->vps, pwattr[pwe - 1], TAG_ANY);
+	rvp = pairfind_da(request->packet->vps, pwattr[pwe], TAG_ANY);
 	if (!cvp || !rvp) {
 		return;
 	}

src/modules/rlm_passwd/rlm_passwd.c
@@ -543,7 +543,7 @@ static rlm_rcode_t CC_HINT(nonnull) mod_passwd_map(void *instance, REQUEST *requ
 	struct mypasswd * pw, *last_found;
 	vp_cursor_t cursor;
 
-	key = pairfind(request->packet->vps, inst->keyattr->attr, inst->keyattr->vendor, TAG_ANY);
+	key = pairfind_da(request->packet->vps, inst->keyattr, TAG_ANY);
 	if (!key) {
 		return RLM_MODULE_NOTFOUND;
 	}

commit bd867c9a38b1678213ff851cbc98c74c355f567c

Look for the correct Yubikey-Counter attribute in the control list
broken in ccced6155a

src/modules/rlm_yubikey/decrypt.c
@@ -23,6 +23,7 @@ rlm_rcode_t rlm_yubikey_decrypt(rlm_yubikey_t *inst, REQUEST *request, char cons
 	yubikey_token_st token;
 
 	DICT_ATTR const *da;
+	DICT_ATTR const *yubikey_counter_da;
 
 	char private_id[(YUBIKEY_UID_SIZE * 2) + 1];
 	VALUE_PAIR *key, *vp;
@@ -114,13 +115,14 @@ rlm_rcode_t rlm_yubikey_decrypt(rlm_yubikey_t *inst, REQUEST *request, char cons
 
 		return RLM_MODULE_FAIL;
 	}
+	yubikey_counter_da = vp->da;
 	vp->vp_integer = counter;
 	vp->vp_length = 4;
 
 	/*
 	 *	Now we check for replay attacks
 	 */
-	vp = fr_pair_find_by_da(request->config, da, TAG_ANY);
+	vp = fr_pair_find_by_da(request->config, yubikey_counter_da, TAG_ANY);
 	if (!vp) {
 		RWDEBUG("Yubikey-Counter not found in control list, skipping replay attack checks");
 		return RLM_MODULE_OK;