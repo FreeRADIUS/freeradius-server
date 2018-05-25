b2666fb8ab32a4e5f401b91b798cf28e5cbb798e
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2018-05-25 15:13:15 +0600

    Unused structure

--- src/lib/io/listen.h
+++ src/lib/io/listen.h
@@ -52,14 +52,3 @@ struct fr_async_t {
 	uint32_t		priority;
 	bool			detached;	//!< if detached, we don't send real replies
 };
-
-/** Information to track src/dst ip/port
- */
-typedef struct fr_ip_srcdst_t {
-	int				if_index;
-
-	fr_ipaddr_t			src_ipaddr;
-	fr_ipaddr_t			dst_ipaddr;
-	uint16_t			src_port;
-	uint16_t 			dst_port;
-} fr_ip_srcdst_t;
