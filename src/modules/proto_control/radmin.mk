a22a4243f62d7805255dd20bd13bf5910c61bae7
Alan T. DeKok <aland@freeradius.org>
2018-07-31 14:15:16 -0400

    use real readline

--- src/modules/proto_control/radmin.mk
+++ src/modules/proto_control/radmin.mk
@@ -6,6 +6,9 @@ endif
 
 SOURCES		:= radmin.c
 
+SRC_CFLAGS	:= $(LIBREADLINE_CFLAGS)
+
 TGT_INSTALLDIR  := ${sbindir}
 TGT_PREREQS	:= $(LIBFREERADIUS_SERVER) libfreeradius-util.a libfreeradius-control.a
 TGT_LDLIBS	:= $(LIBS) $(LIBREADLINE)
+TGT_LDFLAGS	:= $(LDFLAGS) $(LIBREADLINE_LDFLAGS)
