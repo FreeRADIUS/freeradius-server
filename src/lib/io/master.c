ee3fb1cb9646687778ea3325d94270e1f1043776
Alan T. DeKok <aland@freeradius.org>
2018-08-16 09:28:38 -0400

    this needs to be socket_instance for now

--- src/lib/io/master.c
+++ src/lib/io/master.c
@@ -557,7 +557,7 @@ static fr_io_connection_t *fr_io_connection_alloc(fr_io_instance_t *inst, fr_io_
 		 *	Glue in the connection to the listener.
 		 */
 		listen->app_io = &fr_master_app_io;
-		listen->app_io_instance = inst->app_io_instance;
+		listen->app_io_instance = connection;
 
 		connection->app_io_instance = dl_inst->data;
 
