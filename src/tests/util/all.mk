687baad7953fd7f2fce4af45388f9a2c76a3b43e
Alan T. DeKok <aland@freeradius.org>
2018-09-19 07:43:17 -0400

    don't build these test programs
    they're no longer needed

--- src/tests/util/all.mk
+++ src/tests/util/all.mk
@@ -3,6 +3,6 @@ SUBMAKEFILES := ring_buffer_test.mk message_set_test.mk atomic_queue_test.mk con
 #
 #  These require pthread.
 #
-ifneq "$(findstring thread,${CFLAGS})" ""
-SUBMAKEFILES += channel_test.mk worker_test.mk radius1_test.mk schedule_test.mk radius_schedule_test.mk
-endif
+#ifneq "$(findstring thread,${CFLAGS})" ""
+#SUBMAKEFILES += channel_test.mk worker_test.mk radius1_test.mk schedule_test.mk radius_schedule_test.mk
+#endif
