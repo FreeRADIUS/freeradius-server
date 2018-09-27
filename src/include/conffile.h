df783d6e91ae331bea613c195719592c42d574b0
Alan T. DeKok <aland@freeradius.org>
2018-09-26 20:41:57 -0400

    don't print out WARNING WARNING.  One is enough

--- src/include/conffile.h
+++ src/include/conffile.h
@@ -145,7 +145,7 @@ typedef struct timeval _timeval_t;
 #define FR_INTEGER_COND_CHECK(_name, _var, _cond, _new)\
 do {\
 	if (!(_cond)) {\
-		WARN("WARNING: Ignoring \"" _name " = %i\", forcing to \"" _name " = %i\"", _var, _new);\
+		WARN("Ignoring \"" _name " = %i\", forcing to \"" _name " = %i\"", _var, _new);\
 		_var = _new;\
 	}\
 } while (0)
@@ -156,7 +156,7 @@ do {\
 do {\
 	struct timeval _bound = {_bound_sec, _bound_usec};\
 	if (!timercmp(_var, &_bound, _op)) {\
-		WARN("WARNING: Ignoring \"" _name " = %d.%.06d\", forcing to \"" _name " = %d.%06d\"",\
+		WARN("Ignoring \"" _name " = %d.%.06d\", forcing to \"" _name " = %d.%06d\"",\
 			(int)(_var)->tv_sec, (int)(_var)->tv_usec,\
 			(int)_bound.tv_sec, (int)_bound.tv_usec);\
 		*_var = _bound;\
