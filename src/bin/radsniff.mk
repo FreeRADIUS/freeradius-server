9a87972a722f6b3675ab8e295a6d317f4b46ac4d
Alan T. DeKok <aland@freeradius.org>
2018-10-17 14:04:02 -0400

    Revert "this shouldn't be in git"
    This reverts commit 759cab975259bfb3c0d407742d0414c0c762a50c.

---
+++ src/bin/radsniff.mk
@@ -0,0 +1,13 @@
+PCAP_LIBS	:= -lpcap
+
+ifneq ($(PCAP_LIBS),)
+TARGET		:= radsniff
+else
+TARGET		:=
+endif
+
+SOURCES		:= radsniff.c collectd.c
+
+TGT_PREREQS	:= libfreeradius-util.a libfreeradius-radius.a
+TGT_LDLIBS	:= $(LIBS) $(PCAP_LIBS) $(COLLECTDC_LIBS)
+TGT_LDFLAGS     := $(LDFLAGS) $(PCAP_LDFLAGS) $(COLLECTDC_LDFLAGS)
