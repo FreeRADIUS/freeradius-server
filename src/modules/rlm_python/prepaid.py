6879accefdca528e4e0d9d2d14bf675bc7965c2e
Alexander Scheel <ascheel@redhat.com>
2018-10-18 12:40:53 -0400

    Clarify shebangs to be python2
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- src/modules/rlm_python/prepaid.py
+++ src/modules/rlm_python/prepaid.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 #
 # Example Python module for prepaid usage using MySQL
 
--- src/modules/rlm_python/radiusd_test.py
+++ src/modules/rlm_python/radiusd_test.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 #
 # Python module test
 # Miguel A.L. Paraz <mparaz@mparaz.com>
