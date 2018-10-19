2dca9efc4d41f10ade6919885b9914724e50764c
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2018-10-19 10:52:05 -0400

    Formatting

--- src/modules/rlm_passwd/rlm_passwd.c
+++ src/modules/rlm_passwd/rlm_passwd.c
@@ -517,8 +517,9 @@ static void result_add(TALLOC_CTX *ctx, rlm_passwd_t const *inst, REQUEST *reque
 	VALUE_PAIR *vp;
 
 	for (i = 0; i < inst->num_fields; i++) {
-		if (inst->pwd_fmt->field[i] && *inst->pwd_fmt->field[i] && pw->field[i] && i != inst->key_field  && inst->pwd_fmt->listflag[i] == when) {
-			if ( !inst->ignore_empty || pw->field[i][0] != 0 ) { /* if value in key/value pair is not empty */
+		if (inst->pwd_fmt->field[i] && *inst->pwd_fmt->field[i] && pw->field[i] &&
+		    (i != inst->key_field) && inst->pwd_fmt->listflag[i] == when) {
+			if (!inst->ignore_empty || pw->field[i][0] != 0 ) { /* if value in key/value pair is not empty */
 				vp = fr_pair_make(ctx, request->dict,
 						  vps, inst->pwd_fmt->field[i], pw->field[i], T_OP_EQ);
 				if (vp) {
