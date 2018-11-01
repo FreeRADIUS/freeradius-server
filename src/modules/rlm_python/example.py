87c31332c32dfe2c837261d06056fa393c4d8384
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2018-11-01 12:27:00 +0100

    Improve example.py to show possible formats for input and output

--- src/modules/rlm_python/example.py
+++ src/modules/rlm_python/example.py
@@ -7,26 +7,30 @@
 
 import radiusd
 
+# Check post_auth for the most complete example using different
+# input and output formats
+
 def instantiate(p):
   print "*** instantiate ***"
   print p
   # return 0 for success or -1 for failure
 
+
 def authorize(p):
   print "*** authorize ***"
-  print
   radiusd.radlog(radiusd.L_INFO, '*** radlog call in authorize ***')
   print
   print p
   print
   print radiusd.config
-  print
   return radiusd.RLM_MODULE_OK
+
 
 def preacct(p):
   print "*** preacct ***"
   print p
   return radiusd.RLM_MODULE_OK
+
 
 def accounting(p):
   print "*** accounting ***"
@@ -35,25 +39,53 @@ def accounting(p):
   print p
   return radiusd.RLM_MODULE_OK
 
+
 def pre_proxy(p):
   print "*** pre_proxy ***"
   print p
   return radiusd.RLM_MODULE_OK
+
 
 def post_proxy(p):
   print "*** post_proxy ***"
   print p
   return radiusd.RLM_MODULE_OK
 
+
 def post_auth(p):
   print "*** post_auth ***"
-  print p
-  return radiusd.RLM_MODULE_OK
+
+  # This is true when using pass_all_vps_dict
+  if type(p) is dict:
+    print "Request:", p["request"]
+    print "Reply:", p["reply"]
+    print "Config:", p["config"]
+    print "State:", p["session-state"]
+    print "Proxy-Request:", p["proxy-request"]
+    print "Proxy-Reply:", p["proxy-reply"]
+
+  else:
+    print p
+
+  # Dictionary representing changes we want to make to the different VPS
+  update_dict = {
+        "request": (("User-Password", ":=", "A new password"),),
+        "reply": (("Reply-Message", "The module is doing its job"),
+                  ("User-Name", "NewUserName")),
+        "config": (("Cleartext-Password", "A new password"),),
+  }
+
+  return radiusd.RLM_MODULE_OK, update_dict
+  # Alternatively, you could use the legacy 3-tuple output
+  # (only reply and config can be updated)
+  # return radiusd.RLM_MODULE_OK, update_dict["reply"], update_dict["config"]
+
 
 def recv_coa(p):
   print "*** recv_coa ***"
   print p
   return radiusd.RLM_MODULE_OK
+
 
 def send_coa(p):
   print "*** send_coa ***"
