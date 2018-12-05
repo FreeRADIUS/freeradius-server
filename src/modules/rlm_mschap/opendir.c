3e33ecd406dc8a9117544607cd053152c8560c3f
Alan T. DeKok <aland@freeradius.org>
2018-12-05 06:55:09 -0500

    len should be uint32_t, not size_t

--- src/modules/rlm_mschap/opendir.c
+++ src/modules/rlm_mschap/opendir.c
@@ -369,7 +369,7 @@ rlm_rcode_t od_mschap_auth(REQUEST *request, VALUE_PAIR *challenge, VALUE_PAIR *
 				 pStepBuff, NULL);
 	if (status == eDSNoErr) {
 		if (pStepBuff->fBufferLength > 4) {
-			size_t len;
+			uint32_t len;
 
 			memcpy(&len, pStepBuff->fBufferData, sizeof(len));
 			if (len == 40) {
@@ -382,7 +382,7 @@ rlm_rcode_t od_mschap_auth(REQUEST *request, VALUE_PAIR *challenge, VALUE_PAIR *
 						 *response->vp_strvalue,
 						 "MS-CHAP2-Success",
 						 mschap_reply, len+2);
-				RDEBUG2("dsDoDirNodeAuth returns stepbuff: %s (len=%zu)\n", mschap_reply, len);
+				RDEBUG2("dsDoDirNodeAuth returns stepbuff: %s (len=u)\n", mschap_reply, (unsigned int) len);
 			}
 		}
 	}
