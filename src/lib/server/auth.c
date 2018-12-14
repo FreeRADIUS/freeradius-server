3df2a92b04b2d256bf77dbdd8bd99c063b57de4f
Alan T. DeKok <aland@freeradius.org>
2018-12-14 08:39:54 -0500

    include dictionary for FR_USER_NAME

--- src/lib/server/auth.c
+++ src/lib/server/auth.c
@@ -37,6 +37,7 @@ RCSID("$Id$")
 
 #include <freeradius-devel/radius/defs.h>
 
+#include <freeradius-devel/protocol/radius/rfc2865.h>
 #include <freeradius-devel/protocol/freeradius/freeradius.internal.h>
 
 #include <ctype.h>
