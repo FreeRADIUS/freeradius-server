9e9c0e7a8c4fea339baa2b0ea69b7c5f84b63377
Alan T. DeKok <aland@freeradius.org>
2019-01-23 12:59:25 -0500

    other targets are "clean.FOO", not "clean_FOO"

--- scripts/boiler.mk
+++ scripts/boiler.mk
@@ -56,9 +56,9 @@ endif
 #   USE WITH EVAL
 #
 define ADD_CLEAN_RULE
-    clean: clean_$(notdir ${1})
-    .PHONY: clean_$(notdir ${1})
-    clean_$(notdir ${1}):
+    clean: clean.$(notdir ${1})
+    .PHONY: clean.$(notdir ${1})
+    clean.$(notdir ${1}):
 	$(Q)$(strip rm -f ${${1}_BUILD}/${1} $${${1}_OBJS} $${${1}_DEPS} $${${1}_OBJS:%.${OBJ_EXT}=%.[do]}) $(if ${TARGET_DIR},$${TARGET_DIR}/$(notdir ${1}))
 	$${${1}_POSTCLEAN}
 
