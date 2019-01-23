2796346490f5423e8b896ef46ed5c893ab495fe8
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-01-23 14:59:57 +0700

    Include versioning header explicitly?

--- src/lib/tls/missing.h
+++ src/lib/tls/missing.h
@@ -14,6 +14,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
+#include <openssl/opensslv.h>
 #include <openssl/ssl.h>
 
 #if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
