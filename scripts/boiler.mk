56437e3cad05661c7a12d4a6d9485a250b04e0d1
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-01-26 21:34:29 +0700

    Need globbing

--- scripts/boiler.mk
+++ scripts/boiler.mk
@@ -310,7 +310,7 @@ define ANALYZE_C_CMDS
 	$(Q)if $(ANALYZE_C_DUMP) && test -d "$@"; then \
 	    echo "Decode with:"; \
 	    echo -n "echo \""; \
-	    gzip -c "$@/*.html" | base64; \
+	    gzip -c $@/*.html | base64; \
 	    echo "\" | base64 -D | zcat > $@.html"; \
 	fi
 	$(Q)touch $@
