dfe4584446dc7950594351b5abe36f73a722cb8f
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-02-26 13:35:43 +0800

    Notes on setting up PostgreSQL server for macOS

--- scripts/travis/postgresql-setup.sh
+++ scripts/travis/postgresql-setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -e
 
+#
+# To initialise on MacOS
+#    sudo brew install postgresql
+#    pg_ctl -D /usr/local/var/postgres start
+#    /usr/local/opt/postgres/bin/createuser -s postgres
+#
+
 echo "PostgreSQL - Creating database"
-psql -h "${SQL_POSTGRESQL_TEST_SERVER}" -c 'create database radius;' -U postgres
+psql -h "${SQL_POSTGRESQL_TEST_SERVER}" -c 'create database radius;' -U postgres || true
 
 echo "PostgreSQL - Execute schema.sql"
 psql -h "${SQL_POSTGRESQL_TEST_SERVER}" -U postgres radius < raddb/mods-config/sql/main/postgresql/schema.sql
