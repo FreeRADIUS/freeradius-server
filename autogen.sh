baf55f66d34c63e99b910f83b09349735a2395c8
Matthew Newton <matthew-git@newtoncomputing.co.uk>
2019-02-28 15:19:12 +0000

    fix autogen search paths so includes can be found

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,7 @@ parentdir=`dirname $0`
 
 cd $parentdir
 parentdir=`pwd`
+m4include="-I$parentdir -I$parentdir/m4 -Im4"
 
 libtoolize -f -c
 #aclocal
@@ -16,6 +17,6 @@ mysubdirs=`echo $mysubdirs`
 for F in $mysubdirs
 do
 	echo "Configuring in $F..."
-	(cd $F && grep "^AC_CONFIG_HEADER" configure.ac > /dev/null || exit 0; autoheader -I$parentdir)
-	(cd $F && autoconf -I$parentdir)
+	(cd $F && grep "^AC_CONFIG_HEADER" configure.ac > /dev/null || exit 0; autoheader $m4include)
+	(cd $F && autoconf $m4include)
 done
