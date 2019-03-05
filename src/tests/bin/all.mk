dfaed9d03b293a1ef482154ab61d1de8492a3b07
Alan T. DeKok <aland@freeradius.org>
2019-03-04 21:17:33 -0500

    stubs for testing binary utilities.

---
+++ src/tests/bin/all.mk
@@ -0,0 +1,31 @@
+FILES  := \
+	radict
+
+DICT_DIR := $(top_srcdir)/share/dictionary
+
+#
+#  Create the output directory
+#
+.PHONY: $(BUILD_DIR)/tests/bin
+$(BUILD_DIR)/tests/bin:
+	${Q}mkdir -p $@
+
+#
+#  Files in the output dir depend on the bin tests
+#
+$(BUILD_DIR)/tests/bin/%: $(DIR)/% | $(BUILD_DIR)/tests/bin
+	${Q}echo BIN-TEST $(notdir $@)
+	${Q}TESTBIN="$(TESTBIN)" DICT_DIR="$(DICT_DIR)" $<
+	${Q}touch $@
+
+#
+#  Get all of the bin test output files
+#
+TESTS.BIN_FILES := $(addprefix $(BUILD_DIR)/tests/bin/,$(FILES))
+
+$(TESTS.BIN_FILES): $(TESTS.DICT_FILES)
+
+#
+#  Depend on the output files, and create the directory first.
+#
+tests.bin: $(TESTS.BIN_FILES)
