8e778b20df2b363474f915402b152ee405687a0e
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-03-11 15:02:44 +0800

    Add example invocations back

--- src/modules/rlm_lua/example.lua
+++ src/modules/rlm_lua/example.lua
@@ -13,47 +13,60 @@ function tprint (tbl, indent)
 end
 
 function preacct()
-  print("example.lua/preacct() Dummy")
+  print("example.lua/preacct()")
 end
 
 function accounting()
-  print("example.lua/accounting() Dummy")
+  print("example.lua/accounting()")
 end
 
 function pre_proxy()
-  print("example.lua/pre_proxy() Dummy")
+  print("example.lua/pre_proxy()")
 end
 
 function post_proxy()
-  print("example.lua/post_proxy() Dummy")
+  print("example.lua/post_proxy()")
 end
 
 function post_auth()
-  print("example.lua/post_auth() Dummy")
+  print("example.lua/post_auth()")
 end
 
 function recv_coa()
-  print("example.lua/recv_coa() Dummy")
+  print("example.lua/recv_coa()")
 end
 
 function send_coa()
-  print("example.lua/send_coa() Dummy")
+  print("example.lua/send_coa()")
 end
 
 function detach()
-  print("example.lua/detach() Dummy")
+  print("example.lua/detach()")
 end
 
 function xlat()
-  print("example.lua/xlat() Dummy")
+  print("example.lua/xlat()")
 end
 
 function authenticate()
-  print("example.lua/authenticate() Dummy")
+  print("example.lua/authenticate()")
 end
 
 function authorize()
-  print("example.lua/authorize(): <list_request>")
+  -------------------------
+  -- example invocations --
+  -------------------------
+
+  --tprint(get_attribute("user-name"))
+  --tprint(get_attribute("user-password"))
+  --tprint(get_attribute("tunnel-type", "2"))
+  --print(request['user-name'][0])
+  --print(request['user-name'].next_iter())
+  --print(request['user-name'].next_iter())
+  --tprint(request['user-name'])
+  --tprint(request['user-name'])
+
+  print("example.lua/authorize()")
+  print("Request list contents:")
   tprint(request, 2)
-  print("example.lua/authorize: </list_request>")
 end
