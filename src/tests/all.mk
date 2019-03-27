7e0c6c9935270ea3e9f9e9aeec2bcdfeaaf90bd4
Alan T. DeKok <aland@freeradius.org>
2019-03-27 15:16:00 -0400

    build the tests only when running test targets

--- src/tests/all.mk
+++ src/tests/all.mk
@@ -1,4 +1,6 @@
+ifneq "$(findstring test,$(MAKECMDGOALS))" ""
 SUBMAKEFILES := rbmonkey.mk eapol_test/all.mk dict/all.mk trie/all.mk unit/all.mk map/all.mk xlat/all.mk keywords/all.mk util/all.mk auth/all.mk modules/all.mk bin/all.mk daemon/all.mk 
+endif
 
 #
 #  Include all of the autoconf definitions into the Make variable space
