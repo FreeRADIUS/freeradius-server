7934d9bc35015df793ef07482e351c82dc2b1553
Alan T. DeKok <aland@freeradius.org>
2019-03-27 15:20:19 -0400

    more comments

--- src/tests/all.mk
+++ src/tests/all.mk
@@ -1,4 +1,10 @@
-ifneq "$(findstring test,$(MAKECMDGOALS))" ""
+#
+#  The tests do a lot of rooting through files, which slows down non-test builds.
+#
+#  Therefore only include the test subdirectories if we're running the tests.
+#  Or, if we're trying to clean things up.
+#
+ifneq "$(findstring test,$(MAKECMDGOALS))$(findstring clean,$(MAKECMDGOALS))" ""
 SUBMAKEFILES := rbmonkey.mk eapol_test/all.mk dict/all.mk trie/all.mk unit/all.mk map/all.mk xlat/all.mk keywords/all.mk util/all.mk auth/all.mk modules/all.mk bin/all.mk daemon/all.mk 
 endif
 
