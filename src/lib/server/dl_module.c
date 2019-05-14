fc407c3ae0717b12baebd31b8a430877ceba403a
Alan T. DeKok <aland@freeradius.org>
2019-05-14 12:41:26 -0400

    make module_inst_free more robust

--- src/lib/server/dl_module.c
+++ src/lib/server/dl_module.c
@@ -375,22 +375,24 @@ dl_module_t const *dl_module(CONF_SECTION *conf, dl_module_t const *parent, char
  */
 static int _dl_module_instance_free(dl_module_inst_t *dl_module_inst)
 {
-	if (dl_module_inst->module && dl_module_inst->module->common->detach) {
-		dl_module_inst->module->common->detach(dl_module_inst->data);
+	if (dl_module_inst->module) {
+		if (dl_module_inst->module->common->detach) {
+			dl_module_inst->module->common->detach(dl_module_inst->data);
+		}
+
+		/*
+		 *	Remove this instance from the tracking tree.
+		 */
+		rad_assert(dl_module_loader != NULL);
+		rbtree_deletebydata(dl_module_loader->inst_tree, dl_module_inst);
+
+		/*
+		 *	Decrements the reference count. The module object
+		 *	won't be unloaded until all instances of that module
+		 *	have been destroyed.
+		 */
+		talloc_decrease_ref_count(dl_module_inst->module);
 	}
-
-	/*
-	 *	Remove this instance from the tracking tree.
-	 */
-	rad_assert(dl_module_loader != NULL);
-	rbtree_deletebydata(dl_module_loader->inst_tree, dl_module_inst);
-
-	/*
-	 *	Decrements the reference count. The module object
-	 *	won't be unloaded until all instances of that module
-	 *	have been destroyed.
-	 */
-	talloc_decrease_ref_count(dl_module_inst->module);
 
 	/*
 	 *	Ensure sane free order, and that all destructors
