5b55f41ec8bf00c5d73148d68a382562e4944765
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-05-24 14:17:30 -0400

    Fix mod_conn_create in rlm_krb5

--- src/modules/rlm_krb5/krb5.h
+++ src/modules/rlm_krb5/krb5.h
@@ -90,4 +90,4 @@ typedef struct {
 char const *rlm_krb5_error(rlm_krb5_t const *inst, krb5_context context, krb5_error_code code);
 #endif
 
-void *mod_conn_create(TALLOC_CTX *ctx, void *instance, struct timeval const *timeout);
+void *mod_conn_create(TALLOC_CTX *ctx, void *instance, fr_time_delta_t timeout);
