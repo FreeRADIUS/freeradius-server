ce6984d79d953434634fb9c6b400b96922f55826
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-06-16 22:07:38 +0800

    Use all the obscure keywords

--- src/lib/util/base64.h
+++ src/lib/util/base64.h
@@ -46,8 +46,8 @@ extern char const fr_base64_sextet[];
 
 bool		fr_is_base64(char c);
 
-size_t		fr_base64_encode(char *out, size_t outlen, uint8_t const *in, size_t inlen);
-ssize_t		fr_base64_decode(uint8_t *out, size_t outlen, char const *in, size_t inlen);
+size_t		fr_base64_encode(char * restrict out, size_t outlen, uint8_t const * restrict in, size_t inlen);
+ssize_t		fr_base64_decode(uint8_t * restrict out, size_t outlen, char const * restrict in, size_t inlen);
 
 #ifdef __cplusplus
 }
