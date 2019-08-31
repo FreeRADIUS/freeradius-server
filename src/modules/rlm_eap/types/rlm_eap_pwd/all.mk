dea834724554a529f5d83b5cecdea671e13d7619
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-08-30 22:35:47 -0400

    Build utility code for eap_pwd.c

--- src/modules/rlm_eap/types/rlm_eap_pwd/all.mk
+++ src/modules/rlm_eap/types/rlm_eap_pwd/all.mk
@@ -4,7 +4,7 @@ ifneq "$(OPENSSL_LIBS)" ""
 TARGET		:= $(TARGETNAME).a
 endif
 
-SOURCES		:= $(TARGETNAME).c
+SOURCES		:= $(TARGETNAME).c eap_pwd.c
 
 SRC_INCDIRS	:= ${top_srcdir}/src/modules/rlm_eap/ ${top_srcdir}/src/modules/rlm_eap/lib/base/
 
