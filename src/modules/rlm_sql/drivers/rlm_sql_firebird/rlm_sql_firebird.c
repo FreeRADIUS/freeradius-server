e4ede2184372f498ad7b6daaa196f7022e222454
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-09-20 12:08:10 -0500

    unused things..

--- src/modules/rlm_sql/drivers/rlm_sql_firebird/rlm_sql_firebird.c
+++ src/modules/rlm_sql/drivers/rlm_sql_firebird/rlm_sql_firebird.c
@@ -242,7 +242,7 @@ static sql_rcode_t sql_finish_select_query(rlm_sql_handle_t *handle, UNUSED rlm_
 /** End the query
  *
  */
-static sql_rcode_t sql_finish_query(rlm_sql_handle_t *handle, rlm_sql_config_t *config)
+static sql_rcode_t sql_finish_query(UNUSED rlm_sql_handle_t *handle, UNUSED rlm_sql_config_t *config)
 {
 	return 0;
 }
