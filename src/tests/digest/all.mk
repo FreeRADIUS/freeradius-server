15add6496c3be5fb4e5c632193acd7d811195c16
Alan T. DeKok <aland@freeradius.org>
2019-10-11 13:28:01 -0400

    clean up makefile
    formatting, and print out commands that should be run when a
    test fails

--- src/tests/digest/all.mk
+++ src/tests/digest/all.mk
@@ -29,19 +29,20 @@ $(eval $(call RADIUSD_SERVICE,digest,$(OUTPUT)))
 #
 $(OUTPUT)/%: $(DIR)/% test.digest.radiusd_kill test.digest.radiusd_start
 	$(eval TARGET := $(patsubst %.txt,%,$(notdir $@)))
-	${Q}for _num in $$(sed '/^#.*TESTS/!d; s/.*TESTS//g' $<); do \
-		echo "DIGEST-TEST $(TARGET)_$${_num}";                   \
-		cp -f $< $@.request;                                     \
-		echo "Test-Name = \"$(TARGET)\"" >> $@.request;          \
-		echo "Test-Number = \"$${_num}\"" >> $@.request;         \
+	${Q}for _num in $$(sed '/^#.*TESTS/!d; s/.*TESTS//g' $<); do	\
+		echo "DIGEST-TEST $(TARGET)_$${_num}";			\
+		cp -f $< $@.request;					\
+		echo "Test-Name = \"$(TARGET)\"" >> $@.request;		\
+		echo "Test-Number = \"$${_num}\"" >> $@.request;	\
 		if ! $(RADCLIENT_BIN) -f $@.request -xF -d src/tests/digest/config -D share/dictionary 127.0.0.1:$(PORT) auth $(SECRET) > $@.out; then \
-			echo "Problems with $(RADCLIENT_BIN) -f $@_request -xF -d src/tests/digest/config -D share/dictionary 127.0.0.1:$(PORT) auth $(SECRET)"; \
-			cat $@.out;                                          \
-			$(MAKE) test.digest.radiusd_kill;                    \
-			exit 1;                                              \
-		else                                                     \
-			touch $@;                                            \
-		fi;                                                      \
+			echo "FAILED";					\
+			cat $@.out;					\
+			$(MAKE) test.digest.radiusd_kill;		\
+			echo "RADIUSD:   $(RADIUSD_RUN)";		\
+			echo "RADCLIENT: $(RADCLIENT_BIN) -f $@_request -xF -d src/tests/digest/config -D share/dictionary 127.0.0.1:$(PORT) auth $(SECRET)"; \
+			exit 1;						\
+		fi;							\
+		touch $@;						\
 	done
 
 $(TEST): $(FILES)
