e52715e895da2bf64b7a1a5253c39045ee04c87e
Alan T. DeKok <aland@freeradius.org>
2019-10-19 07:57:48 -0400

    don't do locking on files in /dev.  Closes #3057

--- src/modules/rlm_linelog/rlm_linelog.c
+++ src/modules/rlm_linelog/rlm_linelog.c
@@ -144,7 +144,13 @@ static int mod_instantiate(CONF_SECTION *conf, void *instance)
 		return -1;
 	}
 
-	inst->ef = exfile_init(inst, 256, 30, true);
+	/*
+	 *	If the admin wants the logs to go to stdout or stderr,
+	 *	then skip locking / seeking on those files.  Since
+	 *	everything in /dev/ isn't a real file, we can't seek
+	 *	or lock it.
+	 */
+	inst->ef = exfile_init(inst, 256, 30, (strncmp(inst->filename, "/dev/", 5) != 0));
 	if (!inst->ef) {
 		cf_log_err_cs(conf, "Failed creating log file context");
 		return -1;
