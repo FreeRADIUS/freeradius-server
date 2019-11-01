cd9c56e141d5b68dcc555f7787bdf1e7e48f4602
Jorge Pereira <jpereiran@gmail.com>
2019-10-31 22:00:05 -0300

    Move the dhcpv6 unit tests to src/tests/unit/dhcpv6

--- src/tests/unit/all.mk
+++ src/tests/unit/all.mk
@@ -8,6 +8,11 @@
 TEST := test.unit
 
 #
+#	dhcpv6 unit tests
+#
+FILES_DHCPV6 := $(subst $(DIR)/,,$(wildcard $(DIR)/dhcpv6/*.txt))
+
+#
 #  The files are put here in order.  Later tests need
 #  functionality from earlier test.
 #
@@ -29,20 +34,7 @@ FILES  := \
 	eap_sim_decode.txt \
 	eap_sim_error.txt \
 	dhcpv4.txt \
-	dhcpv6_microsoft.txt \
-	dhcpv6_rfc3315.txt \
-	dhcpv6_rfc3319.txt \
-	dhcpv6_rfc3646.txt \
-	dhcpv6_rfc4242.txt \
-	dhcpv6_rfc4280.txt \
-	dhcpv6_rfc4580.txt \
-	dhcpv6_rfc5678.txt \
-	dhcpv6_rfc5908.txt \
-	dhcpv6_rfc5970.txt \
-	dhcpv6_rfc6355.txt \
-	dhcpv6_rfc6610.txt \
-	dhcpv6_rfc7839.txt \
-	dhcpv6_rfc8156.txt \
+	$(FILES_DHCPV6) \
 	regex.txt \
 	escape.txt \
 	condition.txt \
@@ -50,7 +42,6 @@ FILES  := \
 	ethernet.txt
 
 # dict.txt - removed because the unit tests don't allow for protocol namespaces
-
 # command.txt - removed because commands like ":sql" are not parsed properly any more
 
 $(eval $(call TEST_BOOTSTRAP))
@@ -64,7 +55,7 @@ $(FILES.$(TEST)): export TZ = GMT
 #
 #  And the actual script to run each test.
 #
-$(OUTPUT)/%: $(DIR)/% $(TESTBINDIR)/unit_test_attribute
+$(OUTPUT)/%: $(DIR)/ $(TESTBINDIR)/unit_test_attribute
 	${Q}echo UNIT-TEST $(notdir $@)
 	${Q}if ! $(TESTBIN)/unit_test_attribute -D $(top_srcdir)/share/dictionary -d $(top_srcdir)/src/tests/unit -r "$@" $<; then \
 		echo "$(TESTBIN)/unit_test_attribute -D $(top_srcdir)/share/dictionary -d $(top_srcdir)/src/tests/unit -r \"$@\" $<"; \
