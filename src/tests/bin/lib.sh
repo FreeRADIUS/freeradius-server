667b7c891a5850be45de5616694f3b3fa409ab2d
Alan T. DeKok <aland@freeradius.org>
2019-11-05 16:55:29 -0500

    get rid of stupid "error 0"

--- src/tests/bin/lib.sh
+++ src/tests/bin/lib.sh
@@ -1,6 +1,6 @@
 do_test() {
 	if ! $@ 1> build/tests/bin/$(basename $0).log 2>&1; then
-		echo "Failed executing '$@' - error $?"
+		echo "Failed executing '$@'"
 		cat  build/tests/bin/$(basename $0).log
 
 		[ -n "$cb_do_test" ] && eval "$cb_do_test"
