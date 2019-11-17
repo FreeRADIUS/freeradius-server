9aa76675b8e552bf9d25aba7d300dd78cbae9b54
Jorge Pereira <jpereiran@gmail.com>
2019-11-16 23:50:22 -0300

    Clean up

--- src/tests/unit/all.mk
+++ src/tests/unit/all.mk
@@ -13,10 +13,6 @@ TEST := test.unit
 #
 FILES  := $(subst $(DIR)/,,$(call FIND_FILES_SUFFIX,$(DIR),*.txt))
 
-# dict.txt - removed because the unit tests don't allow for protocol namespaces
-
-# command.txt - removed because commands like ":sql" are not parsed properly any more
-
 $(eval $(call TEST_BOOTSTRAP))
 
 #
@@ -24,10 +20,6 @@ $(eval $(call TEST_BOOTSTRAP))
 #  the test outputs.
 #
 $(FILES.$(TEST)): export TZ = GMT
-
-#export ASAN_SYMBOLIZER_PATH=$(shell which llvm-symbolizer)
-#export ASAN_OPTIONS=malloc_context_size=50 detect_leaks=1 symbolize=1
-#export LSAN_OPTIONS=print_suppressions=0 fast_unwind_on_malloc=0
 
 #
 #  And the actual script to run each test.
