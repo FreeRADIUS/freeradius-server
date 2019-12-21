50d69643f0a04536e70ad35eda46b35dbfdf0019
Alan T. DeKok <aland@freeradius.org>
2019-12-21 09:29:41 -0500

    unused variable

--- src/lib/io/worker.c
+++ src/lib/io/worker.c
@@ -1216,7 +1216,7 @@ void fr_worker(fr_worker_t *worker)
  *
  *	This should be run ONLY in single-threaded mode!
  */
-int fr_worker_pre_event(void *uctx, fr_time_t wake)
+int fr_worker_pre_event(void *uctx, UNUSED fr_time_t wake)
 {
 	fr_worker_t *worker = talloc_get_type_abort(uctx, fr_worker_t);
 	REQUEST *request;
