9c1414bef60aab460c51fdd982f6666772f78d21
Terry Burton <tez@terryburton.co.uk>
2020-01-24 20:40:20 +0000

    sqlippool fixes (#3271)
    * Reference the sqlippool module by its name in the default site
    
    The default configuraton for the sqlippool module creates the module
    directly rather using a named instance so the main_pool name does not
    resolve to a module.
    
    Pools are usually differentiated using the Pool-Name control attribute
    such that only a single instance is normally required. Multiple
    instances are only required where multiple pool have different backend
    databases or IP allocation policies (queries, etc.)
    
    * sqlippool: Fix check that the referenced module is an SQL module
    
    Was testing the name of the driver rather than that the module was an
    instance of rlm_sql.
    
    * sqlippool: Qualify the value of attribute_name

--- src/modules/rlm_sqlippool/rlm_sqlippool.c
+++ src/modules/rlm_sqlippool/rlm_sqlippool.c
@@ -446,7 +446,7 @@ static int mod_instantiate(void *instance, CONF_SECTION *conf)
 
 	inst->sql_inst = (rlm_sql_t *) sql_inst->dl_inst->data;
 
-	if (strcmp(inst->sql_inst->driver->name, "sql") != 0) {
+	if (strcmp(cf_section_name1(inst->sql_inst->cs), "sql") != 0) {
 		cf_log_err(conf, "Module \"%s\" is not an instance of the rlm_sql module",
 			      inst->sql_instance_name);
 		return -1;
