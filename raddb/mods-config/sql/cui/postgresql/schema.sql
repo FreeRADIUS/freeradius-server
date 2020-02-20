6cc0edb8f3195438509e7924a956bc1a0fadf36c
Jakub Jirutka <jakub@jirutka.cz>
2020-02-20 10:30:50 +0100

    fix formatting in cui postgresql schema to be consistent

--- raddb/mods-config/sql/cui/postgresql/schema.sql
+++ raddb/mods-config/sql/cui/postgresql/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE cui (
-  clientipaddress INET NOT NULL DEFAULT '0.0.0.0',
-  callingstationid varchar(50) NOT NULL DEFAULT '',
-  username varchar(64) NOT NULL DEFAULT '',
-  cui varchar(128) NOT NULL DEFAULT '',
-  creationdate TIMESTAMP with time zone NOT NULL default 'now()',
-  lastaccounting TIMESTAMP with time zone NOT NULL default '-infinity'::timestamp,
-  PRIMARY KEY  (username, clientipaddress, callingstationid)
+	clientipaddress inet NOT NULL DEFAULT '0.0.0.0',
+	callingstationid varchar(50) NOT NULL DEFAULT '',
+	username varchar(64) NOT NULL DEFAULT '',
+	cui varchar(128) NOT NULL DEFAULT '',
+	creationdate timestamp with time zone NOT NULL DEFAULT 'now()',
+	lastaccounting timestamp with time zone NOT NULL DEFAULT '-infinity'::timestamp,
+	PRIMARY KEY (username, clientipaddress, callingstationid)
 );
 
 CREATE RULE postauth_query AS ON INSERT TO cui
