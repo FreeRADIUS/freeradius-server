949f90cd7383c3dae15af5c0263d84af772bec67
Alan T. DeKok <aland@freeradius.org>
2020-03-09 08:16:44 -0400

    use CS_INT for variable, as that's what the function takes
    Instead of making the variable "int", and then casting it to
    CS_INT when calling the function.  That just won't work.

--- src/modules/rlm_sql/drivers/rlm_sql_freetds/rlm_sql_freetds.c
+++ src/modules/rlm_sql/drivers/rlm_sql_freetds/rlm_sql_freetds.c
@@ -500,9 +500,9 @@ static sql_rcode_t sql_select_query(rlm_sql_handle_t *handle, rlm_sql_config_t *
 static int sql_num_rows(rlm_sql_handle_t *handle, UNUSED rlm_sql_config_t *config)
 {
 	rlm_sql_freetds_conn_t *conn = handle->conn;
-	int	num;
+	CS_INT	num;
 
-	if (ct_res_info(conn->command, CS_ROW_COUNT, (CS_INT *)&num, CS_UNUSED, NULL) != CS_SUCCEED) {
+	if (ct_res_info(conn->command, CS_ROW_COUNT, &num, CS_UNUSED, NULL) != CS_SUCCEED) {
 		ERROR("rlm_sql_freetds: error retrieving row count");
 
 		return RLM_SQL_ERROR;
