8e369ca8b3e32dd23186f90ffde40ef171f543d8
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2020-03-25 15:36:27 -0600

    trunk: Add state machine diagram for trunk connections

--- src/lib/server/trunk.c
+++ src/lib/server/trunk.c
@@ -88,6 +88,7 @@ struct fr_trunk_request_s {
 
 /** Associates request queues with a connection
  *
+ * @dotfile src/lib/server/trunk_conn.gv "Trunk connection state machine"
  */
 struct fr_trunk_connection_s {
 	struct fr_trunk_connection_pub_s pub;		//!< Public fields in the trunk connection.
