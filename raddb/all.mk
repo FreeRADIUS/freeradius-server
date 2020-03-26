576a7388646f7afa92e5ba9b236a6a4fc544d8f7
Alan T. DeKok <aland@freeradius.org>
2020-03-26 13:28:13 -0400

    stop fighting with travis && just revert for now

--- raddb/all.mk
+++ raddb/all.mk
@@ -155,24 +155,28 @@ ${top_srcdir}/raddb/certs/passwords.mk: $(wildcard ${top_srcdir}/raddb/certs/*cn
 	${Q}$(MAKE) -C $(dir $@) $(notdir $@)
 
 define BUILD_CERT
-${1}/${2}/${3}.key: ${1}/${3}.cnf ${1}/passwords.mk | $$(dir $$@)
+${1}/${2}/${3}.key: ${1}/${3}.cnf ${1}/passwords.mk | ${1}/${2}
 	$${Q}echo CERT-KEY ${2}/${3}
-	$${Q}$$(MAKE) -C ${1} ${2}/${3}.key
+#	$${Q}$$(MAKE) -C ${1} ${2}/${3}.key
+	$${Q}cp src/tests/certs/${2}/${3}.key $$@
 	@touch $$@
 
 ${1}/${2}/${3}.csr: ${1}/${2}/${3}.key
 	$${Q}echo CERT-CSR ${2}/${3}
-	$${Q}$$(MAKE) -C ${1} ${2}/${3}.csr
+#	$${Q}$$(MAKE) -C ${1} ${2}/${3}.csr
+	$${Q}cp src/tests/certs/${2}/${3}.csr $$@
 	@touch $$@
 
 ${1}/${2}/${3}.pem: ${1}/${2}/${3}.key
 	$${Q}echo CERT-PEM$= ${2}/${3}
-	$${Q}$$(MAKE) -C ${1} ${2}/${3}.pem
+#	$${Q}$$(MAKE) -C ${1} ${2}/${3}.pem
+	$${Q}cp src/tests/certs/${2}/${3}.pem $$@
 	@touch $$@
 
 ${1}/${2}/${3}.crt: ${1}/${2}/${3}.pem
 	$${Q}echo CERT-CRT ${2}/${3}
-	$${Q}$$(MAKE) -C ${1} ${2}/${3}.crt
+#	$${Q}$$(MAKE) -C ${1} ${2}/${3}.crt
+	$${Q}cp src/tests/certs/${2}/${3}.crt $$@
 	@touch $$@
 
 ifneq "${3}" "ca"
