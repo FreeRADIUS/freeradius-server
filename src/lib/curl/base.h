f6d7205d14149a3ba0a0638892d75427f333b020
Stephen Blackwell <stephenmac98@gmail.com>
2020-04-12 20:15:02 -0400

    Added fr_curl_easy_tls_init and fr_curl_response_certinfo to src/lib/curl/base.c and FR_CURL_SET_OPTION and fr_curl_tls_config to src/lib/curl/base.h

--- src/lib/curl/base.h
+++ src/lib/curl/base.h
@@ -32,6 +32,16 @@ extern "C" {
 #include <curl/curl.h>
 #include <freeradius-devel/server/request.h>
 #include <freeradius-devel/util/event.h>
+#include <freeradius-devel/server/module.h>
+
+#define FR_CURL_SET_OPTION(_x, _y)\
+do {\
+	if ((ret = curl_easy_setopt(randle->candle, _x, _y)) != CURLE_OK) {\
+		char const *_option;\
+		_option = STRINGIFY(_x);\
+		goto error;\
+	}\
+} while (0)
 
 /** Uctx data for timer and I/O functions
  *
@@ -55,6 +65,33 @@ typedef struct {
 	void			*uctx;			//!< Private data for the module using the API.
 } fr_curl_io_request_t;
 
+typedef struct {
+	char const		*tls_certificate_file;
+	char const		*tls_private_key_file;
+	char const		*tls_private_key_password;
+	char const		*tls_ca_file;
+	char const		*tls_ca_issuer_file;
+	char const		*tls_ca_path;
+	char const		*tls_random_file;
+	bool			tls_check_cert;
+	bool			tls_check_cert_cn;
+	bool			tls_extract_cert_attrs;
+} fr_curl_tls_t;
+
+static CONF_PARSER fr_curl_tls_config[] = {
+	{ FR_CONF_OFFSET("ca_file", FR_TYPE_FILE_INPUT, fr_curl_tls_t, tls_ca_file) },
+	{ FR_CONF_OFFSET("ca_issuer_file", FR_TYPE_FILE_INPUT, fr_curl_tls_t, tls_ca_issuer_file) },
+	{ FR_CONF_OFFSET("ca_path", FR_TYPE_FILE_INPUT, fr_curl_tls_t, tls_ca_path) },
+	{ FR_CONF_OFFSET("certificate_file", FR_TYPE_FILE_INPUT, fr_curl_tls_t, tls_certificate_file) },
+	{ FR_CONF_OFFSET("private_key_file", FR_TYPE_FILE_INPUT, fr_curl_tls_t, tls_private_key_file) },
+	{ FR_CONF_OFFSET("private_key_password", FR_TYPE_STRING | FR_TYPE_SECRET, fr_curl_tls_t, tls_private_key_password) },
+	{ FR_CONF_OFFSET("random_file", FR_TYPE_STRING, fr_curl_tls_t, tls_random_file) },
+	{ FR_CONF_OFFSET("check_cert", FR_TYPE_BOOL, fr_curl_tls_t, tls_check_cert), .dflt = "yes" },
+	{ FR_CONF_OFFSET("check_cert_cn", FR_TYPE_BOOL, fr_curl_tls_t, tls_check_cert_cn), .dflt = "yes" },
+	{ FR_CONF_OFFSET("extract_cert_attrs", FR_TYPE_BOOL, fr_curl_tls_t, tls_extract_cert_attrs), .dflt = "no" },
+	CONF_PARSER_TERMINATOR
+};
+
 int			fr_curl_io_request_enqueue(fr_curl_handle_t *mhandle,
 						   REQUEST *request, fr_curl_io_request_t *creq);
 
@@ -65,6 +102,10 @@ fr_curl_handle_t	*fr_curl_io_init(TALLOC_CTX *ctx, fr_event_list_t *el, bool mul
 int			fr_curl_init(void);
 
 void			fr_curl_free(void);
+
+int 				fr_curl_response_certinfo(REQUEST *request, fr_curl_io_request_t *randle);
+
+int 				fr_curl_easy_tls_init (fr_curl_io_request_t *randle, fr_curl_tls_t *conf);
 
 #ifdef __cplusplus
 }
