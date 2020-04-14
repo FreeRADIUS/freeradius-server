52371782b52a86e5bcad0b45c52fe698fd53a716
Jorge Pereira <jpereiran@gmail.com>
2020-04-14 18:47:07 -0300

    Update misc test to use the test's API

--- src/tests/misc/all.mk
+++ src/tests/misc/all.mk
@@ -1,16 +1,21 @@
+#
+#	Unit tests for misc tests
+#
+
+#
+#	Test name
+#
+TEST := test.misc
+
+$(eval $(call TEST_BOOTSTRAP))
+
 $(BUILD_DIR)/bin/test_cursor: src/lib/util/cursor.c
 	${Q}$(CC) $^ -g3 -Wall -DTESTING_CURSOR $(CPPFLAGS) -I${top_srcdir}/src/lib -I${top_srcdir}/src -include src/include/build.h $(TALLOC_LDFLAGS) $(TALLOC_LIBS) -o $@
-
-OUTPUT := $(BUILD_DIR)/tests/misc
-
-.PHONY: $(OUTPUT)
-$(OUTPUT):
-	@${Q}mkdir -p $@
 
 $(OUTPUT)/cursor.log: $(BUILD_DIR)/bin/test_cursor | $(OUTPUT)
 	${Q}if ! $^ > $@ ; then \
 		echo FAILED "$^ > $@"; \
 	fi
 
-test.misc: $(OUTPUT)/cursor.log
+$(TEST): $(OUTPUT)/cursor.log
 
