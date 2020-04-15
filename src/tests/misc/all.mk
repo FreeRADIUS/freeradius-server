97f1a91ae19581f42b705a6e0b7e90ed69783f2e
Alan T. DeKok <aland@freeradius.org>
2020-04-15 14:05:13 -0400

    show that we're running the test

--- src/tests/misc/all.mk
+++ src/tests/misc/all.mk
@@ -13,9 +13,9 @@ $(BUILD_DIR)/bin/test_cursor: src/lib/util/cursor.c
 	${Q}$(CC) $^ -g3 -Wall -DTESTING_CURSOR $(CPPFLAGS) -I${top_srcdir}/src/lib -I${top_srcdir}/src -include src/include/build.h $(TALLOC_LDFLAGS) $(TALLOC_LIBS) -o $@
 
 $(OUTPUT)/cursor.log: $(BUILD_DIR)/bin/test_cursor | $(OUTPUT)
+	@echo "MISC-TEST cursor"
 	${Q}if ! $^ > $@ ; then \
 		echo FAILED "$^ > $@"; \
 	fi
 
 $(TEST): $(OUTPUT)/cursor.log
-
