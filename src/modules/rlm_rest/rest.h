c6c3529d9c396248e0173b340641aeecf85ce3dd
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2020-04-15 14:41:42 -0500

    Can't have non-null for REQUEST *

--- src/modules/rlm_rest/rest.h
+++ src/modules/rlm_rest/rest.h
@@ -300,7 +300,7 @@ int rest_request_config(rlm_rest_t const *instance, rlm_rest_thread_t *thread,
 			rlm_rest_section_t const *section, REQUEST *request,
 			fr_curl_io_request_t *randle, http_method_t method,
 			http_body_type_t type, char const *uri,
-			char const *username, char const *password) CC_HINT(nonnull (1,2,3,4,5,8));
+			char const *username, char const *password) CC_HINT(nonnull (1,2,3,5,8));
 
 int rest_response_decode(rlm_rest_t const *instance,
 			UNUSED rlm_rest_section_t const *section, REQUEST *request,
