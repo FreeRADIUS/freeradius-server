ca24d9ca913c2614260d3d38ecdc3bbc59a0a979
Alan T. DeKok <aland@freeradius.org>
2020-04-20 20:10:33 -0400

    comment out things we don't need

--- src/build/all.mk
+++ src/build/all.mk
@@ -5,13 +5,13 @@ SOURCES = dlopen.c
 #  If we're building this target, then don't try to use it until we know
 #  that building the target succeeds.
 #
-ifneq "$(MAKECMDGOALS)" "$(TARGET)"
-load ${BUILD_DIR}/lib/.libs/libfreeradius-make-dlopen.dylib(dlopen_gmk_setup)
+#ifneq "$(MAKECMDGOALS)" "$(TARGET)"
+#load ${BUILD_DIR}/lib/.libs/libfreeradius-make-dlopen.dylib(dlopen_gmk_setup)
 
-$(info $(dlopen /home/foo/libcrypto,ASN1_verify,/home/user,/foo,/usr/local/Cellar/openssl@1.1/1.1.1d/lib))
-$(info $(dlsym libcrypto,ASN1_verify))
-$(info $(dlclose libcrypto))
+#$(info $(dlopen /home/foo/libcrypto,ASN1_verify,/home/user,/foo,/usr/local/Cellar/openssl@1.1/1.1.1d/lib))
+#$(info $(dlsym libcrypto,ASN1_verify))
+#$(info $(dlclose libcrypto))
 
-$(info $(dlopen libfoobar))
-$(info $(dlerror ))
-endif
+#$(info $(dlopen libfoobar))
+#$(info $(dlerror ))
+#endif
