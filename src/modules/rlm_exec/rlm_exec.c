0665951ea90002a32eba81a280ce6715d616cbf4
Jorge Pereira <jpereiran@gmail.com>
2020-05-08 15:57:12 -0300

    rlm_exec: Add unlang/interpret.h

--- src/modules/rlm_exec/rlm_exec.c
+++ src/modules/rlm_exec/rlm_exec.c
@@ -30,6 +30,7 @@ RCSID("$Id$")
 #include <freeradius-devel/server/base.h>
 #include <freeradius-devel/server/module.h>
 #include <freeradius-devel/util/debug.h>
+#include <freeradius-devel/unlang/interpret.h>
 
 /*
  *	Define a structure for our module configuration.
