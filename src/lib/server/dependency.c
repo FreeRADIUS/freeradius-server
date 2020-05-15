faaaaad4c914bb370a12fcd392584ca4c09195f5
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2020-05-15 16:26:34 -0500

    Update copyright

--- src/lib/server/dependency.c
+++ src/lib/server/dependency.c
@@ -691,7 +691,7 @@ void dependency_version_print(void)
 		DEBUG2("  ");
 	}
 	INFO("FreeRADIUS Version " RADIUSD_VERSION_STRING);
-	INFO("Copyright 1999-2019 The FreeRADIUS server project and contributors");
+	INFO("Copyright 1999-2020 The FreeRADIUS server project and contributors");
 	INFO("There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A");
 	INFO("PARTICULAR PURPOSE");
 	INFO("You may redistribute copies of FreeRADIUS under the terms of the");
