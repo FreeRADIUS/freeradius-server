c9950785d61c6e022bac2e5ffba3484b59de086f
Jorge Pereira <jpereiran@gmail.com>
2020-05-20 13:46:52 -0300

    radsniff: Add test suite

--- src/tests/radsniff/all.mk
+++ src/tests/radsniff/all.mk
@@ -6,42 +6,57 @@
 #	Test name
 #
 TEST  := test.radsniff
-FILES := $(subst $(DIR)/,,$(wildcard $(DIR)/*.txt $(DIR)/*.gz))
+FILES := $(subst $(DIR)/,,$(wildcard $(DIR)/*.txt))
+
+#
+#	.pcap file to be injested.
+#
+#	It was generated with:
+#	e.g:
+#
+#	TZ=UTC tcpdump -i lo0 -c100 -w radius-auth+acct+coa-100pkts.pcap "port 1812 or port 1813 or port 3799"
+#
+PCAP_IN := $(BUILD_DIR)/tests/radsniff/radius-auth+acct+coa-100pkts.pcap
 
 $(eval $(call TEST_BOOTSTRAP))
 
 #
-#	Run the radsniff commands
+#	Uncompress the input .pcap file
 #
 .PRECIOUS: $(OUTPUT)/%.pcap
 $(OUTPUT)/%.pcap: $(DIR)/%.pcap.gz
 	$(Q)gunzip -c $< > $@
 
-$(OUTPUT)/%.txt: $(DIR)/%.txt $(OUTPUT)/%.pcap $(TESTBINDIR)/radsniff
-	$(eval TARGET   := $(patsubst %.txt,%,$(notdir $@)))
-	$(eval FOUND    := $@)
+#
+#	Run the radsniff commands
+#
+$(OUTPUT)/%.txt: $(DIR)/%.txt $(TESTBINDIR)/radsniff $(PCAP_IN)
+	$(eval TARGET   := $(notdir $@))
+	$(eval FOUND    := $(patsubst %.txt,%.out,$@))
 	$(eval CMD_TEST := $(patsubst %.txt,%.cmd,$<))
 	$(eval EXPECTED := $<)
-	$(eval PCAP_IN  := $(patsubst %.txt,%.pcap,$@))
 	$(eval ARGV     := $(shell grep "^#.*ARGV:" $< | cut -f2 -d ':'))
 
-	$(Q)echo "RADSNIFF-TEST INPUT=$(TARGET).pcap ARGV=\"$(ARGV)\""
-	$(Q)if ! $(TESTBIN)/radsniff $(ARGV) -I $(PCAP_IN) -D share/dictionary 1> $(FOUND) 2>&1; then         \
+	$(Q)echo "RADSNIFF-TEST INPUT=$(TARGET) ARGV=\"$(ARGV)\""
+#
+# 	We need that 'TZ=UTC ...' to libpcap pass the same timestamp in anywhere.
+#
+	$(Q)if ! TZ='UTC' $(TESTBIN)/radsniff $(ARGV) -I $(PCAP_IN) -D share/dictionary 1> $(FOUND); then     \
 		echo "FAILED";                                                                                \
 		cat $(FOUND);                                                                                 \
-		echo "RADSNIFF: $(TESTBIN)/radsniff $(ARGV) -I $(PCAP_IN) -D share/dictionary" -xx;           \
+		echo "RADSNIFF: TZ='UTC' $(TESTBIN)/radsniff $(ARGV) -I $(PCAP_IN) -D share/dictionary" -xx;  \
 		rm -f $@;										      \
 		exit 1;                                                                                       \
 	fi
 	$(Q)if [ -e "$(EXPECTED)" ]; then                                                                     \
-		grep -v "^#" $(EXPECTED) > $(FOUND).result;                                                   \
-		if ! cmp -s $(FOUND) $(FOUND).result; then                                                    \
+		grep -v "^#" $(EXPECTED) > $(FOUND).result || true;                                           \
+		if ! cmp $(FOUND) $(FOUND).result; then                                                       \
 			echo "RADSNIFF FAILED $@";                                                                \
 			echo "RADSNIFF: $(TESTBIN)/radsniff $(ARGV) -I $(PCAP_IN) -D share/dictionary -xx";        \
 			echo "ERROR: File $(FOUND).result is not the same as $(EXPECTED)";                        \
 			echo "If you did some update on the radsniff code, please be sure to update the unit tests."; \
 			echo "e.g: $(EXPECTED)";                                                                      \
-			diff $(EXPECTED) $(FOUND).result;                                                             \
+			diff $(FOUND) $(FOUND).result;                                                                \
 			rm -f $@;										      \
 			exit 1;                                                                                       \
 		fi; \
@@ -57,3 +72,4 @@ $(OUTPUT)/%.txt: $(DIR)/%.txt $(OUTPUT)/%.pcap $(TESTBINDIR)/radsniff
 		exit 1;                                                                                       \
 	fi
 	$(Q)touch $@
+
