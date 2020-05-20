a010bebf45085ae3081a59ceb7645254999a2d0f
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2020-05-20 09:46:48 -0500

    Unused

--- src/lib/util/cursor.c
+++ src/lib/util/cursor.c
@@ -1542,10 +1542,9 @@ void test_cursor_replace_3i_end(void)
 void test_cursor_remove_empty(void)
 {
 	fr_cursor_t	cursor;
-	test_item_t	*item_p;
 	test_item_t	*head = NULL;
 
-	item_p = _fr_cursor_init(&cursor, (void **)&head, offsetof(test_item_t, next), test_iter, &cursor, NULL);
+	_fr_cursor_init(&cursor, (void **)&head, offsetof(test_item_t, next), test_iter, &cursor, NULL);
 	TEST_CHECK(!fr_cursor_remove(&cursor));
 }
 
