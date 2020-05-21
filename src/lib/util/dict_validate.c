d0d976841b69df26e7df063cc54831483dc0c5a4
Alan T. DeKok <aland@freeradius.org>
2020-05-21 08:23:30 -0400

    allow attributes of type "struct" with flag "internal"
    These attributes don't do anything right now, so they can't
    be problematic.
    
    The hope is to allow structs for pack / unpack xlats

--- src/lib/util/dict_validate.c
+++ src/lib/util/dict_validate.c
@@ -352,6 +352,7 @@ bool dict_attr_flags_valid(fr_dict_t *dict, fr_dict_attr_t const *parent,
 	case FR_TYPE_STRUCT:
 		flags->length = 0;
 
+		ALLOW_FLAG(internal);
 		if (all_flags) {
 			fr_strerror_printf("Invalid flag for attribute of type 'struct'");
 			return false;
