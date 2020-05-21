7fd2c5309600dc530047af5291125fed0bc6d943
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2020-05-21 09:19:21 -0500

    log: There are many types of error

--- src/lib/server/log.c
+++ src/lib/server/log.c
@@ -634,7 +634,10 @@ void log_request_error(fr_log_type_t type, fr_log_lvl_t lvl, REQUEST *request,
 	for (dst_p = request->log.dst; dst_p; dst_p = dst_p->next) {
 		dst_p->func(type, lvl, request, file, line, fmt, ap, dst_p->uctx);
 	}
-	if (type == L_ERR) vlog_module_failure_msg(request, fmt, ap);
+	if ((type == L_ERR) || (type == L_DBG_ERR) || (type == L_DBG_ERR_REQ)) {
+		vlog_module_failure_msg(request, fmt, ap);
+	}
+
 	va_end(ap);
 }
 
