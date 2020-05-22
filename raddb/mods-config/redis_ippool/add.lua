d7f4332ec28d1a003b01b39ffacdab29def88475
Alexander Clouter <aclouter@networkradius.com>
2020-05-22 15:18:17 +0100

    remove range if it is unset on add.lua

--- raddb/mods-config/redis_ippool/add.lua
+++ raddb/mods-config/redis_ippool/add.lua
@@ -15,9 +15,12 @@ local address_key
 pool_key = "{" .. KEYS[1] .. "}:" .. ippool_key.pool
 ret = redis.call("ZADD", pool_key, "NX", 0, ARGV[1])
 
+address_key = "{" .. KEYS[1] .. "}:" .. ippool_key.address .. ":" .. ARGV[1]
+
 if ARGV[2] then
-	address_key = "{" .. KEYS[1] .. "}:" .. ippool_key.address .. ":" .. ARGV[1]
 	ret = redis.call("HSET", address_key, "range", ARGV[2]) or ret
+else
+	redis.call("HDEL", address_key, "range")
 end
 
 return {
