24994b86b91a1a1d5d7ff0f7af19aed6866b5804
Alan T. DeKok <aland@freeradius.org>
2020-05-25 10:59:18 -0400

    typo

--- src/lib/util/socket.c
+++ src/lib/util/socket.c
@@ -921,7 +921,7 @@ int fr_socket_bind(int sockfd, fr_ipaddr_t const *src_ipaddr, uint16_t *src_port
 		 */
 		if (state == CAP_CLEAR) {
 			fr_strerror_printf_push("Binding to service port (%i) will likely fail as we lack the correct "
-						"capabilities. "
+						"capabilities. ");
 
 			fr_strerror_printf_push("Use the following command to allow this bind: "
 						"setcap cap_net_bind_service+ep <path_to_binary>", *src_port);
