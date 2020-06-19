855182297ff0bfc2f33ecde28cb715d60d782b9f
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2020-06-19 08:53:57 -0500

    Qualify python shebang with a version as rpmbuild complains....

--- src/modules/rlm_python/example.py
+++ src/modules/rlm_python/example.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #
 # Python module example file
 # Miguel A.L. Paraz <mparaz@mparaz.com>
--- src/modules/rlm_python/prepaid.py
+++ src/modules/rlm_python/prepaid.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #
 # Example Python module for prepaid usage using MySQL
 
