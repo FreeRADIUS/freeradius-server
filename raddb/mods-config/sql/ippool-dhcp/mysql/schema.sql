2b008593f3b5e149449e1864b74b4828d687cb76
Nick Porter <nick@portercomputing.co.uk>
2020-07-03 08:46:42 +0100

    MySQL query improvements
    Changes to queries to improve performance, remove un-used queries and
    make pool_key the unique identifier of a client within a pool. Also
    remove inconsistencies around expiry_time being able to be NULL -
    certain combinations of suggested queries would have ended up using the
    pool and never re-issuing expired IPs.

--- raddb/mods-config/sql/ippool-dhcp/mysql/schema.sql
+++ raddb/mods-config/sql/ippool-dhcp/mysql/schema.sql
@@ -1,18 +1,22 @@
-#
-# Table structure for table 'radippool'
-#
+--
+-- Table structure for table 'radippool'
+--
+-- See also "procedure.sql" in this directory for a stored procedure
+-- that is much faster.
+--
+
 CREATE TABLE radippool (
   id                    int(11) unsigned NOT NULL auto_increment,
   pool_name             varchar(30) NOT NULL,
   framedipaddress       varchar(15) NOT NULL default '',
   nasipaddress          varchar(15) NOT NULL default '',
-  calledstationid       VARCHAR(30) NOT NULL,
-  callingstationid      VARCHAR(30) NOT NULL,
-  expiry_time           DATETIME NULL default NULL,
+  calledstationid       VARCHAR(30) NOT NULL default '',
+  callingstationid      VARCHAR(30) NOT NULL default '',
+  expiry_time           DATETIME NOT NULL default NOW(),
   username              varchar(64) NOT NULL default '',
   pool_key              varchar(30) NOT NULL default '',
   PRIMARY KEY (id),
   KEY radippool_poolname_expire (pool_name, expiry_time),
   KEY framedipaddress (framedipaddress),
-  KEY radippool_nasip_poolkey_ipaddress (nasipaddress, pool_key, framedipaddress)
+  KEY radippool_poolname_poolkey_ipaddress (pool_name, pool_key, framedipaddress)
 ) ENGINE=InnoDB;
