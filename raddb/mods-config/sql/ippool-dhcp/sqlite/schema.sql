533fa802cc419a1a310e6049c75fc638c941b4bf
Nick Porter <nick@portercomputing.co.uk>
2020-07-03 08:53:24 +0100

    SQLite query improvements
    Changes to queries to improve performance, remove un-used queries and
    make pool_key the unique identifier of a client within a pool. Also
    remove inconsistencies around expiry_time being able to be NULL -
    certain combinations of suggested queries would have ended up using the
    pool and never re-issuing expired IPs.

--- raddb/mods-config/sql/ippool-dhcp/sqlite/schema.sql
+++ raddb/mods-config/sql/ippool-dhcp/sqlite/schema.sql
@@ -6,13 +6,13 @@ CREATE TABLE radippool (
   pool_name             varchar(30) NOT NULL,
   framedipaddress       varchar(15) NOT NULL default '',
   nasipaddress          varchar(15) NOT NULL default '',
-  calledstationid       VARCHAR(30) NOT NULL,
-  callingstationid      VARCHAR(30) NOT NULL,
-  expiry_time           DATETIME NULL default NULL,
+  calledstationid       VARCHAR(30) NOT NULL default '',
+  callingstationid      VARCHAR(30) NOT NULL default '',
+  expiry_time           DATETIME NOT NULL default (DATETIME('now')),
   username              varchar(64) NOT NULL default '',
-  pool_key              varchar(30) NOT NULL
+  pool_key              varchar(30) NOT NULL default ''
 );
 
 CREATE INDEX radippool_poolname_expire ON radippool(pool_name, expiry_time);
 CREATE INDEX radippool_framedipaddress ON radippool(framedipaddress);
-CREATE INDEX radippool_nasip_poolkey_ipaddress ON radippool(nasipaddress, pool_key, framedipaddress);
+CREATE INDEX radippool_nasip_poolkey_ipaddress ON radippool(pool_name, pool_key, framedipaddress);
