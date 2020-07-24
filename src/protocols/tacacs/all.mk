20dc4e66a93a876f4c95ce6703bbfe8f8cfe6e7f
Jorge Pereira <jpereiran@gmail.com>
2020-07-24 18:58:39 -0300

    tacacs: Adjusting library build to match all.mk from other protocols

--- src/protocols/tacacs/all.mk
+++ src/protocols/tacacs/all.mk
@@ -1,4 +1,9 @@
+#
+# Makefile
+#
+# Version:      $Id$
+#
 TARGET		:= libfreeradius-tacacs.a
-
-SRC_CFLAGS	:= -DNO_ASSERT
 SOURCES		:= base.c decode.c encode.c
+SRC_CFLAGS	:= -D_LIBRADIUS -DNO_ASSERT -I$(top_builddir)/src
+TGT_PREREQS	:= libfreeradius-util.a
