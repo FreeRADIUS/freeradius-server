9e723f5b62662fa0bdab269eec0f8b463f0c5d96
Nick Porter <nick@portercomputing.co.uk>
2020-08-17 17:08:34 +0100

    Update DHCP queries to reflect new names

--- raddb/mods-config/sql/ippool-dhcp/sqlite/schema.sql
+++ raddb/mods-config/sql/ippool-dhcp/sqlite/schema.sql
@@ -4,7 +4,7 @@
 CREATE TABLE dhcpstatus (
   status_id             int PRIMARY KEY,
   status		varchar(10) NOT NULL
-)
+);
 
 INSERT INTO dhcpstatus (status_id, status) VALUES (1, 'dynamic'), (2, 'static'), (3, 'declined'), (4, 'disabled');
 
@@ -24,8 +24,8 @@ CREATE INDEX dhcpippool_framedipaddress ON dhcpippool(framedipaddress);
 CREATE INDEX dhcpippool_poolname_poolkey ON dhcpippool(pool_name, pool_key, framedipaddress);
 
 -- Example of how to put IPs in the pool
--- INSERT INTO radippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.10');
--- INSERT INTO radippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.11');
--- INSERT INTO radippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.12');
--- INSERT INTO radippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.13');
+-- INSERT INTO dhcpippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.10');
+-- INSERT INTO dhcpippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.11');
+-- INSERT INTO dhcpippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.12');
+-- INSERT INTO dhcpippool (pool_name, framedipaddress) VALUES ('local', '192.168.5.13');
 
