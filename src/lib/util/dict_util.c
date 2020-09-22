f55d6468f8c5e11b749b9a2e2849d3e3dfe3ed31
Alan T. DeKok <aland@freeradius.org>
2020-09-22 09:36:28 -0400

    always include 'da' in it's own tlv_stack

--- src/lib/util/dict_util.c
+++ src/lib/util/dict_util.c
@@ -383,7 +383,7 @@ fr_dict_attr_t *dict_attr_alloc_name(TALLOC_CTX *ctx, fr_dict_attr_t const *pare
 
 	if (parent) depth = parent->depth + 1;
 
-	da = talloc_zero_size(ctx, sizeof(fr_dict_attr_t) + sizeof(da->tlv_stack[0]) * depth);
+	da = talloc_zero_size(ctx, sizeof(fr_dict_attr_t) + sizeof(da->tlv_stack[0]) * (depth + 1));
 	if (!da) return NULL;
 
 	talloc_set_type(da, fr_dict_attr_t);
@@ -403,12 +403,14 @@ fr_dict_attr_t *dict_attr_alloc_name(TALLOC_CTX *ctx, fr_dict_attr_t const *pare
 		da->parent = parent;
 		da->depth = depth;
 
-		fr_assert(depth > 0);
-
 		memcpy(&da->tlv_stack[0], &parent->tlv_stack[0],
-		       sizeof(da->tlv_stack[0]) * (depth - 1));
-		da->tlv_stack[depth - 1] = parent;
+		       sizeof(da->tlv_stack[0]) * depth);
 	}
+
+	/*
+	 *	Always set the last stack entry to ourselves.
+	 */
+	da->tlv_stack[depth] = da;
 
 	return da;
 }
