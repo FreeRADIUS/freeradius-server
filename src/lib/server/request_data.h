49f854452a47aa405edfebd175504d2e39a59d28
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2020-11-13 16:12:01 -0600

    Add doxygen headers for request_data_add
    It's annoying to try and puzzle out what all the arguments mean.

--- src/lib/server/request_data.h
+++ src/lib/server/request_data.h
@@ -35,10 +35,54 @@ typedef struct request_data_s request_data_t;
 
 void		request_data_list_init(fr_dlist_head_t *data);
 
+/** Add opaque data to a request_t
+ *
+ * The unique ptr is meant to be a module configuration, and the unique
+ * integer allows the caller to have multiple opaque data associated with a request_t.
+ *
+ * @param[in] _request		to associate data with.
+ * @param[in] _unique_ptr	Identifier for the data.
+ * @param[in] _unique_int	Qualifier for the identifier.
+ * @param[in] _opaque		Data to associate with the request.  May be NULL.
+ * @param[in] _free_on_replace	Free opaque data if this request_data is replaced.
+ * @param[in] _free_on_parent	Free opaque data if the request is freed.
+ *				Must not be set if the opaque data is also parented by
+ *				the request or state (double free).
+ * @param[in] _persist		Transfer request data to an #fr_state_entry_t, and
+ *				add it back to the next request we receive for the
+ *				session.
+ * @return
+ *	- -2 on bad arguments.
+ *	- -1 on memory allocation error.
+ *	- 0 on success.
+ */
 #define request_data_add(_request, _unique_ptr, _unique_int, _opaque, _free_on_replace, _free_on_parent, _persist) \
 		_request_data_add(_request, _unique_ptr, _unique_int, NULL, _opaque,  \
 				  _free_on_replace, _free_on_parent, _persist, __FILE__, __LINE__)
 
+
+/** Add opaque data to a request_t
+ *
+ * The unique ptr is meant to be a module configuration, and the unique
+ * integer allows the caller to have multiple opaque data associated with a request_t.
+ *
+ * @param[in] _request		to associate data with.
+ * @param[in] _unique_ptr	Identifier for the data.
+ * @param[in] _unique_int	Qualifier for the identifier.
+ * @param[in] _type		Type of data i.e. fr_pair_t.
+ * @param[in] _opaque		Data to associate with the request.  May be NULL.
+ * @param[in] _free_on_replace	Free opaque data if this request_data is replaced.
+ * @param[in] _free_on_parent	Free opaque data if the request is freed.
+ *				Must not be set if the opaque data is also parented by
+ *				the request or state (double free).
+ * @param[in] _persist		Transfer request data to an #fr_state_entry_t, and
+ *				add it back to the next request we receive for the
+ *				session.
+ * @return
+ *	- -2 on bad arguments.
+ *	- -1 on memory allocation error.
+ *	- 0 on success.
+ */
 #define request_data_talloc_add(_request, _unique_ptr, _unique_int, _type, _opaque, _free_on_replace, _free_on_parent, _persist) \
 		_request_data_add(_request, _unique_ptr, _unique_int, STRINGIFY(_type), _opaque, \
 				  _free_on_replace, _free_on_parent, _persist, __FILE__, __LINE__)
