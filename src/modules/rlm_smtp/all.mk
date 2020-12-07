272b7063e0fe4da3d50ae67c3946d65204dca220
Terry Burton <tez@terryburton.co.uk>
2020-12-07 20:42:43 +0000

    Require cURL >= 7.56.0 for rlm_smtp
    Uses rlm_mime_init() el al.

--- src/modules/rlm_smtp/all.mk
+++ src/modules/rlm_smtp/all.mk
@@ -1,12 +1,22 @@
 #  Check to see if we libfreeradius-curl, as that's a hard dependency
 #  which in turn depends on json-c.
 TARGETNAME	:=
+VERSION		:=
 -include $(top_builddir)/src/lib/curl/all.mk
 TARGET		:=
 
 ifneq "$(TARGETNAME)" ""
+
+#  Require cURL >= 7.56.0
+CURL_X		:= $(word 1,$(subst ., ,$(VERSION)))
+CURL_Y		:= $(word 2,$(subst ., ,$(VERSION)))
+CURL_GOOD	:= $(shell test $(CURL_X) -gt 7 -o \( $(CURL_X) -eq 7 -a $(CURL_Y) -ge 56 \) && echo true)
+
+ifeq ($(CURL_GOOD),true)
 TARGET		:= rlm_smtp.a
 TGT_PREREQS	+= libfreeradius-curl.a
+endif
+
 endif
 
 SOURCES		:= rlm_smtp.c
