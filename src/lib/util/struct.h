6759125e1fd608dc491cbafd3bb63c2d14e15a37
Nick Porter <nick@portercomputing.co.uk>
2020-12-22 17:23:54 +0000

    s/fr_cursor/fr_dcursor/ in util/struct.c

--- src/lib/util/struct.h
+++ src/lib/util/struct.h
@@ -32,20 +32,20 @@ RCSIDH(struct_h, "$Id$")
 extern "C" {
 #endif
 
-typedef ssize_t (*fr_decode_value_t)(TALLOC_CTX *ctx, fr_cursor_t *cursor, fr_dict_t const *dict,
+typedef ssize_t (*fr_decode_value_t)(TALLOC_CTX *ctx, fr_dcursor_t *cursor, fr_dict_t const *dict,
 				     fr_dict_attr_t const *parent,
 				     uint8_t const *data, size_t const data_len, void *decoder_ctx);
 
-ssize_t fr_struct_from_network(TALLOC_CTX *ctx, fr_cursor_t *cursor,
+ssize_t fr_struct_from_network(TALLOC_CTX *ctx, fr_dcursor_t *cursor,
 			       fr_dict_attr_t const *parent, uint8_t const *data, size_t data_len,
 			       void *decoder_ctx,
 			       fr_decode_value_t decode_value, fr_decode_value_t decode_tlv) CC_HINT(nonnull(2,3,4));
 
 typedef ssize_t (*fr_encode_dbuff_t)(fr_dbuff_t *dbuff, fr_da_stack_t *da_stack, unsigned int depth,
-				     fr_cursor_t *cursor, void *encoder_ctx);
+				     fr_dcursor_t *cursor, void *encoder_ctx);
 	
 ssize_t fr_struct_to_network(fr_dbuff_t *dbuff, fr_da_stack_t *da_stack, unsigned int depth,
-			     fr_cursor_t *cursor, void *encoder_ctx,
+			     fr_dcursor_t *cursor, void *encoder_ctx,
 			     fr_encode_dbuff_t encode_value, fr_encode_dbuff_t encode_tlv) CC_HINT(nonnull(1,2,4));
 
 fr_pair_t *fr_raw_from_network(TALLOC_CTX *ctx, fr_dict_attr_t const *parent,
