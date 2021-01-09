a48ae1a02e8766b0dcdbefdcc929144301d0df65
Nick Porter <nick@portercomputing.co.uk>
2021-01-09 15:00:17 +0000

    s/fr_cursor/fr_dcursor/ in dhcpv4/packet.c

--- src/protocols/dhcpv4/dhcpv4.h
+++ src/protocols/dhcpv4/dhcpv4.h
@@ -170,7 +170,7 @@ ssize_t		fr_dhcpv4_encode_option(fr_dbuff_t *dbuff, fr_dcursor_t *cursor, void *
  */
 uint8_t const	*fr_dhcpv4_packet_get_option(dhcp_packet_t const *packet, size_t packet_size, fr_dict_attr_t const *da);
 
-int		fr_dhcpv4_decode(TALLOC_CTX *ctx, uint8_t const *data, size_t data_len, fr_cursor_t *cursor, unsigned int *code);
+int		fr_dhcpv4_decode(TALLOC_CTX *ctx, uint8_t const *data, size_t data_len, fr_dcursor_t *cursor, unsigned int *code);
 
 int		fr_dhcpv4_packet_encode(fr_radius_packet_t *packet, fr_pair_list_t *list);
 
