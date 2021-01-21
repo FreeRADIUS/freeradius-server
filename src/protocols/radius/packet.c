f8e21e54e51e84ab3c44c7aaf8613f74a60b3c71
Alan T. DeKok <aland@freeradius.org>
2021-01-21 14:58:31 -0500

    try again to shut up clang scanner
    which complains in CI.  Not in local tests.

--- src/protocols/radius/packet.c
+++ src/protocols/radius/packet.c
@@ -179,7 +179,6 @@ int fr_radius_packet_decode(fr_radius_packet_t *packet, fr_pair_list_t *list,
 	 */
 	hdr = (radius_packet_t *)packet->data;
 	ptr = hdr->data;
-	fr_assert(ptr != NULL);
 	packet_length = packet->data_len - RADIUS_HEADER_LENGTH;
 	num_attributes = 0;
 
@@ -194,6 +193,7 @@ int fr_radius_packet_decode(fr_radius_packet_t *packet, fr_pair_list_t *list,
 		/*
 		 *	This may return many VPs
 		 */
+		fr_assert(ptr != NULL);
 		my_len = fr_radius_decode_pair(packet, &cursor, dict_radius, ptr, packet_length, &packet_ctx);
 		if (my_len < 0) {
 		fail:
