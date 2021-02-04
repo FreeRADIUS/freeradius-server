759befd7ae2b1ebb7c9df5fb06324d9adc5a8439
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-02-04 10:50:54 +0000

    Whitespace

--- src/lib/util/dict.h
+++ src/lib/util/dict.h
@@ -305,7 +305,7 @@ int			fr_dict_attr_add(fr_dict_t *dict, fr_dict_attr_t const *parent, char const
 					 fr_type_t type, fr_dict_attr_flags_t const *flags) CC_HINT(nonnull(1,2,3));
 
 int			fr_dict_enum_add_name(fr_dict_attr_t *da, char const *name,
-						   fr_value_box_t const *value, bool coerce, bool replace);
+					      fr_value_box_t const *value, bool coerce, bool replace);
 
 int			fr_dict_enum_add_name_next(fr_dict_attr_t *da, char const *name) CC_HINT(nonnull);
 
