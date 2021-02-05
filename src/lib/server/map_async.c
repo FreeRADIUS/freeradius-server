60dacea5aa49481db8a0491dc2549ffa24c36992
Nick Porter <nick@portercomputing.co.uk>
2021-02-05 18:09:30 +0000

    Ensure fr_map_list_t is initialised when allocating vp_list_mod_t

--- src/lib/server/map_async.c
+++ src/lib/server/map_async.c
@@ -43,7 +43,10 @@ RCSID("$Id$")
 
 static inline vp_list_mod_t *list_mod_alloc(TALLOC_CTX *ctx)
 {
-	return talloc_zero(ctx, vp_list_mod_t);
+	vp_list_mod_t *mod;
+	mod = talloc_zero(ctx, vp_list_mod_t);
+	fr_map_list_init(&mod->mod);
+	return mod;
 }
 
 static inline map_t *map_alloc(TALLOC_CTX *ctx)
