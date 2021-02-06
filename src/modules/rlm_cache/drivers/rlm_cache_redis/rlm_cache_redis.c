357b37071c2e56f820598d17dfa3c83f0b6c1a99
Nick Porter <nick@portercomputing.co.uk>
2021-02-06 15:21:15 +0000

    Cannot just point two map items at the start of a list now

--- src/modules/rlm_cache/drivers/rlm_cache_redis/rlm_cache_redis.c
+++ src/modules/rlm_cache/drivers/rlm_cache_redis/rlm_cache_redis.c
@@ -263,7 +263,7 @@ static cache_status_t cache_entry_insert(UNUSED rlm_cache_config_t const *config
 	rlm_cache_redis_t	*driver = instance;
 	TALLOC_CTX		*pool;
 
-	map_t		*map;
+	map_t			*map = NULL;
 
 	fr_redis_conn_t		*conn;
 	fr_redis_cluster_state_t	state;
@@ -295,7 +295,6 @@ static cache_status_t cache_entry_insert(UNUSED rlm_cache_config_t const *config
 					.op	= T_OP_SET,
 					.lhs	= driver->created_attr,
 					.rhs	= &created_value,
-					.next	= &expires
 				};
 
 	/*
@@ -314,9 +313,8 @@ static cache_status_t cache_entry_insert(UNUSED rlm_cache_config_t const *config
 	tmpl_init_shallow(&expires_value, TMPL_TYPE_DATA, T_BARE_WORD, "<TEMP>", 6);
 	fr_value_box_init(&expires_value.data.literal, FR_TYPE_DATE, NULL, true);
 	tmpl_value(&expires_value)->vb_date = c->expires;
-	expires.next = c->maps;	/* Head of the list */
 
-	for (cnt = 0, map = &created; map; cnt++, map = map->next);
+	cnt = c->maps.num_elements + 2;
 
 	/*
 	 *	The majority of serialized entries should be under 1k.
@@ -338,7 +336,21 @@ static cache_status_t cache_entry_insert(UNUSED rlm_cache_config_t const *config
 	/*
 	 *	Add the maps to the command string in reverse order
 	 */
-	for (map = &created; map; map = map->next) {
+	if (fr_redis_tuple_from_map(pool, argv_p, argv_len_p, &created) < 0) {
+		REDEBUG("Failed encoding map as Redis K/V pair");
+		talloc_free(pool);
+		return CACHE_ERROR;
+	}
+	argv_p += 3;
+	argv_len_p += 3;
+	if (fr_redis_tuple_from_map(pool, argv_p, argv_len_p, &expires) < 0) {
+		REDEBUG("Failed encoding map as Redis K/V pair");
+		talloc_free(pool);
+		return CACHE_ERROR;
+	}
+	argv_p += 3;
+	argv_len_p += 3;
+	while ((map = fr_dlist_next(&c->maps, map))) {
 		if (fr_redis_tuple_from_map(pool, argv_p, argv_len_p, map) < 0) {
 			REDEBUG("Failed encoding map as Redis K/V pair");
 			talloc_free(pool);
