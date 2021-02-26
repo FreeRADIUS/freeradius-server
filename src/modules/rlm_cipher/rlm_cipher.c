a480efad3292147fb8cb626c37009bd44ebff732
Nick Porter <nick@portercomputing.co.uk>
2021-02-26 18:10:22 +0000

    Amend cipher_rsa_decrypt_xlat() to take fr_value_box_list_t

--- src/modules/rlm_cipher/rlm_cipher.c
+++ src/modules/rlm_cipher/rlm_cipher.c
@@ -614,7 +614,7 @@ static xlat_action_t cipher_rsa_sign_xlat(TALLOC_CTX *ctx, fr_dcursor_t *out,
  */
 static xlat_action_t cipher_rsa_decrypt_xlat(TALLOC_CTX *ctx, fr_dcursor_t *out,
 					     request_t *request, UNUSED void const *xlat_inst, void *xlat_thread_inst,
-					     fr_value_box_t **in)
+					     fr_value_box_list_t *in)
 {
 	rlm_cipher_rsa_thread_inst_t	*xt = talloc_get_type_abort(*((void **)xlat_thread_inst),
 								    rlm_cipher_rsa_thread_inst_t);
@@ -626,18 +626,19 @@ static xlat_action_t cipher_rsa_decrypt_xlat(TALLOC_CTX *ctx, fr_dcursor_t *out,
 	size_t				plaintext_len;
 
 	fr_value_box_t			*vb;
+	fr_value_box_t			*in_head = fr_dlist_head(in);
 
-	if (!*in) {
+	if (!in_head) {
 		REDEBUG("decrypt requires one or more arguments (<ciphertext>...)");
 		return XLAT_ACTION_FAIL;
 	}
 
-	if (fr_value_box_list_concat(ctx, *in, in, FR_TYPE_OCTETS, true) < 0) {
+	if (fr_value_box_list_concat(ctx, in_head, in, FR_TYPE_OCTETS, true) < 0) {
 		REDEBUG("Failed concatenating arguments to form plaintext");
 		return XLAT_ACTION_FAIL;
 	}
-	ciphertext = (*in)->vb_octets;
-	ciphertext_len = (*in)->vb_length;
+	ciphertext = in_head->vb_octets;
+	ciphertext_len = in_head->vb_length;
 
 	/*
 	 *	Decrypt the ciphertext
