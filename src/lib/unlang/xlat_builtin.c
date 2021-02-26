4d42f6bd223f47074c0a68bfeb4da25aea923ac0
Nick Porter <nick@portercomputing.co.uk>
2021-02-26 17:58:03 +0000

    Amend xlat_func_urlquote() to take fr_value_box_list_t

--- src/lib/unlang/xlat_builtin.c
+++ src/lib/unlang/xlat_builtin.c
@@ -3127,28 +3127,29 @@ static xlat_action_t xlat_func_toupper(TALLOC_CTX *ctx, fr_dcursor_t *out,
  */
 static xlat_action_t xlat_func_urlquote(TALLOC_CTX *ctx, fr_dcursor_t *out,
 					request_t *request, UNUSED void const *xlat_inst, UNUSED void *xlat_thread_inst,
-					fr_value_box_t **in)
+					fr_value_box_list_t *in)
 {
 	char const	*p, *end;
 	char		*buff_p;
 	size_t		outlen = 0;
 	fr_value_box_t	*vb;
+	fr_value_box_t	*in_head = fr_dlist_head(in);
 
 	/*
 	 * Nothing to do if input is empty
 	 */
-	if (!(*in)) return XLAT_ACTION_DONE;
+	if (!in_head) return XLAT_ACTION_DONE;
 
 	/*
 	 * Concatenate all input
 	 */
-	if (fr_value_box_list_concat(ctx, *in, in, FR_TYPE_STRING, true) < 0) {
+	if (fr_value_box_list_concat(ctx, in_head, in, FR_TYPE_STRING, true) < 0) {
 		RPEDEBUG("Failed concatenating input");
 		return XLAT_ACTION_FAIL;
 	}
 
-	p = (*in)->vb_strvalue;
-	end = p + (*in)->vb_length;
+	p = in_head->vb_strvalue;
+	end = p + in_head->vb_length;
 
 	/*
 	 * Calculate size of output
@@ -3170,7 +3171,7 @@ static xlat_action_t xlat_func_urlquote(TALLOC_CTX *ctx, fr_dcursor_t *out,
 	MEM(fr_value_box_bstr_alloc(vb, &buff_p, vb, NULL, outlen, false) == 0);
 
 	/* Reset p to start position */
-	p = (*in)->vb_strvalue;
+	p = in_head->vb_strvalue;
 
 	while (p < end) {
 		if (isalnum(*p)) {
