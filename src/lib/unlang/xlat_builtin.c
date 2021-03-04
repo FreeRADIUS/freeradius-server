6a13b10c571f91256abed9753e84ea868c94e349
Nick Porter <nick@portercomputing.co.uk>
2021-03-04 16:50:52 +0000

    Define arg for strlen xlat

--- src/lib/unlang/xlat_builtin.c
+++ src/lib/unlang/xlat_builtin.c
@@ -2761,6 +2761,11 @@ static xlat_action_t xlat_func_string(UNUSED TALLOC_CTX *ctx, fr_dcursor_t *out,
 }
 
 
+static xlat_arg_parser_t const xlat_func_strlen_arg = {
+	.concat = true,
+	.type = FR_TYPE_STRING
+};
+
 /** Print length of given string
  *
  * Example:
@@ -2773,29 +2778,20 @@ static xlat_action_t xlat_func_string(UNUSED TALLOC_CTX *ctx, fr_dcursor_t *out,
  * @ingroup xlat_functions
  */
 static xlat_action_t xlat_func_strlen(TALLOC_CTX *ctx, fr_dcursor_t *out,
-				      request_t *request, UNUSED void const *xlat_inst,
+				      UNUSED request_t *request, UNUSED void const *xlat_inst,
 				      UNUSED void *xlat_thread_inst, fr_value_box_list_t *in)
 {
 	fr_value_box_t	*vb;
 	fr_value_box_t	*in_head = fr_dlist_head(in);
 
-	if (!in_head) {
-		MEM(vb = fr_value_box_alloc(ctx, FR_TYPE_SIZE, NULL, false));
-		vb->vb_size = 0;
-		fr_dcursor_append(out, vb);
-		return XLAT_ACTION_DONE;
-	}
-
-	/*
-	 *	Concatenate all input
-	 */
-	if (fr_value_box_list_concat(ctx, in_head, in, FR_TYPE_STRING, true) < 0) {
-		RPEDEBUG("Failed concatenating input");
-		return XLAT_ACTION_FAIL;
-	}
-
 	MEM(vb = fr_value_box_alloc(ctx, FR_TYPE_SIZE, NULL, false));
-	vb->vb_size = strlen(in_head->vb_strvalue);
+
+	if (!in_head) {
+		vb->vb_size = 0;
+	} else {
+		vb->vb_size = strlen(in_head->vb_strvalue);
+	}
+
 	fr_dcursor_append(out, vb);
 
 	return XLAT_ACTION_DONE;
@@ -3419,8 +3415,8 @@ do { \
 #  endif
 #endif
 
-	xlat_register(NULL, "strlen", xlat_func_strlen, false);
 	XLAT_REGISTER_MONO("string", xlat_func_string, xlat_func_string_arg);
+	XLAT_REGISTER_MONO("strlen", xlat_func_strlen, xlat_func_strlen_arg);
 	xlat_register(NULL, "sub", xlat_func_sub, false);
 	xlat_register(NULL, "tolower", xlat_func_tolower, false);
 	xlat_register(NULL, "toupper", xlat_func_toupper, false);
