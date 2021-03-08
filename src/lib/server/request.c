3c8115a4ff42f062ab841ee29e255e2f6d79b2be
Alan T. DeKok <aland@freeradius.org>
2021-03-08 10:27:31 -0500

    assert that the various heap IDs are marked as "free"

--- src/lib/server/request.c
+++ src/lib/server/request.c
@@ -290,6 +290,9 @@ static inline CC_HINT(always_inline) int request_init(char const *file, int line
  */
 static int _request_free(request_t *request)
 {
+	fr_assert(request->time_order_id <= 0);
+	fr_assert(request->runnable_id <= 0);
+
 	/*
 	 *	Reinsert into the free list if it's not already
 	 *	in the free list.
