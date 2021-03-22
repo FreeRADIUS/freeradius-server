542422a46fd59305aa62eec00fe45ee905311297
Terry Burton <tez@terryburton.co.uk>
2021-03-22 00:22:33 +0000

    Linter: Python fixes

--- src/modules/rlm_python/example.py
+++ src/modules/rlm_python/example.py
@@ -7,59 +7,69 @@
 
 import freeradius
 
+
 def instantiate(p):
-  print("*** instantiate ***")
-  print(p)
-  # return 0 for success or -1 for failure
+    print("*** instantiate ***")
+    print(p)
+    # return 0 for success or -1 for failure
+
 
 def authorize(p):
-  print("*** authorize ***")
-  print("")
-  freeradius.log(freeradius.L_INFO, '*** log call in authorize ***')
-  print("")
-  print(p)
-  print("")
-  print(freeradius.config)
-  print("")
-  return freeradius.RLM_MODULE_OK
+    print("*** authorize ***")
+    print("")
+    freeradius.log(freeradius.L_INFO, "*** log call in authorize ***")
+    print("")
+    print(p)
+    print("")
+    print(freeradius.config)
+    print("")
+    return freeradius.RLM_MODULE_OK
+
 
 def preacct(p):
-  print("*** preacct ***")
-  print(p)
-  return freeradius.RLM_MODULE_OK
+    print("*** preacct ***")
+    print(p)
+    return freeradius.RLM_MODULE_OK
+
 
 def accounting(p):
-  print("*** accounting ***")
-  freeradius.log(freeradius.L_INFO, '*** log call in accounting (0) ***')
-  print("")
-  print(p)
-  return freeradius.RLM_MODULE_OK
+    print("*** accounting ***")
+    freeradius.log(freeradius.L_INFO, "*** log call in accounting (0) ***")
+    print("")
+    print(p)
+    return freeradius.RLM_MODULE_OK
+
 
 def pre_proxy(p):
-  print("*** pre_proxy ***")
-  print(p)
-  return freeradius.RLM_MODULE_OK
+    print("*** pre_proxy ***")
+    print(p)
+    return freeradius.RLM_MODULE_OK
+
 
 def post_proxy(p):
-  print("*** post_proxy ***")
-  print(p)
-  return freeradius.RLM_MODULE_OK
+    print("*** post_proxy ***")
+    print(p)
+    return freeradius.RLM_MODULE_OK
+
 
 def post_auth(p):
-  print("*** post_auth ***")
-  print(p)
-  return freeradius.RLM_MODULE_OK
+    print("*** post_auth ***")
+    print(p)
+    return freeradius.RLM_MODULE_OK
+
 
 def recv_coa(p):
-  print("*** recv_coa ***")
-  print(p)
-  return freeradius.RLM_MODULE_OK
+    print("*** recv_coa ***")
+    print(p)
+    return freeradius.RLM_MODULE_OK
+
 
 def send_coa(p):
-  print("*** send_coa ***")
-  print(p)
-  return freeradius.RLM_MODULE_OK
+    print("*** send_coa ***")
+    print(p)
+    return freeradius.RLM_MODULE_OK
+
 
 def detach(p):
-  print("*** goodbye from example.py ***")
-  return freeradius.RLM_MODULE_OK
+    print("*** goodbye from example.py ***")
+    return freeradius.RLM_MODULE_OK
--- src/tests/modules/python/mod_shared_storage.py
+++ src/tests/modules/python/mod_shared_storage.py
@@ -1,10 +1,14 @@
 import freeradius
 import shared
 
+
 def authorize(p):
-  freeradius.log(freeradius.L_DBG, 'Python - shared_attribute=' + str(hasattr(shared, 'shared_attribute')))
-  if not hasattr(shared, 'shared_attribute'):
-    setattr(shared, 'shared_attribute', True)
-    return freeradius.RLM_MODULE_NOOP
-  else:
-     return freeradius.RLM_MODULE_OK
+    freeradius.log(
+        freeradius.L_DBG,
+        "Python - shared_attribute=" + str(hasattr(shared, "shared_attribute")),
+    )
+    if not hasattr(shared, "shared_attribute"):
+        setattr(shared, "shared_attribute", True)
+        return freeradius.RLM_MODULE_NOOP
+    else:
+        return freeradius.RLM_MODULE_OK
