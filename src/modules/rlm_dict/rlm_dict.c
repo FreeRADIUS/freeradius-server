9149adc0377dc29e24890eda0bc9e2f9958163f3
Nick Porter <nick@portercomputing.co.uk>
2021-03-29 15:27:36 +0100

    v4: Convert dict xlats to new api (#4039)
    * Convert %(attr_by_num: ) to new xlat api
    
    * Convert %(attr_by_oid: ) to new xlat api
    
    * Convert %(vendor: ) to new xlat api
    
    * Convert %(vendor_num: ) to new xlat api
    
    * Convert %(attr: ) to new xlat api
    
    * Convert %(attr_num: ) to new xlat api

--- src/modules/rlm_dict/rlm_dict.c
+++ src/modules/rlm_dict/rlm_dict.c
@@ -27,138 +27,192 @@ RCSID("$Id$")
 #include <freeradius-devel/server/module.h>
 #include <freeradius-devel/util/debug.h>
 
-/** Xlat for %{attr_by_num:\<number\>}
+static xlat_arg_parser_t const xlat_dict_attr_by_num_args[] = {
+	{ .required = true, .single = true, .type = FR_TYPE_UINT32 },
+	XLAT_ARG_PARSER_TERMINATOR
+};
+
+/** Xlat for %(attr_by_num:\<number\>)
  *
  * @ingroup xlat_functions
  */
-static ssize_t xlat_dict_attr_by_num(TALLOC_CTX *ctx, char **out, UNUSED size_t outlen,
-				     UNUSED void const *mod_inst, UNUSED void const *xlat_inst,
-				     request_t *request, char const *fmt)
+static xlat_action_t xlat_dict_attr_by_num(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request,
+					  UNUSED void const *xlat_inst, UNUSED void *xlat_thread_inst,
+					  fr_value_box_list_t *in)
 {
-	char			*q;
-	unsigned int		number;
 	fr_dict_attr_t const	*da;
+	fr_value_box_t		*attr = fr_dlist_head(in);
+	fr_value_box_t		*vb;
 
-	*out = NULL;
-
-	number = (unsigned int)strtoul(fmt, &q, 10);
-	if ((q == fmt) || (*q != '\0')) {
-		REDEBUG("Trailing garbage \"%s\" in attribute number string \"%s\"", q, fmt);
-		return -1;
-	}
-
-	da = fr_dict_attr_child_by_num(fr_dict_root(request->dict), number);
+	da = fr_dict_attr_child_by_num(fr_dict_root(request->dict), attr->vb_uint32);
 	if (!da) {
-		REDEBUG("No attribute found with number %u", number);
-		return -1;
+		REDEBUG("No attribute found with number %pV", attr);
+		return XLAT_ACTION_FAIL;
 	}
 
-	*out = talloc_typed_strdup(ctx, da->name);
+	MEM(vb = fr_value_box_alloc_null(ctx));
 
-	return talloc_array_length(*out) - 1;
+	if (fr_value_box_bstrndup(ctx, vb, NULL, da->name, strlen(da->name), false) < 0) {
+		talloc_free(vb);
+		return XLAT_ACTION_FAIL;
+	}
+
+	fr_dcursor_append(out, vb);
+	return XLAT_ACTION_DONE;
 }
 
-/** Xlat for %{attr_by_oid:\<oid\>}
+static xlat_arg_parser_t const xlat_dict_attr_by_oid_args[] = {
+	{ .required = true, .single = true, .type = FR_TYPE_STRING },
+	XLAT_ARG_PARSER_TERMINATOR
+};
+
+/** Xlat for %(attr_by_oid:\<oid\>)
  *
  * @ingroup xlat_functions
  */
-static ssize_t xlat_dict_attr_by_oid(TALLOC_CTX *ctx, char **out, UNUSED size_t outlen,
-				     UNUSED void const *mod_inst, UNUSED void const *xlat_inst,
-				     request_t *request, char const *fmt)
+static xlat_action_t xlat_dict_attr_by_oid(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request,
+					   UNUSED void const *xlat_inst, UNUSED void *xlat_thread_inst,
+					   fr_value_box_list_t *in)
 {
 	unsigned int		attr = 0;
 	fr_dict_attr_t const	*parent = fr_dict_root(request->dict);
 	fr_dict_attr_t const	*da;
-	ssize_t		ret;
+	ssize_t			ret;
+	fr_value_box_t		*attr_vb = fr_dlist_head(in);
+	fr_value_box_t		*vb;
 
-	ret = fr_dict_attr_by_oid_legacy(fr_dict_internal(), &parent, &attr, fmt);
+	ret = fr_dict_attr_by_oid_legacy(fr_dict_internal(), &parent, &attr, attr_vb->vb_strvalue);
 	if (ret <= 0) {
-		REMARKER(fmt, -(ret), "%s", fr_strerror());
-		return ret;
+		REMARKER(attr_vb->vb_strvalue, -(ret), "%s", fr_strerror());
+		return XLAT_ACTION_FAIL;
 	}
 
 	da = fr_dict_attr_child_by_num(parent, attr);
 
-	*out = talloc_typed_strdup(ctx, da->name);
-	return talloc_array_length(*out) - 1;
+	MEM(vb = fr_value_box_alloc_null(ctx));
+
+	if (fr_value_box_bstrndup(ctx, vb, NULL, da->name, strlen(da->name), false) < 0) {
+		talloc_free(vb);
+		return XLAT_ACTION_FAIL;
+	}
+
+	fr_dcursor_append(out, vb);
+	return XLAT_ACTION_DONE;
 }
 
+static xlat_arg_parser_t const xlat_vendor_args[] = {
+	{ .required = true, .single = true, .type = FR_TYPE_STRING },
+	XLAT_ARG_PARSER_TERMINATOR
+};
 
 /** Return the vendor of an attribute reference
  *
  * @ingroup xlat_functions
  */
-static ssize_t xlat_vendor(TALLOC_CTX *ctx, char **out, UNUSED size_t outlen,
-			   UNUSED void const *mod_inst, UNUSED void const *xlat_inst,
-			   request_t *request, char const *fmt)
+static xlat_action_t xlat_vendor(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request,
+				 UNUSED void const *xlat_inst, UNUSED void *xlat_thread_inst,
+				 fr_value_box_list_t *in)
 {
-	fr_pair_t *vp;
-	fr_dict_vendor_t const *vendor;
+	fr_pair_t		*vp;
+	fr_dict_vendor_t const	*vendor;
+	fr_value_box_t		*attr = fr_dlist_head(in);
+	fr_value_box_t		*vb;
 
-	fr_skip_whitespace(fmt);
-
-	if ((xlat_fmt_get_vp(&vp, request, fmt) < 0) || !vp) return 0;
+	if ((xlat_fmt_get_vp(&vp, request, attr->vb_strvalue) < 0) || !vp) return XLAT_ACTION_FAIL;
 
 	vendor = fr_dict_vendor_by_da(vp->da);
-	if (!vendor) return 0;
+	if (!vendor) return XLAT_ACTION_FAIL;
 
-	*out = talloc_typed_strdup(ctx, vendor->name);
-	return talloc_array_length(*out) - 1;
+	MEM(vb = fr_value_box_alloc_null(ctx));
+
+	if (fr_value_box_bstrndup(ctx, vb, NULL, vendor->name, strlen(vendor->name), false) < 0) {
+		talloc_free(vb);
+		return XLAT_ACTION_FAIL;
+	}
+
+	fr_dcursor_append(out, vb);
+	return XLAT_ACTION_DONE;
 }
+
+static xlat_arg_parser_t const xlat_vendor_num_args[] = {
+	{ .required = true, .single = true, .type = FR_TYPE_STRING },
+	XLAT_ARG_PARSER_TERMINATOR
+};
 
 /** Return the vendor number of an attribute reference
  *
  * @ingroup xlat_functions
  */
-static ssize_t xlat_vendor_num(TALLOC_CTX *ctx, char **out, UNUSED size_t outlen,
-			       UNUSED void const *mod_inst, UNUSED void const *xlat_inst,
-			       request_t *request, char const *fmt)
+static xlat_action_t xlat_vendor_num(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request,
+				     UNUSED void const *xlat_inst, UNUSED void *xlat_thread_inst,
+			       	     fr_value_box_list_t *in)
 {
-	fr_pair_t *vp;
+	fr_pair_t	*vp;
+	fr_value_box_t	*attr = fr_dlist_head(in);
+	fr_value_box_t	*vb;
 
-	fr_skip_whitespace(fmt);
+	if ((xlat_fmt_get_vp(&vp, request, attr->vb_strvalue) < 0) || !vp) return XLAT_ACTION_FAIL;
 
-	if ((xlat_fmt_get_vp(&vp, request, fmt) < 0) || !vp) return 0;
-
-	*out = talloc_typed_asprintf(ctx, "%i", fr_dict_vendor_num_by_da(vp->da));
-	return talloc_array_length(*out) - 1;
+	MEM(vb = fr_value_box_alloc_null(ctx));
+	fr_value_box_uint32(vb, NULL, fr_dict_vendor_num_by_da(vp->da), false);
+	fr_dcursor_append(out, vb);
+	return XLAT_ACTION_DONE;
 }
+
+static xlat_arg_parser_t const xlat_attr_args[] = {
+	{ .required = true, .single = true, .type = FR_TYPE_STRING },
+	XLAT_ARG_PARSER_TERMINATOR
+};
 
 /** Return the attribute name of an attribute reference
  *
  * @ingroup xlat_functions
  */
-static ssize_t xlat_attr(TALLOC_CTX *ctx, char **out, size_t outlen,
-			 UNUSED void const *mod_inst, UNUSED void const *xlat_inst,
-			 request_t *request, char const *fmt)
+static xlat_action_t xlat_attr(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request,
+			       UNUSED void const *xlat_inst, UNUSED void *xlat_thread_inst,
+			       fr_value_box_list_t *in)
 {
-	fr_pair_t *vp;
+	fr_pair_t	*vp;
+	fr_value_box_t	*attr = fr_dlist_head(in);
+	fr_value_box_t	*vb;
 
-	fr_skip_whitespace(fmt);
+	if ((xlat_fmt_get_vp(&vp, request, attr->vb_strvalue) < 0) || !vp) return XLAT_ACTION_FAIL;
 
-	if ((xlat_fmt_get_vp(&vp, request, fmt) < 0) || !vp) return 0;
-	strlcpy(*out, vp->da->name, outlen);
+	MEM(vb = fr_value_box_alloc_null(ctx));
 
-	*out = talloc_typed_strdup(ctx, vp->da->name);
-	return talloc_array_length(*out) - 1;
+	if (fr_value_box_bstrndup(ctx, vb, NULL, vp->da->name, strlen(vp->da->name), false) < 0) {
+		talloc_free(vb);
+		return XLAT_ACTION_FAIL;
+	}
+
+	fr_dcursor_append(out, vb);
+	return XLAT_ACTION_DONE;
 }
+
+static xlat_arg_parser_t const xlat_attr_num_args[] = {
+	{ .required = true, .single = true, .type = FR_TYPE_STRING },
+	XLAT_ARG_PARSER_TERMINATOR
+};
 
 /** Return the attribute number of an attribute reference
  *
  * @ingroup xlat_functions
  */
-static ssize_t xlat_attr_num(TALLOC_CTX *ctx, char **out, UNUSED size_t outlen,
-			     UNUSED void const *mod_inst, UNUSED void const *xlat_inst,
-			     request_t *request, char const *fmt)
+static xlat_action_t xlat_attr_num(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request,
+				   UNUSED void const *xlat_inst, UNUSED void *xlat_thread_inst,
+				   fr_value_box_list_t *in)
 {
-	fr_pair_t *vp;
+	fr_pair_t	*vp;
+	fr_value_box_t	*attr = fr_dlist_head(in);
+	fr_value_box_t	*vb;
 
-	fr_skip_whitespace(fmt);
+	if ((xlat_fmt_get_vp(&vp, request, attr->vb_strvalue) < 0) || !vp) return XLAT_ACTION_FAIL;
 
-	if ((xlat_fmt_get_vp(&vp, request, fmt) < 0) || !vp) return 0;
+	MEM(vb = fr_value_box_alloc_null(ctx));
 
-	*out = talloc_typed_asprintf(ctx, "%i", vp->da->attr);
-	return talloc_array_length(*out) - 1;
+	fr_value_box_uint32(vb, NULL, vp->da->attr, false);
+	fr_dcursor_append(out, vb);
+	return XLAT_ACTION_DONE;
 }
 
 /*
@@ -173,12 +227,19 @@ static ssize_t xlat_attr_num(TALLOC_CTX *ctx, char **out, UNUSED size_t outlen,
  */
 static int mod_bootstrap(void *instance, UNUSED CONF_SECTION *conf)
 {
-	xlat_register_legacy(instance, "attr_by_num", xlat_dict_attr_by_num, NULL, NULL, 0, 0);
-	xlat_register_legacy(instance, "attr_by_oid", xlat_dict_attr_by_oid, NULL, NULL, 0, 0);
-	xlat_register_legacy(instance, "vendor", xlat_vendor, NULL, NULL, 0, 0);
-	xlat_register_legacy(instance, "vendor_num", xlat_vendor_num, NULL, NULL, 0, 0);
-	xlat_register_legacy(instance, "attr", xlat_attr, NULL, NULL, 0, 0);
-	xlat_register_legacy(instance, "attr_num", xlat_attr_num, NULL, NULL, 0, 0);
+	xlat_t	*xlat;
+	xlat = xlat_register(instance, "attr_by_num", xlat_dict_attr_by_num, false);
+	xlat_func_args(xlat, xlat_dict_attr_by_num_args);
+	xlat = xlat_register(instance, "attr_by_oid", xlat_dict_attr_by_oid, false);
+	xlat_func_args(xlat, xlat_dict_attr_by_oid_args);
+	xlat = xlat_register(instance, "vendor", xlat_vendor, false);
+	xlat_func_args(xlat, xlat_vendor_args);
+	xlat = xlat_register(instance, "vendor_num", xlat_vendor_num, false);
+	xlat_func_args(xlat, xlat_vendor_num_args);
+	xlat = xlat_register(instance, "attr", xlat_attr, false);
+	xlat_func_args(xlat, xlat_attr_args);
+	xlat = xlat_register(instance, "attr_num", xlat_attr_num, false);
+	xlat_func_args(xlat, xlat_attr_num_args);
 
 	return 0;
 }
