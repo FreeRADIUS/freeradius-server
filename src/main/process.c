03c5098ffbda19b63eac4fb323cf2b7a905296f3
Alan T. DeKok <aland@freeradius.org>
2021-04-01 10:09:51 -0400

    process coa replies through the coa server

--- src/main/process.c
+++ src/main/process.c
@@ -2520,6 +2520,11 @@ static int process_proxy_reply(REQUEST *request, RADIUS_PACKET *reply)
 	if (request->home_server && request->home_server->server) {
 		request->server = request->home_server->server;
 
+#ifdef WITH_COA_TUNNEL
+	} else if (request->home_server && request->home_server->coa_server) {
+		request->server = request->home_server->coa_server;
+#endif
+
 	} else if (request->home_pool && request->home_pool->virtual_server) {
 		request->server = request->home_pool->virtual_server;
 	}
