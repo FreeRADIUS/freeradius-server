4cb6b367ccb96bc9539501294d1e8ec093c93acf
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-04-02 18:22:51 +0100

    Missing header

--- src/lib/server/trigger.c
+++ src/lib/server/trigger.c
@@ -31,6 +31,7 @@ RCSID("$Id$")
 #include <freeradius-devel/unlang/function.h>
 #include <freeradius-devel/unlang/interpret.h>
 #include <freeradius-devel/util/debug.h>
+#include <waitpid.h>
 
 /** Whether triggers are enabled globally
  *
