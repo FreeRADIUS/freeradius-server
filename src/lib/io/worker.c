83a8a4995571387723171dcc30d8fab739acf749
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-04-06 11:07:07 +0100

    Fix asserts

--- src/lib/io/worker.c
+++ src/lib/io/worker.c
@@ -969,9 +969,9 @@ static void _worker_request_done_external(request_t *request, UNUSED rlm_rcode_t
 	 *	@todo - check that the stack is at frame 0, otherwise
 	 *	more things have gone wrong.
 	 */
-	fr_assert_msg(request->parent || (request->log.unlang_indent == 0),
+	fr_assert_msg(request_is_internal(request) || request_is_detached(request) || (request->log.unlang_indent == 0),
 		      "Request %s bad log indentation - expected 0 got %u", request->name, request->log.unlang_indent);
-	fr_assert_msg(!unlang_interpret_is_resumable(request),
+	fr_assert_msg((request->master_state == REQUEST_STOP_PROCESSING) || !unlang_interpret_is_resumable(request),
 		      "Request %s is marked as yielded at end of processing", request->name);
 
 	RDEBUG("Done request");
