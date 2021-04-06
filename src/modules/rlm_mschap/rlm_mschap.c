53e5ad8c043e1e99836d4fc9712cde589759ff14
Alan T. DeKok <aland@freeradius.org>
2021-04-06 08:22:28 -0400

    clarify message

--- src/modules/rlm_mschap/rlm_mschap.c
+++ src/modules/rlm_mschap/rlm_mschap.c
@@ -616,7 +616,7 @@ static int mod_instantiate(CONF_SECTION *conf, void *instance)
 			return -1;
 		}
 #else
-		cf_log_err_cs(conf, "'winbind' auth not enabled at compiled time");
+		cf_log_err_cs(conf, "'winbind' is not enabled in this build.");
 		return -1;
 #endif
 	}
