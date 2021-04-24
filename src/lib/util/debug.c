af58dfb8a6c45522c8c00f88ace1707048364579
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-04-23 19:11:56 -0500

    Remov errant commas

--- src/lib/util/debug.c
+++ src/lib/util/debug.c
@@ -170,10 +170,10 @@ char const CC_HINT(used) *__lsan_default_suppressions(void)
 		/* Perl >= 5.32.0 - Upstream bug, tracked by https://github.com/Perl/perl5/issues/18108 */
 		"leak:perl_construct"
 #elif defined(__linux__)
-		"leak:kqueue\n",
-		"leak:*getpwnam_r*\n",			/* libc startup leak - reported by heaptrack */
-		"leak:initgroups\n",			/* libc startup leak - reported by heaptrack */
-		"leak:_dl_init",			/* dl startup leak - reported by heaptrack */
+		"leak:kqueue\n"
+		"leak:*getpwnam_r*\n"			/* libc startup leak - reported by heaptrack */
+		"leak:initgroups\n"			/* libc startup leak - reported by heaptrack */
+		"leak:_dl_init"				/* dl startup leak - reported by heaptrack */
 #elif defined(__FreeBSD__)
 		NULL
 #endif
