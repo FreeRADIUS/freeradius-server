9be04a9e6b0d1d22c587902b01fb1e4ba654bb4b
Alan T. DeKok <aland@freeradius.org>
2021-05-25 10:56:54 -0400

    Revert "remove unused variable"
    This reverts commit 6ecfe311143227a8955c61a74510725e9fac57fd.

--- src/main/client.c
+++ src/main/client.c
@@ -550,7 +550,7 @@ RADCLIENT_LIST *client_list_parse_section(CONF_SECTION *section, bool tls_requir
 RADCLIENT_LIST *client_list_parse_section(CONF_SECTION *section, UNUSED bool tls_required)
 #endif
 {
-	bool		in_server = false;
+	bool		global = false, in_server = false;
 	CONF_SECTION	*cs;
 	RADCLIENT	*c = NULL;
 	RADCLIENT_LIST	*clients = NULL;
@@ -579,6 +579,7 @@ RADCLIENT_LIST *client_list_parse_section(CONF_SECTION *section, UNUSED bool tls
 	}
 
 	if (cf_top_section(section) == section) {
+		global = true;
 		clients->name = "global";
 		clients->server = NULL;
 	}
