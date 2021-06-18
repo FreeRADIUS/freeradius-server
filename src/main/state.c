8e204e3b6ab408106a422a55dc32e00a67e12ef3
Alan T. DeKok <aland@freeradius.org>
2021-06-18 09:16:40 -0400

    check for postauth_client_lost sooner
    so that we free entries immediately if the config option is not
    set.

--- src/main/state.c
+++ src/main/state.c
@@ -312,14 +312,28 @@ static state_entry_t *fr_state_cleanup_find(fr_state_t *state)
 		}
 
 		/*
+		 *	Not yet time to clean it up.
+		 */
+		if (entry->cleanup > now) {
+			continue;
+		}
+
+		/*
+		 *	We're not running the "client lost" section.
+		 *	Just nuke the entry now.
+		 */
+		if (!main_config.postauth_client_lost) {
+			state_entry_free(state, entry);
+			continue;
+		}
+
+		/*
 		 *	Old enough that the request has been removed.
 		 *	We can add it to the cleanup list.
 		 */
-		if (entry->cleanup < now) {
-			(*tail) = entry;
-			state_entry_unlink(state, entry);
-			tail = &entry->next;
-		}
+		state_entry_unlink(state, entry);
+		(*tail) = entry;
+		tail = &entry->next;
 	}
 
 	return head;
@@ -335,30 +349,28 @@ static void fr_state_cleanup(state_entry_t *head)
 	if (!head) return;
 
 	for (entry = head; entry != NULL; entry = next) {
+		REQUEST *request;
+
 		next = entry->next;
 
-		if (main_config.postauth_client_lost) {
-			REQUEST *request;
+		request = fr_state_cleanup_request(entry);
+		if (request) {
+			RDEBUG2("No response from client, cleaning up expired state");
+			RDEBUG2("Restoring &session-state");
 
-			request = fr_state_cleanup_request(entry);
-			if (request) {
-				RDEBUG2("No response from client, cleaning up expired state");
-				RDEBUG2("Restoring &session-state");
+			/*
+			 *	@todo - print out message
+			 *	saying where the handler was
+			 *	in the process?  i.e. "sent
+			 *	server cert", etc.  This will
+			 *	require updating the EAP code
+			 *	to put a new attribute into
+			 *	the session state list.
+			 */
 
-				/*
-				 *	@todo - print out message
-				 *	saying where the handler was
-				 *	in the process?  i.e. "sent
-				 *	server cert", etc.  This will
-				 *	require updating the EAP code
-				 *	to put a new attribute into
-				 *	the session state list.
-				 */
+			rdebug_pair_list(L_DBG_LVL_2, request, request->state, "&session-state:");
 
-				rdebug_pair_list(L_DBG_LVL_2, request, request->state, "&session-state:");
-
-				request_inject(request);
-			}
+			request_inject(request);
 		}
 
 		talloc_free(entry);
