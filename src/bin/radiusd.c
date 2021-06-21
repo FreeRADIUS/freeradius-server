3118dd7ff80494715286a51633bba398dc5befc3
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-06-20 23:46:31 -0500

    Wrap OpenSSL stuff in WITH_TLS

--- src/bin/radiusd.c
+++ src/bin/radiusd.c
@@ -123,9 +123,10 @@ static int thread_instantiate(TALLOC_CTX *ctx, fr_event_list_t *el, UNUSED void 
 {
 	if (modules_thread_instantiate(ctx, el) < 0) return -1;
 	if (xlat_thread_instantiate(ctx) < 0) return -1;
+#ifdef WITH_TLS
 	if (fr_openssl_thread_init(main_config->openssl_async_pool_init,
 				   main_config->openssl_async_pool_max) < 0) return -1;
-
+#endif
 	return 0;
 }
 
