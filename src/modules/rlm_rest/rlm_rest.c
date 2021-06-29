4f7d836fd0ae6494068f9cf5f6940c36f84a3c72
Nick Porter <nick@portercomputing.co.uk>
2021-06-29 10:54:36 +0100

    Update documentation for %(rest: )

--- src/modules/rlm_rest/rlm_rest.c
+++ src/modules/rlm_rest/rlm_rest.c
@@ -354,7 +354,7 @@ static xlat_arg_parser_t const rest_xlat_args[] = {
  *
  * Example:
 @verbatim
-%{rest:http://example.com/}
+%(rest:http://example.com/)
 @endverbatim
  *
  * @ingroup xlat_functions
