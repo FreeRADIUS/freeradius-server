af96bf80578b40f51f82a4bb399a1dc99178d4a1
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-09-15 10:22:26 -0500

    typo

--- src/lib/util/strerror_tests.c
+++ src/lib/util/strerror_tests.c
@@ -223,7 +223,7 @@ static void strerror_const_benchmark(void)
 	printf("const pop rate %" PRIu64 "\n", rate);
 
 	/* shared runners are terrible for performance tests */
-	if (!getenv("NO_PERFORMANCE_TESTS")) TEST_CHECK(rate > 400000);
+	if (!getenv("NO_PERFORMANCE_TESTS")) TEST_CHECK(rate > 1000000);
 }
 
 
