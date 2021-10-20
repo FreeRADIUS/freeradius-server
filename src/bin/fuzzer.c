5f0bcd12cf09de446056364229cf11d0d0556b02
Alan T. DeKok <aland@freeradius.org>
2021-10-20 19:58:16 -0400

    minimum includes

--- src/bin/fuzzer.c
+++ src/bin/fuzzer.c
@@ -24,8 +24,10 @@
  */
 RCSID("$Id$")
 
-#include <freeradius-devel/util/base.h>
 #include <freeradius-devel/util/dl.h>
+#include <freeradius-devel/util/conf.h>
+#include <freeradius-devel/util/dict.h>
+#include <freeradius-devel/util/atexit.h>
 #include <freeradius-devel/util/syserror.h>
 #include <freeradius-devel/util/strerror.h>
 #include <freeradius-devel/io/test_point.h>
