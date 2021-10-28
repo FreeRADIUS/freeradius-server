88ed6c52727e0a26db0b5285dae22e4853cbb978
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-10-28 14:33:04 -0400

    Opt

--- src/lib/util/strerror_tests.mk
+++ src/lib/util/strerror_tests.mk
@@ -2,7 +2,6 @@ TARGET		:= strerror_tests
 
 SOURCES		:= strerror_tests.c time.c
 
-SRC_CFLAGS	:= -O2
 TGT_LDLIBS	:= $(LIBS) $(GPERFTOOLS_LIBS)
 TGT_LDFLAGS	:= $(LDFLAGS) $(GPERFTOOLS_LDFLAGS)
 TGT_PREREQS	:= libfreeradius-util.a
