d50f42f186f3093e0d9e6f32a502bd40b5bf2754
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-10-28 23:56:16 -0400

    Fix another overflow

--- src/lib/util/table.c
+++ src/lib/util/table.c
@@ -406,7 +406,7 @@ static void const *table_ordered_value_by_longest_prefix(size_t *match_len,
 
 		offset = TABLE_IDX(table, i, element_size);
 
-		for (j = 0; (j < (size_t)name_len) && (name[j] == (ELEM_STR(offset))[j]); j++);
+		for (j = 0; (j < (size_t)name_len) && (j < ELEM_LEN(offset)) && (name[j] == (ELEM_STR(offset))[j]); j++);
 
 		/*
 		 *	If we didn't get to the end of the
