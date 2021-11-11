a9b63ac7db7c0a35813d61ce93fbb9e085ee7fd6
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2021-11-11 16:01:08 -0600

    Use correct mctx fields in rlm_rest

--- src/modules/rlm_rest/rest.c
+++ src/modules/rlm_rest/rest.c
@@ -1639,7 +1639,7 @@ size_t rest_get_handle_data(char const **out, fr_curl_io_request_t *randle)
 static int rest_request_config_body(module_ctx_t const *mctx, rlm_rest_section_t const *section,
 				    request_t *request, fr_curl_io_request_t *randle, rest_read_t func)
 {
-	rlm_rest_t const *inst = talloc_get_type_abort(mctx->inst, rlm_rest_t);
+	rlm_rest_t const *inst = talloc_get_type_abort(mctx->inst->data, rlm_rest_t);
 	rlm_rest_curl_context_t	*uctx = talloc_get_type_abort(randle->uctx, rlm_rest_curl_context_t);
 	ssize_t len;
 
@@ -1717,8 +1717,8 @@ int rest_request_config(module_ctx_t const *mctx, rlm_rest_section_t const *sect
 			http_body_type_t type,
 			char const *uri, char const *username, char const *password)
 {
-	rlm_rest_t const	*inst = talloc_get_type_abort(mctx->inst, rlm_rest_t);
-	rlm_rest_thread_t const	*t = talloc_get_type_abort(mctx->inst, rlm_rest_thread_t);
+	rlm_rest_t const	*inst = talloc_get_type_abort(mctx->inst->data, rlm_rest_t);
+	rlm_rest_thread_t const	*t = talloc_get_type_abort(mctx->thread, rlm_rest_thread_t);
 	rlm_rest_curl_context_t *ctx = talloc_get_type_abort(randle->uctx, rlm_rest_curl_context_t);
 	CURL			*candle = randle->candle;
 	fr_time_delta_t		timeout;
