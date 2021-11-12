56a925233fefe1e66dc90d7a2a6b2620412094c6
Terry Burton <tez@terryburton.co.uk>
2021-11-12 21:29:38 +0000

    Scheduled fuzzing: Merge and push pack the corpus

---
+++ scripts/ci/commit_lfs_file_update.py
@@ -0,0 +1,24 @@
+#
+#  Called from the ci-scheduled-fuzzing.yml workflow to push back the merged
+#  fuzzer corpus
+#
+
+import os
+
+from github import Github
+
+repo_env = os.environ["GITHUB_REPOSITORY"]
+branch_env = os.environ["GITHUB_REF"]
+token_env = os.environ["GITHUB_TOKEN"]
+
+filename = os.environ["FILE"]
+contents = os.environ["CONTENTS"]
+
+print("About to commit update of " + filename + " to " + repo_env + ":" + branch_env)
+
+gh = Github(token_env)
+repo = gh.get_repo(repo_env)
+fc = repo.get_contents(filename, branch_env)
+repo.update_file(fc.path, "Scheduled fuzzing: Update " + fc.path, contents, fc.sha, branch=branch_env)
+
+print("Committed")
