e516e7efdf4392a070b8f4c4b761b45adfff077e
Alan T. DeKok <aland@freeradius.org>
2021-11-17 14:48:46 -0500

    return correct error

--- src/bin/unit_test_attribute.c
+++ src/bin/unit_test_attribute.c
@@ -1235,7 +1235,9 @@ static size_t command_calc(command_result_t *result, command_file_ctx_t *cc,
 		fr_value_box_init(out, type, NULL, false);
 	}
 
-	if (fr_value_calc(cc->tmp_ctx, out, type, a, op, b) < 0) RETURN_PARSE_ERROR(0);
+	if (fr_value_calc(cc->tmp_ctx, out, type, a, op, b) < 0) {
+		RETURN_OK_WITH_ERROR();
+	}
 
 	slen = fr_value_box_print(&FR_SBUFF_OUT(data, COMMAND_OUTPUT_MAX), out, NULL);
 	if (slen <= 0) RETURN_OK_WITH_ERROR();
