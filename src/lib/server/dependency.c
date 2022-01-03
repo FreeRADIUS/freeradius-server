9edc7a7b6950e72da48f4879ae7463201b7f64a1
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2022-01-03 13:34:22 -0600

    2022

--- src/lib/server/dependency.c
+++ src/lib/server/dependency.c
@@ -620,7 +620,7 @@ void dependency_version_print(void)
 		DEBUG2("  ");
 	}
 	INFO("FreeRADIUS Version " RADIUSD_VERSION_STRING);
-	INFO("Copyright 1999-2021 The FreeRADIUS server project and contributors");
+	INFO("Copyright 1999-2022 The FreeRADIUS server project and contributors");
 	INFO("There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A");
 	INFO("PARTICULAR PURPOSE");
 	INFO("You may redistribute copies of FreeRADIUS under the terms of the");
