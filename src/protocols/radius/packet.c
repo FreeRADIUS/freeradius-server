484703b0d1f5f1d6105d271c9f9a75d558150909
Alan T. DeKok <aland@freeradius.org>
2022-01-03 19:25:15 -0500

    initialize packet_ctx.end

--- src/protocols/radius/packet.c
+++ src/protocols/radius/packet.c
@@ -169,6 +169,7 @@ int fr_radius_packet_decode(TALLOC_CTX *ctx, fr_pair_list_t *out,
 	}
 
 	packet_ctx.tmp_ctx = talloc(packet, uint8_t);
+	packet_ctx.end = packet->data + packet->data_len;
 
 	/*
 	 *	Extract attribute-value pairs
