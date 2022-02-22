c071201ea7b4a7dbb8910ac5a9efdfffb160f943
Jorge Pereira <jpereiran@gmail.com>
2022-02-22 19:13:30 -0300

    eap-fast: Fix missing header

--- src/modules/rlm_eap/types/rlm_eap_fast/eap_fast_crypto.h
+++ src/modules/rlm_eap/types/rlm_eap_fast/eap_fast_crypto.h
@@ -1,3 +1,32 @@
+/*
+ * eap_fast_crypto.h
+ *
+ * Version:     $Id$
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Copyright 2003 Alan DeKok <aland@freeradius.org>
+ * Copyright 2006 The FreeRADIUS server project
+ */
+
+#ifndef _EAP_FAST_CRYPTO_H
+#define _EAP_FAST_CRYPTO_H
+
+RCSIDH(eap_fast_crypto_h, "$Id$")
+
+
 int eap_fast_encrypt(uint8_t const *plaintext, size_t plaintext_len,
 		     uint8_t const *aad, size_t aad_len,
 		     uint8_t const *key, uint8_t *iv, unsigned char *ciphertext,
@@ -6,3 +35,5 @@ int eap_fast_encrypt(uint8_t const *plaintext, size_t plaintext_len,
 int eap_fast_decrypt(uint8_t const *ciphertext, size_t ciphertext_len,
 		     uint8_t const *aad, size_t aad_len,
 		     uint8_t const *tag, uint8_t const *key, uint8_t const *iv, uint8_t *plaintext);
+
+#endif /* _EAP_FAST_CRYPTO_H */
