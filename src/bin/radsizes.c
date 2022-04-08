cabfb46a5731a2e9d07dcda45fc366d95d2c35cf
Alan T. DeKok <aland@freeradius.org>
2022-04-08 10:00:00 -0400

    add a few things, pretty-print the strings, sort alphabetically

--- src/bin/radsizes.c
+++ src/bin/radsizes.c
@@ -3,19 +3,24 @@
 #include <freeradius-devel/unlang/xlat_priv.h>
 #include <freeradius-devel/server/tmpl.h>
 #include <freeradius-devel/util/value.h>
+#include <freeradius-devel/util/rb.h>
 
 int main(UNUSED int argc, UNUSED char **argv)
 {
-#define SIZEOF(_struct)	printf("%s %zu bytes\n", STRINGIFY(_struct), sizeof(_struct))
+#define SIZEOF(_struct)	printf("%-24s\t%zu bytes\n", STRINGIFY(_struct), sizeof(_struct))
 
-	SIZEOF(xlat_exp_t);
-	SIZEOF(xlat_call_t);
-	SIZEOF(tmpl_t);
-	SIZEOF(tmpl_rules_t);
-	SIZEOF(tmpl_attr_rules_t);
-	SIZEOF(tmpl_xlat_rules_t);
-	SIZEOF(fr_value_box_t);
 	SIZEOF(fr_pair_t);
+	SIZEOF(fr_pair_list_t);
+	SIZEOF(fr_rb_node_t);
+	SIZEOF(fr_value_box_t);
+
+	SIZEOF(tmpl_t);
+	SIZEOF(tmpl_attr_rules_t);
+	SIZEOF(tmpl_rules_t);
+	SIZEOF(tmpl_xlat_rules_t);
+
+	SIZEOF(xlat_call_t);
+	SIZEOF(xlat_exp_t);
 
 	return 0;
 }
