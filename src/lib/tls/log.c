66b279503d20f3b22ebc36772d71769122694ab5
James Jones <jejones3141@gmail.com>
2022-06-22 16:12:01 -0500

    Annotate issues Coverity see that we have decided to ignore. (#4553)
    This change deals with the following CIDs:
    1503889, 1503901, 1503908, 1503916, 1503936, 1504024, 1504280,
    1504282, 1503894, 1503899, 1504013
    
    Most of these are sbuff/dbuff operations that can only fail if
    the buffers can't be reallocated or have reached their specified
    maximum size.
    
    In 1503899, we know, but Coverity doesn't, that no log_keyword
    passed to _make_vlog() will overrun the fixed-size buffer it uses.
    
    1503894 is what Coverity thinks is a copy/paste error that clearly
    makes sense as written.

--- src/lib/tls/log.c
+++ src/lib/tls/log.c
@@ -591,6 +591,7 @@ static int tls_log_request_bio_write_cb(BIO *bio, char const *in, int len)
 	 *	to aggregate it, then look for new line chars
 	 *	as an indication we need to print the line.
 	 */
+	/* coverity[checked_return] */
 	fr_sbuff_in_bstrncpy(&lb->sbuff, in, len);
 
 	/*
@@ -674,6 +675,7 @@ static int tls_log_global_bio_write_cb(BIO *bio, char const *in, int len)
 	 *	to aggregate it, then look for new line chars
 	 *	as an indication we need to print the line.
 	 */
+	/* coverity[checked_return] */
 	fr_sbuff_in_bstrncpy(&lb->sbuff, in, len);
 
 	/*
