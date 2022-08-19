11ab4c304178e6cd0514832ccf0177824861da48
Nick Porter <nick@portercomputing.co.uk>
2022-08-19 16:41:37 +0100

    Add samba to test build

---
+++ scripts/ci/samba-setup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Remove previous samba config and stop all services
+sudo systemctl stop smbd
+sudo systemctl disable smbd
+sudo systemctl mask smbd
+
+sudo systemctl stop nmbd
+sudo systemctl disable nmbd
+sudo systemctl mask nmbd
+
+sudo rm /etc/samba/smb.conf
+
+# Enable the AD-DC samba service
+sudo systemctl unmask samba-ad-dc
+sudo systemctl enable samba-ad-dc
+sudo systemctl stop samba-ad-dc
+
+# Remove other samba data
+for DIR in $(/usr/sbin/smbd -b | awk '/LOCKDIR|STATEDIR|CACHEDIR|PRIVATE_DIR/{print $2}'); do
+	sudo rm -rf "$DIR"/*
+done
+
+sudo rm -f /etc/krb5.conf
+
+# Configure the domain
+sudo /usr/bin/samba-tool domain provision \
+	--realm="example.com" \
+	--domain="EXAMPLE" \
+	--adminpass="secret123!" \
+	--use-rfc2307 \
+	--server-role=dc \
+	--dns-backend=SAMBA_INTERNAL
+
+sudo cp /var/lib/samba/private/krb5.conf /etc
+
+# Sort resolver
+if ! grep -q "nameserver 127.0.0.1" /etc/resolv.conf; then
+	sudo mv /etc/resolv.conf /etc/resolv.conf.tmp
+	echo "search example.com" | sudo tee /etc/resolv.conf
+	echo "nameserver 127.0.0.1" | sudo tee -a /etc/resolv.conf
+	grep "^nameserver" /etc/resolv.conf.tmp | sudo tee -a /etc/resolv.conf
+fi
+
+# Allow non TLS LDAP connections to Samba
+sudo sed -i 's/\[global\]/\[global\]\n\tldap server require strong auth = no/' /etc/samba/smb.conf
+
+# Start the domain controller
+sudo systemctl start samba-ad-dc
