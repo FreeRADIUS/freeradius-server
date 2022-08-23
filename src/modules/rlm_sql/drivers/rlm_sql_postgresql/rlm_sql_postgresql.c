6f111862981689a121126e732de26501f017719d
Alan T. DeKok <aland@freeradius.org>
2022-08-22 23:10:12 -0400

    allow setting of application_name

--- src/modules/rlm_sql/drivers/rlm_sql_postgresql/rlm_sql_postgresql.c
+++ src/modules/rlm_sql/drivers/rlm_sql_postgresql/rlm_sql_postgresql.c
@@ -58,6 +58,7 @@ RCSID("$Id$")
 typedef struct rlm_sql_postgres_config {
 	char const	*db_string;
 	bool		send_application_name;
+	char const     	*application_name;
 } rlm_sql_postgres_config_t;
 
 typedef struct rlm_sql_postgres_conn {
@@ -71,6 +72,7 @@ typedef struct rlm_sql_postgres_conn {
 
 static const CONF_PARSER driver_config[] = {
 	{ "send_application_name", FR_CONF_OFFSET(PW_TYPE_BOOLEAN, rlm_sql_postgres_config_t, send_application_name), "no" },
+	{ "application_name", FR_CONF_OFFSET(PW_TYPE_STRING, rlm_sql_postgres_config_t, application_name), NULL },
 	CONF_PARSER_TERMINATOR
 };
 
@@ -81,7 +83,8 @@ static int mod_instantiate(CONF_SECTION *conf, rlm_sql_config_t *config)
 #endif
 
 	rlm_sql_postgres_config_t	*driver;
-	char 				application_name[NAMEDATALEN];
+	char 				buffer[NAMEDATALEN];
+	char const	       		*application_name = NULL;
 	char				*db_string;
 
 #if defined(HAVE_OPENSSL_CRYPTO_H) && (defined(HAVE_PQINITOPENSSL) || defined(HAVE_PQINITSSL))
@@ -104,16 +107,22 @@ static int mod_instantiate(CONF_SECTION *conf, rlm_sql_config_t *config)
 	 *	Allow the user to set their own, or disable it
 	 */
 	if (driver->send_application_name) {
-		CONF_SECTION	*cs;
-		char const	*name;
+		if (driver->application_name && *driver->application_name) {
+			application_name = driver->application_name;
+		} else {
+			CONF_SECTION	*cs;
+			char const	*name;
 
-		cs = cf_item_parent(cf_section_to_item(conf));
+			cs = cf_item_parent(cf_section_to_item(conf));
 
-		name = cf_section_name2(cs);
-		if (!name) name = cf_section_name1(cs);
+			name = cf_section_name2(cs);
+			if (!name) name = cf_section_name1(cs);
 
-		snprintf(application_name, sizeof(application_name),
-			 "FreeRADIUS " RADIUSD_VERSION_STRING " - %s (%s)", main_config.name, name);
+			snprintf(buffer, sizeof(buffer),
+				 "FreeRADIUS " RADIUSD_VERSION_STRING " - %s (%s)", main_config.name, name);
+
+			application_name = buffer;
+		}
 	}
 
 	/*
