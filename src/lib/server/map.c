06aa7d6d0c34503a71f210b13e28f0d49191fce9
James Jones <jejones3141@gmail.com>
2022-09-12 18:40:27 -0500

    Cast remaining non-checked fr_pair_list_copy() calls to void (CIDs below) (#4682)
    CIDs: #1504051, #1507255

--- src/lib/server/map.c
+++ src/lib/server/map.c
@@ -2104,14 +2104,14 @@ int map_to_request(request_t *request, map_t const *map, radius_map_getvalue_t f
 
 		if (fr_dlist_num_elements(&leaf) > 1) {
 			while ((extent = fr_dlist_tail(&leaf))) {
-				fr_pair_list_copy(extent->list_ctx, extent->list, &src_list);
+				(void) fr_pair_list_copy(extent->list_ctx, extent->list, &src_list);
 				fr_dlist_talloc_free_tail(&leaf);
 			}
 			/* Free all the src vps */
 			fr_pair_list_free(&src_list);
 		} else {
 			extent = fr_dlist_head(&leaf);
-			fr_pair_list_copy(extent->list_ctx, extent->list, &src_list);
+			(void) fr_pair_list_copy(extent->list_ctx, extent->list, &src_list);
 			fr_dlist_talloc_free_head(&leaf);
 		}
 
