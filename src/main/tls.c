3d08027f30c6d9c1eaccf7d60c68c8f7d78017c3
Alan T. DeKok <aland@freeradius.org>
2022-10-26 07:31:43 -0400

    fix cert order only for lookup=0.  Fixes #4785

--- src/main/tls.c
+++ src/main/tls.c
@@ -3015,7 +3015,14 @@ int cbtls_verify(int ok, X509_STORE_CTX *ctx)
 	 */
 	if (lookup > 1) {
 		if (!my_ok) lookup = 1;
-	} else {
+
+	} else if (lookup == 0) {
+		/*
+		 *	This flag is only set for outbound
+		 *	connections.  And then allows us to remap SSL
+		 *	offset 0 (server) to our offset 1 (also
+		 *	server).
+		 */
 		lookup = (SSL_get_ex_data(ssl, FR_TLS_EX_INDEX_FIX_CERT_ORDER) != NULL);
 	}
 
