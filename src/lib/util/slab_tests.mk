2968921c770c7222adb82a6cb057c25c70efa9c7
Nick Porter <nick@portercomputing.co.uk>
2022-12-21 19:25:07 +0000

    Tests for slab allocator

---
+++ src/lib/util/slab_tests.mk
@@ -0,0 +1,6 @@
+TARGET		:= slab_tests$(E)
+SOURCES		:= slab_tests.c
+
+TGT_LDLIBS	:= $(LIBS) $(GPERFTOOLS_LIBS)
+TGT_LDFLAGS	:= $(LDFLAGS) $(GPERFTOOLS_LDFLAGS)
+TGT_PREREQS	:= libfreeradius-util$(L)
