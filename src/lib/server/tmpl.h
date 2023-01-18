509257fc4fba97d5fb1d96979ffe53a2c196edc7
Nick Porter <nick@portercomputing.co.uk>
2023-01-18 13:44:20 +0000

    Fix up tmpl_attr_tail_is_unresolved()

--- src/lib/server/tmpl.h
+++ src/lib/server/tmpl.h
@@ -764,12 +764,12 @@ static inline bool tmpl_attr_tail_is_unresolved(tmpl_t const *vpt)
 {
 	tmpl_attr_t *ar;
 
-	tmpl_assert_type(tmpl_is_attr_unresolved(vpt));
+	tmpl_assert_type(tmpl_contains_attr(vpt));
 
 	ar = tmpl_attr_list_tail(tmpl_attr(vpt));
 	if (unlikely(!ar)) return false;
 
-	return ar_is_normal(ar);
+	return ar_is_unresolved(ar);
 }
 
 /** Return true if the last attribute reference is "raw"
