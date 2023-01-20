bf636415e8d8a9812b3f5b0b3de33157a4dd4f85
Alan T. DeKok <aland@freeradius.org>
2023-01-20 08:04:19 -0500

    print out sizes of configuration things, too

--- src/bin/radsizes.c
+++ src/bin/radsizes.c
@@ -2,13 +2,14 @@
 
 #include <freeradius-devel/unlang/xlat_priv.h>
 #include <freeradius-devel/server/tmpl.h>
+#include <freeradius-devel/server/cf_priv.h>
 #include <freeradius-devel/util/value.h>
 #include <freeradius-devel/util/rb.h>
 #include <freeradius-devel/util/tlist.h>
 
 int main(UNUSED int argc, UNUSED char **argv)
 {
-#define SIZEOF(_struct)	printf("%-24s\t%zu bytes\n", STRINGIFY(_struct), sizeof(_struct))
+#define SIZEOF(_struct)	printf("%-24s\t%zu\tbytes\n", STRINGIFY(_struct), sizeof(_struct))
 
 	SIZEOF(fr_dict_attr_t);
 	SIZEOF(fr_dict_attr_flags_t);
@@ -16,6 +17,10 @@ int main(UNUSED int argc, UNUSED char **argv)
 
 	SIZEOF(fr_dlist_t);
 	SIZEOF(fr_dlist_head_t);
+
+	SIZEOF(CONF_ITEM);
+	SIZEOF(CONF_PAIR);
+	SIZEOF(CONF_SECTION);
 
 	SIZEOF(fr_pair_t);
 	SIZEOF(fr_pair_list_t);
