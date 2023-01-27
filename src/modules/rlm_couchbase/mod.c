b91b322783c3cdef41a3d9fd05234012fff3276b
Nick Porter <nick@portercomputing.co.uk>
2023-01-27 14:29:07 +0000

    Remove final instance of tmpl_pair_list_t

--- src/modules/rlm_couchbase/mod.c
+++ src/modules/rlm_couchbase/mod.c
@@ -364,16 +364,16 @@ int mod_attribute_to_element(const char *name, json_object *map, void *buf)
  * @param[in] out	Cursor to append maps to.
  * @param[in] request	The request to which the generated pairs should be added.
  * @param[in] json	The JSON object representation of the user document.
- * @param[in] list	The pair list PAIR_LIST_CONTROL or PAIR_LIST_REPLY.
+ * @param[in] list	The pair list fr_request_attr_control or fr_request_attr_reply
  * @return
  *	- 1 if no section found.
  *	- 0 on success.
  *	- <0 on error.
  */
-int mod_json_object_to_map(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request, json_object *json, tmpl_pair_list_t list)
+int mod_json_object_to_map(TALLOC_CTX *ctx, fr_dcursor_t *out, request_t *request, json_object *json, fr_dict_attr_t const *list)
 {
 	json_object	*list_obj;
-	char const	*list_name = fr_table_str_by_value(pair_list_table, list, "<INVALID>");
+	char const	*list_name = list->name;
 
 	/*
 	 *	Check for a section matching the specified list
