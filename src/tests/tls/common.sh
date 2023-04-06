9b7929e07ced07b2451c7ab3dda5e0a99ba11362
Alan T. DeKok <aland@freeradius.org>
2023-04-06 14:55:16 -0400

    need this, too

---
+++ src/tests/tls/common.sh
@@ -0,0 +1,11 @@
+#
+#  Common definitions.
+#
+DIR=$(dirname $0)/../../..
+PROGRAM=$(basename $0)
+
+export RADDB=$DIR/raddb
+export TEST_PATH=$(dirname $0)/
+export LIB_PATH=$DIR/build/lib/local/
+export FR_LIBRARY_PATH=$DIR/build/lib/local/.libs/
+
