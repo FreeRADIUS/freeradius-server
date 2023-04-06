02b4f1080f020a9c1ff4fa5e2c00f4b8d8b8df9a
Alan T. DeKok <aland@freeradius.org>
2023-04-06 13:00:07 -0400

    allow FR_LIBRARY_PATH to pass through, if it's already set

--- scripts/jlibtool.c
+++ scripts/jlibtool.c
@@ -2254,9 +2254,9 @@ static int run_mode(command_t *cmd)
 		setenv("DYLD_FALLBACK_LIBRARY_PATH", l, 1);
 #endif
 
-		l = getenv("FR_LIBRARY_PATH");
-		if (!l) l = "./build/lib/local/.libs";
-		setenv("FR_LIBRARY_PATH", l, 1);
+		if (!getenv("FR_LIBRARY_PATH")) {
+			setenv("FR_LIBRARY_PATH", "./build/lib/local/.libs", 1);
+		}
 
 		rv = run_command(cmd, cmd->arglist);
 		if (rv) goto finish;
