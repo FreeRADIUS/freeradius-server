d04bc34c2e210b4df63d09f9a0a3d3991db470e1
Alan T. DeKok <aland@freeradius.org>
2023-04-21 08:31:58 -0400

    depend on the proto_detail libraries, too
    and only succeed if the test succeeds

--- src/tests/detail/all.mk
+++ src/tests/detail/all.mk
@@ -15,15 +15,16 @@ $(eval $(call TEST_BOOTSTRAP))
 # 	so we copy it manually to the output directory (always), and then
 #	put the server logs into the output file.
 #
-$(OUTPUT)/%: $(DIR)/%
+$(OUTPUT)/%: $(DIR)/% $(addprefix ${BUILD_DIR}/lib/,proto_detail.la proto_detail_file.la proto_detail_work.la)
 	$(eval DIR := $(dir $<))
 	${Q}echo "DETAIL $(notdir $<)"
 	${Q}cp $< $(dir $@)/detail.txt
-	${Q}if ! $(TEST_BIN)/radiusd -d $(DIR)/config -D ${top_srcdir}/share/dictionary -X > $@; then \
-		tail $@; \
+	${Q}if ! $(TEST_BIN)/radiusd -d $(DIR)/config -D ${top_srcdir}/share/dictionary -X > $@.log; then \
+		tail $@.log; \
 		echo "cp $< $(dir $@)/detail.txt; $(TEST_BIN)/radiusd -d $(DIR)/config -D ${top_srcdir}/share/dictionary -X "; \
 		exit 1; \
 	fi
+	${Q}touch $@
 
 .NO_PARALLEL: $(TEST)
 $(TEST):
