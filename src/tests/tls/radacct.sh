78b2ac67090ee4222e7a0ad4d52e3a4d4270612c
Alan T. DeKok <aland@freeradius.org>
2023-05-15 10:24:57 -0400

    Quick hacks to allow multiple copies running on the same machine
    so that tests don't conflict, and people don't need to coordinate

--- src/tests/tls/radacct.sh
+++ src/tests/tls/radacct.sh
@@ -4,4 +4,4 @@
 #
 #  Run radclient acct
 #
-exec $DIR/build/make/jlibtool --mode=execute $FR_DEBUGGER $DIR/build/bin/local/radclient -d $(dirname $0)/home -D $DIR/share/ -f acct $@ localhost acct testing123
+exec $DIR/build/make/jlibtool --mode=execute $FR_DEBUGGER $DIR/build/bin/local/radclient -d $(dirname $0)/home -D $DIR/share/ -f acct $@ localhost:${FR_LOCAL_PREFIX}1813 acct testing123
--- src/tests/tls/radclient.sh
+++ src/tests/tls/radclient.sh
@@ -4,4 +4,4 @@
 #
 #  Run radclient auth
 #
-exec $DIR/build/make/jlibtool --mode=execute $FR_DEBUGGER $DIR/build/bin/local/radclient -d $(dirname $0)/home -D $DIR/share/ -f user_password $@ localhost auth testing123
+exec $DIR/build/make/jlibtool --mode=execute $FR_DEBUGGER $DIR/build/bin/local/radclient -d $(dirname $0)/home -D $DIR/share/ -f user_password $@ localhost:${FR_LOCAL_PREFIX}1812 auth testing123
