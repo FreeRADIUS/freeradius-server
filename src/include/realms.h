9828d2b95068dea3c2bb82e8cde55337270fda89
Alan T. DeKok <aland@freeradius.org>
2023-05-25 14:53:38 -0400

    add "nonblock" configuration to home servers

--- src/include/realms.h
+++ src/include/realms.h
@@ -63,6 +63,7 @@ typedef struct home_server {
 
 	bool			dual;			//!< One of a pair of homeservers on consecutive ports.
 	bool			dynamic;		//!< is this a dynamically added home server?
+	bool			nonblock;		//!< Enable a socket non-blocking to the home server.
 	char const		*virtual_server;		//!< For internal proxying
 	char const		*parent_server;
 
