ff6185ddf8918fd4619802464407377a174fd56d
Margaret Cullen <margaret@painless-security.com>
2023-05-25 14:03:20 +0000

    Make TLS nonblocking changes to add write handler to event.c/.h

--- src/include/event.h
+++ src/include/event.h
@@ -40,7 +40,6 @@ typedef void (*fr_event_fd_handler_t)(fr_event_list_t *el, int sock, void *ctx);
 fr_event_list_t *fr_event_list_create(TALLOC_CTX *ctx, fr_event_status_t status);
 
 int fr_event_list_num_fds(fr_event_list_t *el);
-int fr_event_list_full(fr_event_list_t *el);
 int fr_event_list_num_elements(fr_event_list_t *el);
 
 int fr_event_insert(fr_event_list_t *el,
@@ -54,13 +53,12 @@ int fr_event_now(fr_event_list_t *el, struct timeval *when);
 
 int fr_event_fd_insert(fr_event_list_t *el, int type, int fd,
 			 fr_event_fd_handler_t handler, void *ctx);
+int fr_event_fd_write_handler(fr_event_list_t *el, int type, int fd,
+			      fr_event_fd_handler_t write_handler, void *ctx);
 int fr_event_fd_delete(fr_event_list_t *el, int type, int fd);
 int fr_event_loop(fr_event_list_t *el);
 void fr_event_loop_exit(fr_event_list_t *el, int code);
 bool fr_event_loop_exiting(fr_event_list_t *el);
-
-void fr_event_fd_want_read(fr_event_list_t *el, int fd);
-void fr_event_fd_want_write(fr_event_list_t *el, int fd);
 
 #ifdef __cplusplus
 }
