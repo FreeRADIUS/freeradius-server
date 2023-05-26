7382287a48f47fede9e2be3d8078bdbdc2f67906
Jorge Pereira <jpereira@freeradius.org>
2023-05-25 22:54:24 -0300

    make BFD use flat or nested attributes

--- src/process/bfd/base.c
+++ src/process/bfd/base.c
@@ -139,7 +139,7 @@ RESUME_NO_MCTX(recv_bfd)
 			state = vp->vp_uint32;
 		} else {
 			vp = fr_pair_find_by_da(&request->reply_pairs, NULL, attr_bfd_packet);
-			if (vp) vp = fr_pair_find_by_da(&vp->vp_group, NULL, attr_bfd_state);
+			if (vp) vp = fr_pair_find_by_da_nested(&vp->vp_group, NULL, attr_bfd_state);
 			if (vp) state = vp->vp_uint8;
 		}
 	}
