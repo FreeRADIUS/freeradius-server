af25b87355795ca9491ebe75983d60b544b2ba2b
Del <78494584+delexagon@users.noreply.github.com>
2023-07-05 09:19:50 -0400

    Removed unnecessary run_test.sh output

--- src/tests/tls-load/run_test.sh
+++ src/tests/tls-load/run_test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Other shells not tested
 
-yes | sudo docker compose rm
+yes | docker compose rm &> /dev/null
+if ! [ "$?" -eq 0 ]; then
+  echo "Docker failed"
+  exit 1
+fi
 
 ### ARGUMENT PARSING ###
 # Note -s and -b require the freeradius docker image to be rebuilt, meaning it must be removed, or renamed with -i.
-while getopts ':n:l:i:b:s:o:' OPTION; do
+while getopts ':n:l:i:d:r:o:bm' OPTION; do
   case "$OPTION" in
     n)
       num_requests="$OPTARG"
@@ -16,22 +20,33 @@ while getopts ':n:l:i:b:s:o:' OPTION; do
     i)
       image="$OPTARG"
       ;;
-    s)
-      subdir="$OPTARG"
+    d)
+      temp="$(tail -1 build_config)"
+      echo "$OPTARG" > build_config
+      echo "$temp" >> build_config
       ;;
-    b)
-      branch="$OPTARG"
+    r)
+      temp="$(head -1 build_config)"
+      echo "$temp" > build_config
+      echo "$OPTARG" >> build_config
       ;;
     o)
       output_dir="$OPTARG"
       ;;
+    m)
+      release=$(git rev-parse HEAD)
+      ;;
+    b)
+      rebuild=1
+      ;;
     ?)
-      echo "Usage: $0 [-n number_of_requests_per_realm] [-l log_level (0=none, 1,2,3=radiusd -f, -fx, -fxx)] [-b repository_branch_to_build_docker_image] [-s repository_Dockerfile_directory] [-o output_log_dir] number_of_realms"
+      echo "Usage: $0 [-n number_of_requests_per_realm] [-l log_level (0=none, 1,2,3=radiusd -f, -fx, -fxx)] [-b repository_branch_to_build_docker_image] [-s repository_Dockerfile_directory] [-o output_log_dir] [-r freeradius_directory] number_of_realms"
       exit 1
       ;;
   esac
 done
 shift "$(($OPTIND-1))"
+
 max_container_num=100
 num_realms="$1"
 # Need the directory name we're running from - docker names our containers based on it (which the proxy needs as hostnames)
@@ -41,24 +56,27 @@ if [[ "/$num_realms" = "/" ]]; then
   echo "No container num given"
   exit 1
 fi
+if [ "$num_realms" -gt "$max_container_num" ]; then
+  echo "You have tried to create more than $max_container_num docker containers, exiting load testing script without running"
+  exit 1
+fi
 if [[ "/$num_requests" = "/" ]]; then
-  num_requests=1000
+  num_requests=5000
 fi
 if [[ "/$log_level" = "/" ]]; then
   log_level=1
 fi
-if [[ "/$subdir" != "/" ]]; then
-  sed -i "s|#\(.*\):\(.*\) ###build|#\1:$subdir ###build|g" docker-compose.yml
-fi
-if [[ "/$branch" != "/" ]]; then
-  sed -i "s|#\(.*\):\(.*\) ###build|#$branch:\2 ###build|g" docker-compose.yml
-fi
+
 if [[ "/$image" != "/" ]]; then
   sed -i "s|:.*###image|: $image ###image|g" docker-compose.yml
+else
+  image=$(cat docker-compose.yml | grep -m 1 "###image" | awk '{print $2}')
 fi
-if [ "$num_realms" -gt "$max_container_num" ]; then
-  echo "You have tried to create more than $max_container_num docker containers, exiting load testing script without running"
-  exit 1
+
+if [ "1$rebuild" -eq 11 ] || ! docker images | grep "$image" > /dev/null; then
+  docker image rm "$image"
+  # Can't cache, we want the most recent commit
+  docker build "$(head -1 build_config)" --build-arg=release="$(tail -1 build_config)" --build-arg=source=https://github.com/FreeRADIUS/freeradius-server.git --no-cache -t "$image"
 fi
 
 sed -i "s|-.*###PWD|- $PWD/test:/test ###PWD|g" docker-compose.yml 
@@ -66,19 +84,24 @@ sed -i "s|-.*###PWD|- $PWD/test:/test ###PWD|g" docker-compose.yml
 
 ### CONFIG ###
 # Used to pass values to containers, since they can only see the test/ directory
-echo "$compose_dir"  | sudo tee    "test/config"
-echo "$num_requests" | sudo tee -a "test/config"
-echo "$log_level"    | sudo tee -a "test/config"
+echo "$compose_dir"  >  "test/config"
+echo "$num_requests" >> "test/config"
+echo "$log_level"    >> "test/config"
 
 ### DOCKER ###
-sudo docker compose up -d --scale home="$num_realms" --scale client="$num_realms"
+docker compose up -d --scale home="$num_realms" --scale client="$num_realms" &>/dev/null
+if ! [ "$?" -eq 0 ]; then
+  echo "Docker failed"
+  exit 1
+fi
 
 # Check for when all of the client containers have exited
-while sudo docker compose ps | grep client; do
+while docker compose ps | grep client > /dev/null; do
   sleep 3
 done
-sudo docker compose stop
-sudo rm test/containers/proxy-running
+
+docker compose stop &>/dev/null
+rm -f test/containers/proxy-running
 
 # Move all the output to an external directory if specified
 if [[ "/$output_dir" != "/" ]]; then
@@ -86,12 +109,12 @@ if [[ "/$output_dir" != "/" ]]; then
 fi
 
 # Check that the number of clients that were created is the same as the number of clients that were created and exited successfully
-if [ $(sudo docker compose ps --all | grep -c client) -eq $(sudo docker compose ps --all | grep -c "client.*Exited (0)") ]; then
-  echo "Test succeeded"
+if [ $(docker compose ps --all | grep -c client) -eq $(docker compose ps --all | grep -c "client.*Exited (0)") ]; then
+  echo "TLS load test succeeded"
+  yes | docker compose rm &> /dev/null
   exit 0
 else
-  echo "Test failed"
+  echo "TLS load test failed"
+  yes | docker compose rm &> /dev/null
   exit 1
 fi
-
-
