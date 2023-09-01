60a582fc14b92894446e1eece6e6a2f596095cb2
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2023-09-01 17:33:12 -0600

    ldap: Setup script fixes
    Don't duplicate the ENTIRE script just to start a second LDAP instance.  Add paths for homebrew on ARM.  Kill existing slapd instances so we don't get errors about the database already existing.
    
    ff

--- scripts/ci/ldap-setup.sh
+++ scripts/ci/ldap-setup.sh
@@ -1,44 +1,75 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Allow setup script to work with homebrew too
-export PATH="/usr/local/opt/openldap/libexec:$PATH"
+export PATH="/usr/local/opt/openldap/libexec:/opt/homebrew/opt/openldap/libexec:/opt/symas/lib:$PATH"
+
+suffix=$(echo "${0##*/}" | sed -E 's/^ldap(.*)-setup.sh$/\1/')
+
+# Kill any old processes
+[ -e "/tmp/slapd${suffix}.pid" ] && kill $(cat /tmp/slapd${suffix}.pid)
+
+base_dir="/tmp/ldap${suffix}"
+cert_dir="${base_dir}/certs"
+data_dir="${base_dir}/db"
+schema_dir="${base_dir}/schema"
+
+echo "base_dir \"${data_dir}\""
 
 # Clean out any existing DB
-rm -rf /tmp/ldap/db
+rm -rf "${data_dir}"
 # Create directory we can write DB files to
-mkdir -p /tmp/ldap/db/
+mkdir -p "${data_dir}"
 
 # Change db location to /tmp as we can't write to /var
-sed -i -e 's/\/var\/lib\/ldap/\/tmp\/ldap\/db/' src/tests/salt-test-server/salt/ldap/base.ldif
+sed -i -e "s/\/var\/lib\/ldap/\/tmp\/ldap${suffix}\/db/" src/tests/salt-test-server/salt/ldap/base${suffix}.ldif
 
 # Create a directory we can link schema files into
-if [ -d /tmp/ldap/schema ]; then
+if [ -d "${schema_dir}" ]; then
     echo "Schema dir already linked"
 # Debian
 elif [ -d /etc/ldap/schema ]; then
-    ln -fs /etc/ldap/schema /tmp/ldap/schema
+    ln -fs /etc/ldap/schema "${schema_dir}"
 # Symas packages
 elif [ -d /opt/symas/etc/openldap/schema ]; then
-    ln -fs /opt/symas/etc/openldap/schema /tmp/ldap/schema
+    ln -fs /opt/symas/etc/openldap/schema "${schema_dir}"
 # Redhat
 elif [ -d /etc/openldap/schema ]; then
-    ln -fs /etc/openldap/schema /tmp/ldap/schema
-# macOS (homebrew)
+    ln -fs /etc/openldap/schema "${schema_dir}"
+# macOS (homebrew x86)
 elif [ -d /usr/local/etc/openldap/schema ]; then
-    ln -fs /usr/local/etc/openldap/schema /tmp/ldap/schema
+    ln -fs /usr/local/etc/openldap/schema "${schema_dir}"
+# macOS (homebrew ARM)
+elif [ -d /opt/homebrew/opt/openldap/schema ]; then
+    ln -fs /opt/homebrew/opt/openldap/schema "${schema_dir}"
 else
     echo "Can't locate OpenLDAP schema dir"
     exit 1
 fi
 
-if [ -e /opt/symas/lib/slapd ]; then
-  SLAPD=/opt/symas/lib/slapd
+# Clean out any old certificates
+rm -rf "${cert_dir}"
+# Create certificate directory
+mkdir -p "${cert_dir}"
+# Ensure we have some certs generated
+make -C raddb/certs
+
+# Copy certificates - whilst not stricltly LDAP certs they work fine for these tests
+cp raddb/certs/rsa/ca.pem "${cert_dir}/cacert.pem"
+cp raddb/certs/rsa/server.pem "${cert_dir}/servercert.pem"
+openssl rsa -in raddb/certs/rsa/server.key -out "${cert_dir}/serverkey.pem" -passin pass:whatever
+
+if [ -z "${suffix}" ]; then
+    ldap_port="3890"
+    ldaps_port="6360"
+    ldap_socket="ldapi://%2Ftmp%2Fldap%2Fsocket"
 else
-  SLAPD=slapd
+    ldap_port=$((3890+${suffix}))
+    ldaps_port=$((6360+${suffix}))
+    ldap_socket="ldapi://%2Ftmp%2Fldap${suffix}%2Fsocket"
 fi
 
 # Start slapd
-$SLAPD -d any -h "ldap://127.0.0.1:3890/ ldapi://%2Ftmp%2Fldap%2Fsocket" -f scripts/ci/ldap/slapd.conf 2>&1 > /tmp/ldap/slapd.log &
+slapd -d any -h "ldap://127.0.0.1:${ldap_port}/ ldaps://127.0.0.1:${ldaps_port}/ ${ldap_socket}" -f scripts/ci/ldap/slapd${suffix}.conf 2>&1 > ${base_dir}/slapd.log &
 
 # Wait for LDAP to start
 sleep 1
@@ -46,7 +77,7 @@ sleep 1
 # Add test data
 count=0
 while [ $count -lt 10 ] ; do
-    if ldapadd -v -x -H ldap://127.0.0.1:3890/ -D "cn=admin,cn=config" -w secret -f src/tests/salt-test-server/salt/ldap/base.ldif ; then
+    if ldapadd -v -x -H "${ldap_socket}" -D "cn=admin,cn=config" -w secret -f src/tests/salt-test-server/salt/ldap/base${suffix}.ldif ; then
         break 2
     else
         echo "ldap add failed, retrying..."
@@ -58,6 +89,6 @@ done
 # Exit code gets overwritten, so we check for failure using count
 if [ $count -eq 10 ]; then
     echo "Error configuring server"
-    cat /tmp/ldap/slapd.log
+    cat ${base_dir}/slapd.log
     exit 1
 fi
