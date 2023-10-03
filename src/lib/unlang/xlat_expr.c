4fb4dc125dfbc2fb89b766dd7deb60f2fcbed7f1
Alan T. DeKok <aland@freeradius.org>
2023-10-03 15:32:50 -0400

    use correct API

--- src/lib/unlang/xlat_expr.c
+++ src/lib/unlang/xlat_expr.c
@@ -413,7 +413,7 @@ static xlat_action_t xlat_binary_op(TALLOC_CTX *ctx, fr_dcursor_t *out,
 				break;
 
 			case FR_TYPE_OCTETS:
-				fr_value_box_strdup_shallow(a, NULL, (void const *) "", false);
+				fr_value_box_memdup_shallow(a, NULL, (void const *) "", 0, false);
 				break;
 
 			default:
