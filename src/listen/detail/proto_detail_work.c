e58b9151bd57557f2fcf9a99367e03d5d66e08e6
Alan T. DeKok <aland@freeradius.org>
2023-10-12 10:08:56 -0400

    if we're not retransmitting, mark done even on failure

--- src/listen/detail/proto_detail_work.c
+++ src/listen/detail/proto_detail_work.c
@@ -581,6 +581,8 @@ static ssize_t mod_write(fr_listen_t *li, void *packet_ctx, UNUSED fr_time_t req
 	fr_assert(thread->fd >= 0);
 
 	if (!buffer[0]) {
+		if (!inst->retransmit) goto mark_done;
+
 		if (fr_time_eq(track->retry.start, fr_time_wrap(0))) {
 			fr_retry_init(&track->retry, fr_time(), &inst->retry_config);
 		} else {
