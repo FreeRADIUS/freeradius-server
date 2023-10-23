2373aab49dc7628275e618fbfd465a8d266b2f1a
Alan T. DeKok <aland@freeradius.org>
2023-10-23 10:19:21 -0400

    remove old tests which we choose to not support

--- src/tests/keywords/all.mk
+++ src/tests/keywords/all.mk
@@ -57,10 +57,11 @@ endif
 #  All of the "update" tests which should also be run with "-S rewrite_update=yes"
 #
 #  update-remove-index		Remove by index _and_ value.  Not implemented in edit code
+#  update-group-*		subsections are not implemented in compile_update_to_edit()
 #
 
 #  Tests for the "update" keyword
-KEYWORD_UPDATE_TESTS :=   update-remove-index 
+#KEYWORD_UPDATE_TESTS :=   update-remove-index 
 
 # Tests for rewriting "update"
 KEYWORD_UPDATE_REWRITE_TESTS := update-all update-array update-delete update-remove-any update-group update-hex update-remove-value update-index update-list-error update-remove-list update-prepend unknown-update update-error update-error-2 update-exec-error update-list-null-rhs update-exec update-error-3 update-group-error update-null-value-assign vendor-specific-error update-filter
