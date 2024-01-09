e56eab9e5341ba541c32a50d4406adb5f1f05880
Nick Porter <nick@portercomputing.co.uk>
2024-01-09 11:01:01 +0000

    Attributes marked as secret are automatically hidden

--- src/modules/rlm_yubikey/rlm_yubikey.c
+++ src/modules/rlm_yubikey/rlm_yubikey.c
@@ -344,15 +344,8 @@ static unlang_action_t CC_HINT(nonnull) mod_authorize(rlm_rcode_t *p_result, mod
 		 */
 		MEM(fr_pair_value_bstr_realloc(password, NULL, password_len) == 0);
 
-		RINDENT();
-		if (RDEBUG_ENABLED3) {
-			RDEBUG3("&request.%pP", vp);
-			RDEBUG3("&request.%pP", password);
-		} else {
-			RDEBUG2("&request.%s := <<< secret >>>", vp->da->name);
-			RDEBUG2("&request.%s := <<< secret >>>", password->da->name);
-		}
-		REXDENT();
+		RDEBUG2("&request.%pP", vp);
+		RDEBUG2("&request.%pP", password);
 
 		/*
 		 *	So the ID split code works on the non password portion.
@@ -369,6 +362,7 @@ static unlang_action_t CC_HINT(nonnull) mod_authorize(rlm_rcode_t *p_result, mod
 	if (inst->id_len) {
 		MEM(pair_update_request(&vp, attr_yubikey_public_id) >= 0);
 		fr_pair_value_bstrndup(vp, passcode, inst->id_len, true);
+		RDEBUG2("&request.%pP", vp);
 	}
 
 	if (!inst->auth_type) {
