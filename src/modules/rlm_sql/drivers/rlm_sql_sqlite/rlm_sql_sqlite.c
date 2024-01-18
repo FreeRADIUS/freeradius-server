8337881cd4174ea58ba3917384a0c965492e215c
Nick Porter <nick@portercomputing.co.uk>
2024-01-18 17:03:06 +0000

    sqlite3_prepare functions prepare only the next query in the string
    and return a pointer to the character after what was parsed - so this
    provides a more robust method of parsing the SQL to execute than simply
    looking for ';' followed by '\n' or '\0'.
    
    E.g. if there are comments which end the line with a ';' that fails with
    the old parsing.
    In addition, if there were ';' in data inside a string, the previous
    parsing would have thrown away the portion of the string before that.

--- src/modules/rlm_sql/drivers/rlm_sql_sqlite/rlm_sql_sqlite.c
+++ src/modules/rlm_sql/drivers/rlm_sql_sqlite/rlm_sql_sqlite.c
@@ -232,9 +232,9 @@ static void sql_print_error(sqlite3 *db, int status, char const *fmt, ...)
 static int sql_loadfile(TALLOC_CTX *ctx, sqlite3 *db, char const *filename)
 {
 	ssize_t		len;
-	int		statement_cnt = 0;
+	int		statement_len, statement_cnt = 0;
 	char		*buffer;
-	char		*p, *q;
+	char const	*p;
 	int		cl;
 	FILE		*f;
 	struct stat	finfo;
@@ -308,7 +308,7 @@ static int sql_loadfile(TALLOC_CTX *ctx, sqlite3 *db, char const *filename)
 			if ((*p != 0x0a) && (*p != 0x0d) && (*p != '\t')) break;
 			cl = 1;
 		} else {
-			cl = fr_utf8_char((uint8_t *) p, -1);
+			cl = fr_utf8_char((uint8_t const *) p, -1);
 			if (!cl) break;
 		}
 	}
@@ -319,29 +319,9 @@ static int sql_loadfile(TALLOC_CTX *ctx, sqlite3 *db, char const *filename)
 		return -1;
 	}
 
-	/*
-	 *	Statement delimiter is ;\n
-	 */
 	p = buffer;
-	while ((q = strchr(p, ';'))) {
-		uint32_t statement_len;
-
-		if ((q[1] != '\n') && (q[1] != '\0')) {
-			p = q + 1;
-			statement_cnt++;
-			continue;
-		}
-
-#ifndef NDEBUG
-		if ((q - p) > (1 << 20)) {
-			ERROR("Failed preparing statement %i: too long!", statement_cnt);
-			talloc_free(buffer);
-			return -1;
-		}
-#endif
-
-		statement_len = q - p;
-
+	while (*p) {
+		statement_len = len - (p - buffer);
 #ifdef HAVE_SQLITE3_PREPARE_V2
 		status = sqlite3_prepare_v2(db, p, statement_len, &statement, &z_tail);
 #else
@@ -353,6 +333,11 @@ static int sql_loadfile(TALLOC_CTX *ctx, sqlite3 *db, char const *filename)
 			talloc_free(buffer);
 			return -1;
 		}
+
+		/*
+		 *	No SQL statement was found
+		 */
+		if (!statement) break;
 
 		status = sqlite3_step(statement);
 		if (sql_check_error(db, status) != RLM_SQL_OK) {
@@ -370,7 +355,7 @@ static int sql_loadfile(TALLOC_CTX *ctx, sqlite3 *db, char const *filename)
 		}
 
 		statement_cnt++;
-		p = q + 1;
+		p = z_tail;
 	}
 
 	talloc_free(buffer);
