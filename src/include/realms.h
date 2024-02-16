828341a7bd69ed4ebfe4e36d8fbfede6c5c50399
Alan T. DeKok <aland@freeradius.org>
2024-02-16 09:12:35 -0500

    add and set require_message_authenticator for home servers

--- src/include/realms.h
+++ src/include/realms.h
@@ -64,6 +64,8 @@ typedef struct home_server {
 	bool			dual;			//!< One of a pair of homeservers on consecutive ports.
 	bool			dynamic;		//!< is this a dynamically added home server?
 	bool			nonblock;		//!< Enable a socket non-blocking to the home server.
+	bool			require_ma;		//!< for all replies to Access-Request and Status-Server
+
 	char const		*virtual_server;		//!< For internal proxying
 	char const		*parent_server;
 
