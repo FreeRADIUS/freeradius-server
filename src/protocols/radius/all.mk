30de995d8bc295327c66278c9db141c179c63aff
Alan T. DeKok <aland@freeradius.org>
2024-03-09 10:43:40 -0500

    move bio stuff to compile flag
    until such time as we ca n figure out why it works for gcc in CI,
    but doesn't work for clang in CI, but does work with clang locally

--- src/protocols/radius/all.mk
+++ src/protocols/radius/all.mk
@@ -6,18 +6,24 @@
 TARGET		:= libfreeradius-radius$(L)
 
 SOURCES		:= base.c \
-		   client.c \
-		   client_udp.c \
-		   client_tcp.c \
 		   decode.c \
 		   encode.c \
-		   id.c \
 		   list.c \
 		   packet.c \
 		   tcp.c \
-		   abinary.c \
-		   bio.c
+		   abinary.c
 
 SRC_CFLAGS	:= -D_LIBRADIUS -DNO_ASSERT -I$(top_builddir)/src
 
-TGT_PREREQS	:= libfreeradius-util$(L) libfreeradius-bio$(L)
+TGT_PREREQS	:= libfreeradius-util$(L)
+
+ifneq "$(WITH_BIO)" ""
+SOURCES		+= \
+		   client.c \
+		   client_udp.c \
+		   client_tcp.c \
+		   id.c \
+		   bio.c
+
+TGT_PREREQS	+= libfreeradius-util$(L)
+endif
