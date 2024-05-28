36c5be1a674f9668640be95bcd737eb655c54428
Matthew Newton <matthew-git@newtoncomputing.co.uk>
2024-05-28 22:30:01 +0100

    crossbuild: add git safe directory

--- scripts/crossbuild/crossbuild.mk
+++ scripts/crossbuild/crossbuild.mk
@@ -156,6 +156,7 @@ crossbuild.${1}.up: $(DD)/stamp-up.${1}
 $(DD)/docker.refresh.${1}: $(DD)/stamp-up.${1}
 	${Q}echo "REFRESH ${1}"
 	${Q}docker container exec $(CB_CPREFIX)${1} sh -c 'rsync -a /srv/src/ /srv/local-src/'
+	${Q}docker container exec $(CB_CPREFIX)${1} sh -c 'git config --global --add safe.directory /srv/local-src'
 	${Q}docker container exec $(CB_CPREFIX)${1} sh -c 'git config -f /srv/local-src/config core.bare true'
 	${Q}docker container exec $(CB_CPREFIX)${1} sh -c 'git config -f /srv/local-src/config --unset core.worktree || true'
 	${Q}docker container exec $(CB_CPREFIX)${1} sh -c '[ -d /srv/build ] || git clone /srv/local-src /srv/build'
