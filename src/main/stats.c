5e38ab4fe203aa2400fba032013b56f229aa5cd0
Alan T. DeKok <aland@freeradius.org>
2024-06-24 16:18:39 -0400

    process.c already updates the home server stats

--- src/main/stats.c
+++ src/main/stats.c
@@ -253,25 +253,21 @@ void request_stats_final(REQUEST *request)
 	switch (request->proxy->code) {
 	case PW_CODE_ACCESS_REQUEST:
 		proxy_auth_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
 		break;
 
 #ifdef WITH_ACCOUNTING
 	case PW_CODE_ACCOUNTING_REQUEST:
 		proxy_acct_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
 		break;
 #endif
 
 #ifdef WITH_COA
 	case PW_CODE_COA_REQUEST:
 		proxy_coa_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
 		break;
 
 	case PW_CODE_DISCONNECT_REQUEST:
 		proxy_dsc_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
 		break;
 #endif
 
@@ -352,7 +348,6 @@ void request_stats_final(REQUEST *request)
 
  done:
 #endif /* WITH_PROXY */
-
 
 	if (request->max_time) {
 		switch (request->packet->code) {
