99402cfdee6cc974e46864a1469d33ae3bba1e77
Alan T. DeKok <aland@freeradius.org>
2024-07-10 16:44:25 -0400

    use INSTALL, not PROGRAM_INSTALL

--- src/main/radsecret.mk
+++ src/main/radsecret.mk
@@ -2,4 +2,4 @@ install: $(R)/$(bindir)/radsecret
 
 $(R)/$(bindir)/radsecret: ${top_srcdir}/src/main/radsecret
 	@$(ECHO) INSTALL radsecret
-	$(Q)${PROGRAM_INSTALL} -c -m 755 $< $@
+	$(Q)${INSTALL} -c -m 755 $< $@
