27b93671d039b15b6f634d86a93d8a743b2fed74
Alan T. DeKok <aland@freeradius.org>
2024-08-08 17:33:18 -0400

    script to cross-check antora files

---
+++ doc/antora/missing.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+
+#
+#  check for missing files in antora
+#
+#
+#  ./missing.pl $(find . -name "nav.adoc" -print)
+#
+
+
+
+use strict;
+use warnings;
+use File::Basename;
+use File::Find;
+
+my %used;
+my %exists;
+
+while (@ARGV) {
+    my $filename = shift;
+
+    my $dir = dirname($filename);
+
+    my @adoc = `find $dir -name "*.adoc" -print`;
+
+    foreach my $name (@adoc) {
+	next if $name =~ /nav.adoc/;
+	next if $name =~ /partials/;
+	chop $name;
+
+	$exists{$name}++;
+    }
+
+    open(my $FILE, "<", $filename) or die "Failed to open $filename: $!\n";
+
+    while (<$FILE>) {
+	next if !/xref:(.*?).adoc/;
+	
+	$used{"$dir/pages/$1.adoc"}++
+
+    }
+
+    foreach my $name (sort {$a cmp $b} keys %used) {
+	next if -e $name;
+
+	print "REF-NO-FILE: ", $name, "\n";
+    }
+
+    foreach my $name (sort {$a cmp $b} keys %exists) {
+	next if defined $used{$name};
+
+	print "FILE-NO-REF: ", $name, "\n";
+    }
+
+    close $FILE;
+}
