dbc5c3593eeb1a43329718cca8029a6f4f4df5f4
Nick Porter <nick@portercomputing.co.uk>
2024-08-14 08:16:18 +0100

    UNUSED

--- src/lib/unlang/xlat_expr.c
+++ src/lib/unlang/xlat_expr.c
@@ -460,7 +460,7 @@ static xlat_arg_parser_t const binary_cmp_xlat_args[] = {
 
 static xlat_action_t xlat_cmp_op(TALLOC_CTX *ctx, fr_dcursor_t *out,
 				 UNUSED xlat_ctx_t const *xctx,
-				 request_t *request, fr_value_box_list_t *in,
+				 UNUSED request_t *request, fr_value_box_list_t *in,
 				 fr_token_t op)
 {
 	int rcode;
