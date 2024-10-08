3335f6001e0583b1955022b5f27847255e1ff7e4
Alan T. DeKok <aland@freeradius.org>
2024-10-08 11:24:47 -0400

    we don't need an intermediate file

--- src/lib/bio/all.mk
+++ src/lib/bio/all.mk
@@ -1,3 +1,18 @@
-SUBMAKEFILES := libfreeradius-bio.mk
+TARGET := libfreeradius-bio$(L)
 
-#		bio_tests.mk
+SOURCES	:=		\
+	base.c		\
+	buf.c		\
+	fd.c		\
+	fd_open.c	\
+	haproxy.c	\
+	mem.c		\
+	network.c	\
+	null.c		\
+	packet.c	\
+	pipe.c		\
+	queue.c		\
+	dedup.c		\
+	retry.c
+
+TGT_PREREQS	:= libfreeradius-util$(L)
