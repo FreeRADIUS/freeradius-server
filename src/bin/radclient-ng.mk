5a603ddf6300896f1348f5b9e90b651bcdc66903
Alan T. DeKok <aland@freeradius.org>
2024-10-10 09:30:34 -0400

    Revert "add more OpenSSL magic"
    This reverts commit 801ae0311868e6e26688db856d432313aa56894e.

--- src/bin/radclient-ng.mk
+++ src/bin/radclient-ng.mk
@@ -6,7 +6,6 @@ SOURCES		:= radclient-ng.c ${top_srcdir}/src/modules/rlm_mschap/smbdes.c \
 TGT_PREREQS	:= libfreeradius-radius$(L) libfreeradius-radius-bio$(L) libfreeradius-bio$(L)
 
 SRC_CFLAGS	:= -I${top_srcdir}/src/modules/rlm_mschap
-TGT_LDLIBS	:= $(LIBS) $(OPENSSL_LIBS)
-TGT_LDFLAGS	:= $(OPENSSL_FLAGS)
+TGT_LDLIBS	:= $(LIBS)
 
 TGT_INSTALLDIR	:= $(BUILD_DIR)/bin/ignore
