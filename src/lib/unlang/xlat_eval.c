7a040539405ef71fa23aeacb4cfb1e3dd1e66c2a
Nick Porter <nick@portercomputing.co.uk>
2024-10-21 17:44:49 +0100

    Revert "Don't load the RADIUS dictionary in the xlat code"
    This reverts commit 6cf9cd8db3a700b1ceb056cc14a0d03201966ed8.
    
    Breaks CI tests of foreign protocol decoding

--- src/lib/unlang/xlat_eval.c
+++ src/lib/unlang/xlat_eval.c
@@ -40,9 +40,11 @@ RCSID("$Id$")
 static int instance_count = 0;
 
 static fr_dict_t const *dict_freeradius;
+static fr_dict_t const *dict_radius;
 
 static fr_dict_autoload_t xlat_eval_dict[] = {
 	{ .out = &dict_freeradius, .proto = "freeradius" },
+	{ .out = &dict_radius, .proto = "radius" },
 	{ NULL }
 };
 
