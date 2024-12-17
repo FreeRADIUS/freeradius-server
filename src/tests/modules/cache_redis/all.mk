cbc0d5334c01653e5ff9db579539401a8f2ce7fa
Nick Porter <nick@portercomputing.co.uk>
2024-12-17 17:21:42 +0000

    Better comments

--- src/tests/modules/cache_redis/all.mk
+++ src/tests/modules/cache_redis/all.mk
@@ -1,7 +1,7 @@
 #
-#  Test the "redis" module
+#  Test the "redis" cache module driver
 #
 cache_redis.test:
 
-# Don't test redis if REDIS_TEST_SERVER ENV is not set
+# Don't test redis driver if CACHE_REDIS_TEST_SERVER ENV is not set
 cache_redis_require_test_server := 1
