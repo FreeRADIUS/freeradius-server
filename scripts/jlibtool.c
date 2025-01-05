07b965e116022aa9bf309f6685e5783f5895be34
Alan T. DeKok <aland@freeradius.org>
2025-01-05 08:59:40 -0500

    rename to --show-config.  Fixes #5442
    This avoids conflict with clang's --config option.
    
    We should probably instead have a special "--" option which
    signifies "end of jlibtool options.
    
    Arguably jlibtool should have

--- scripts/jlibtool.c
+++ scripts/jlibtool.c
@@ -327,25 +327,26 @@ static void usage(int code)
 	printf("Usage: jlibtool [OPTIONS...] COMMANDS...\n");
 	printf("jlibtool is a replacement for GNU libtool with similar functionality.\n\n");
 
-	printf("  --config	 show all configuration variables\n");
-	printf("  --debug	  enable verbose shell tracing\n");
-	printf("  --dry-run	display commands without modifying any files\n");
-	printf("  --help	   display this help message and exit\n");
-	printf("  --mode=MODE	   use operational mode MODE (you *must* set mode)\n");
+	printf("  --debug	 enable verbose shell tracing\n");
+	printf("  --dry-run	 display commands without modifying any files\n");
+	printf("  --help	 display this help message and exit\n");
+	printf("  --mode=MODE	 use operational mode MODE (you *must* set mode)\n");
 
 	printf("  --silent	 don't print informational messages\n");
-	printf("  --tag=TAG	Ignored for libtool compatibility\n");
-	printf("  --version	print version information\n");
+	printf("  --tag=TAG	 Ignored for libtool compatibility\n");
+	printf("  --version	 print version information\n");
 
 
 	printf("  --shared	 Build shared libraries when using --mode=link\n");
-	printf("  --export-all	   Try to export 'def' file on some platforms\n");
+	printf("  --show-config	 show all configuration variables\n");
+	printf("                 Or --show-config=VALUE to see just one value\n");
+	printf("  --export-all	 Try to export 'def' file on some platforms\n");
 
 	printf("\nMODE must be one of the following:\n\n");
-	printf("  compile	  compile a source file into a jlibtool object\n");
-	printf("  execute	  automatically set library path, then run a program\n");
-	printf("  install	  install libraries or executables\n");
-	printf("  link	     create a library or an executable\n");
+	printf("  compile	 compile a source file into a jlibtool object\n");
+	printf("  execute	 automatically set library path, then run a program\n");
+	printf("  install	 install libraries or executables\n");
+	printf("  link	         create a library or an executable\n");
 
 	printf("\nMODE-ARGS can be the following:\n\n");
 	printf("  -export-dynamic  accepted and ignored\n");
@@ -780,7 +781,7 @@ static int parse_long_opt(char const *arg, command_t *cmd)
 	} else if (strcmp(var, "help") == 0) {
 		usage(0);
 
-	} else if (strcmp(var, "config") == 0) {
+	} else if (strcmp(var, "show-config") == 0) {
 		print_config(value);
 
 		exit(0);
