aa52525b071c5e67f9c1c690004dda6dee8fde6a
Matthew Newton <matthew-git@newtoncomputing.co.uk>
2025-01-23 19:48:07 +0000

    No sock for detail listeners. Fixes #5485

--- src/main/process.c
+++ src/main/process.c
@@ -5735,6 +5735,10 @@ static void event_new_fd(rad_listen_t *this)
 			this->status = RAD_LISTEN_STATUS_KNOWN;
 
 #ifdef WITH_TLS
+			if (this->type == RAD_LISTEN_DETAIL) {
+				return;
+			}
+
 			sock = this->data;
 			if (!sock->write_handler) return;
 
