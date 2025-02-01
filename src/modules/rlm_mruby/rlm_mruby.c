0c5477d65bbb45acc7596f0cf45ebd800aefcfdf
Alan T. DeKok <aland@freeradius.org>
2025-02-01 09:37:29 -0500

    set attribute prefix to AUTO

--- src/modules/rlm_mruby/rlm_mruby.c
+++ src/modules/rlm_mruby/rlm_mruby.c
@@ -362,7 +362,8 @@ static void add_vp_tuple(TALLOC_CTX *ctx, request_t *request, fr_pair_list_t *vp
 					&(tmpl_rules_t){
 						.attr = {
 							.dict_def = request->dict,
-							.list_def = request_attr_reply
+							.list_def = request_attr_reply,
+							.prefix = TMPL_ATTR_REF_PREFIX_AUTO,
 						}
 					}) <= 0) {
 			ERROR("Failed to find attribute %s", ckey);
