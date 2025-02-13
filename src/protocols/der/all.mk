e65ebe9e421b0698844449d8eae2d26c851bd760
Ethan Thompson <ethan.thompson@networkradius.com>
2025-02-13 12:48:15 -0500

    Add DER encoder

--- src/protocols/der/all.mk
+++ src/protocols/der/all.mk
@@ -6,9 +6,10 @@
 TARGET		:= libfreeradius-der$(L)
 
 SOURCES		:= base.c \
+		   encode.c
 
 #		   decode.c \
-#		   encode.c
+
 
 SRC_CFLAGS	:= -I$(top_builddir)/src -DNO_ASSERT
 TGT_PREREQS	:= libfreeradius-util$(L)
