7a8621688ce28832c408b7322c408e917e62e583
Alan T. DeKok <aland@freeradius.org>
2025-02-20 09:03:44 -0500

    add copyright and license

--- src/protocols/der/der.h
+++ src/protocols/der/der.h
@@ -1,3 +1,30 @@
+/*
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/**
+ * $Id$
+ *
+ * @file protocols/der/der.c
+ * @brief Structures and prototypes for base DER functionality.
+ *
+ * @author Ethan Thompson (ethan.thompson@inkbridge.io)
+ *
+ * @copyright 2025 Network RADIUS SAS (legal@networkradius.com)
+ */
+
 #include <freeradius-devel/build.h>
 #include <freeradius-devel/util/dict.h>
 #include <freeradius-devel/util/types.h>
