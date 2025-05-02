31830de460ed4d35d7c934a3c1b3e34b7b2e2e86
Alan T. DeKok <aland@freeradius.org>
2025-05-02 06:18:18 -0400

    update copyright

--- src/lib/server/dependency.c
+++ src/lib/server/dependency.c
@@ -454,7 +454,7 @@ void dependency_version_print(void)
 #endif
 		DEBUG2("  ");
 	}
-	INFO("Copyright 1999-2024 The FreeRADIUS server project and contributors");
+	INFO("Copyright 1999-2025 The FreeRADIUS server project and contributors");
 	INFO("There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A");
 	INFO("PARTICULAR PURPOSE");
 	INFO("You may redistribute copies of FreeRADIUS under the terms of the");
