07213b081b02be23813d8632f6f5c4ca33297a4d
Nick Porter <nick@portercomputing.co.uk>
2025-05-13 15:10:12 +0100

    Update sample mruby script with correct log function call

--- src/modules/rlm_mruby/example.rb
+++ src/modules/rlm_mruby/example.rb
@@ -4,27 +4,27 @@
 
 module Radiusd
     def self.instantiate
-        radlog(L_DBG, "[mruby]Running ruby instantiate")
+        log(L_DBG, "[mruby]Running ruby instantiate")
         return RLM_MODULE_OK
     end
     def self.authenticate(request)
-        radlog(L_DBG, "[mruby]Running ruby authenticate")
+        log(L_DBG, "[mruby]Running ruby authenticate")
         return RLM_MODULE_NOOP
     end
     def self.recv_access_request(request)
-        radlog(L_ERR, "[mruby]Running ruby recv_access_request")
-        radlog(L_WARN, "Authorize: #{request.inspect}(#{request.class})")
-        radlog(L_WARN, "Authorize: #{request.request.inspect}(#{request.request.class})")
+        log(L_DBG, "[mruby]Running ruby recv_access_request")
+        log(L_WARN, "Authorize: #{request.inspect}(#{request.class})")
+        log(L_WARN, "Authorize: #{request.request.inspect}(#{request.request.class})")
         reply = [["Framed-MTU", 1500]]
         control = [["Password.Cleartext", "hello"], ["Tmp-String-0", "!*", "ANY"]]
         return [RLM_MODULE_UPDATED, reply, control]
     end
     def self.send_access_accept(request)
-        radlog(L_DBG, "[mruby]Running ruby send_access_accept")
+        log(L_DBG, "[mruby]Running ruby send_access_accept")
         return RLM_MODULE_NOOP
     end
     def self.recv_accounting_request(request)
-        radlog(L_DBG, "[mruby]Running ruby accounting")
+        log(L_DBG, "[mruby]Running ruby accounting")
         return RLM_MODULE_NOOP
     end
 end
