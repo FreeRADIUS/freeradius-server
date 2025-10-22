7814c4c42e333170c1de7836a282731fe8c741d1
Alan T. DeKok <aland@freeradius.org>
2025-10-22 16:59:25 +0200

    quiet analyzer

--- src/main/process.c
+++ src/main/process.c
@@ -2836,7 +2836,7 @@ static int process_proxy_reply(REQUEST *request, RADIUS_PACKET *reply, uint32_t 
 				 *	Error-Cause.
 				 */
 				if ((vp->vp_integer == PW_CODE_PROTOCOL_ERROR) &&
-				    ((vp = fr_pair_find_by_num(request->proxy_reply->vps, PW_ERROR_CAUSE, 0, TAG_ANY)) == NULL)) {
+				    !fr_pair_find_by_num(request->proxy_reply->vps, PW_ERROR_CAUSE, 0, TAG_ANY)) {
 					if (!error_cause) error_cause = PW_ERROR_CAUSE_PROXY_PROCESSING_ERROR;
 
 					vp = fr_pair_afrom_num(request->proxy_reply, PW_ERROR_CAUSE, 0);
