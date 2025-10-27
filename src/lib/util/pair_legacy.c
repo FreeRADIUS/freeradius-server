45035d73945c0de18be8782e4ab1225d7b25a4b2
Alan T. DeKok <aland@freeradius.org>
2025-10-27 17:32:47 -0400

    handle ALIAS attributes when parsing
    so that we create the full tree, and not a partial one

--- src/lib/util/pair_legacy.c
+++ src/lib/util/pair_legacy.c
@@ -519,8 +519,19 @@ redo:
 			goto leaf;
 		} else {
 		leaf:
-			if (fr_pair_append_by_da(relative->ctx, &vp, relative->list, da) < 0) {
-				return fr_sbuff_error(&our_in);
+			/*
+			 *	In the common case, we jump ahead one level.
+			 *
+			 *	But for ALIAS, we may jump ahead multiple levels.
+			 */
+			if ((relative->da->depth + 1) == da->depth) {
+				if (fr_pair_append_by_da(relative->ctx, &vp, relative->list, da) < 0) {
+					return fr_sbuff_error(&our_in);
+				}
+			} else {
+				vp = fr_pair_afrom_da_depth_nested(relative->ctx, relative->list, da,
+								   relative->da->depth);
+				if (!vp) return fr_sbuff_error(&our_in);
 			}
 		}
 
