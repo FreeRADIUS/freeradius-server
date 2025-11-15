67b2d0513a9bc662e815dbe58434ef95a66178c4
Alan T. DeKok <aland@freeradius.org>
2025-11-15 14:47:02 -0500

    add missing ';'

--- src/tests/ldap_sync/persistent_search/all.mk
+++ src/tests/ldap_sync/persistent_search/all.mk
@@ -53,7 +53,7 @@ $(OUTPUT)/%: $(DIR)/% | $(TEST).trigger_clear $(TEST).radiusd_kill $(TEST).radiu
 	${Q}sleep .1
 	${Q}if [ ! -e $(OUT_DIR)/$(OUT).out ] ; then	\
 		$(MAKE) --no-print-directory test.ldap_sync/persistent_search.radiusd_kill; \
-		cat $(OUT_DIR)/radiusd.log					\
+		cat $(OUT_DIR)/radiusd.log;					\
 		echo "LDAP_SYNC FAILED $(TARGET) - expected output file not produced";	\
 		rm -rf $(BUILD_DIR)/tests/test.ldap_sync/persistent_search;	\
 		exit 1;								\
@@ -62,7 +62,7 @@ $(OUTPUT)/%: $(DIR)/% | $(TEST).trigger_clear $(TEST).radiusd_kill $(TEST).radiu
 
 	${Q}if [ -e "$(EXPECTED)" ] && ! cmp -s $(FOUND) $(EXPECTED); then	\
 		$(MAKE) --no-print-directory test.ldap_sync/persistent_search.radiusd_kill; \
-		cat $(OUT_DIR)/radiusd.log					\
+		cat $(OUT_DIR)/radiusd.log;					\
 		echo "LDAP_SYNC FAILED $(TARGET)";				\
 		rm -rf $(BUILD_DIR)/tests/test.ldap_sync/persistent_search;	\
 		exit 1;								\
