9ad20c2198a5875266e53245e36e19e55a664aea
Alan T. DeKok <aland@freeradius.org>
2025-11-22 09:26:32 -0500

    some fields can be raw

--- src/lib/util/struct.c
+++ src/lib/util/struct.c
@@ -1016,7 +1016,7 @@ ssize_t fr_struct_to_network(fr_dbuff_t *dbuff,
 		/*
 		 *	We have no more "flat" VPs.
 		 */
-		fr_assert(vp->da->parent == key_da);
+		fr_assert((vp->da->parent == key_da) || vp->da->flags.is_unknown || vp->da->flags.is_raw);
 
 		slen = encode_keyed_struct(&work_dbuff, vp, da_stack, depth,
 					   cursor, encode_ctx, encode_value, encode_pair);
