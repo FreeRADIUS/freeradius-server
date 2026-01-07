5a3ab7d6b98a5681747618bcc5128832ea1fd330
Alan T. DeKok <aland@freeradius.org>
2026-01-07 14:58:09 -0500

    more fixes in preparation for long-lived children
    move the "fork child" to a separate function.
    
    If we're asked to fork a child, check whether or not one is already
    running.
    
    waitpid() for the child.
    
    write() to a found child, and if the write fails, assume that the
    child is dead.

--- scripts/build/make/fork.c
+++ scripts/build/make/fork.c
@@ -30,6 +30,7 @@
 #include <ctype.h>
 
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <sys/errno.h>
 #include <unistd.h>
 #include <signal.h>
@@ -80,25 +81,77 @@ static int fr_cloexec(int fd)
 	return 0;
 }
 
-/*
- *	$(fork-bg program,arg1,arg2,arg3,...)
+static void cleanup(unsigned int i)
+{
+	int status;
+
+	free(child[i].name);
+	close(child[i].stdin[WRITER]); /* our writer FD */
+	close(child[i].stdout[READER]); /* our reader FD */
+
+	/*
+	 *	Try to be polite, and clean up the child.
+	 */
+	(void) waitpid(child[i].pid, &status, WNOHANG);
+
+	child[i] = (fork_t) {};
+
+	if (i < min_child) {
+		min_child = i;
+
+	} else if ((i + 1) == max_child) {
+		max_child--;
+	}
+
+	if (min_child == max_child) {
+		min_child = 0;
+		max_child = 1;
+	}
+}
+
+/** Find a pre-existing child with the correct key.
  *
- *	Will run a child process, using a pipe for stdin/stdout.
- *
- *	Will return an opaque key which can later be used to interact with the process.
  */
-static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__((unused)) unsigned int argc, char **argv)
+static int child_find(char const *key)
+{
+	unsigned int i;
+
+	/*
+	 *	Find a pre-existing child of the same key.  If we find a child that already exists, then we
+	 *	just return that.
+	 */
+	for (i = 0; i < max_child; i++) {
+		if (!child[i].name) continue;
+
+		/*
+		 *	Found a matchin key.  See if the process is alive.  If not, wipe out the old entry.
+		 */
+		if (strcmp(child[i].name, key) == 0) {
+			if (write(child[i].stdin[WRITER], " ", 1) < 0) {
+				cleanup(i);
+				return -1;
+			}
+
+			/*
+			 *	It's still alive!
+			 */
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+/** Fork a new child, and associate it with the key,
+ *
+ */
+static int child_fork(unsigned int argc, char **argv, unsigned int *next_argc)
 {
 	char	*in;
 	pid_t	self;
 	unsigned  i, my_argc, envc = 0;
 	char	*my_argv[MAX_CHILD];
 	char	*env[MAX_CHILD];
-
-	if (max_child == MAX_CHILD) {
-		ERROR("Too many children");
-		return NULL;
-	}
 
 #if 0
 	fprintf(stderr, "ARGC = %u\n", argc);
@@ -112,6 +165,10 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 	 *	current make process, and doesn't return.
 	 *
 	 *	Also, calling ERROR() in the child causes all kinds of GNU make insanity.
+	 *
+	 *	argv[0] is the key to use.
+	 *	argv[1..n] could be environment variables.
+	 *	argv[n+1] is the program to run.
 	 */
 	my_argc = 0;
 	for (i = 1; i < argc; i++) {
@@ -122,21 +179,21 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 
 		in = argv[i];
 
-		if (my_argc == MAX_CHILD) {
-			ERROR("Too many arguments");	// doesn't return
-			return NULL;			// needed to quiet the C compiler
+		if (my_argc == max_child) {
+			ERROR("too many arguments");	// doesn't return
+			return -1;			// needed to quiet the c compiler
 		}
 
 		/*
-		 *	Loop over 'in', splitting on spaces.  But NOT on spaces inside of quotes.
+		 *	loop over 'in', splitting on spaces.  but not on spaces inside of quotes.
 		 */
 		p = in;
 		while (*p) {
 			while (isspace((unsigned int) *p)) p++;
 
 			if (*p == '`') {
-				ERROR("Unexpected back-tick");
-				return NULL;
+				ERROR("unexpected back-tick");
+				return -1;
 			}
 
 			/*
@@ -153,7 +210,7 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 				allow_env = false;
 
 				/*
-				 *	Shift the entire string left, and de-quote it.
+				 *	shift the entire string left, and de-quote it.
 				 */
 				while (*p && (*p != quote)) {
 					if (*p == '\\') {
@@ -164,40 +221,40 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 				}
 
 				if (*p != quote) {
-					ERROR("Missing end quote %c", quote);
-					return NULL;
+					ERROR("missing end quote %c", quote);
+					return -1;
 				}
 
 				p++;
 				*q = '\0';
 
 				if (*p && !isspace((unsigned int) *p)) {
-					ERROR("Unexpected text after quote %c", quote);
-					return NULL;
+					ERROR("unexpected text after quote %c", quote);
+					return -1;
 				}
 				continue;
 			}
 
 			/*
-			 *	Bare word.  Copy it, but don't allow quotes.
+			 *	bare word.  copy it, but don't allow quotes.
 			 */
 			start = p;
 
 			while (*p) {
 				if (isspace((unsigned int) *p)) {
 					*p = '\0';
-					p++; /* don't leave it pointing at the NUL byte */
+					p++; /* don't leave it pointing at the nul byte */
 					break;
 				}
 
 				if ((*p == '"') || (*p == '\'') || (*p == '`')) {
-					ERROR("Unexpected quote %c", *p);
-					return NULL;
+					ERROR("unexpected quote %c", *p);
+					return -1;
 				}
 
 				if (*p == '\\') {
-					ERROR("Unexpected escape");
-					return NULL;
+					ERROR("unexpected escape");
+					return -1;
 				}
 
 				if (allow_env && (*p == '=')) {
@@ -209,14 +266,14 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 			}
 
 			/*
-			 *	We allow environment variables, and it's "foo=...".  Set the environment
-			 *	variable appropriately, and skip the rest of the string.  The value of the
+			 *	we allow environment variables, and it's "foo=...".  set the environment
+			 *	variable appropriately, and skip the rest of the string.  the value of the
 			 *	environment variable can contain spaces.
 			 */
 			if (is_env) {
-				if (envc == MAX_CHILD) {
-					ERROR("Too many environment variables");
-					return NULL;
+				if (envc == max_child) {
+					ERROR("too many environment variables");
+					return -1;
 				}
 
 				env[envc++] = start;
@@ -225,6 +282,17 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 
 			my_argv[my_argc++] = start;
 			allow_env = false;
+		}
+
+		/*
+		 *	we have found the argument which contains the program and it's arguments.  stop
+		 *	processing.
+		 *
+		 *	the remaining input to the function is the text to write to the program.
+		 */
+		if (!allow_env) {
+			*next_argc = i + 1;
+			break;
 		}
 	}
 
@@ -250,28 +318,35 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 	 */
 	if (pipe(child[min_child].stdin) < 0) {
 		ERROR("Failed opening stdin for child - %s", strerror(errno));
-		return NULL;
+		return -1;
 	}
 	if (fr_cloexec(child[min_child].stdin[WRITER]) < 0) { /* parents writer FD */
 		ERROR("Failed flagging stdin for child - %s", strerror(errno));
-		return NULL;
+		return -1;
 	}
 
 	if (pipe(child[min_child].stdout) < 0) {
 		ERROR("Failed opening stdout for child - %s", strerror(errno));
-		return NULL;
+		return -1;
 	}
 	if (fr_cloexec(child[min_child].stdout[READER]) < 0) { /* parents reader FD */
 		ERROR("Failed flagging stdout for child - %s", strerror(errno));
-		return NULL;
+		return -1;
 	}
 	self = getpid();
 
-	child[min_child].name = strdup(argv[0]);
-
 	child[min_child].pid = fork();
+
+	if (child[min_child].pid == (pid_t) -1) {
+		ERROR("Failed forking child - %s", strerror(errno));
+		return -1;
+	}
+
+	/*
+	 *	We're in the parent, remember the correct key, do cleanups, and return.
+	 */
 	if (child[min_child].pid) {
-		char *out;
+		child[min_child].name = strdup(argv[0]);
 
 		/*
 		 *	Close the descriptors used by the child, we
@@ -280,15 +355,13 @@ static char *make_fork_bg(__attribute__((unused)) char const *nm, __attribute__(
 		(void) close(child[min_child].stdin[READER]); /* childs reader FD */
 		(void) close(child[min_child].stdout[WRITER]); /* childs writer FD */
 
+		i = min_child;
 		min_child++;
 		if (min_child == max_child) {
 			max_child++;
 		}
 
-		out = gmk_alloc(1);
-		out[0] = '\0';
-
-		return out;
+		return i;
 	}
 
 	/*
@@ -345,28 +418,44 @@ fail:
 	_exit(1);
 }
 
-static void cleanup(unsigned int i)
+/*
+ *	$(fork-bg key,program arg1 arg2 ...)
+ *
+ *	Will run a child process, using a pipe for stdin/stdout.
+ *
+ *	Will return an opaque key which can later be used to interact with the process.
+ */
+static char *make_fork_bg(__attribute__((unused)) char const *nm, unsigned int argc, char **argv)
 {
-	free(child[i].name);
-	close(child[i].stdin[WRITER]); /* our writer FD */
-	close(child[i].stdout[READER]); /* our reader FD */
+	int num;
+	char *out;
 
-	child[i] = (fork_t) {};
+	num = child_find(argv[0]);
+	if (num < 0) {
+		unsigned int next_argc;
 
-	if (i < min_child) {
-		min_child = i;
+		if (max_child == MAX_CHILD) {
+			ERROR("Too many children");
+			return NULL;
+		}
 
-	} else if (i == min_child) {
-		min_child++;
-
-	} else if ((i + 1) == max_child) {
-		max_child--;
+		num = child_fork(argc, argv, &next_argc);
+		if (num < 0) {
+			/*
+			 *	The ERROR macro should have killed us already.
+			 */
+			return NULL;
+		}
 	}
 
-	if (min_child == max_child) {
-		min_child = 0;
-		max_child = 1;
-	}
+	/*
+	 *	@todo - Write the last argument(s) to the pipe.
+	 */
+
+	out = gmk_alloc(1);
+	out[0] = '\0';
+
+	return out;
 }
 
 /*
@@ -411,7 +500,7 @@ static char *make_kill(__attribute__((unused)) char const *nm, __attribute__((un
 
 int fork_gmk_setup(void)
 {
-	gmk_add_function("fork-bg", &make_fork_bg, 2, 30, 0); /* min 2, max 30, please expand the input string */
+	gmk_add_function("fork-bg", &make_fork_bg, 2, 2, 0); /* min 2, max 2, please expand the input string */
 	gmk_add_function("kill-bg", &make_kill, 1, 1, 0); /* min 1, max 1, please expand the input string */
 
 	return 1;
