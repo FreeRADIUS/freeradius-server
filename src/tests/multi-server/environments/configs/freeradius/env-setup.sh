ac18bf59bf1026fd96e56a4ad69b7d3f95ed94b8
Marc-Andre Casavant <marc.casavant@inkbridge.io>
2026-01-27 13:23:44 -0500

    load-generator server configuration of modules and virtual server has been moved into radiusd.conf.  Using a template for the radius module configs and additional updates to the compose environment and testcase to improve the logs during runtime.

--- src/tests/multi-server/environments/configs/freeradius/env-setup.sh
+++ src/tests/multi-server/environments/configs/freeradius/env-setup.sh
@@ -1,3 +1,4 @@
+apt-get update
 apt-get install -y iputils-ping iproute2 ipcalc
 IP_CIDR=$(ip -o -f inet addr show eth0 | awk '{print $4}')
 TEST_SUBNET=$(ipcalc -n "$IP_CIDR" | grep Network | awk '{print $2}')
