a4bbcab120700521ffd046a842a41d63044f1537
Alan T. DeKok <aland@freeradius.org>
2026-02-18 09:13:17 -0500

    fix not_before -> not_after typo

--- src/lib/tls/cert.c
+++ src/lib/tls/cert.c
@@ -72,7 +72,7 @@ int fr_tls_cert_is_valid(fr_unix_time_t *not_before_p, fr_unix_time_t *not_after
 	if (fr_time_lt(fr_time_from_sec(not_after), now)) {
 		fr_strerror_printf("Certificate has expired.  "
 				   "Validity period (notAfter) ends %pV, current time is %pV",
-				   fr_box_date(fr_unix_time_from_time(not_before)), fr_box_date(fr_time_to_unix_time(now)));
+				   fr_box_date(fr_unix_time_from_time(not_after)), fr_box_date(fr_time_to_unix_time(now)));
 		return -2;
 	}
 
