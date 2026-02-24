ddbc154736c189292362d2b44ab949d3120689fa
Alan T. DeKok <aland@freeradius.org>
2026-02-24 11:40:16 -0500

    tweak variables to quiet static analyzer

--- src/lib/eap/compose.c
+++ src/lib/eap/compose.c
@@ -272,13 +272,13 @@ rlm_rcode_t eap_compose(eap_session_t *eap_session)
 		/* Should never enter here */
 		REDEBUG("Reply code %u is unknown, rejecting the request", reply->code);
 		request->reply->code = FR_RADIUS_CODE_ACCESS_REJECT;
-		reply->code = FR_EAP_CODE_FAILURE;
+		eap_packet->code = reply->code = FR_EAP_CODE_FAILURE;
 		rcode = RLM_MODULE_REJECT;
 		break;
 	}
 
 	RDEBUG2("Sending EAP %s (code %d) ID %d length %i",
-		eap_codes[eap_packet->code], eap_packet->code, reply->id,
+		eap_codes[reply->code], reply->code, reply->id,
 		eap_packet->length[0] * 256 + eap_packet->length[1]);
 
 	return rcode;
