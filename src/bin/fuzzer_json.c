ea6e9140de6e3c52da6c5c3d991715655d760e00
Alan T. DeKok <aland@freeradius.org>
2026-03-17 08:56:51 -0400

    format to follow coding style / standards

--- src/bin/fuzzer_json.c
+++ src/bin/fuzzer_json.c
@@ -1,3 +1,27 @@
+/*
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/**
+ * $Id$
+ *
+ * @file src/bin/fuzzer_json.c
+ * @brief Functions to fuzz json
+ * */
+RCSID("$Id$")
+
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -12,65 +36,73 @@ typedef struct fr_jpath_node_s fr_jpath_node_t;
 
 /* External declarations for functions */
 extern ssize_t fr_jpath_parse(void *ctx, fr_jpath_node_t **head, 
-                               char const *in, size_t inlen);
+			      char const *in, size_t inlen);
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    void *ctx = NULL;
-    json_object *json_obj = NULL;
-    char *json_str = NULL;
-    char *jpath_str = NULL;
-    fr_jpath_node_t *jpath_head = NULL;
-    size_t split_point;
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	void *ctx = NULL;
+	size_t split_point;
 
-    /* Need at least 2 bytes */
-    if (size < 2) {
-        return 0;
-    }
+	/* Need at least 2 bytes */
+	if (size < 2) {
+		return 0;
+	}
 
-    /* Limit input size to prevent timeouts */
-    if (size > 8192) {
-        return 0;
-    }
+	/* Limit input size to prevent timeouts */
+	if (size > 8192) {
+		return 0;
+	}
 
-    /* Initialize talloc context */
-    ctx = talloc_init("fuzzer_json");
-    if (!ctx) {
-        return 0;
-    }
+	/* Initialize talloc context */
+	ctx = talloc_init("fuzzer_json");
+	if (!ctx) {
+		return 0;
+	}
 
-    /* Use first byte to determine split between JSON and jpath */
-    split_point = (data[0] * size) / 256;
-    if (split_point >= size - 1) {
-        split_point = size / 2;
-    }
+	/*
+	 *	Use first byte to determine split between JSON and jpath
+	 */
+	split_point = (data[0] * size) / 256;
+	if (split_point >= size - 1) {
+		split_point = size / 2;
+	}
 
-    /* JSON string to parse with json-c */
-    if (split_point > 1) {
-        json_str = talloc_strndup(ctx, (const char *)(data + 1), split_point - 1);
-        if (json_str) {
-            /* Parse JSON - exercises json-c library */
-            json_obj = json_tokener_parse(json_str);
-            if (json_obj) {
-                json_object_put(json_obj);
-                json_obj = NULL;
-            }
-        }
-    }
+	/*
+	 *	JSON string to parse with json-c
+	 */
+	if (split_point > 1) {
+		char *str = NULL;
+		json_object *json_obj = NULL;
 
-    /* jpath expression string to parse with FreeRADIUS */
-    if (split_point < size - 1) {
-        size_t jpath_len = size - split_point - 1;
-        if (jpath_len > 0) {
-            jpath_str = talloc_strndup(ctx, 
-                                       (const char *)(data + split_point + 1), 
-                                       jpath_len);
-        }
+		str = talloc_strndup(ctx, (const char *)(data + 1), split_point - 1);
+		if (str) {
+			json_obj = json_tokener_parse(str);
+			if (json_obj) {
+				json_object_put(json_obj);
+				json_obj = NULL;
+			}
+		}
+	}
+
+	/*
+	 *	jpath expression string to parse with FreeRADIUS
+	 */
+	if (split_point < size - 1) {
+		size_t len = size - split_point - 1;
+		char *str = NULL;
+		fr_jpath_node_t *jpath_head = NULL;
+
+		if (jpath_len > 0) {
+			str = talloc_strndup(ctx,
+					     (const char *)(data + split_point + 1), 
+					     len);
+		}
         
-        if (jpath_str) {
-            (void)fr_jpath_parse(ctx, &jpath_head, jpath_str, jpath_len);
-        }
-    }
+		if (str) {
+			(void) fr_jpath_parse(ctx, &jpath_head, str, len);
+		}
+	}
 
-    talloc_free(ctx);
-    return 0;
+	talloc_free(ctx);
+	return 0;
 }
--- src/bin/fuzzer_json.mk
+++ src/bin/fuzzer_json.mk
@@ -1,13 +1,10 @@
-TARGET                  := fuzzer_json$(E)
-SOURCES                 := fuzzer_json.c
+TARGET			:= fuzzer_json$(E)
+SOURCES			:= fuzzer_json.c
 
-TGT_PREREQS             := libfreeradius-json$(L) libfreeradius-server$(L) \
-                           libfreeradius-util$(L) libfreeradius-unlang$(L) \
-                           libfreeradius-io$(L)
+TGT_PREREQS		:= libfreeradius-json$(L) libfreeradius-util$(L)
 
-SRC_CFLAGS              := -fsanitize=fuzzer
-SRC_CFLAGS              += -I$(top_builddir)/src/lib/json/
-SRC_CFLAGS              += -I/usr/include/json-c
+SRC_CFLAGS		:= -fsanitize=fuzzer
+SRC_CFLAGS		+= -I$(top_builddir)/src/lib/json/
 
-TGT_LDFLAGS             := -fsanitize=fuzzer
-TGT_LDLIBS              := $(LIBS) -ltalloc -ljson-c -ldl
+TGT_LDFLAGS		:= -fsanitize=fuzzer
+TGT_LDLIBS		:= $(LIBS) -ltalloc -ljson-c -ldl
