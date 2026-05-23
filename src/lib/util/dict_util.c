4afd98f6581b471ae3503bd4f895b8638f2629bd
Alan T. DeKok <aland@freeradius.org>
2026-05-23 13:52:09 -0400

    simplify definitions of allowed_chars
    to make them clearer, and to have more re-use

--- src/lib/util/dict_util.c
+++ src/lib/util/dict_util.c
@@ -40,22 +40,15 @@ RCSID("$Id$")
 
 fr_dict_gctx_t *dict_gctx = NULL;	//!< Top level structure containing global dictionary state.
 
+#ifdef __clang__
+#pragma clang diagnostic ignored "-Wgnu-designator"
+#endif
+
 #define DICT_ATTR_ALLOWED_CHARS \
 	['-'] = true, ['/'] = true, ['_'] = true, \
-	['0'] = true, ['1'] = true, ['2'] = true, ['3'] = true, ['4'] = true, \
-	['5'] = true, ['6'] = true, ['7'] = true, ['8'] = true, ['9'] = true, \
-	['A'] = true, ['B'] = true, ['C'] = true, ['D'] = true, ['E'] = true, \
-	['F'] = true, ['G'] = true, ['H'] = true, ['I'] = true, ['J'] = true, \
-	['K'] = true, ['L'] = true, ['M'] = true, ['N'] = true, ['O'] = true, \
-	['P'] = true, ['Q'] = true, ['R'] = true, ['S'] = true, ['T'] = true, \
-	['U'] = true, ['V'] = true, ['W'] = true, ['X'] = true, ['Y'] = true, \
-	['Z'] = true, \
-	['a'] = true, ['b'] = true, ['c'] = true, ['d'] = true, ['e'] = true, \
-	['f'] = true, ['g'] = true, ['h'] = true, ['i'] = true, ['j'] = true, \
-	['k'] = true, ['l'] = true, ['m'] = true, ['n'] = true, ['o'] = true, \
-	['p'] = true, ['q'] = true, ['r'] = true, ['s'] = true, ['t'] = true, \
-	['u'] = true, ['v'] = true, ['w'] = true, ['x'] = true, ['y'] = true, \
-	['z'] = true
+	[ '0' ... '9' ] = true, \
+	[ 'A' ... 'Z' ] = true, \
+	[ 'a' ... 'z' ] = true
 
 /** Characters allowed in a single dictionary attribute name
  *
@@ -76,21 +69,8 @@ bool const fr_dict_attr_nested_allowed_chars[SBUFF_CHAR_CLASS] = {
  *
  */
 bool const fr_dict_enum_allowed_chars[SBUFF_CHAR_CLASS] = {
-	['+'] = true, ['-'] = true, ['.'] = true, ['/'] = true, ['_'] = true,
-	['0'] = true, ['1'] = true, ['2'] = true, ['3'] = true, ['4'] = true,
-	['5'] = true, ['6'] = true, ['7'] = true, ['8'] = true, ['9'] = true,
-	['A'] = true, ['B'] = true, ['C'] = true, ['D'] = true, ['E'] = true,
-	['F'] = true, ['G'] = true, ['H'] = true, ['I'] = true, ['J'] = true,
-	['K'] = true, ['L'] = true, ['M'] = true, ['N'] = true, ['O'] = true,
-	['P'] = true, ['Q'] = true, ['R'] = true, ['S'] = true, ['T'] = true,
-	['U'] = true, ['V'] = true, ['W'] = true, ['X'] = true, ['Y'] = true,
-	['Z'] = true,
-	['a'] = true, ['b'] = true, ['c'] = true, ['d'] = true, ['e'] = true,
-	['f'] = true, ['g'] = true, ['h'] = true, ['i'] = true, ['j'] = true,
-	['k'] = true, ['l'] = true, ['m'] = true, ['n'] = true, ['o'] = true,
-	['p'] = true, ['q'] = true, ['r'] = true, ['s'] = true, ['t'] = true,
-	['u'] = true, ['v'] = true, ['w'] = true, ['x'] = true, ['y'] = true,
-	['z'] = true
+	 DICT_ATTR_ALLOWED_CHARS,
+	['+'] = true, ['.'] = true,
 };
 
 /** Default protocol rules set for every dictionary
