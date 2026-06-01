607dedd32df6378d616661dc1fd83c2fc3ec56ed
Marc-Andre Casavant <marc.casavant@inkbridge.io>
2026-06-01 14:54:38 -0400

    WIP: New script to start valgrind

---
+++ scripts/profiling/start_valgrind.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Run FreeRADIUS under valgrind/callgrind for CPU profiling.
+#
+# Usage: start_valgrind.sh --fr_src_dir <dir>
+#
+#   --fr_src_dir  Root of the FreeRADIUS source tree. Must be the working
+#                 directory used when building (i.e. where ./scripts/bin/radiusd
+#                 lives). Results are written to <dir>/prof-results/.
+#
+# Run this from inside the freeradius4-profiling-deps container with the
+# source tree mounted at <dir>.
+
+fr_src_dir=""
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --fr_src_dir)
+            fr_src_dir="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$fr_src_dir" ]]; then
+    echo "Usage: $0 --fr_src_dir <dir>" >&2
+    exit 1
+fi
+
+mkdir -p "${fr_src_dir}/prof-results"
+
+valgrind \
+  --tool=callgrind \
+  --log-file="${fr_src_dir}/prof-results/valgrind.log" \
+  --callgrind-out-file="${fr_src_dir}/prof-results/callgrind.out.%p" \
+  --trace-children=yes \
+  --separate-threads=no \
+  --dump-instr=yes \
+  --collect-jumps=yes \
+  --cache-sim=yes \
+  --branch-sim=yes \
+  --keep-debuginfo=yes \
+  --instr-atstart=yes \
+  ./scripts/bin/radiusd -f -l stdout -S resources.talloc_skip_cleanup=yes \
+  > "${fr_src_dir}/prof-results/freeradius.log" 2>&1
+
