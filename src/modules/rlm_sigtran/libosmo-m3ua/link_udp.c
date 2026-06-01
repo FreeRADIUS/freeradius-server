efd24a059d67706035ef14576bd328b3527f67d9
Alexander Bainbridge-Sedivy <alex.bainbridge@inkbridge.io>
2026-06-01 14:17:56 -0400

    rlm_sigtran/link_udp: dead code

--- src/modules/rlm_sigtran/libosmo-m3ua/link_udp.c
+++ src/modules/rlm_sigtran/libosmo-m3ua/link_udp.c
@@ -152,7 +152,6 @@ static int udp_read_cb(struct osmo_fd *fd)
 		     "The MSU payload does not fit: %u + %zu > %d on link %d/%s of %d/%s.\n",
 		     length, sizeof(*hdr), rc,
 		     link->nr, link->name, link->set->nr, link->set->name);
-		rc = 0;
 		rc = -1;
 		goto exit;
 	}
