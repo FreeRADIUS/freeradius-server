82ed1d556d781f11c2dbdbbe887bf59c54e2a4a8
Alan T. DeKok <aland@freeradius.org>
2026-06-04 09:27:21 -0400

    add license and copyright

--- src/lib/util/backtrace.c
+++ src/lib/util/backtrace.c
@@ -1,3 +1,27 @@
+/*
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/** Functions which we wish were included in the standard talloc distribution
+ *
+ * @file src/lib/util/backtrace.c
+ *
+ * @copyright 2026 The FreeRADIUS server project
+ */
+RCSID("$Id$")
+
 #include <dlfcn.h>
 
 #include <freeradius-devel/util/backtrace.h>
