3ff21add1b9fce27d4a806b4233c88b08085d9c8
Nick Porter <nick@portercomputing.co.uk>
2026-06-25 08:23:54 +0100

    Correct parsing of main log timestamp option

--- src/lib/server/main_config.c
+++ src/lib/server/main_config.c
@@ -123,6 +123,7 @@ static const conf_parser_t initial_log_config[] = {
 	{ FR_CONF_OFFSET("logdir", main_config_t, log_dir), .dflt = "${local_state_dir}/log"},
 	{ FR_CONF_OFFSET("file", main_config_t, log_file), .dflt = "${logdir}/radius.log" },
 	{ FR_CONF_OFFSET("suppress_secrets", main_config_t, suppress_secrets), .dflt = "yes" },
+	{ FR_CONF_OFFSET_IS_SET("timestamp", FR_TYPE_BOOL, 0, main_config_t, log_timestamp) },
 	CONF_PARSER_TERMINATOR
 };
 
@@ -159,7 +160,7 @@ static const conf_parser_t lib_dir_on_read_config[] = {
 static const conf_parser_t log_config[] = {
 	{ FR_CONF_OFFSET("colourise", main_config_t, do_colourise) },
 	{ FR_CONF_OFFSET("line_number", main_config_t, log_line_number) },
-	{ FR_CONF_OFFSET("timestamp", main_config_t, log_timestamp) },
+	{ FR_CONF_OFFSET_IS_SET("timestamp", FR_TYPE_BOOL, 0, main_config_t, log_timestamp) },
 	{ FR_CONF_OFFSET("use_utc", main_config_t, log_dates_utc) },
 	CONF_PARSER_TERMINATOR
 };
