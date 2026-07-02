70b9f9d620863a5e26b03306fc645351736946ff
Nick Porter <nick@portercomputing.co.uk>
2026-07-02 17:52:21 +0100

    Rework rlm_rediswho to use redis cluster thread

--- src/modules/rlm_rediswho/rlm_rediswho.c
+++ src/modules/rlm_rediswho/rlm_rediswho.c
@@ -34,7 +34,6 @@ RCSID("$Id$")
 #include <freeradius-devel/util/debug.h>
 
 #include <freeradius-devel/redis/base.h>
-#include <freeradius-devel/redis/cluster.h>
 #include <freeradius-devel/redis/cluster_async.h>
 
 typedef struct {
@@ -42,8 +41,6 @@ typedef struct {
 						//!< Must be first field in this struct.
 
 	CONF_SECTION 		*tls_conf;	//!< TLS CONF_SECTION
-
-	fr_redis_cluster_t	*cluster;	//!< Pool O pools
 
 	fr_coord_reg_t		*coord_reg;		//!< Coordinator registration.
 	fr_coord_pair_reg_t	*coord_pair_reg;	//!< Coord pair registration.
@@ -66,6 +63,18 @@ typedef struct {
 	fr_redis_cluster_thread_t	*rtcluster;	//!< Per thread Redis cluster.
 	fr_coord_worker_t		*cw;		//!< Coord-worker for fetching cluster map.
 } rlm_rediswho_thread_t;
+
+/** Resume context for rediswho module calls.
+ */
+typedef struct {
+	char const		*trim;			//!< Format string for trim command.
+	char const		*expire;		//!< Format string for expire command.
+	fr_redis_command_set_t	*cmds;			//!< Command set for module call.
+	fr_redis_async_cmd_t	*cmd;			//!< Async command context.
+	char			*cmd_str;		//!< Formatted command currently being run.
+	uint8_t const		*key;			//!< Key value to identify cluster slot.
+	int			ret;			//!< Value returned in Redis reply.
+} rediswho_rctx_t;
 
 static conf_parser_t section_config[] = {
 	{ FR_CONF_OFFSET_FLAGS("insert", CONF_FLAG_REQUIRED | CONF_FLAG_XLAT, rlm_rediswho_t, insert) },
@@ -116,33 +125,58 @@ fr_dict_attr_autoload_t rlm_rediswho_dict_attr[] = {
 	DICT_AUTOLOAD_TERMINATOR
 };
 
-/*
- *	Query the database executing a command with no result rows
+/** Process redisReply from  rediswho command
+ *
+ * The reply is expected to either be an integer or status.
  */
-static int rediswho_command(rlm_rediswho_t const *inst, request_t *request, char const *fmt)
+static void rediswho_results(request_t *request, UNUSED fr_redis_command_t *cmd, redisReply *reply, void *rctx)
 {
-	fr_redis_conn_t		*conn;
+	rediswho_rctx_t	*rediswho_rctx = talloc_get_type_abort(rctx, rediswho_rctx_t);
 
-	int 			ret = -1;
+	switch (reply->type) {
+	case REDIS_REPLY_INTEGER:
+		rediswho_rctx->ret = reply->integer;
+		return;
 
-	fr_redis_cluster_state_t	state;
-	fr_redis_rcode_t		status = REDIS_RCODE_ERROR;
-	redisReply		*reply = NULL;
-	int			s_ret;
+	case REDIS_REPLY_STATUS:
+		if (strcmp(reply->str, "OK") != 0) {
+			REDEBUG("Redis returned %s", reply->str);
+			rediswho_rctx->ret = -1;
+			return;
+		}
 
-	uint8_t	const		*key = NULL;
-	size_t			key_len = 0;
+		rediswho_rctx = 0;
+		return;
+
+	default:
+		REDEBUG("Bad result type, expected integer, got %s",
+			fr_table_str_by_value(redis_reply_types, reply->type, "<UNKNOWN>"));
+		rediswho_rctx->ret = -1;
+		return;
+	}
+
+}
+
+/*
+ *	Enqueue a Redis command which will return a single integer or status result.
+ */
+static fr_redis_async_rcode_t rediswho_command(rlm_rediswho_thread_t *thread, request_t *request,
+					       char const *fmt, rediswho_rctx_t *rctx)
+{
+	fr_redis_async_rcode_t	ret;
+	int			cmd_len;
+	size_t			key_len;
 
 	int			argc;
 	char const		*argv[MAX_REDIS_ARGS];
 	char			argv_buf[MAX_REDIS_COMMAND_LEN];
 
-	if (!fmt || !*fmt) return 0;
+	if (!fmt || !*fmt) return REDIS_ASYNC_RCODE_ERROR;
 
 	argc = rad_expand_xlat(request, fmt, MAX_REDIS_ARGS, argv, false, sizeof(argv_buf), argv_buf);
 	if (argc < 0) {
 		RPEDEBUG("Invalid command: %s", fmt);
-		return -1;
+		return REDIS_ASYNC_RCODE_ERROR;
 	}
 
 	/*
@@ -153,78 +187,158 @@ static int rediswho_command(rlm_rediswho_t const *inst, request_t *request, char
 	 *	a redirect.
 	 */
 	if (argc > 1) {
-		key = (uint8_t const *)argv[1];
-	 	key_len = strlen((char const *)key);
+		key_len = strlen(argv[1]);
+		rctx->key = talloc_memdup(rctx, argv[1], key_len);
 	}
-
-	for (s_ret = fr_redis_cluster_state_init(&state, &conn, inst->cluster, request, key, key_len, false);
-	     s_ret == REDIS_RCODE_TRY_AGAIN;	/* Continue */
-	     s_ret = fr_redis_cluster_state_next(&state, &conn, inst->cluster, request, status, &reply)) {
-		reply = redisCommandArgv(conn->handle, argc, argv, NULL);
-		status = fr_redis_command_status(conn, reply);
-	}
-	if (s_ret != REDIS_RCODE_SUCCESS) {
-		RERROR("Failed inserting accounting data");
-	error:
-		fr_redis_reply_free(&reply);
-		return -1;
-	}
-	if (!fr_cond_assert(reply)) goto error;
 
 	/*
-	 *	Write the response to the debug log
+	 *	If there's a previous formatted command, clean up
 	 */
-	fr_redis_reply_print(L_DBG_LVL_2, reply, request, 0, status);
-
-	switch (reply->type) {
-	case REDIS_REPLY_ERROR:
-		break;
-
-	case REDIS_REPLY_INTEGER:
-		if (reply->integer > 0) ret = reply->integer;
-		break;
-
-	/*
-	 *	We don't know to interpret this, the user has probably messed
-	 *	up the queries, so print an error message and fail.
-	 */
-	default:
-		REDEBUG("Expected type \"integer\" got type \"%s\"",
-			fr_table_str_by_value(redis_reply_types, reply->type, "<UNKNOWN>"));
-		break;
+	if (rctx->cmd_str) {
+		redisFreeCommand(rctx->cmd_str);
+		rctx->cmd_str = NULL;
+		fr_redis_command_set_clear(rctx->cmds);
 	}
-	fr_redis_reply_free(&reply);
+
+	cmd_len = redisFormatCommandArgv(&rctx->cmd_str, argc, argv, NULL);
+	if (cmd_len < 0) {
+		RERROR("Failed formatting redis commmand");
+		return REDIS_ASYNC_RCODE_ERROR;
+	}
+	fr_redis_command_preformatted_add(rctx->cmds, rctx->cmd_str, cmd_len, rediswho_results, rctx);
+
+	rctx->cmd = fr_redis_async_cmd_start(rctx, request, &ret, thread->rtcluster, rctx->key, key_len,
+					     rctx->cmds, false, NULL);
 
 	return ret;
 }
 
-static unlang_action_t mod_accounting_all(unlang_result_t *p_result, rlm_rediswho_t const *inst, request_t *request,
-					  char const *insert,
-					  char const *trim,
-					  char const *expire)
+static void mod_accounting_cancel(module_ctx_t const *mctx, request_t *request, UNUSED fr_signal_t action)
 {
-	int ret;
+	rediswho_rctx_t	*rctx = talloc_get_type_abort(mctx->rctx, rediswho_rctx_t);
 
-	ret = rediswho_command(inst, request, insert);
-	if (ret < 0) RETURN_UNLANG_FAIL;
+	RDEBUG2("Forcibly cancelling Redis command");
 
-	/* Only trim if necessary */
-	if (trim && (inst->trim_count >= 0) && (ret > inst->trim_count)) {
-		if (rediswho_command(inst, request, trim) < 0) RETURN_UNLANG_FAIL;
+	fr_redis_async_cmd_cancel(rctx->cmd);
+}
+
+static inline unlang_action_t rediswho_rcode_check(request_t *request, fr_redis_command_set_t *cmds,
+						   fr_redis_async_cmd_t *cmd, module_ctx_t const *mctx,
+						   module_method_t resume, unlang_module_signal_t cancel)
+{
+	switch (fr_redis_command_set_rcode(cmds)) {
+	case REDIS_ASYNC_RCODE_MOVE:
+	{
+		rlm_rediswho_t const	*inst = talloc_get_type_abort_const(mctx->mi->data, rlm_rediswho_t);
+		rlm_rediswho_thread_t	*thread = talloc_get_type_abort(mctx->thread, rlm_rediswho_thread_t);
+
+		if (inst->conf.use_cluster_map) fr_redis_cluster_thread_map_get(thread->rtcluster, thread->cw,
+										inst->coord_pair_reg, false);
+	}
+		FALL_THROUGH;
+
+	case REDIS_ASYNC_RCODE_ASK:
+		if (fr_redis_async_cmd_redirect(cmd) != REDIS_ASYNC_RCODE_SUCCESS) return UNLANG_ACTION_FAIL;
+		return unlang_module_yield(request, resume, cancel, ~FR_SIGNAL_CANCEL, mctx->rctx);
+
+	case REDIS_ASYNC_RCODE_TRY_AGAIN:
+		if (fr_redis_async_cmd_resend(cmd) != REDIS_ASYNC_RCODE_SUCCESS) return UNLANG_ACTION_FAIL;
+		return unlang_module_yield(request, resume, cancel, ~FR_SIGNAL_CANCEL, mctx->rctx);
+
+	default:
+		break;
+	}
+	return UNLANG_ACTION_CALCULATE_RESULT;
+}
+
+static unlang_action_t CC_HINT(nonnull) mod_accounting_resume(unlang_result_t *p_result, module_ctx_t const *mctx,
+							      request_t *request)
+{
+	rediswho_rctx_t	*rctx = talloc_get_type_abort(mctx->rctx, rediswho_rctx_t);
+
+	if (rediswho_rcode_check(request, rctx->cmds, rctx->cmd, mctx, mod_accounting_resume,
+				 mod_accounting_cancel) == UNLANG_ACTION_YIELD) return UNLANG_ACTION_YIELD;
+
+	if (rctx->ret < 0) {
+		RPERROR("Redis command failed");
+		RETURN_UNLANG_FAIL;
 	}
 
-	if (rediswho_command(inst, request, expire) < 0) RETURN_UNLANG_FAIL;
 	RETURN_UNLANG_OK;
+}
+
+static unlang_action_t CC_HINT(nonnull) mod_accounting_expire(unlang_result_t *p_result, module_ctx_t const *mctx,
+							      request_t *request)
+{
+	rediswho_rctx_t		*rctx = talloc_get_type_abort(mctx->rctx, rediswho_rctx_t);
+	rlm_rediswho_thread_t	*thread = talloc_get_type_abort(mctx->thread, rlm_rediswho_thread_t);
+	fr_redis_async_rcode_t	ret;
+
+	if (rediswho_rcode_check(request, rctx->cmds, rctx->cmd, mctx, mod_accounting_expire,
+				 mod_accounting_cancel) == UNLANG_ACTION_YIELD) return UNLANG_ACTION_YIELD;
+
+	if (rctx->ret < 0) {
+		RPERROR("Redis command failed");
+		RETURN_UNLANG_FAIL;
+	}
+
+	ret = rediswho_command(thread, request, rctx->expire, rctx);
+
+	REDIS_ASYNC_START_RCODE_PROCESS(ret, thread->rtcluster, thread->cw, thread->inst->coord_pair_reg,
+					"Failed to enqueue Redis cache find commands", UNLANG_ACTION_FAIL)
+
+	return unlang_module_yield(request, mod_accounting_resume, mod_accounting_cancel, 0, rctx);
+}
+
+static unlang_action_t CC_HINT(nonnull) mod_accounting_trim(unlang_result_t *p_result, module_ctx_t const *mctx,
+							    request_t *request)
+{
+	rediswho_rctx_t		*rctx = talloc_get_type_abort(mctx->rctx, rediswho_rctx_t);
+	rlm_rediswho_t const	*inst = talloc_get_type_abort_const(mctx->mi->data, rlm_rediswho_t);
+	rlm_rediswho_thread_t	*thread = talloc_get_type_abort(mctx->thread, rlm_rediswho_thread_t);
+	fr_redis_async_rcode_t	ret;
+
+	if (rediswho_rcode_check(request, rctx->cmds, rctx->cmd, mctx, mod_accounting_trim,
+				 mod_accounting_cancel) == UNLANG_ACTION_YIELD) return UNLANG_ACTION_YIELD;
+
+	if (rctx->ret < 0) {
+		RPERROR("Redis command failed");
+		RETURN_UNLANG_FAIL;
+	}
+
+	if ((inst->trim_count > 0) && (rctx->ret > inst->trim_count)) {
+		ret = rediswho_command(thread, request, rctx->trim, rctx);
+
+		REDIS_ASYNC_START_RCODE_PROCESS(ret, thread->rtcluster, thread->cw, thread->inst->coord_pair_reg,
+						"Failed to enqueue Redis cache find commands", UNLANG_ACTION_FAIL)
+
+		return unlang_module_yield(request, mod_accounting_expire, mod_accounting_cancel, 0, rctx);
+	}
+
+	ret = rediswho_command(thread, request, rctx->expire, rctx);
+
+	REDIS_ASYNC_START_RCODE_PROCESS(ret, thread->rtcluster, thread->cw, thread->inst->coord_pair_reg,
+					"Failed to enqueue Redis cache find commands", UNLANG_ACTION_FAIL)
+
+	return unlang_module_yield(request, mod_accounting_resume, mod_accounting_cancel, 0, rctx);
+}
+
+static int _rediswho_rctx_free(rediswho_rctx_t *rctx)
+{
+	if (rctx->cmd_str) redisFreeCommand(rctx->cmd_str);
+	return 0;
 }
 
 static unlang_action_t CC_HINT(nonnull) mod_accounting(unlang_result_t *p_result, module_ctx_t const *mctx, request_t *request)
 {
-	rlm_rediswho_t const		*inst = talloc_get_type_abort_const(mctx->mi->data, rlm_rediswho_t);
+	rlm_rediswho_thread_t		*thread = talloc_get_type_abort(mctx->thread, rlm_rediswho_thread_t);
 	CONF_SECTION			*conf = mctx->mi->conf;
 	fr_pair_t			*vp;
 	fr_dict_enum_value_t const	*dv;
 	CONF_SECTION			*cs;
 	char const			*insert, *trim, *expire;
+	fr_redis_async_rcode_t		ret;
+	rediswho_rctx_t			*rctx;
 
 	vp = fr_pair_find_by_da(&request->request_pairs, NULL, attr_acct_status_type);
 	if (!vp) {
@@ -258,7 +372,22 @@ static unlang_action_t CC_HINT(nonnull) mod_accounting(unlang_result_t *p_result
 		RETURN_UNLANG_NOOP;
 	}
 
-	return mod_accounting_all(p_result, inst, request, insert, trim, expire);
+	MEM(rctx = talloc(unlang_interpret_frame_talloc_ctx(request), rediswho_rctx_t));
+	*rctx = (rediswho_rctx_t) {
+		.ret = -1,
+		.trim = trim,
+		.expire = expire
+	};
+	rctx->cmds = fr_redis_command_set_alloc(rctx, request, NULL, NULL, NULL, false);
+	talloc_set_destructor(rctx, _rediswho_rctx_free);
+
+	ret = rediswho_command(thread, request, insert, rctx);
+
+	REDIS_ASYNC_START_RCODE_PROCESS(ret, thread->rtcluster, thread->cw, thread->inst->coord_pair_reg,
+					"Failed to enqueue Redis cache find commands", UNLANG_ACTION_FAIL)
+
+	return unlang_module_yield(request, trim ? mod_accounting_trim : mod_accounting_expire,
+				   mod_accounting_cancel, ~FR_SIGNAL_CANCEL, rctx);
 }
 
 static int mod_thread_instantiate(module_thread_inst_ctx_t const *mctx)
@@ -321,11 +450,8 @@ static int mod_instantiate(module_inst_ctx_t const *mctx)
 {
 	rlm_rediswho_t	*inst = talloc_get_type_abort(mctx->mi->data, rlm_rediswho_t);
 	CONF_SECTION	*subcs = cf_section_find(mctx->mi->conf, "redis", NULL);
-	CONF_SECTION	*conf = mctx->mi->conf;
 
 	inst->conf.log_prefix = mctx->mi->name;
-	inst->cluster = fr_redis_cluster_alloc(inst, conf, &inst->conf, NULL, NULL, NULL);
-	if (!inst->cluster) return -1;
 
 	if (inst->conf.use_tls) {
 		inst->tls_conf = cf_section_find(subcs, "tls", CF_IDENT_ANY);
