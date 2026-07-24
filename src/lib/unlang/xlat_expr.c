d724ec8f4323067888ac8bbf18d88bf026169541
Alexander Bainbridge-Sedivy <alex.bainbridge@inkbridge.io>
2026-07-24 12:01:34 -0400

    lib/unlang/xlat_expr: parent xlat_logical_and result box from rctx->ctx (latent UAF)

--- src/lib/unlang/xlat_expr.c
+++ src/lib/unlang/xlat_expr.c
@@ -1294,7 +1294,7 @@ static bool xlat_logical_and(xlat_logical_rctx_t *rctx, fr_value_box_list_t cons
 	if (!found) return false;
 
 	if (!rctx->box) {
-		MEM(rctx->box = fr_value_box_alloc_null(rctx));
+		MEM(rctx->box = fr_value_box_alloc_null(rctx->ctx));
 	} else {
 		fr_value_box_clear(rctx->box);
 	}
