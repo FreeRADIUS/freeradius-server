2601b0cd6b661053ca69fc4077758fd1495e6881
Nick Porter <nick@portercomputing.co.uk>
2026-07-24 17:21:11 +0100

    Remove unused defines

--- src/lib/util/conf.h
+++ src/lib/util/conf.h
@@ -1,11 +1,7 @@
 /* Default Database File Names */
 
 #define RADIUS_DIR			CONFDIR
-#define RADACCT_DIR			RADIR
 #define L_DST_DIR			LOGDIR
 
 #define FR_DICTIONARY_FILE		"dictionary"
 #define FR_DICTIONARY_INTERNAL_DIR	"freeradius"
-#define RADIUS_CLIENTS			"clients"
-#define RADIUS_NASLIST			"naslist"
-#define RADIUS_REALMS			"realms"
