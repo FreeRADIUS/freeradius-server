472f525c8fa0df1c33aa7c16cdc71d53ab2a2b64
Matthew Newton <matthew-git@newtoncomputing.co.uk>
2026-07-27 22:09:11 +0100

    use app token for fuzzer corpus push
    standard token can't bypass the repo ruleset, so mirror what is
    also done in the merge-upstream job

--- scripts/ci/commit_lfs_file_update.py
+++ scripts/ci/commit_lfs_file_update.py
@@ -8,10 +8,11 @@ import base64
 
 from github import Github
 from github import Auth
+from github.GithubException import UnknownObjectException
 
 repo_env = os.environ["GITHUB_REPOSITORY"]
 branch_env = os.environ["GITHUB_REF"]
-token_env = os.environ["GITHUB_TOKEN"]
+token_env = os.environ["APP_TOKEN"]
 
 filename = os.environ["FILE"]
 contents = base64.b64decode(os.environ["CONTENTS"])
@@ -21,7 +22,11 @@ print("About to commit update of " + filename + " to " + repo_env + ":" + branch
 auth = Auth.Token(token_env)
 gh = Github(auth=auth)
 repo = gh.get_repo(repo_env)
-fc = repo.get_contents(filename, branch_env)
-repo.update_file(fc.path, "Scheduled fuzzing: Update " + fc.path, contents, fc.sha, branch=branch_env)
+
+try:
+	fc = repo.get_contents(filename, branch_env)
+	repo.update_file(fc.path, "Scheduled fuzzing: Update " + fc.path, contents, fc.sha, branch=branch_env)
+except UnknownObjectException:
+	repo.create_file(filename, "Scheduled fuzzing: Add " + filename, contents, branch=branch_env)
 
 print("Committed")
