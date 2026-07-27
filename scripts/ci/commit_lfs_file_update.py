d437a608af994e3415382b45e1af19f212c8ac9a
Matthew Newton <matthew-git@newtoncomputing.co.uk>
2026-07-27 19:41:41 +0100

    fix deprecated auth arg in PyGithub CI checkin script

--- scripts/ci/commit_lfs_file_update.py
+++ scripts/ci/commit_lfs_file_update.py
@@ -7,6 +7,7 @@ import os
 import base64
 
 from github import Github
+from github import Auth
 
 repo_env = os.environ["GITHUB_REPOSITORY"]
 branch_env = os.environ["GITHUB_REF"]
@@ -17,7 +18,8 @@ contents = base64.b64decode(os.environ["CONTENTS"])
 
 print("About to commit update of " + filename + " to " + repo_env + ":" + branch_env)
 
-gh = Github(token_env)
+auth = Auth.Token(token_env)
+gh = Github(auth=auth)
 repo = gh.get_repo(repo_env)
 fc = repo.get_contents(filename, branch_env)
 repo.update_file(fc.path, "Scheduled fuzzing: Update " + fc.path, contents, fc.sha, branch=branch_env)
