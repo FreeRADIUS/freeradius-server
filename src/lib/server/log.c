aBainbridge11/Alex-edits

log.c: log_request_perror passes original va_list instead of the va_copy (completes #247)

src/lib/server/log.c
@@ -702,7 +702,7 @@ void log_request_perror(fr_log_type_t type, fr_log_lvl_t lvl, request_t *request
 			va_list copy;
 			va_copy(copy, ap);
 
-			dst_p->func(type, lvl, request, file, line, fmt, ap, dst_p->uctx);
+			dst_p->func(type, lvl, request, file, line, fmt, copy, dst_p->uctx);
 
 			va_end(copy);
 		}

FreeRADIUS/freeradius-server

src/lib/server/log.c
@@ -699,7 +699,7 @@ void log_request_perror(fr_log_type_t type, fr_log_lvl_t lvl, request_t *request
 			va_list copy;
 			va_copy(copy, ap);
 
-			dst->func(type, lvl, request, file, line, fmt, ap, dst->uctx);
+			dst->func(type, lvl, request, file, line, fmt, copy, dst->uctx);
 
 			va_end(copy);
 		}