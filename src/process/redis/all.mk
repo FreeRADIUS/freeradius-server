f2d510db81a5aa68a57f7cb2d4d3e9fc5de604d9
Nick Porter <nick@portercomputing.co.uk>
2026-08-24 17:03:26 +0100

    Only build process_redis if hiredis is available

--- src/process/redis/all.mk
+++ src/process/redis/all.mk
@@ -1,7 +1,12 @@
-TARGETNAME	:= process_redis
+#  This needs to be cleared explicitly, as the libfreeradius-redis.mk
+#  might not always be available, and the TARGETNAME from the previous
+#  target may stick around.
+TARGETNAME=
+-include $(top_builddir)/src/lib/redis/all.mk
 
 ifneq "$(TARGETNAME)" ""
-TARGET		:= $(TARGETNAME)$(L)
+  TARGETNAME	:= process_redis
+  TARGET	:= $(TARGETNAME)$(L)
 endif
 
 SOURCES		:= base.c
