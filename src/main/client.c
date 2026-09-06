6d6c74fbcb0613d5592366a0e2fb89a213d21a21
Alan T. DeKok <aland@freeradius.org>
2026-09-05 21:24:03 -0400

    check parse->name, not parse

--- src/main/client.c
+++ src/main/client.c
@@ -1480,7 +1480,7 @@ RADCLIENT *client_afrom_request(RADCLIENT_LIST *clients, REQUEST *request)
 				if (parse->offset == dynamic_config[i].offset) break;
 			}
 
-			if (!parse) break;
+			if (!parse->name) break;
 
 			cp = cf_pair_alloc(c->cs, parse->name, strvalue, T_OP_SET, T_BARE_WORD, T_SINGLE_QUOTED_STRING);
 		}
@@ -1499,7 +1499,7 @@ RADCLIENT *client_afrom_request(RADCLIENT_LIST *clients, REQUEST *request)
 			for (parse = client_config; parse->name; parse++) {
 				if (parse->type == PW_TYPE_BOOLEAN) break;
 			}
-			if (!parse) break;
+			if (!parse->name) break;
 
 			cp = cf_pair_alloc(c->cs, parse->name, strvalue, T_OP_SET, T_BARE_WORD, T_BARE_WORD);
 		}
