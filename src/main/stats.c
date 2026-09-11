5fc043d1323ad34f0ab5501a08d6d10f63686f43
Alan T. DeKok <aland@freeradius.org>
2026-09-11 12:35:14 -0400

    don't count stats twice
    they're now fully counted on the inbound path

--- src/main/stats.c
+++ src/main/stats.c
@@ -287,37 +287,6 @@ void request_stats_final(REQUEST *request)
 	}
 
 #ifdef WITH_PROXY
-	if (!request->proxy || !request->home_server) goto done;	/* simplifies formatting */
-
-	switch (request->proxy->code) {
-	case PW_CODE_ACCESS_REQUEST:
-		proxy_auth_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
-		break;
-
-#ifdef WITH_ACCOUNTING
-	case PW_CODE_ACCOUNTING_REQUEST:
-		proxy_acct_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
-		break;
-#endif
-
-#ifdef WITH_COA
-	case PW_CODE_COA_REQUEST:
-		proxy_coa_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
-		break;
-
-	case PW_CODE_DISCONNECT_REQUEST:
-		proxy_dsc_stats.total_requests += request->num_proxied_requests;
-		request->home_server->stats.total_requests += request->num_proxied_requests;
-		break;
-#endif
-
-	default:
-		break;
-	}
-
 	if (!request->proxy_reply) goto done;	/* simplifies formatting */
 
 #undef INC
@@ -325,9 +294,8 @@ void request_stats_final(REQUEST *request)
 
 	switch (request->proxy_reply->code) {
 	case PW_CODE_ACCESS_ACCEPT:
-		INC(total_access_accepts);
-	proxy_stats:
-		INC(total_responses);
+	case PW_CODE_ACCESS_REJECT:
+	case PW_CODE_ACCESS_CHALLENGE:
 		stats_time(&proxy_auth_stats, NULL,
 			   &request->proxy->timestamp,
 			   &request->proxy_reply->timestamp);
@@ -336,32 +304,9 @@ void request_stats_final(REQUEST *request)
 			   &request->proxy_reply->timestamp);
 		break;
 
-	case PW_CODE_ACCESS_REJECT:
-		INC(total_access_rejects);
-		goto proxy_stats;
-
-	case PW_CODE_ACCESS_CHALLENGE:
-		INC(total_access_challenges);
-		goto proxy_stats;
-
-#ifdef WITH_ACCOUNTING
-	case PW_CODE_ACCOUNTING_RESPONSE:
-		proxy_acct_stats.total_responses++;
-		request->home_server->stats.total_responses++;
-		stats_time(&proxy_acct_stats, NULL,
-			   &request->proxy->timestamp,
-			   &request->proxy_reply->timestamp);
-		stats_time(&request->home_server->stats, NULL,
-			   &request->proxy->timestamp,
-			   &request->proxy_reply->timestamp);
-		break;
-#endif
-
 #ifdef WITH_COA
 	case PW_CODE_COA_ACK:
 	case PW_CODE_COA_NAK:
-		proxy_coa_stats.total_responses++;
-		request->home_server->stats.total_responses++;
 		stats_time(&proxy_coa_stats, NULL,
 			   &request->proxy->timestamp,
 			   &request->proxy_reply->timestamp);
@@ -372,8 +317,6 @@ void request_stats_final(REQUEST *request)
 
 	case PW_CODE_DISCONNECT_ACK:
 	case PW_CODE_DISCONNECT_NAK:
-		proxy_dsc_stats.total_responses++;
-		request->home_server->stats.total_responses++;
 		stats_time(&proxy_dsc_stats, NULL,
 			   &request->proxy->timestamp,
 			   &request->proxy_reply->timestamp);
@@ -384,8 +327,6 @@ void request_stats_final(REQUEST *request)
 #endif
 
 	default:
-		proxy_auth_stats.total_unknown_types++;
-		request->home_server->stats.total_unknown_types++;
 		break;
 	}
 
