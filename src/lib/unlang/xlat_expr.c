6f4cc9f20f4348b20fbda5575453210c01846a68
Alan T. DeKok <aland@freeradius.org>
2026-09-13 13:06:15 -0400

    the second argument can be NULL in fuzzer builds

--- src/lib/unlang/xlat_expr.c
+++ src/lib/unlang/xlat_expr.c
@@ -167,13 +167,11 @@ static fr_slen_t xlat_expr_print_binary(fr_sbuff_t *out, xlat_exp_t const *node,
 	FR_SBUFF_IN_CHAR_RETURN(out, ' ');
 
 	child = xlat_exp_next(node->call.args, child);
-#ifdef WITH_VERIFY_PTR
-	if (!child) xlat_debug(node);
-#else
-	fr_assert(child != NULL);
-#endif
-
-	xlat_print_node(out, node->call.args, child, e_rules, 0);
+	if (child) {
+		xlat_print_node(out, node->call.args, child, e_rules, 0);
+	} else {
+		FR_SBUFF_IN_STRCPY_RETURN(out, "???");
+	}
 
 	FR_SBUFF_IN_CHAR_RETURN(out, ')');
 
