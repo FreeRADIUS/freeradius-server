7a54a3523000d98bb3c762304fcd9b4cefb78022
Nick Porter <nick@portercomputing.co.uk>
2026-09-14 16:22:13 +0100

    Pacify Coverity (CID #1701145)

--- src/lib/io/app_io.c
+++ src/lib/io/app_io.c
@@ -47,10 +47,10 @@ char const *fr_app_io_socket_name(TALLOC_CTX *ctx, fr_app_io_t const *app_io,
 			strlcpy(dst_buf, "::", sizeof(dst_buf));
 		}
 	} else {
-		fr_value_box_print(&FR_SBUFF_OUT(dst_buf, sizeof(dst_buf)), fr_box_ipaddr(*dst_ipaddr), NULL);
+		(void) fr_value_box_print(&FR_SBUFF_OUT(dst_buf, sizeof(dst_buf)), fr_box_ipaddr(*dst_ipaddr), NULL);
 	}
 
-	if (src_ipaddr) fr_value_box_print(&FR_SBUFF_OUT(src_buf, sizeof(src_buf)), fr_box_ipaddr(*src_ipaddr), NULL);
+	if (src_ipaddr) (void) fr_value_box_print(&FR_SBUFF_OUT(src_buf, sizeof(src_buf)), fr_box_ipaddr(*src_ipaddr), NULL);
 
 	if (!interface) {
 		if (!src_ipaddr) {
