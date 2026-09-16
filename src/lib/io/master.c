50452bd1a5b8b443c15ea3260da9387915d80227
Alan T. DeKok <aland@freeradius.org>
2026-09-15 21:44:59 -0400

    clear "in hash table" flag when it's removed from the hash table

--- src/lib/io/master.c
+++ src/lib/io/master.c
@@ -893,7 +893,10 @@ static fr_io_connection_t *fr_io_connection_alloc(fr_io_instance_t const *inst,
 	if (client->ht) {
 		size_t pre_size = fr_hash_table_num_elements(client->ht);
 
-		if (nak) (void) fr_hash_table_delete(client->ht, nak);
+		if (nak) {
+			(void) fr_hash_table_delete(client->ht, nak);
+			nak->in_parent_hash = false;
+		}
 		ret = fr_hash_table_insert(client->ht, connection);
 		client->ready_to_delete = false;
 		connection->in_parent_hash = true;
